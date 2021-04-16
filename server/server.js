824d4d76344eef0616d68ab3cbdc925a94ebe524
Chris Owen <chris@thecodethatchriswrote.com>
2021-04-16 13:51:20 +0100

    Add install instructions

--- server/server.js
+++ server/server.js
@@ -1,16 +1,8 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-
 const app = express();
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.use("/api/movies", movies);
-
 const port = process.env.PORT || 5000;
-server = app.listen(port, () => console.log(`Server running on port ${port}`));
 
-router.get("/", (req, res) => {});
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// GET "/"
+app.get("/", (req, res) => {});
