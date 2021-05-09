78aa3ea55c1043787762f7f090d16d23016116b0
Biruk Kebede <biruk.key@gmail.com>
2021-05-09 15:53:57 +0100

    Created a MiniYouTube component that returns the static data provided

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import "./App.css";
+import MiniYouTube from './MiniYouTube';
 
 function App() {
   return (
@@ -6,6 +8,7 @@ function App() {
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
+      <MiniYouTube />
     </div>
   );
 }
---
+++ client/src/MiniYouTube.js
@@ -0,0 +1,27 @@
+import React from "react";
+import exampleresponse from "./exampleresponse.json";
+import ReactPlayer from "react-player";
+
+const MiniYouTube = () => {
+  return (
+    <div className="display-wrapper">
+      {exampleresponse.map((response) => {
+        return (
+          <div className="video-and-title">
+            <h3>{response.title}</h3>
+            <div className="video-container">
+              <ReactPlayer
+                width="560"
+                height="315"
+                className="embedded-video"
+                url={response.url.toString()}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default MiniYouTube;
