79cebeadffdb49e87a854d67a75f7b2581a4b22c
ltteh <67978926+ltteh@users.noreply.github.com>
2021-05-16 00:44:51 +0100

    initiate react and test data

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import "./App.css";
+import React from "react";
+import exampleresponse from "./Data/exampleresponse.json";
 
 function App() {
   return (
@@ -6,6 +8,8 @@ function App() {
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
+      <h3>{console.log(exampleresponse)}</h3>
+      <h3>{console.log(exampleresponse[0].title)}</h3>
     </div>
   );
 }
