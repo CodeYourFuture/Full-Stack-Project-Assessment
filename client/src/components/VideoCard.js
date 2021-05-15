0b7b5cc8ed882bc38aa44747064a97e81c4ea5af
Michael <antenehnigussie1@yahoo.com>
2021-05-15 19:41:34 +0100

    Added more elements. Updated JavaScript code.

--- client/src/components/VideoCard.js
+++ client/src/components/VideoCard.js
@@ -1,18 +1,44 @@
+import React from "react";
+
 const Card = ({ data }) => {
-  const videoID = data.url.substring(data.url.indexOf("=") + 1);
+  const id = data.url.substring(data.url.indexOf("=") + 1);
+  let title = data.title;
+  if (title.length > 30) {
+    title = `${title.substring(0, 30)}...`;
+  }
+
+  // EVENT HANDLERS
+  const removeVideo = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="card">
-      <h3 className="title">{data.title}</h3>
+      <h4 className="title">{title}</h4>
       <iframe
-        width="560"
-        height="315"
-        src={`https://www.youtube.com/embed/${videoID}`}
+        src={`https://www.youtube.com/embed/${id}`}
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
+      <div className="controls">
+        <div className="votes">
+          <span id="num-votes">
+            <i>num-votes</i>
+          </span>{" "}
+          <span id="vote-up">
+            <i className="fas fa-thumbs-up"></i>
+          </span>
+          <span id="vote-down">
+            <i className="fas fa-thumbs-down"></i>
+          </span>
+        </div>
+        <button id="remove" onClick={removeVideo}>
+          Remove
+        </button>
+      </div>
     </div>
   );
-}
+};
 export default Card;
