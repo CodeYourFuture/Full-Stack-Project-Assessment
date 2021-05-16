b1d4dc05fd5e8204e6f93f318367fdd4155e02cc
delete <patryk.r.nowak@gmail.com>
2021-05-16 12:36:03 +0100

    code added to index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,30 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// REDUX
+import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+
+import videoReducer from "./store/reducers/videos"
+
+const rootReducer = combineReducers({
+  videos: videoReducer
+})
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+)
+
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+
+,
   document.getElementById("root")
 );
