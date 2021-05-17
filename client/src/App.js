5cacbfbc46f9a34ae84abb23091ae2a190fa2067
Chizim <garrohat@yahoo.co.uk>
2021-05-17 19:07:35 +0100

    Still trying delete

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,19 @@ import Search from "./component/Search";
 import VideoRatings from "./component/VideoRatings";
 import Videos from "./component/Videos";
 import VideoTitle from "./component/VideoTitle";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import data from "./data/exampleresponse.json";
 import AddVid from "./component/AddVid";
 
 function App() {
   const [searchVal, setSearchVal] = useState();
-  const [deleteVid, setDeleteVid] = useState([]);
+  const [videos, setVideos] = useState(data);
+
   const handleDeleteVid = (index) => {
-    data.splice(index, 1);
-    setDeleteVid(deleteVid);
+    const findVidIndex = videos.findIndex((vid) => vid.id === index);
+    videos.splice(findVidIndex, 1);
+    setVideos([...videos]);
   };
   return (
     <div className="App">
@@ -22,13 +24,13 @@ function App() {
         <h1 className="text-white bg-primary">Video Recommendation</h1>
       </header>
       <AddVid />
-      <Search searchVal = {searchVal} setSearchVal = {setSearchVal}/>
-      {data.map((video, index) => (
+      <Search searchVal={searchVal} setSearchVal={setSearchVal} />
+      {videos.map((video, index) => (
         <div key={index} className="container">
           <VideoTitle title={video.title} />
           <VideoRatings rating={video.rating} />
           <Videos video={video.url} />
-          <DeleteVideo handleDeleteVid = {handleDeleteVid}/>
+          <DeleteVideo id = {video.id} handleDeleteVid={handleDeleteVid} />
         </div>
       ))}
     </div>
--- client/src/component/DeleteVideo.js
+++ client/src/component/DeleteVideo.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const DeleteVideo = ({ handleDeleteVid }) => {
+const DeleteVideo = ({ id, handleDeleteVid }) => {
   return (
     <div>
       <button className="btn btn-danger" onClick={() => handleDeleteVid()}>
