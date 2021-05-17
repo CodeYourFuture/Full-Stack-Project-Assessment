fe6256e919da44df2fd40afb329db21ed86b12b3
SenaitEkubai <Senaitekubai@gmail.com>
2021-05-17 13:30:20 +0100

    deleted a file

--- client/src/DeleteVideo.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const DeleteVideo = (props) => {
-  return (
-    <div className="delete">
-      <button className=" btn btn-primary">Delete</button>
-    </div>
-  );
-};
-export default DeleteVideo;
