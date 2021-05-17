9ab6a52e8dbf85d2251d3a05f363483f73634809
Chizim <garrohat@yahoo.co.uk>
2021-05-17 13:13:55 +0100

    Added increase and decrease ratings

--- client/src/component/VideoRatings.js
+++ client/src/component/VideoRatings.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const VideoRatings = ({ rating }) => {
-  return (
+  const [ratings, setRatings] = useState(rating);
+
+  const handleRating = (increase) => {
+    const newRating = increase ? ratings + 1 : ratings - 1;
+    setRatings(newRating);
+  };
+  return (  
     <div className="votes">
-      <button className="btn btn-success">
+      <button className="btn btn-success" onClick={()=>handleRating(true)}>
         <i className="fas fa-thumbs-up vote">&#128077;</i>
       </button>
-      <p>Votes</p>
+      <p>Votes: {ratings}</p>
 
-      <button className="btn btn-success">
+      <button className="btn btn-success" onClick={()=>handleRating(false)}>
         <i className="fas fa-thumbs-down vote">&#128078;</i>
       </button>
     </div>
