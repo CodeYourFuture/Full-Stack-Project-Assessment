02d3dabd4727c4e2bc6ebc6a3f7e0792eeb56520
SenaitEkubai <Senaitekubai@gmail.com>
2021-05-17 13:30:46 +0100

    cretaed new file Video

--- client/src/Video.js
+++ client/src/Video.js
@@ -1,21 +1,44 @@
-import React from "react";
-import Votes from "./Votes";
+import "./App.css";
+import { useState } from "react";
 
-const Video = (props) => {
+function Video({ id, title, url, rating, deleteVideo }) {
+  let [numberOfVotes, setNumberOfVotes] = useState(rating);
+
+  // votes
+  function vote(event) {
+    if (event.target.className === "fa fa-thumbs-up") {
+      setNumberOfVotes(numberOfVotes++);
+    } else {
+      setNumberOfVotes(numberOfVotes--);
+    }
+  }
   return (
-    <div className="video">
-      <h5>{props.videoTitle}</h5>
-      <Votes />
+    <div id={id}>
+      <h5>{title}</h5>
+      {/* separate div for votes  */}
+      <div className="votes">
+        <i className="fa fa-thumbs-up" onClick={vote}></i>
+        <p className="voteCount">{numberOfVotes}</p>
+        <i className="fa fa-thumbs-down" onClick={vote}></i>
+      </div>
+      {/* iframe for video  */}
       <iframe
         width="560"
         height="315"
-        src={props.url.replace("watch?v=", "embed/")}
+        src={url.replace("watch?v=", "embed/")}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       ></iframe>
+      {/* delete button */}
+      <div>
+        <button className=" btn btn-primary" onClick={deleteVideo}>
+          Delete
+        </button>
+      </div>
     </div>
   );
-};
+}
+
 export default Video;
