ea864fc9824f087200d5257115fa165e6a9540f6
Amanuel <weditedi25@gmail.com>
2021-05-18 17:31:09 +0100

    initial commit

---
+++ client/VideoInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function VideoInfo() {
+  return <div>hiiiiiiiii</div>;
+}
+
+export default VideoInfo;
--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
+import React from "react";
 import "./App.css";
+import movieData from "./movieData.json";
+// import VideoInfo from "./VideoInfo";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header bg-dark">
         <h1>Video Recommendation</h1>
       </header>
+      <VideoInfo />
     </div>
+  );
+}
+
+function VideoInfo() {
+  return (
+    <ul>
+      {movieData.map((movie) => (
+        <li>{movie.title}</li>
+      ))}
+    </ul>
   );
 }
 
