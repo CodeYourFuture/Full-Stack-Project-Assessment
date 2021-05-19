4204b9f33f8551a072776fb1f1b647600561c0a2
Michael <antenehnigussie1@yahoo.com>
2021-05-19 20:43:31 +0100

    Refactored code. Fixed bug. Fixed incorrect votes display upon changing videos' sort order.

--- client/src/components/VideoCard.js
+++ client/src/components/VideoCard.js
@@ -1,51 +1,63 @@
-import React, { useState } from "react";
-import { removeVideoFromServer, updateVideoRatingOnServer } from "../functions";
+import React, { useEffect, useState } from "react";
+import {
+  fetchSingleVideoData,
+  removeVideoFromServer,
+  updateVideoRatingOnServer,
+} from "../functions";
 
-const Card = ({ data }) => {
-  const [votes, setVotes] = useState(data.rating);
-  const id = data.url.substring(data.url.indexOf("=") + 1);
-  let title = data.title;
+const Card = (props) => {
+  const [cardIsUpdated, setCardIsUpdated] = useState(false);
+  const [numVotes, setVotes] = useState();
+
+  const componentId = props.data.id;
+  const videoId = props.data.url.substring(props.data.url.indexOf("=") + 1);
+  let title = props.data.title;
   if (title.length > 30) {
     title = `${title.substring(0, 30)}...`;
   }
+  // SIDE EFFECT
+  useEffect(() => {
+    if (cardIsUpdated) {
+      const response = fetchSingleVideoData(componentId);
+      if (response) {
+        response.then((data) => setVotes(data.rating));
+      }
+      setCardIsUpdated(false);
+      return;
+    } else {
+      setVotes(props.data.rating);
+    }
+  }, [props.data.rating, cardIsUpdated, componentId]);
+
   // EVENT HANDLERS
   const removeVideo = (event) => {
     event.preventDefault();
-    const videoId = Number(data.id);
-    removeVideoFromServer(videoId);
+    removeVideoFromServer(componentId);
   };
-
-  const voteUpOrDown = (event) => {
-    const videoId = Number(data.id);
-    let numVotes = 0;
-    event.target.className === "vote-up"
-      ? (numVotes = votes + 1)
-      : (numVotes = votes - 1);
-    setVotes(numVotes);
-    updateVideoRatingOnServer(videoId, numVotes);
+  const handleVoting = async (plusOrMinus) => {
+    const status = await updateVideoRatingOnServer(componentId, plusOrMinus);
+    if (status === 200) {
+      setCardIsUpdated(true);
+    }
   };
 
   return (
-    <div id={data.id} className="card">
+    <div id={componentId} className="card">
       <p className="title">{title}</p>
       <iframe
-        src={`https://www.youtube.com/embed/${id}`}
+        src={`https://www.youtube.com/embed/${videoId}`}
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen
       ></iframe>
       <div className="controls">
-        <div className="votes">
-          <button className="vote vote-up" aria-label="button-vote-up">
-            <span className="fas fa-thumbs-up" onClick={voteUpOrDown}></span>
-          </button>{" "}
-          <span className="num-votes">{votes}</span>{" "}
-          <button className="vote vote-down" aria-label="button-vote-down">
-            <span className="fas fa-thumbs-down" onClick={voteUpOrDown}></span>
-          </button>
-        </div>
-        <button className="remove" aria-label="button-remove" onClick={removeVideo}>
+        <Votes onVoteClicked={handleVoting} votes={numVotes} />
+        <button
+          className="remove"
+          aria-label="button-remove"
+          onClick={removeVideo}
+        >
           Remove
         </button>
       </div>
@@ -53,3 +65,24 @@ const Card = ({ data }) => {
   );
 };
 export default Card;
+
+export const Votes = (props) => {
+  const voteUpOrDown = async (event) => {
+    let plusOrMinus = "";
+    event.target.className.toString().includes("thumbs-up")
+      ? (plusOrMinus = "plus")
+      : (plusOrMinus = "minus");
+    props.onVoteClicked(plusOrMinus);
+  };
+  return (
+    <div className="votes">
+      <button className="vote vote-up" aria-label="button-vote-up">
+        <span className="fas fa-thumbs-up" onClick={voteUpOrDown}></span>
+      </button>{" "}
+      <span className="num-votes">{props.votes}</span>{" "}
+      <button className="vote vote-down" aria-label="button-vote-down">
+        <span className="fas fa-thumbs-down" onClick={voteUpOrDown}></span>
+      </button>
+    </div>
+  );
+};
