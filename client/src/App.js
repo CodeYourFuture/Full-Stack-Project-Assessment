3312d89d0b20b5df0f742df616525d7dd2c535f6
Michael <antenehnigussie1@yahoo.com>
2021-05-19 16:48:32 +0100

    Moved some code to YouTubeVideosContainer.

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,8 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import YouTubeVideoAdder from "./components/VideoAdder";
 import YouTubeVideosContainer from "./components/VideoCards";
-import { fetchVideoData, sortVideosByRating } from "./functions";
 
 function App() {
-  const [videos, setVideos] = useState([]);
-  const API_LINK = `http://localhost:5000`;
-  // FETCH VIDEO DATA
-  useEffect(() => {
-    fetchVideoData(API_LINK).then((data) =>
-      setVideos(sortVideosByRating(data))
-    );
-  }, [API_LINK]);
-
   return (
     <div className="App">
       <header className="App-header">
@@ -21,7 +10,7 @@ function App() {
       </header>
       <div className="container">
         <YouTubeVideoAdder />
-        <YouTubeVideosContainer content={videos} />
+        <YouTubeVideosContainer />
       </div>
     </div>
   );
