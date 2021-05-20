04446260ec6bc80696e1f5db35dd37c84c56c936
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-05-20 23:02:44 +0100

    AllVideosContent component added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import "./App.css";
+import AllVideoFiles from "./components/allVideoContents.js";
+
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
+      <AllVideoFiles />
     </div>
   );
 }
