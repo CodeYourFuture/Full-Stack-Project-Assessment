6c36ed43236b505dc942125fbf854689a1fe8bc9
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-05-21 16:01:15 +0100

    onClick handler added

--- client/src/components/deleteVideo.js
+++ client/src/components/deleteVideo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import VideoFrame from './videoFrames.js';
 const DeleteVideo = (props) =>{
-    return <button onClick = {props.handleDelete} Data = {props.Data}>DELETE</button>    
+    return <button onClick = {props.handleDelete} >DELETE</button>    
 }
 export default DeleteVideo;
