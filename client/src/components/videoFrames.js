686d00cd4cd1be7b2e16efaba36d265ca3ac9c14
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-05-21 16:02:12 +0100

    onClick for delete.js added

--- client/src/components/videoFrames.js
+++ client/src/components/videoFrames.js
@@ -1,20 +1,20 @@
 import {React, useState} from 'react';
 import "../App.css";
 import DeleteVideo from './deleteVideo.js';
+import VoteCount from './voteCount.js';
 
 const VideoFrames = (props) => {
     const Data = props.Data;
     const handleDelete = props.handleDelete;
         return (
     <div className = "videoframes">     
-        {/* <OneVideoFrame Data = {Data} handleDelete = {handleDelete} /> */}
         {Data.map((obj, i) => {
                 const videoID = obj["url"].split("=")[1]
                 return( 
-                <div className = "oneVideoFrame">
-                    <h4 key = {i}>{obj["title"]}</h4>
-                    <h4>0 votes</h4>
-                    <iframe width="560" height="315" 
+                <div key = {i} className = "oneVideoFrame">
+                    <h4>{obj["title"]}</h4>
+                    <VoteCount upDownVoteCount = {props.upDownVoteCount} handleUpVoteCount = {props.handleUpVoteCount}/>
+                    <iframe width="500" height="315" 
                     src={`https://www.youtube.com/embed/${videoID}`} 
                     title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
                     </iframe>
