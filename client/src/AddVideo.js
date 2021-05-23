74049fed35aca89fff52146e67ad4bd972f347ea
SenaitEkubai <Senaitekubai@gmail.com>
2021-05-23 23:48:29 +0100

    get, post and delete requests

--- client/src/AddVideo.js
+++ client/src/AddVideo.js
@@ -12,7 +12,7 @@ const AddVideo = ({ handleChange, setTitle, setUrl, handleSubmit }) => {
       >
         Add Video
       </p>
-      {click ? (
+      {click && (
         <div className="add-video">
           <p>Title</p>
           <input
@@ -37,7 +37,7 @@ const AddVideo = ({ handleChange, setTitle, setUrl, handleSubmit }) => {
             </button>
           </div>
         </div>
-      ) : null}
+      )}
     </div>
   );
 };
