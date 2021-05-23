927a2676c3dfc816d3611dadcf8a857c31934b86
jon <jonprosser84@gmail.com>
2021-05-23 17:07:31 +0100

    started axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import "./App.css";
-import videos from '../src/data';
+// import videos from '../src/data';
 import Header from '../src/components/Header';
 import Add from '../src/components/Add';
 import Card from '../src/components/Card';
 import Footer from './components/Footer';
+
+
 
 function createCard(video, delete2) {
   return (
@@ -21,10 +24,25 @@ function createCard(video, delete2) {
 }
 
 
+
+
 function App() {
 
-  const [vid, setVid] = useState(videos);
+
+  const [vid, setVid] = useState([]);
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/videos')
+      .then(res => {
+        console.log(res)
+        setVid(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, []);
+
 
 
   const deleteVid = (id) => {
@@ -49,8 +67,7 @@ function App() {
 
       <div className="main-container">
 
-        {vid.filter((video) => video.title.toLowerCase().includes(search)).map(item => createCard(item, deleteVid))}
-
+        {videos.filter((video) => video.title.toLowerCase().includes(search)).map(item => createCard(item, deleteVid))}
 
       </div>
 
--- server/server.js
+++ server/server.js
@@ -10,6 +10,11 @@ let videos = require("./videos.json");
 //middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 
 // GET "/"
