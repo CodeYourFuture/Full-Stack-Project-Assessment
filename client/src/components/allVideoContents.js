1653c47d025a8c493b3b78290d349e0715cda57a
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-05-24 16:48:08 +0100

    handler for the addVideo declared and added to the VideoFrames component

--- client/src/components/allVideoContents.js
+++ client/src/components/allVideoContents.js
@@ -8,47 +8,65 @@ import VideoFrames from './videoFrames.js';
 
 
 const AllVideoFiles = () => {
-    const [deleteVideos, setDeleteVideos] = useState(Data);
-    let [upDownVoteCount, setUpDownVoteCount] = useState(0);
-
-    const handleUpVoteCount = (event) => {
-        const noOfVoteCount = event.currentTarget;
-        console.log(noOfVoteCount);
-        if( noOfVoteCount){
-            setUpDownVoteCount(upDownVoteCount + 1)
-        }
-    }
+    const [allVideos, setAllVideos] = useState(Data);   
 
     const handleDelete = (event) => {        
         const videoTitle = event.currentTarget.parentNode.childNodes[0].textContent;
-        const removedVideo = []
-        const videoToDelete = deleteVideos.filter((obj) => {
+        const videoToDelete = allVideos.filter((obj) => {
             if(!(obj["title"] === videoTitle)){
             return obj;
             }
         })
-        setDeleteVideos(videoToDelete);
+        setAllVideos(videoToDelete);
     }
     const handleSearch = (event) => {
         const searchedVideo = event.target.value.toLowerCase();
         console.log(searchedVideo);
         const selectedVideo = [];
-        const selectVideo = deleteVideos.filter((obj) => {
+         allVideos.filter((obj) => {
             if(obj["title"].toLowerCase().includes(searchedVideo)){
                 selectedVideo.push(obj)
             }
         })
-         setDeleteVideos(selectedVideo);
+         setAllVideos(selectedVideo);
+    }
+
+    const handleAddVideo = (event) => {
+        const videoAdd = event.currentTarget.parentNode.childNodes[0].childNodes[1].childNodes[0].childNodes[1];
+        const videoToAdd = event.target.textContent;
+        console.log(videoAdd)
+        if(videoToAdd === "Add Video") {
+            if (event.currentTarget.childNodes[1].classList.contains("displayclass")){
+                event.currentTarget.childNodes[1].classList.remove("displayclass")
+            } else {
+                event.currentTarget.childNodes[1].classList.add("displayclass")
+            }
+        } else if ( videoToAdd === "Cancel") {
+            event.currentTarget.childNodes[1].classList.add("displayclass")
+        } else if (videoToAdd === "ADD") {
+           const title = event.currentTarget.parentNode.childNodes[0].childNodes[1].childNodes[0].childNodes[1].value;
+           const url = event.currentTarget.parentNode.childNodes[0].childNodes[1].childNodes[0].childNodes[3].value;
+        
+        const newVideoToAdd = {
+            "title": {title},
+            "url": {url},
+            "rating": 0
+        }
+        console.log(newVideoToAdd);
+        allVideos.push(newVideoToAdd);
+        console.log(allVideos);
+    }
+    setAllVideos(allVideos);
     }
 
         
 return (
     <div>
         <div className = "addVideoSearch">
-            <AddVideo />
+            <AddVideo handleAddVideo ={handleAddVideo}/>
             <Search handleSearch = {handleSearch} />
         </div>
-    <VideoFrames Data = {deleteVideos} handleDelete = {handleDelete} upDownVoteCount = {upDownVoteCount} handleUpVoteCount = {handleUpVoteCount} />
+        <VideoFrames Data = {allVideos} handleDelete = {handleDelete} />
     
     </div>
 )
