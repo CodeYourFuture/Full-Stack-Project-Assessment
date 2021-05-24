243b395481feb300e011862e94d5e35c1c3b87f4
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-05-24 16:46:23 +0100

    component to add a new video created

--- client/src/components/addVideo.js
+++ client/src/components/addVideo.js
@@ -1,12 +1,16 @@
-const AddVideo = () => {
+const AddVideo = (props) => {
     return(
-        <div className = "addvideo">
+        <div onClick = {props.handleAddVideo} className = "addvideo">
             <h3>Add Video</h3>
-            Title <input></input>
-            URL <input></input>
+            <div className = "displayclass">
+                <div className = "titleUrl">
+                    Title <input></input>
+                    URL <input></input>
+                </div>
             <div className = "addNdel">
-                <button className = "btn btn-danger">Cancel</button>
-                <button className = "btn btn-warning">ADD</button>
+                <button>Cancel</button>
+                <button>ADD</button>
+            </div>
             </div>
        </div>
     )
