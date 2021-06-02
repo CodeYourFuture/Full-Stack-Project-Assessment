bbfc815c7d744b2bdfa3a5f3f8fd7b72d7e70556
Michael <antenehnigussie1@yahoo.com>
2021-06-02 14:13:42 +0100

    Added feature: search video(s) by title.

--- client/src/components/VideoCards.js
+++ client/src/components/VideoCards.js
@@ -6,6 +6,7 @@ const VideoCards = (props) => {
   const [orderParam, setOrderParam] = useState("");
   const [buttonCaption, setButtonCaption] = useState("Ascending");
   const [videos, setVideos] = useState([]);
+  const [searchText, setSearchText] = useState("");
   // FETCH VIDEOS' DATA FROM SERVER
   useEffect(() => {
     const response = fetchVideosData(orderParam);
@@ -22,19 +23,39 @@ const VideoCards = (props) => {
       setOrderParam("?order=desc");
     }
   };
-  
+
+  const handleSearch = (event) => {
+    setSearchText(event.target.value);
+  };
+
   return (
     <div>
-      <div className="video-sorter">
-        <label htmlFor="sort">Sort by rating:</label>
-        <button id="sort" aria-label="button-sort" onClick={sortVideos}>
-          {buttonCaption}
-        </button>
+      <div className="video-searcher-sorter">
+        <div>
+          <label htmlFor="search">Search:</label>{" "}
+          <input
+            type="text"
+            id="search"
+            value={searchText}
+            onChange={handleSearch}
+            placeholder="enter video title here..."
+          />
+        </div>
+        <div>
+          <label htmlFor="sort">Sort videos by rating:</label>{" "}
+          <button id="sort" aria-label="button-sort" onClick={sortVideos}>
+            {buttonCaption}
+          </button>
+        </div>
       </div>
       <div className="cards-container">
-        {videos.map((videoData, index) => (
-          <Card key={index} data={videoData} onDelete={props.onDelete} />
-        ))}
+        {videos
+          .filter((v) =>
+            v.title.toUpperCase().includes(searchText.toUpperCase())
+          )
+          .map((videoData, index) => (
+            <Card key={index} data={videoData} onDelete={props.onDelete} />
+          ))}
       </div>
     </div>
   );
