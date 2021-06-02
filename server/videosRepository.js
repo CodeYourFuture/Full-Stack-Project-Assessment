7689b21877b6a9f79330ce4c979af65726b8b408
Michael <antenehnigussie1@yahoo.com>
2021-06-02 14:07:04 +0100

    Replaced Client by Pool. Also removed code deemed not necessary.

--- server/videosRepository.js
+++ server/videosRepository.js
@@ -1,24 +1,11 @@
-const { Client } = require("pg");
+const { Pool } = require("pg");
 
-const client = new Client({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
-client.connect();
-
-client.query(
-  "SELECT table_schema,table_name FROM information_schema.tables;",
-  (err, res) => {
-    if (err) throw err;
-    for (let row of res.rows) {
-      console.log(JSON.stringify(row));
-    }
-    client.end();
-  }
-);
 
 // QUERIES
 const getAllVideosQuery = `SELECT * FROM videos ORDER BY rating`;
@@ -30,33 +17,33 @@ const getVideoByUrlQuery = `SELECT * FROM videos WHERE url = $1`;
 
 // get all videos
 function getAllVideos(sortOrder) {
-  return client.query(`${getAllVideosQuery} ${sortOrder}`);
+  return pool.query(`${getAllVideosQuery} ${sortOrder}`);
 }
 
 // search/find a video from list by video id
 function getVideoById(vId) {
-  return client.query(getVideoByIdQuery, [vId]);
+  return pool.query(getVideoByIdQuery, [vId]);
 }
 
-// 
+//
 async function getVideoByUrl(vUrl) {
-  return client.query(getVideoByUrlQuery,[vUrl]);
+  return pool.query(getVideoByUrlQuery, [vUrl]);
 }
 
 // add video to list
-function addNewVideo({id,title,url,datePosted}) {
-  // const 
-  return client.query(addNewVideoQuery, [id,title,url]);
+function addNewVideo({ id, title, url, datePosted }) {
+  // const
+  return pool.query(addNewVideoQuery, [id, title, url]);
 }
 
 // update user rating of a video
 function updateVideoRating(vId, vRating) {
-  return client.query(updateVideoRatingQuery, [vRating, vId]);
+  return pool.query(updateVideoRatingQuery, [vRating, vId]);
 }
 
 // remove video from list
 function deleteVideoById(vId) {
-  return client.query(deleteVideoByIdQuery, [vId]);
+  return pool.query(deleteVideoByIdQuery, [vId]);
 }
 
 module.exports = {
@@ -65,5 +52,5 @@ module.exports = {
   getVideoById,
   updateVideoRating,
   deleteVideoById,
-  getVideoByUrl
+  getVideoByUrl,
 };
