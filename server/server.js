029af1739fcaf4f23c2d2a43e6e47a7e261171dc
SenaitEkubai <Senaitekubai@gmail.com>
2021-06-10 00:59:15 +0100

    updated db config

--- server/server.js
+++ server/server.js
@@ -5,16 +5,27 @@ const { Pool } = require("pg");
 
 const password = process.env.PASSWORD;
 const host = process.env.HOST;
+const prt = process.env.DB_PORT;
 const user = process.env.USER;
 const database = process.env.DATABASE;
+//const uri = process.env.URI;
 
 const dbConfig = {
   host,
-  port: 5432,
+  prt,
   user,
   password,
   database,
 };
+/* 
+const dbConfig = {
+  host,
+  prt,
+  user,
+  password,
+  database,
+  uri,
+}; */
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
@@ -28,17 +39,28 @@ app.use(
 
 const pool = new Pool(dbConfig);
 let videos = `select  * from videos order by rating desc`;
-
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error("Error acquiring client", err.stack);
+  }
+  client.query("SELECT NOW()", (err, result) => {
+    release();
+    if (err) {
+      return console.error("Error executing query", err.stack);
+    }
+    console.log(result.rows);
+  });
+});
 // GET "/"
 
 app.get("/", (req, res) => {
-  if (req.query.order === "asc") {
+  /*  if (req.query.order === "asc") {
     videos = videos.replace("desc", "asc");
   }
   if (req.query.order === "desc") {
     videos = videos.replace("asc", "desc");
   }
-
+  console.log("getting /"); */
   pool
     .query(videos)
     .then((result) => {
@@ -67,26 +89,43 @@ app.get("/:id", async function (req, res) {
 
 //post request add a video with title and url
 
-let idCounter = 1;
-let rating = 0;
-app.post("/", (req, res) => {
-  const video = req.body;
-  if (video.title && video.url) {
-    const newVideo = {
-      id: ++idCounter, // ++idCounter because the previous id needs to be remembered
-      title: video.title,
-      url: video.url,
-      rating: rating,
-    };
-    videos.push(newVideo);
-    res.status(201).json({
-      id: newVideo.id,
-    });
+app.post("/", async function (req, res) {
+  const newVideoTitle = req.body.title;
+  const newVideoUrl = req.body.url;
+
+  if (newVideoTitle && newVideoUrl) {
+    await pool
+      .query("insert into videos (title, url) values ($1,$2)", [
+        newVideoTitle,
+        newVideoUrl,
+      ])
+      .catch((error) => res.send(error));
+    const newVideoCreated = await pool
+      .query("select id from videos where title = $1 and url=$2 ", [
+        newVideoTitle,
+        newVideoUrl,
+      ])
+      .then((result) => {
+        console.log(result.rows);
+        if (result.rows.length > 0) {
+          console.log(result.rows[result.rows.length - 1].id);
+
+          res.json({ id: result.rows[result.rows.length - 1].id });
+        } else {
+          res.status(400);
+        }
+      });
+
+    // console.log(newVideoCreated);
+    /*
+    if (newVideoCreated.rows.length > 0) {
+      res.json({ id: newVideoCreated.rows[0].id });
+    } else {
+      res.status(400);
+    }
   } else {
-    res.send({
-      result: "failure",
-      message: "Video could not be saved",
-    });
+    res.status(400);
+   */
   }
 });
 
