e42a50ce233a91ef91908b974fe3705c885d87f7
SenaitEkubai <Senaitekubai@gmail.com>
2021-06-10 00:57:14 +0100

    added submission form

--- client/src/AddVideo.js
+++ client/src/AddVideo.js
@@ -13,6 +13,41 @@ const AddVideo = ({ handleChange, setTitle, setUrl, handleSubmit }) => {
         Add Video
       </p>
       {click && (
+        <form action="/" method="post" className="add-video">
+          {" "}
+          <label for="fname">Title: </label>
+          <input
+            type="text"
+            name="title"
+            placeholder="type video title"
+            onChange={handleChange}
+            value={setTitle}
+          />
+          <br></br>
+          <label for="lname">Url: </label>
+          <input
+            type="text"
+            name="url"
+            placeholder="type url"
+            onChange={handleChange}
+            value={setUrl}
+          />
+          <br></br>
+          <input
+            type="button"
+            value="cancel"
+            className="btn btn-warning"
+            onClick={() => setClick(false)}
+          />
+          <input
+            type="submit"
+            value="Submit"
+            className="btn btn-primary"
+            onClick={handleSubmit}
+          />
+        </form>
+      )}
+      {/* {click && (
         <div className="add-video">
           <p>Title</p>
           <input
@@ -37,7 +72,7 @@ const AddVideo = ({ handleChange, setTitle, setUrl, handleSubmit }) => {
             </button>
           </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 };
