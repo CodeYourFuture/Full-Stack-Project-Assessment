39e8b3251776dfab7cd6adf3a5edb7acb2daaaf5
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-06-10 20:33:56 +0100

    handleDelete moved here and linked to database to delete in database from frontend

--- client/src/components/deleteVideo.js
+++ client/src/components/deleteVideo.js
@@ -1,5 +1,46 @@
 import React from 'react';
 const DeleteVideo = (props) =>{
-    return <button onClick = {props.handleDelete} className="btn btn-danger" >DELETE</button>    
+    const handleDelete = (event) => {        
+        const videoTitle = event.currentTarget.parentNode.parentNode.childNodes[0].textContent;
+        const videoToDelete = props.allVideos.filter((obj) => {
+            if((obj["title"] === videoTitle)){
+            return obj;
+            }
+        })
+        fetch("http://127.0.0.1:5000/:ID", {
+                        method: "DELETE",
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                          },
+                          body: JSON.stringify(videoToDelete)
+                    })
+                    .then((response) => response.json())          
+                              .then(data => {
+                                props.setDisplayVideos(data);
+                            })
+                            .catch(error => console.error(error));      
+
+        // props.allVideos.current = videosRemaining;
+        props.setDisplayVideos(props.allVideos);
+    }
+
+    return <button onClick = {handleDelete} className="btn btn-danger" >DELETE</button>    
 }
 export default DeleteVideo;
+
+// fetch("http://127.0.0.1:5000", {
+//             method: "POST",
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json'
+//               },
+//               body: JSON.stringify(newVideoToAdd)
+//         })
+//           .then((response) => response.json())          
+//           .then(data => {
+//             props.setDisplayVideos(data);
+//         })
+//         .catch(error => console.error(error));
+
+        
