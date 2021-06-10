4a2b5e3efc9a90b143cfc67bdcd9a0d6a8307f4f
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-06-10 20:35:55 +0100

    props for deletebutton passed

--- client/src/components/videoFrames.js
+++ client/src/components/videoFrames.js
@@ -4,7 +4,7 @@ import OneVideoFrame from './oneVideoFrame';
 
 
 const VideoFrames = (props) => {
-
+  console.log(props.Data)
     function sortAllVideos(allVideos) {
         allVideos.sort((a, b) => {
           if (a.rating > b.rating) {
@@ -18,13 +18,19 @@ const VideoFrames = (props) => {
     return (    
           <div className = "videoframes"> 
           {/* {sortAllVideos(props.Data)} */}
-       {props.Data.map((obj, index) => {
+          {props.Data.map((obj, index) => {
+            
             const videoID = obj["url"].split("=")[1];
             const rating = obj["rating"];
             const videoTitle = obj["title"];      
        return (    
      <div key = {index}>         
-        <OneVideoFrame videoID = {videoID} rating = {rating} videoTitle = {videoTitle} index = {index} handleDelete = {props.handleDelete} />        
+        <OneVideoFrame videoID = {videoID} 
+        rating = {rating} 
+        videoTitle = {videoTitle} 
+        index = {index} 
+        setDisplayVideos = {props.setDisplayVideos} 
+        allVideos = {props.allVideos} />        
      </div>
         ) } )}
         </div>
