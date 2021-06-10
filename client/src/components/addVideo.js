da2967772b61cd00585c7ff50f388cac00866fae
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-06-10 20:24:12 +0100

    handler moved to addVideo and linked to database for adding a new video

--- client/src/components/addVideo.js
+++ client/src/components/addVideo.js
@@ -1,6 +1,63 @@
-const AddVideo = (props) => {
+const AddVideo = (props) => {    
+    const handleAddVideo = (event) => {
+        const videoToAdd = event.target.textContent;
+        if(videoToAdd === "Add Video") {
+            if (event.currentTarget.childNodes[1].classList.contains("displayclass")){
+                event.currentTarget.childNodes[1].classList.remove("displayclass")
+            } else {
+                event.currentTarget.childNodes[1].classList.add("displayclass")
+            }
+        } else if ( videoToAdd === "Cancel") {
+            event.currentTarget.childNodes[1].classList.add("displayclass")
+        } else if (videoToAdd === "ADD") {
+           const title = event.currentTarget.parentNode.childNodes[0].childNodes[1].childNodes[0].childNodes[1].value;
+           event.currentTarget.parentNode.childNodes[0].childNodes[1].childNodes[0].childNodes[1].value = "";
+           const url = event.currentTarget.parentNode.childNodes[0].childNodes[1].childNodes[0].childNodes[3].value;
+           event.currentTarget.parentNode.childNodes[0].childNodes[1].childNodes[0].childNodes[3].value = ""
+           const urlValidation = /(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9_-]+)/;
+           const videoList = []
+           const newVideoToAdd = {
+                 title,
+                "rating": 0,
+                 url                
+        } 
+
+        if (!(title.toString().trim().length === 0) && (url.match(urlValidation))){
+            // videoList.push(newVideoToAdd);
+            // console.log(props.allVideos);
+            // props.allVideos = [{...props.allVideos, ...videoList}]
+            // console.log(props.allVideos);
+            // props.setDisplayVideos(props.allVideos)    
+         
+
+        fetch("http://127.0.0.1:5000", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify(newVideoToAdd)
+        })
+          .then((response) => response.json())          
+          .then(data => {
+            props.setDisplayVideos(data);
+        })
+        .catch(error => console.error(error));
+    //     if (!(title.toString().trim().length === 0) && (url.match(urlValidation))){
+    //     videoList.push(newVideoToAdd);
+    //     props.allVideos.current = [...props.allVideos.current, ...videoList]
+    //     props.setDisplayVideos(props.allVideos.current)    
+    } 
+    else {
+            alert("Enter a valid title or URL")
+        }
+    }    
+    }
+
+
+
     return(
-        <div onClick = {props.handleAddVideo} className = "addvideo">
+        <div onClick = {handleAddVideo} className = "addvideo">
             <h3>Add Video</h3>
             <div className = "displayclass">
                 <div className = "titleUrl">
