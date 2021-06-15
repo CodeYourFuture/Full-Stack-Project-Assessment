ec8706fce0a6071655c32951d42ef671a124873f
yunusfirat <yunusfirat44@gmail.com>
2021-06-15 17:02:57 +0100

    Bootstrap, context.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
+        <button className="btn btn-primary">djlad</button>
       </header>
     </div>
   );
