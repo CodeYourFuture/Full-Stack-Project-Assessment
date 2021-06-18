995cf5d1be55b2a2db236e38d8c72b70107d92a2
ASLANDENIZ <dkelemence@gmail.com>
2021-06-19 00:23:38 +0100

    Video adding working now

--- client/src/components/VideoForm.js
+++ client/src/components/VideoForm.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import SearchBox from "./SearchBox";
 import { useGlobalContext } from "../Context"
+import uuid from 'react-native-uuid';
+
+
 
 const VideoForm = () => {
     const { data, setData } = useGlobalContext();
     const [click, setClick] = useState(false)
     const [video, setVideo] = useState(
         {
-            "id": 283634,
+            "id": uuid.v4(),
             "title": "",
             "url": "",
             "rating": 0
@@ -23,7 +26,7 @@ const VideoForm = () => {
         if (e.target.name === "title") {
             setVideo(
                 {
-                    "id": 283634,
+                    "id": uuid.v4(),
                     "title": value,
                     "url": video.url,
                     "rating": 0
@@ -33,7 +36,7 @@ const VideoForm = () => {
         if (e.target.name === "url") {
             setVideo(
                 {
-                    "id": 283634,
+                    "id": uuid.v4(),
                     "title": video.title,
                     "url": value,
                     "rating": 0
@@ -46,23 +49,16 @@ const VideoForm = () => {
 
     function handleAddVideo(e) {
         e.preventDefault()
-        setData(data.concat(video))
-        console.log(data)
-        console.log("handle a video")
+        video.title && video.url ? setData(data.concat(video)) : alert("Please fill both title and url sections")
+        setVideo(
+            {
+                "id": uuid.v4(),
+                "title": "",
+                "url": "",
+                "rating": 0
+            }
+        )
     }
-
-    // function handleChangeUrl(e) {
-    //     const url = e.target.value
-
-    //     setVideo(
-    //         {
-    //             "id": 283634,
-    //             "title":"",
-    //             "url": url,
-    //             "rating": 0
-    //         }
-    //     )
-    // }
 
 
     return (
@@ -70,24 +66,26 @@ const VideoForm = () => {
             <button type="button" className="btn btn-primary" onClick={handleClick}>Primary</button>
             {click ?
                 <div>
-                    <form className="w-25 ml-3">
+                    <form className="w-25 ml-3" noValidate>
                         <div className="mb-3">
-                            <label className="form-label">Title</label>
+                            <label htmlFor="title"  className="form-label">Title</label>
                             <input
+                                id="title"
                                 type="text"
                                 name="title"
                                 value={video.title}
                                 className="form-control"
-                                onChange={handleChange} />
+                                onChange={handleChange} required />
                         </div>
                         <div className="mb-3">
-                            <label className="form-label">URL</label>
+                            <label htmlFor="url" className="form-label">URL</label>
                             <input
                                 name="url"
+                                id="url"
                                 type="url"
                                 value={video.url}
                                 className="form-control"
-                                onChange={handleChange} />
+                                onChange={handleChange} required />
                         </div>
                         <button type="submit" className="btn btn-primary m-2" onClick={handleAddVideo}>Submit</button>
                         <button type="cancel" className="btn btn-warning m-2" onClick={handleClick}>Cancel</button>
