d17df646c1e45d369ed2cd7f65d74809d7b6e190
yunusfirat <yunusfirat44@gmail.com>
2021-06-18 02:39:02 +0100

    added some styling

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -3,7 +3,6 @@ import { useGlobalContext } from "../context";
 import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
 const Video = () => {
     const { data, setData, updateData } = useGlobalContext();
-    console.log(data)
     const upVote = (rating, id) => {
         let updatedData = data.map((element) => element.id !== id ? element : { id: element.id, title: element.title, url: element.url, rating: rating + 1 })
         setData(updatedData);
@@ -22,28 +21,42 @@ const Video = () => {
                 const { id, title, url, rating, date } = video;
                 console.log(date)
                 const videoId = url.split('watch?v=');
+                const videostyle = 
+                    {
+                        width:"76%",
+                        height:"99%",
+                        border:"2px black solid", 
+                        textAlign:"center", 
+                        backgroundColor:"#254e70",
+                        borderRadius:"10px",
+                        margin:"30px"
+                        }
+                
                 return (
-                    <div key={id} className="flex-column d-block" style={{width:"80%"}}>
-                        <div className="m-3 " style={{ width: "30rem", height: "25rem", border: "3px red solid" }}>
+                    <div key={id} className="flex-column" 
+                    style={videostyle}>
+                        <div className="m-3" style={{height:"40%"}}>
                             <iframe
                                 src={`https://www.youtube.com/embed/${videoId[1]}`}
                                 title="YouTube video player"
-                                style={{ width: "100%", height: "100%", boxSizing: "border-box" }}
-                                frameBorder="0"
+                                allowfullscreen
                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                allowFullScreen></iframe>
+                                style={{ width: "100%", height: "100%",border: "3px red solid",borderRadius:"20px" }}
+                                ></iframe>
                         </div>
-                        <div className="media-body flex-column ml-3" style={{ width: "88%", border:"2px grey solid" }}>
-                            <div><p className="mt-0 mb-1 ml-2 fs-6 ">{title}</p></div>
-                            <div className="d-flex justify-content-around">
-                                <div className="mr-2 ml-2"><FaThumbsUp style={{ cursor: "pointer" }} color="green" onClick={() => upVote(rating, id)} />
-                                    <span className="mr-2 ml-2">{rating}</span>
-                                    <FaThumbsDown color="red" style={{ cursor: "pointer" }} onClick={() => downVote(rating, id)} /></div>
-                                {date !== undefined ? <div><p className="mt-0 mb-1 ml-2 fs-6 post">Posted At {date}</p></div> : 
-                                <div><p className="mt-0 mb-1 ml-2 fs-6 post">Posted add: long time ago</p></div>}
-                                <button className="btn btn-danger mb-3 mr-2" onClick={() => remove(id)}>Delete</button>
+                        <div className="media-body flex-column ml-3 mt-2" style={{ width: "90%",height:"42%", border:"2px black solid", borderRadius:"10px" }}>
+                            <div><h3 className="m-2 fs-6 ">{title}</h3></div>
+                            <div className="d-flex justify-content-around mt-3">
+                                <div className="m-3">
+                                    <FaThumbsUp style={{ cursor: "pointer" }} color="green"  size="20px"  onClick={() => upVote(rating, id)} />
+                                    <span className="m-3"> {rating}</span>
+                                    <FaThumbsDown color="red" size="20px" style={{ cursor: "pointer", marginLeft:"15px" }} onClick={() => downVote(rating, id)} />
+                                </div>
+                                {date !== undefined ? <div><p className="mr-3 fs-6 post">Posted At {date}</p></div> :
+                                 <div><p className="m-3 fs-6 post ">Posted at: <br></br> long time ago</p></div>}
                             </div>
                         </div>
+                                <button className="btn btn-danger m-2" onClick={() => remove(id)}>Delete</button>
                     </div>
 
                 )
