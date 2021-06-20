3e02757c11f8d03ecd8720cb1366decb042208a1
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-06-20 08:49:50 +0100

    level100 step3
    Add upVote downVote
    Delete

--- client/src/components/VideoContents.js
+++ client/src/components/VideoContents.js
@@ -1,33 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import data from "../exampleresponse.json";
+import VideoCard from "./VideoCard";
 
 function VideoContents() {
+  const [dataArray, setDataArray] = useState(data);
+
+  function handleDelete(id) {
+    console.log(id);
+    setDataArray(dataArray.filter((data) => data.id !== id));
+  }
+
   return (
     <div className="row d-flex justify-content-center m-3">
-      {data.map((video, index) => {
+      {dataArray.map((video, index) => {
         return (
-          <div
-            key={video.id}
-            className="card col-12 col-md-3 m-3"
-            style={{ width: "18rem" }}
-          >
-            <iframe
-              className="card-img-top"
-              src={`https://www.youtube.com/embed/${
-                video.url.split("watch?v=")[1]
-              }`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
-
-            <div className="card-body">
-              <h5 className="card-title">{video.title}</h5>
-              <p className="card-text">Rating: {video.rating}</p>
-              <button className="btn btn-primary">Delete</button>
-            </div>
-          </div>
+          <VideoCard key={index} data={video} handleDelete={handleDelete} />
         );
       })}
     </div>
