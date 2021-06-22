cb136985d4177f640815f6bfb36eca8900b4f512
Oksana Kostenko <kseniavlad31@gmail.com>
2021-06-22 21:24:56 +0100

    adds useRef to AddVideoForm

--- client/src/components/AddVideoForm.js
+++ client/src/components/AddVideoForm.js
@@ -1,22 +1,31 @@
-// import Search from "./Search";
+import { useRef } from "react";
 
-const AddVideoForm = () => {
+const AddVideoForm = ({ handleAddVideo }) => {
+  const titleRef = useRef(null);
+  const urlRef = useRef(null);
+
   return (
-    <>
-      <form>
-        <label htmlFor="movie-title">
-          Title
-          <input type="text" name="movie-title" id="movie-title" />
-        </label>
-        <label htmlFor="movie-url">
-          URL
-          <input type="text" name="movie-url" id="movie-url" />
-        </label>{" "}
-        <br />
-        <button>CANCEL</button>
-        <button>ADD</button> <br />
-      </form>
-    </>
+    <form>
+      <label htmlFor="movie-title">
+        Title
+        <input ref={titleRef} type="text" name="movie-title" id="movie-title" />
+      </label>
+      <label htmlFor="movie-url">
+        URL
+        <input ref={urlRef} type="text" name="movie-url" id="movie-url" />
+      </label>
+      <br />
+      <button>CANCEL</button>
+      <button
+        onClick={(event) => {
+          event.preventDefault();
+          handleAddVideo(titleRef.current.value, urlRef.current.value);
+        }}
+      >
+        ADD
+      </button>{" "}
+      <br />
+    </form>
   );
 };
 
