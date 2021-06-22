b304732456f7afa6d841fd7a1d766905f2ef8f70
Oksana Kostenko <kseniavlad31@gmail.com>
2021-06-22 12:27:33 +0100

    adds voting option

--- client/src/components/SingleVideo.js
+++ client/src/components/SingleVideo.js
@@ -1,34 +1,53 @@
+import { useState } from "react";
 import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
 import "./SingleVideo.css";
+
 const SingleVideo = ({ video }) => {
-  // console.log(video.url);
-  // console.log(
-  //   "yoyooyyyo",
-  //   `https://www.youtube.com/embed/${video.url.split("=")[1]}`
-  // );
+  const { title, url, rating } = video;
+  const [votes, setVotes] = useState(0);
+
+  const handleUpVote = () => {
+    setVotes(1);
+  };
+
+  const handleDownVote = () => {
+    setVotes(-1);
+  };
+
   return (
     <div className="video-card">
-      <h3>{video.title}</h3>
+      <h3>{title}</h3>
       <div className="votes-container">
         <div>
-          <FaThumbsUp />
+          <FaThumbsUp
+            onClick={handleUpVote}
+            className={votes === 1 ? "voted thumb-icon" : "thumb-icon"}
+            size={30}
+          />
         </div>
         <div>
-          <p>10votes</p>
+          <p>{rating + votes} votes</p>
         </div>
         <div>
-          <FaThumbsDown />
+          <FaThumbsDown
+            onClick={handleDownVote}
+            className={votes === -1 ? "voted thumb-icon" : "thumb-icon"}
+            size={30}
+          />
         </div>
       </div>
-      <iframe
-        width="360"
-        height="300"
-        src={`https://www.youtube.com/embed/${video.url.split("=")[1]}`}
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <div>
+        <iframe
+          width="360"
+          height="300"
+          src={`https://www.youtube.com/embed/${url.split("=")[1]}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <button>Delete</button>
+      </div>
     </div>
   );
 };
