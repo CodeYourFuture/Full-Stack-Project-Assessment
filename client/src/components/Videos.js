fb457dae48e0d9c971bba2f59b41c9fd3dfd0657
Oksana Kostenko <kseniavlad31@gmail.com>
2021-06-22 14:45:45 +0100

    changes in Videos component

--- client/src/components/Videos.js
+++ client/src/components/Videos.js
@@ -1,13 +1,49 @@
+import { useState } from "react";
+import staticData from "../exampleresponse.json";
+import AddVideoForm from "./AddVideoForm";
+import Search from "./Search";
 import SingleVideo from "./SingleVideo";
 import "./Videos.css";
 
-const Videos = ({ videosData }) => {
+const Videos = () => {
+  const [videosData, setVideosData] = useState(staticData);
+  const [searchValue, setSearchValue] = useState("");
+
+  // delete video
+  const handleDeleteVideoClick = (id) => {
+    const newVideosData = videosData.filter((video) => {
+      return video.id !== id;
+    });
+    setVideosData(newVideosData);
+  };
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+    setSearchValue(event.target.value);
+  };
+
+  // filter by user input
+  const filteredVideos = videosData.filter((data) => {
+    return data.title.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
   return (
-    <section className="videos">
-      {videosData.map((video, index) => {
-        return <SingleVideo video={video} key={index} />;
-      })}
-    </section>
+    <>
+      <AddVideoForm />
+      <Search handleSearch={handleSearch} searchValue={searchValue} />
+      <section className="videos">
+        {filteredVideos.map((video, index) => {
+          return (
+            <SingleVideo
+              video={video}
+              key={index}
+              searchValue={searchValue}
+              handleDeleteVideoClick={handleDeleteVideoClick}
+            />
+          );
+        })}
+      </section>
+    </>
   );
 };
 
