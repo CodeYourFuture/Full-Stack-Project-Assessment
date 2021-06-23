d4d6acb5093db80dbbd78ec0a5d24414d02ce6a4
Hozan Ali <hozan949@gmail.com>
2021-06-23 03:02:29 +0100

    created server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,69 @@
+const { request } = require("express");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+let videos = require("./exampleresponse.json");
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+    res.json(videos);
 });
+
+// POST "/"
+app.post("/", (req, res) => {
+    let video = {
+        id: 0,
+        title: req.body.title,
+        url: req.body.url,
+        rating: 0
+    }
+
+    if (req.body.title && req.body.url) {
+        videos.unshift(video);
+        res.json(videos)
+    } else {
+        res.status(400).json({
+            "result": "failure",
+            "message": "Video could not be saved"
+        })
+    }
+});
+
+//GET "/:id"
+app.get("/:id", (req, res) => {
+    let videoId = +req.params.id;
+    const found = videos.some(video => video.id === videoId);
+
+    if (found) {
+        let selectedVideo = videos.filter(video => video.id === videoId);
+        res.json(selectedVideo);
+    } else {
+        res.status(404).json({
+            "result": "failure",
+            "message": `No Video with ID ${videoId}`
+        })
+    }
+});
+
+//DELETE "/:id"
+app.delete("/:id", (req, res) => {
+    let videoId = +req.params.id;
+    const found = videos.some(video => video.id === videoId);
+
+    if (found) {
+        videos = videos.filter(video => video.id !== videoId);
+        res.json({});
+    } else {
+        res.status(404).json({
+            "result": "failure",
+            "message": "Video could not be deleted"
+        })
+    }
+})
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
