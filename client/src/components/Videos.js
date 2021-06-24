7af933f281bb622ad223ba6f356c5d90ecb9393f
Hozan Ali <hozan949@gmail.com>
2021-06-24 23:20:14 +0100

    server side level 299 part 1

--- client/src/components/Videos.js
+++ client/src/components/Videos.js
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DisplayVideos from "./DisplayVideos";
 import AddVideo from "./AddVideo";
 
 
 function Videos() {
-    const [videos, setVideos] = useState(require("../data/exampleresponse.json"));
+    const [videos, setVideos] = useState([]);
 
-    function remove(video){
-      setVideos( videos.filter(item => item !== video))
+    useEffect(() => {
+        fetch('http://localhost:5000/')
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    throw new Error(`Encountered Something Unexpected ${response.status}`)
+                }
+            })
+            .then(
+                data => {
+                    setVideos(data);
+                },
+                error => {
+                    console.log(error)
+                });
+    }, [])
+
+    function remove(video) {
+        setVideos(videos.filter(item => item !== video))
     }
 
     return (
         <>
             <div className="videos-list">
                 <AddVideo />
-                {videos.map((video, index) =>(
-                <DisplayVideos key={index} video={video} remove={remove}/>
+                {videos.map((video, index) => (
+                    <DisplayVideos key={index} video={video} remove={remove} />
                 ))}
             </div>
         </>
--- server/server.js
+++ server/server.js
@@ -1,12 +1,30 @@
 const { request } = require("express");
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
 
-let videos = require("./exampleresponse.json");
-
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors())
+
+let videos = require("./exampleresponse.json");
+
+//GET "/?order=asc OR desc"
+app.get("/", (req, res) => {
+    if (req.query.order) {
+        let order = req.query.order.toLowerCase()
+        let asc = videos.sort((videoA, videoB) => videoA.rating - videoB.rating);
+        let desc = videos.sort((videoA, videoB) => videoB.rating - videoA.rating);
+
+        const orders = { asc, desc };
+
+        res.json(orders[order]);
+    } else {
+        let defaultOrder = videos.sort((videoA, videoB) => videoB.rating - videoA.rating);
+        res.json(defaultOrder);
+    }
+});
 
 // GET "/"
 app.get("/", (req, res) => {
