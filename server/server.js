59ba248663143ed7ecfaca6cb00ff46326813e62
Hozan Ali <hozan949@gmail.com>
2021-06-30 22:48:53 +0100

    Completed Level 300
    The password to connect to the database is not added.

--- server/server.js
+++ server/server.js
@@ -1,48 +1,66 @@
-const { request } = require("express");
 const express = require("express");
 const cors = require('cors');
-const app = express();
-const port = process.env.PORT || 5000;
 
+const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-let videos = require("./exampleresponse.json");
+const port = process.env.PORT || 5000;
+
+const { Pool } = require("pg");
+
+const pool = new Pool({
+    user: "osuceosjjbpuuq",
+    host: "ec2-54-155-87-214.eu-west-1.compute.amazonaws.com",
+    database: "db4tgrcf7b55ru",
+    password: "",
+    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
 
 //GET "/?order=asc OR desc"
 app.get("/", (req, res) => {
-    if (req.query.order) {
-        let order = req.query.order.toLowerCase()
-        let asc = videos.sort((videoA, videoB) => videoA.rating - videoB.rating);
-        let desc = videos.sort((videoA, videoB) => videoB.rating - videoA.rating);
+    pool
+        .query("SELECT * FROM videos")
+        .then((result) => {
+            if (req.query.order) {
+                let videos = result.rows
+                let order = req.query.order.toLowerCase()
 
-        const orders = { asc, desc };
-
-        res.json(orders[order]);
-    } else {
-        let defaultOrder = videos.sort((videoA, videoB) => videoB.rating - videoA.rating);
-        res.json(defaultOrder);
-    }
-});
-
-// GET "/"
-app.get("/", (req, res) => {
-    res.json(videos);
+                order === 'asc' ? res.json(videos.sort((videoA, videoB) => videoA.rating - videoB.rating)) :
+                    order === "desc" ? res.json(videos.sort((videoA, videoB) => videoB.rating - videoA.rating)) :
+                        res.status(400).json({
+                            "result": "failure",
+                            "message": "Please enter 'asc' or 'desc' in your search"
+                        });
+            } else {
+                let defaultOrder = result.rows.sort((videoA, videoB) => videoB.rating - videoA.rating);
+                res.json(defaultOrder);
+            }
+        })
+        .catch((e) => console.error(e));
 });
 
 // POST "/"
 app.post("/", (req, res) => {
-    let video = {
-        id: 0,
-        title: req.body.title,
-        url: req.body.url,
-        rating: 0
-    }
+    const title = req.body.title;
+    const url = req.body.url;
+    const rating = 0;
 
     if (req.body.title && req.body.url) {
-        videos.unshift(video);
-        res.json(videos)
+        const query =
+            "INSERT INTO videos (title, url, rating) VALUES ($1, $2, $3)";
+        pool
+            .query(query, [title, url, rating])
+            .then(() => res.json({
+                "result": "success",
+                "message": "Video is saved "
+            }))
+            .catch((error) => console.error(error));
     } else {
         res.status(400).json({
             "result": "failure",
@@ -53,35 +71,44 @@ app.post("/", (req, res) => {
 
 //GET "/:id"
 app.get("/:id", (req, res) => {
-    let videoId = +req.params.id;
-    const found = videos.some(video => video.id === videoId);
+    const videoId = +req.params.id;
 
-    if (found) {
-        let selectedVideo = videos.filter(video => video.id === videoId);
-        res.json(selectedVideo);
-    } else {
-        res.status(404).json({
-            "result": "failure",
-            "message": `No Video with ID ${videoId}`
+    pool
+        .query("SELECT * FROM videos WHERE id=$1", [videoId])
+        .then((result) => {
+            if (result.rowCount) {
+                res.send(result.rows)
+            } else {
+                res.status(404).json({
+                    "result": "failure",
+                    "message": `No Video with ID ${videoId}`
+                })
+            }
         })
-    }
+        .catch((error) => console.error(error));
 });
 
 //DELETE "/:id"
 app.delete("/:id", (req, res) => {
-    let videoId = +req.params.id;
-    const found = videos.some(video => video.id === videoId);
+    const videoId = +req.params.id;
 
-    if (found) {
-        videos = videos.filter(video => video.id !== videoId);
-        res.json({});
-    } else {
-        res.status(404).json({
-            "result": "failure",
-            "message": "Video could not be deleted"
+    pool
+        .query("DELETE FROM videos WHERE id=$1 RETURNING *", [videoId])
+        .then((result) => {
+            console.log(result)
+            if (result.rowCount) {
+                res.json({
+                    "result": "success",
+                    "message": `Video ${videoId} is deleted`
+                });
+            } else {
+                res.json({
+                    "result": "failure",
+                    "message": "Video could not be deleted"
+                });
+            }
         })
-    }
+        .catch((error) => console.error(error));
 })
-
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
