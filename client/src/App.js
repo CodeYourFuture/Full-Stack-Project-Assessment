f32b8da0b532ec80fda12a12afd17137e4ee782d
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-08-06 19:00:47 +0200

    Setup and basic display

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Video Recommendation</h1>
+			</header>
+			<div className="Container">
+				<button>Add Video</button>
+				<div id="search">
+					<label for="searchInput">Search:</label>
+					<input
+						type="text"
+						id="searchInput"
+						placeholder="type here..."
+					></input>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
