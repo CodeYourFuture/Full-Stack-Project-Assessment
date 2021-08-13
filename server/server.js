65587d2da15ba6428536a906006820f1e5376600
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-08-13 14:47:29 +0200

    Add in post method

--- server/server.js
+++ server/server.js
@@ -6,15 +6,29 @@ const videos = require("./exampleresponse.json");
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
 // let videos = [];
+app.use(express.json());
 
 // GET "/"
-app.route("/").get((req, res) => {
-	res.json(videos);
-})
-.post((req,res)=>{
-  
-})
-
-
+app
+	.route("/")
+	.get((req, res) => {
+		res.json(videos);
+	})
+	.post((req, res) => {
+		const { title, url } = req.body;
+		if (title && url) {
+			videos.push({
+				id: Math.ceil(Math.random() * videos.length * 125),
+				title: title,
+				url: url,
+			});
+			res.json(videos[videos.length - 1].id);
+		} else {
+			res.json({
+				result: "failure",
+				message: "Video could not be saved",
+			});
+		}
+	});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
