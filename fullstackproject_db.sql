71603408533a6e9804ab23089da074ded20309bf
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2021-08-19 22:12:50 +0200

    Update fullstackproject_db.sql

--- fullstackproject_db.sql
+++ fullstackproject_db.sql
@@ -7,8 +7,7 @@ CREATE TABLE videos (
   rating   INT NOT NULL
 );
 
-INSERT INTO videos (title, url, rating) VALUES ('Git and GitHub for Beginners','https://www.youtube.com/watch?v=RGOj5yH7evk',28000);
-INSERT INTO videos (title, url, rating) VALUES ('You need to learn Python RIGHT NOW!! // EP 1','https://www.youtube.com/watch?v=mRMmlo_Uqcs'.15000);
-INSERT INTO videos (title, url, rating) VALUES ('Clutching Kills, I Got it AGAIN!','https://www.youtube.com/watch?v=lxC24Ks6aJU'111);
-INSERT INTO videos (title, url, rating) VALUES ('Seether-Fine Again', 'https://www.youtube.com/watch?v=Li2PVKMZCo4');
-INSERT INTO videos (title, url, rating) VALUES ('I bought a DDoS attack on the DARK WEB','https://www.youtube.com/watch?v=eZYtnzODpW4&t=2s'91000 );
+INSERT INTO videos (title, url, rating) VALUES ('Never Gonna Give You Up','https://www.youtube.com/watch?v=dQw4w9WgXcQ',23 );
+INSERT INTO videos (title, url, rating) VALUES ('The Coding Train','https://www.youtube.com/watch?v=HerCR8bw_GE',230 );
+INSERT INTO videos (title, url, rating) VALUES ('Mac & Cheese | Basics with Babish','https://www.youtube.com/watch?v=FUeyrEN14Rk', 11 );
+INSERT INTO videos (title, url, rating) VALUES ('Belkıs ÖOzener - Sevemedim Kara Gozlum','https://www.youtube.com/watch?v=ccYt6LEQtP8', 0 );
