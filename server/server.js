7c11ef5da4a5b97eb22c9266e7def930fbe57eef
BrettScheepers <brettscheepers3100@gmail.com>
2021-08-20 15:29:39 +0200

    Completed 300

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
-var cors = require('cors')
-var app = express()
-
+const cors = require('cors')
+const app = express()
 const port = process.env.PORT || 5000;
+const bodyparser = require('body-parser')
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "sql-fullstack",
+  password: "Styles3100",
+  port: 5432,
+});
 
 app.use(cors())
-
-const bodyparser = require('body-parser')
-
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
@@ -17,64 +23,100 @@ let videos = require('./exampleresponse.json');
 
 // GET "/"
 app.get("/", (req, res) => {
-  res.json(videos);
+  pool
+    .query('SELECT * FROM videos')
+    .then((result) => res.json(result.rows))
+    .catch((e) => console.error(e))
 });
 
 // GET "/{id}"
 app.get("/:id", (req, res) => {
   let id = parseInt(req.params.id)
 
-  let foundVid = videos.find(el => el.id == id)
+  // let foundVid = videos.find(el => el.id == id)
 
-  if (foundVid) res.json(foundVid)
-  else res.json({
-    "result": "failure",
-    "message": "Video could not be found"
-  })
+  // if (foundVid) res.json(foundVid)
+  // else res.status(400).json({
+  //   "result": "failure",
+  //   "message": "Video could not be found"
+  // })
+
+  pool
+    .query('SELECT id FROM videos')
+    .then((result) => {
+      if (!result.rows.some(el => el.id == id)) {
+        res.status(400).json({
+          "result": "failure",
+          "message": "Video could not be found"
+        })
+      } else {
+        pool
+          .query('SELECT * FROM videos WHERE id = $1', [id])
+          .then((result) => res.json(result.rows))
+          .catch((e) => console.error(e))
+      }
+    })
 });
 
 // Post "/"
 app.post("/", (req, res) => {
-  console.log(req.body)
   let { title, url } = req.body
 
   if (!title || !url || url.replace("https://www.youtube.com/watch?v=","").length !== 11) {
-    res.json({
-      "result": "failure",
-      "message": "Video could not be saved"
+    return res.status(400).json({
+      "msg": "Video could not be saved"
     })
   } 
   else {
-    let ids = videos.map(el => el.id)
-    let newId
+    const query = 'INSERT INTO videos (title, url, rating) VALUES ($1, $2, $3)'
 
-    do {
-      newId = Math.floor(Math.random() * 600000)
-    } while (ids.includes(newId))
-
-    let newVid = {}
-    newVid.id = newId
-    newVid.title = title
-    newVid.url = url
-    newVid.rating = 0
-
-    videos.push(newVid)
-    
-    res.json({ "id": newVid.id })
+    pool
+      .query(query, [title, url, 0])
+      .then(() => res.status(200).json({ msg: "Video added successfully" }))
+      .catch((e) => console.error(e))
   }
+})
+
+// Post "/rating/:id"
+app.post("/rating/:id", (req, res) => {
+  let id = parseInt(req.params.id)
+  let { input } = req.body
+  console.log(input)
+
+  pool
+    .query('SELECT rating FROM videos WHERE id = $1', [id])
+    .then((result) => {
+      let rating = result.rows[0].rating
+
+      if (input === "up") rating++
+      if (input === "down" && rating !== 0) rating--
+
+      pool
+        .query('UPDATE videos SET rating = $1 WHERE id = $2', [rating, id])
+        .then(() => res.status(200).json({ msg: "Video added successfully", updated: true }))
+        .catch((e) => console.error(e))
+    })
 })
 
 // Delete "/{id}"
 app.delete("/:id", (req, res) => {
   let id = parseInt(req.params.id)
 
-  let foundVid = videos.find(el => el.id == id)
-
-  if (foundVid) res.json({})
-  else res.json({
-    "result": "failure",
-    "message": "Video could not be deleted"
-  })
+  pool
+    .query('SELECT id FROM videos')
+    .then((result) => {
+      if (!result.rows.some(el => el.id == id)) {
+        res.status(400).json({
+          "result": "failure",
+          "message": "Video could not be found"
+        })
+      } else {
+        pool
+          .query('DELETE FROM videos WHERE id = $1', [id])
+          .then(() => res.send("Video deleted successfully"))
+          .catch((e) => console.error(e))
+      }
+    })
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
