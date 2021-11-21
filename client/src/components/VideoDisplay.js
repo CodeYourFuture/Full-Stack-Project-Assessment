ef543207d455e08e856d0fc6d7fe0bb9c4695580
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-11-21 20:37:00 +0000

    front-end start

--- client/src/components/VideoDisplay.js
+++ client/src/components/VideoDisplay.js
@@ -1,14 +1,31 @@
 const VideoDisplay = ( prop ) =>
 
-
 {
+  let id = prop.video.url.substr(-11, prop.video.url.length);
+
+
     return (
-      <>
-        <ul className='Video-display'>
-                <li key= {prop.index }> id {prop.video.id} </li>
-                <li>{ prop.video.title} </li>
-                <li> {prop.video.url}</li>
-                <li>{prop.video.rating} </li>
+        <>
+        <ul className="Video-display">
+          {/* <li key= {prop.index }> id {prop.video.id} </li> */}
+          <li>{prop.video.title} </li>
+          <li>{prop.video.rating} </li>
+               
+          <li>
+            <iframe
+              width="460"
+              height="415"
+                        src={`https://www.youtube.com/embed/${id}`}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </li>
+
+          <li>
+            <button> delete </button>
+          </li>
         </ul>
       </>
     );
