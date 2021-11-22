512ea54f805c05aabc84cd29e150c7afbaa06f4f
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-11-22 11:31:38 +0000

    feat: add video

--- client/src/components/AddVideo.js
+++ client/src/components/AddVideo.js
@@ -3,19 +3,25 @@ const AddVideo = () => {
     return (
       <>
         <h2 className="addVideo">Add Video</h2>
-            <form className="addvideo-form" method="post" action="submit">
-                <div>
-          <label className="addvideo" for="title" name="title">
-                    Title:</label>
-            <input type="text" name="title" required width='500px'/>
-                   </div><div>
-                        
-            <label className="addvideo" for="url" name="url">  URL:</label>
-                        <input type="text" name="url" required />
-                        
-           </div>
-                        
-          
+        <form className="addvideo-form" method="post" action="submit">
+          <div className="addvideo">
+            <label for="title" name="title">
+              Title:
+            </label>
+            <input className="input" type="text" name="title" required />
+          </div>
+          <div className="addvideo">
+            <label for="url" name="url">
+              {" "}
+              URL:
+            </label>
+            <input className="input" type="text" name="url" required />
+          </div>
+
+          <div className="addvideo">
+            <button>Add</button>
+            <button>Delete</button>
+          </div>
         </form>{" "}
       </>
     );
