551d9f02fbd380d1ad945bb5c1e2c202709e064f
musayuksel <musayuxel@gmail.com>
2021-11-25 12:17:50 +0000

    feat: Add VideoCard component

--- client/src/components/VideoCard.js
+++ client/src/components/VideoCard.js
@@ -1,5 +1,39 @@
 import React from "react";
+import VideoVote from "./VideoVote";
 
-export default function VideoCard() {
-  return <div></div>;
+export default function VideoCard({ videos, setVideos }) {
+  function handleDelete(id) {
+    setVideos((previousVideos) =>
+      previousVideos.filter((video) => video.id !== id)
+    );
+  }
+  //take youtube id from url
+  const videoCards = videos.map((video) => {
+    const youtubeID = video.url.slice(
+      video.url.indexOf("v=") + 2
+    );
+    return (
+      <li key={video.id} className="videoCard">
+        <p>{video.title}</p>
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${youtubeID}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <VideoVote />
+        <button
+          className="deleteButton"
+          onClick={() => handleDelete(video.id)}
+        >
+          DELETE
+        </button>
+      </li>
+    );
+  });
+
+  return <ul>{videoCards}</ul>;
 }
