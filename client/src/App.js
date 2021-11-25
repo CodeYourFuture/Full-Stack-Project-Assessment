0dcaba618031a7394412990f48f914af3bc71413
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-25 13:09:01 +0000

    feat: initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
+import React from 'react';
 import "./App.css";
+import Header from './components/Header'
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
-    </div>
+    <Header />
   );
 }
 
---
+++ client/src/components/VideoCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function VideoCard () {
+    return (
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/{VIDEO_ID_GOES_HERE}"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+    );
+}
