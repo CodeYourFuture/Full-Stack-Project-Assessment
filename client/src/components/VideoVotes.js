78502d89ef88b115a8f5048e88be426b1f3dd02b
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-25 13:42:57 +0000

    feat: add vote icons and functions

--- client/src/components/VideoVotes.js
+++ client/src/components/VideoVotes.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function VideoVotes () {
-    
+  const [ votes , setVotes] = useState(0);
+  function increaseVotesByOne() {
+    setVotes(votes + 1);
+  }
+  function decreaseVotesByOne() {
+    if (votes > 0) {
+      setVotes(votes - 1);
+    }
+  }
+
+  return (
+    <div className="vote-icons">
+      <i class="fas fa-thumbs-up" onClick={increaseVotesByOne}></i>
+      <i class="fas fa-thumbs-down" onClick={decreaseVotesByOne}></i>
+    </div>
+  );
 }
