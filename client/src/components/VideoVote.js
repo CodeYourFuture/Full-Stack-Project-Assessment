42201b581a2c211a1235fe88f0c14dff728f5a99
musayuksel <musayuxel@gmail.com>
2021-11-25 11:21:10 +0000

    feat: Add VideoVote component

--- client/src/components/VideoVote.js
+++ client/src/components/VideoVote.js
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function VideoVote() {
-  return <div></div>;
+  const [voteCount, setVoteCount] = useState(0);
+  function handleLike() {
+    setVoteCount((prev) => prev + 1);
+  }
+  function handleDislike() {
+    setVoteCount((prev) => (prev > 0 ? prev - 1 : 0));
+  }
+  return (
+    <section className="voteContainer">
+      <i
+        className="fa fa-thumbs-up"
+        onClick={() => handleLike()}
+      ></i>
+      <p>Likes: {voteCount}</p>
+      <i
+        className="fa fa-thumbs-down"
+        onClick={() => handleDislike()}
+      ></i>
+    </section>
+  );
 }
