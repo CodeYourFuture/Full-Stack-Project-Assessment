7a48b53249be9da2426eb3115a878e1ad6998ccd
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-25 13:09:40 +0000

    feat: added header

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Video Recommendation</h1>
+      </header>
+    </div>
+  );
+}
