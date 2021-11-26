4c7561b59e7a8d7ce0cf9a3110054fcc008a1f65
musayuksel <musayuxel@gmail.com>
2021-11-26 15:30:03 +0000

    feat: Add search video function

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,23 @@ import VideoAdd from "./components/VideoAdd";
 
 function App() {
   const [videos, setVideos] = useState(exampleVideos);
-
+  function searchVideos(event) {
+    const searchKey = event.target.value.toLowerCase();
+    const searchedVideos = exampleVideos.filter((video) =>
+      video.title.toLowerCase().includes(searchKey)
+    );
+    setVideos(searchedVideos);
+  }
   return (
     <div className="App">
       <VideoAdd setVideos={setVideos} />
+      <input
+        id="searchVideos"
+        type="text"
+        placeholder="Search video..."
+        // value={title}
+        onChange={searchVideos}
+      />
       <VideoCard videos={videos} setVideos={setVideos} />
     </div>
   );
