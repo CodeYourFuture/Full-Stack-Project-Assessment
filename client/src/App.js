0975001ef6e0c25c790e81b6a9d333625b54fce0
Mor Khojasteh <mori.kh1987@gmail.com>
2021-11-26 19:20:35 +0000

    Add Search Component

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,22 @@ import React, { useState } from "react";
 import "./App.css";
 import VideoCard from "./components/VideoCard";
 import data from "./exampleresponse.json";
+import Search from "./components/Search";
 
 
 const App = () => {
   const [videoData, setVideoData] = useState(data);
+  const [search, setSearch] = useState("");
+  const searchingData = videoData.filter((video) =>
+    video.title.toUpperCase().includes(search.toUpperCase())
+  );
   return (
     <div className="App">
       <header className="card text-white bg-info mt-2 pt-2" style={{ height: '80px' }} >
         <h1>Video Recommendation</h1>
       </header>
-      <VideoCard videoData={videoData}/>
+      <Search setSearch={setSearch} search={search} />
+      <VideoCard videoData={videoData} searchingData={searchingData}/>
     </div>
   );
 }
--- client/src/components/VideoCard.js
+++ client/src/components/VideoCard.js
@@ -3,16 +3,18 @@ import React from 'react';
 const VideoCard = ({videoData}) => {
   //console.log(videos);
     return (
-      <div className="App">
+      <div className="flex-container">
         {videoData.map((video) => {
         //console.log(video);
         return (
           <div key={video.id}>
-          <p>{video.title}</p>
-          <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video.url.slice(32)}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+          <p className="title">{video.title}</p>
+          <iframe className="video" src={`https://www.youtube.com/embed/${video.url.slice(32)}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </div>
         )
+
         })}
+         
         hello there!!!
       </div>
     );
