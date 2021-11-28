5ed54dd4bdd39a03caeccf87ea14a7de754e07d3
musayuksel <musayuxel@gmail.com>
2021-11-28 21:00:48 +0000

    feat:add fetch function for post request, refactor:update main fetcdata function for methods

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,6 +1,10 @@
-export default async function fetchData(endPoint) {
+export default async function fetchData(
+  endPoint,
+  methodObj = { method: "GET" }
+) {
   const response = await fetch(
-    `http://127.0.0.1:5000${endPoint}`
+    `http://127.0.0.1:5000${endPoint}`,
+    methodObj
   );
   return response;
 }
