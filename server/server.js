d1b25ea452201cf2e8e10e23f383573eb3698b9b
Mya <myaskylar14@gmail.com>
2021-11-29 18:37:45 +0000

    Add POST request

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const videoData = require("../client/src/data/exampleresponse.json")
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 const port = process.env.PORT || 5000;
 
 
@@ -12,6 +15,32 @@ let videos = [];
 // GET "/"
 app.get("/", (req, res) => {
  res.send(videoData)
+});
+
+app.post("/", (req, res) => {
+  const newVideo = req.body;
+
+  if (!newVideo.title || !newVideo.url) {  
+    res.status(404).send({
+      result: "failure",
+      message: "Video could not be saved",
+    });
+    return;
+  }
+
+    const newVcData = {
+      id: videoData[videoData.length - 1].id + 1,
+      title: newVideo.title,
+      url: newVideo.url,
+    };
+    console.log(newVideo);
+
+    if (newVideo) {
+      videoData.push(newVcData);
+      res.status(201).send({id:newVcData.id});
+    } else {
+      res.status(400).send("error");
+    }
 });
 
 app.get("/:id", (req, res) => {
