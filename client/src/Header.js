0b59cd78df93ce5fd617ba4b136d8783a116af53
ali bavarsad <a.bavarsad007@gmail.com>
2021-11-29 15:37:02 +0000

    add bootstrap and Header component

---
+++ client/src/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="App-header bg-primary text-light p-4">
+      <h1>Video Recommendation</h1>
+    </header>
+  );
+};
+
+export default Header;
