5da5560cc47a9f898181a242a805db7d96584e18
Anza-Azam <83141825+Anza-Azam@users.noreply.github.com>
2021-11-30 09:32:31 +0000

    added get routes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
+let cors = require("cors");
+app.use(cors());
 const port = process.env.PORT || 5000;
+const data = require('../exampleresponse.json');
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = data;
 
 // GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  
+  res.status(200).json(data);
+});
+
+app.get("/:id", (req, res) => {
+  let videoId = Number(req.params.id);
+  let result = data.find(video => video.id === videoId)
+  if(result) {
+    res.status(200).json(result);
+  }
+  else res.status(400).send('video not found')
 });
