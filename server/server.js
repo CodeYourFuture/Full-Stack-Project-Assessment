5d73230cc275f8097d229b88a7edc8149dfa08c1
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-02 10:24:36 +0000

    feat: add get videos

--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.json());
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+const videos = require("./exampleresponse.json");
 
 // GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.send(videos);
 });
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
