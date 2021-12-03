9fd1755ea3c1d95f7c867988b9bd8e952c889b09
omerffaruk <farukyamak11044@gmail.com>
2021-12-03 22:38:44 +0000

    feat: search feature enabled for search bar

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchButton from './SearchButton';
 
-const Search = () => {
+const Search = ({ search }) => {
+  const [searchInput, setSearchInput] = useState("");
+  const handleSearchInput = (e) => {
+    setSearchInput(e.target.value);
+  } 
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    search(searchInput);
+  }
     return (
       <div>
-        <form className="form-group search-box">
+        <form className="form-group search-box" onSubmit={handleSearchSubmit} >
           <input
             type="text"
             id="customerName"
             // className="form-control"
             placeholder="Search for a video"
-            // value={searchInput}
-            // onChange={handleSearchInput}
+            value={searchInput}
+            onChange={handleSearchInput}
           />
           <SearchButton />
         </form>
