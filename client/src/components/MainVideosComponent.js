67de1da3830b41966cfaa43fbcfce41ea31bde22
omerffaruk <farukyamak11044@gmail.com>
2021-12-03 22:47:49 +0000

    feat: data fetched from the local server instead of a local file

--- client/src/components/MainVideosComponent.js
+++ client/src/components/MainVideosComponent.js
@@ -1,31 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import AddAndSearchVideo from "./AddAndSearchVideo";
 import Videos from "./Videos";
-import data from '../exampleresponse.json'
+// import data from '../exampleresponse.json'
 
 const MainVideosComponent = () => {
-  const [videos, setVideos] = useState(data);
-  // const url = 'https://....';
-  // const getVideos = () => {
-  //   fetch(url)
-  //     .then((res) => {
-  //       console.log(res);
-  //       res.json();
-  //     })
-  //     .then((data) => setVideos(data))
-  // }
-  // useEffect(() => {
-  //     getVideos();
-  // }, [])
-  // const search = (searchVal) => {
-  //     searchVal = searchVal.toLowerCase();
-  //     const searchedVideos = videos.filter((video) => video.title.toLowerCase().includes(searchVal));
-  //     setVideos(searchedVideos);
-  // }
+  const [videos, setVideos] = useState("");
+  const fetchVideosData = async (url) => {
+    try {
+      const response = await fetch(url);
+      if (response.status === 200) {
+        const json = await response.json();
+        setVideos(json);
+      } else {
+        const json = response.json();
+        alert(json.msg);
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+  useEffect(() => {
+    fetchVideosData("http://127.0.0.1:5000");
+  }, [])
+  const search = (searchVal) => {
+      searchVal = searchVal.toLowerCase();
+      const searchedVideos = videos.filter((video) => video.title.toLowerCase().includes(searchVal));
+      setVideos(searchedVideos);
+  }
   return (
     <section>
-      <AddAndSearchVideo videos={data} setVideos={setVideos} />
-      <Videos videos={data} setVideos={setVideos} />
+      <AddAndSearchVideo videos={videos} setVideos={setVideos} search={search} />
+      <Videos videos={videos} setVideos={setVideos} />
     </section>
   );
 }
