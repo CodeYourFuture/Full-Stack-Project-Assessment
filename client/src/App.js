de9d0a00845f492bb9c9d88fc00894f58ab084e8
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-12-04 01:38:41 +0000

    Introduced components to App.js!
    you're welcome!

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
-import Header from "./Header";
+import Header from "./components/Header";
+import Cards from "./components/cards";
+import sampleData from "./data/sampleData.json";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        {sampleData.map((video, index) => (
+          <Cards key={index} data={video} />
+        ))}
+      </div>
     </div>
   );
 }
