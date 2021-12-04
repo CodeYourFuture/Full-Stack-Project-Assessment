18bdad94ea9864e3b96c2d6077fb77f9765bc969
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-12-04 01:27:51 +0000

    Header created

--- client/src/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
-    );
-};
-
-export default Header;
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="App-header">
+      <h1>
+        <i class="fa fa-film" aria-hidden="true"></i> Video Recommendation{" "}
+        <i class="fa fa-film" aria-hidden="true"></i>
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
