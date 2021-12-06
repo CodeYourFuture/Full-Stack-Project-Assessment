d3ada0d437ce3c2a526c9f02bf6a818d5eb8de92
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-06 23:00:16 +0000

    feat: add new video

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,15 @@
 import React from "react";
+import AddNewVideo from "./AddNewVideo";
 import SearchVideo from "./SearchVideo"
 
-export default function Header({videos, setVideos}) {
+export default function Header({videos, setVideos, fetchData}) {
   return (
     <div className="header">
       <header className="App-header">
         <h1>Video Recommendation</h1>
         <nav>
           <SearchVideo videos={videos} setVideos={setVideos} />
+          <AddNewVideo fetchData={fetchData} />
         </nav>
       </header>
     </div>
