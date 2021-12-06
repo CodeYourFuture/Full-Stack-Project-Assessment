b95efbcc163e1d6315981a943c828c7555f86bc8
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-12-06 22:31:32 +0000

    feat: add cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-
+app.use(cors());
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
 const videos = require("./exampleresponse.json");
