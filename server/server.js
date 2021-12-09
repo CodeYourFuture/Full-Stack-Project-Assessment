0fdb8ebce7ac3f875c8bbac77e3bf32809acceba
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-12-09 11:55:21 +0000

    Tidy and refactor server code

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//GET endpoint `/order`
+//GET endpoint `/order` to retrieve the complete and sorted video list
 app.get("/", (req, res) => {
   pool.query("SELECT * FROM youtube_videos", (error, result) => {
     const sortOrder = req.query.order;
@@ -35,7 +35,9 @@ app.get("/", (req, res) => {
       : orderedData.sort((v1, v2) => v2.rating - v1.rating);
 
     error
-      ? res.status(400).json({ success: false })
+      ? res
+          .status(400)
+          .json({ result: "success", message: "unable to retrieve video list" })
       : res.status(200).json(orderedData);
   });
 });
@@ -44,47 +46,53 @@ app.get("/", (req, res) => {
 app.get("/:id", (req, res) => {
   const id = parseInt(req.params.id);
   pool.query(
-    `select * FROM youtube_videos AS yv WHERE yv.id = ${id};`,
+    `select * FROM youtube_videos WHERE id = ${id};`,
     (error, result) => {
       error
         ? res.status(400).json({
             result: "failure",
-            message: "No Video with that id",
+            message: `No Video with id:${id} in list`,
           })
         : res.status(200).json(result.rows);
     }
   );
 });
 
-// POST endpoint `/` to add new `video` content with valid field check
+// POST endpoint `/` to add new video content with valid field check
 app.post("/", (req, res) => {
   const title = req.body.title;
   const url = req.body.url;
   const uploaded = req.body.uploaded;
 
   pool.query(
-    `INSERT INTO youtube_videos (title, url, rating, uploaded) VALUES('${title}', '${url}', '${0}', '${uploaded}');`,
+    `INSERT INTO youtube_videos (title, url, rating, uploaded) VALUES('${title}', '${url}', '${0}', '${uploaded}') RETURNING id;`,
     (error, result) => {
       title === undefined || url === undefined || error
-        ? res
-            .status(400)
-            .json({ result: "failure", message: "Video could not be saved" })
-        : res.status(201).json({ id: result.rows.id });
+        ? res.status(400).json({
+            result: "failure",
+            message: `Video "${title}" could not be saved`,
+          })
+        : res.status(201).json({
+            result: "success",
+            message: `Video "${title}" was saved`,
+            id: result.rows[0].id,
+          });
     }
   );
 });
 
-// DELETE endpoint `/:id` with feedback
+// DELETE endpoint `/:id`
 app.delete("/:id", (req, res) => {
   const id = parseInt(req.params.id);
 
   pool.query(
-    `DELETE FROM youtube_videos AS yv WHERE yv.id = ${id};`,
+    `DELETE FROM youtube_videos WHERE id = ${id};`,
     (error, result) => {
       error
-        ? res
-            .status(400)
-            .json({ result: "failure", message: "Video could not be deleted" })
+        ? res.status(400).json({
+            result: "failure",
+            message: `Video with id:${id} could not be deleted`,
+          })
         : res.status(200).json({});
     }
   );
@@ -96,7 +104,7 @@ app.put("/:id", (req, res) => {
   const videoRating = req.body.rating;
 
   pool.query(
-    `UPDATE youtube_videos SET rating = ${videoRating} where id = ${videoId}`,
+    `UPDATE youtube_videos SET rating = ${videoRating} WHERE id = ${videoId}`,
     (error, result) => {
       error
         ? res.status(400).json({
