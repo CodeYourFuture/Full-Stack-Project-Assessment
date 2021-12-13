0b1eb889bccf7658336f93d12f312084859515b3
Benjamin-GitHub <benjaminmhrdd@gmail.com>
2021-12-13 18:48:57 +0000

    Add video

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Cards from "./components/Cards";
 import sampleData from "./data/sampleData.json";
+import AddVideos from "./components/AddVideos";
 
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <AddVideos video={video} setVideo={setVideo} />
       <div id="container">
-        <Cards video={video} setVideo={setVideo}/>
+        <Cards video={video} setVideo={setVideo} />
       </div>
     </div>
   );
