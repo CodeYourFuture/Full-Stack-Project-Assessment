8a8d8a52e62c8f8a23477daa9d753ccb2499136c
konikalily <konika.l.biswas@gmail.com>
2021-12-17 22:16:22 +0000

    level 250 not working!!

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 let videos = require("../client/src/exampleresponse.json");
 const cors = require("cors");
 app.use(express.urlencoded({ extended: false }));
