a35ce227e550c05a1009f603ab72f37f3302b48a
omerffaruk <farukyamak11044@gmail.com>
2021-12-17 17:08:42 +0000

    fix: app.delete('/:id') and app.get('/:id') end points updated for Heroku deployment. Local videos array deleted and pool.query methods used to connect with database deployed at Heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const uuid = require('uuid');
 
 const { Pool } = require("pg");
 
@@ -27,73 +26,6 @@ app.use((req, res, next) => {
   next();
 });
 
-
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-const videos = [
-  {
-    id: 523523,
-    title: "Never Gonna Give You Up",
-    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    rating: 23,
-  },
-  {
-    id: 523427,
-    title: "The Coding Train",
-    url: "https://www.youtube.com/watch?v=HerCR8bw_GE",
-    rating: 230,
-  },
-  {
-    id: 82653,
-    title: "Mac & Cheese | Basics with Babish",
-    url: "https://www.youtube.com/watch?v=FUeyrEN14Rk",
-    rating: 2111,
-  },
-  {
-    id: 858566,
-    title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
-    url: "https://www.youtube.com/watch?v=xbs7FT7dXYc",
-    rating: 11,
-  },
-  {
-    id: 453538,
-    title:
-      "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
-    url: "https://www.youtube.com/watch?v=4As0e4de-rI",
-    rating: 3211,
-  },
-  {
-    id: 283634,
-    title: "Learn Unity - Beginner's Game Development Course",
-    url: "https://www.youtube.com/watch?v=gB1F9G0JXOo",
-    rating: 211,
-  },
-  {
-    id: 562824,
-    title: "Cracking Enigma in 2021 - Computerphile",
-    url: "https://www.youtube.com/watch?v=RzWB5jL5RX0",
-    rating: 111,
-  },
-  {
-    id: 442452,
-    title: "Coding Adventure: Chess AI",
-    url: "https://www.youtube.com/watch?v=U4ogK0MIzqk",
-    rating: 671,
-  },
-  {
-    id: 536363,
-    title: "Coding Adventure: Ant and Slime Simulations",
-    url: "https://www.youtube.com/watch?v=X-iSQQgOd1A",
-    rating: 76,
-  },
-  {
-    id: 323445,
-    title: "Why the Tour de France is so brutal",
-    url: "https://www.youtube.com/watch?v=ZacOS8NBK6U",
-    rating: 73,
-  },
-];
-;
 
 // GET "/"
 app.get("/", (req, res) => {
@@ -136,27 +68,33 @@ app.post('/', (req, res) => {
 
 app.get('/:id', (req, res) => {
   const id = req.params.id;
-  const video = videos.filter((vid) => vid.id == id);
-  res.send(video);
+  const query = 'SELECT * FROM videos WHERE id=$1';
+  pool.query(query, [id])
+  .then((result) => {
+    if(result.rows.length === 0) {
+      res.status(400).send(`There is no video with the id of ${id}`);
+    } else {
+      res.status(200).send(result.rows);
+    }
+  })
 })
 
 app.delete('/:id', (req, res) => {
-  const id = req.params.id;
-  const videoToDeleteIndex = videos.findIndex((vid) => vid.id == id);
-  if(videoToDeleteIndex === -1) {
-    res.send({
-      result: "failure",
-      message: "Video could not be deleted",
-    });
-    return;
-  } else {
-    videos.splice(videoToDeleteIndex, 1);
-    res.send({"message": `Video with the id of ${id} deleted.`});
-  }
+  const videoId = req.params.id;
+  const selectVideoQuery = 'SELECT * FROM videos WHERE id=$1';
+  pool.query(selectVideoQuery, [videoId])
+  .then((result) => {
+    if(result.rows.length === 0) {
+      res.status(400).send(`Could not delete! There is no video with the id of ${videoId}!`);
+    }else {
+      const deleteQuery = 'DELETE FROM videos WHERE id=$1';
+      pool.query(deleteQuery, [videoId])
+      .then(() => {
+        res.send(`The video with the id of ${videoId} has been deleted!`);
+      })
+    }
+  })
 })
-
-
-
 
 
 
