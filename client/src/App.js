b6ad3e10dc87469cad958258cc45e00072979e4d
TomRafferty <86563641+TomRafferty@users.noreply.github.com>
2022-01-27 17:27:48 +0000

    imported data and made a few changes
    I've had to move some things around to make this work for the placeholder data, I figured that this wouldn't matter as this is only for the placeholder data.
    I've also added some commented out code ready for live data.

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
-    </div>
-  );
-}
+// placeholder data:
+// import "../../exampleresponse.json"; <-- This causes problems so I moved the .json
+// to this directory:
+import originalData from "./exampleresponse.json";
+
+  // ready for real data:
+  // import { useEffect } from "react";
+
+  function App() {
+    //   ready for real data:
+    //   useEffect(() => {
+    //     fetch('url')
+    //     .then(res => {
+    //       if(res.ok){
+    //         return res.json()
+    //       }
+    //       throw res;
+    //     })
+    //     .then(data => {
+    //       //set the data to a variable
+    //     })
+    //     .catch(error => {
+    //       console.log(`ERROR: ${error}`);
+    //     })
+    //   }, [])
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Video Recommendation</h1>
+        </header>
+      </div>
+    );
+  };
 
 export default App;
