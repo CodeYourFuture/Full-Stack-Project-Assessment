969c491d6d25c781dff853adada92189e2eb32c0
Maria Chira <chiramariageorgiana@gmail.com>
2022-01-28 23:04:49 +0000

    Level 1 completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,70 @@
+import React, { useState } from "react";
 import "./App.css";
+import "./index.js";
+import Jumbotron from "./Jumbotron";
+import AddVideo from "./AddVideo";
+import VideoCard from "./VideoCard";
+import ExampleResponse from "./exampleresponse.json";
+import Footer from "./Footer";
 
 function App() {
+  const [id, setId] = useState(0);
+  const [allVideos, setAllVideos] = useState(ExampleResponse);
+
+  const decreaseRating = (id) => {
+    const updatedVideos = allVideos.map((video) => {
+      return {
+        ...video,
+        rating:
+          video.id === id ? (video.rating = video.rating - 1) : video.rating
+      };
+    });
+    setAllVideos(updatedVideos);
+  };
+
+  const increaseRating = (id) => {
+    const updatedVideos = allVideos.map((video) => {
+      return {
+        ...video,
+        rating:
+          video.id === id ? (video.rating = video.rating + 1) : video.rating,
+      };
+    });
+    setAllVideos(updatedVideos);
+  };
+
+  const deleteVideo = (id) => {
+    const updatedVideos = allVideos.filter((video) => video.id !== id);
+    setAllVideos(updatedVideos);
+  };
+
+  const addVideo = (title, url) => {
+    const newVideo = {
+      id: id,
+      title: title,
+      url: url,
+      rating: 0,
+    };
+    setId(id + 1);
+    allVideos.push(newVideo);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
+      <Jumbotron />
+      <AddVideo addVideo={addVideo} />
+      <div className="VideoCardsContainer">
+        {allVideos.map((video, index) => (
+          <VideoCard
+            key={index}
+            video={video}
+            addVote={increaseRating}
+            removeVote={decreaseRating}
+            deleteVideo={deleteVideo}
+          />
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/VideoCard.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./VideoCard.css"
+
+const VideoCard = (props) => {
+
+    const idLength = 11;
+    const videoId = props.video.url.split("").splice(-idLength).join("");
+
+    const handleLike = () => {
+        props.addVote(props.video.id);
+    }
+
+    const handleDislike = () => {
+      props.removeVote(props.video.id);
+    }
+
+    const handleDelete = () => {
+      props.deleteVideo(props.video.id)
+    }
+    
+    return (
+      <div className="VideoCard">
+        <h1 className="Title">{props.video.title}</h1>
+        <iframe
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <div className="LikeSection">
+          <div className="Like" onClick={handleLike}>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Thumb_up_icon_2.svg/1200px-Thumb_up_icon_2.svg.png"
+              alt="thumbs up icon"
+            />
+            <p className="Votes">{props.video.rating}</p>
+          </div>
+          <div className="Dislike" onClick={handleDislike}>
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/25/25395.png"
+              alt="thumbs up icon"
+            />
+          </div>
+        </div>
+        <button className="DeleteBtn" onClick={handleDelete}>Remove Video</button>
+      </div>
+    );
+}
+
+export default VideoCard;
