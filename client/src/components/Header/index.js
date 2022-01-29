4f7d7a4829367bcd28dcae9620ecacc99f28b8fa
smkamaldar <smkamaldar@gmail.com>
2022-01-29 21:49:49 +0000

    simple header component

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+    return (
+        <header className="header">
+            <h4 className="header__title">iTube</h4>
+        </header>
+    )
+}
+
+export default Header;
