ba6677b740870b9a6607423d6a42e636b080a596
smkamaldar <smkamaldar@gmail.com>
2022-01-30 02:36:16 +0000

    WIP: adding onclick for like , dislike, delete

--- client/src/components/VideoCard/index.js
+++ client/src/components/VideoCard/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./videoCard.css";
 
 function VideoCard(props) {
-    const { title, url, rating } = props
+    const { id, title, url, rating, onLike, onDislike, onDelete } = props
     return (
         <div className="video-card">
             <iframe className="video-card__video"
@@ -13,7 +13,9 @@ function VideoCard(props) {
             <div className="video-card__footer">
                 <h6 className="footer__title">{title}</h6>
                 <div className="footer__actions">
-
+                    <i className="far fa-thumbs-up" onClick={() => onLike(id)}>{rating}</i>
+                    <i className="far fa-thumbs-down" onClick={() => onDislike(id)}></i>
+                    <button className="footer__delete" onClick={() => onDelete(id)}>Delete</button>
                 </div>
             </div>
         </div>
--- client/src/containers/VideoCards/index.js
+++ client/src/containers/VideoCards/index.js
@@ -4,14 +4,21 @@ import mockData from "../../exampleresponse.json"
 import "./videoCards.css"
 
 function VideoCards() {
+    const handleLike = id => { console.log(id) }
+    const handleDislike = id => { console.log(id) }
+    const handleDelete = id => { console.log(id) }
     return (
         <div className="video-cards">
             {mockData.map((data) => {
                 return <VideoCard
                     key={data.id}
+                    id={data.id}
                     title={data.title}
                     url={data.url.replace("watch?v=", "embed/")}
                     rating={data.rating}
+                    onLike={handleLike}
+                    onDislike={handleDislike}
+                    onDelete={handleDelete}
                 />
             })}
         </div>
