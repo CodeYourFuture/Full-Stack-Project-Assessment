20a4d11ddddb83e082a809947703197bba7a164c
smkamaldar <smkamaldar@gmail.com>
2022-01-30 18:26:31 +0000

    adding AddButton - styling footer-action

---
+++ client/src/components/AddButton/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./addButton.css";
+
+function AddButton() {
+    return (
+        <button className="add-button">+</button>
+    )
+}
+export default AddButton;
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import AddButton from "../../components/AddButton";
 import VideoCards from "../../containers/VideoCards";
+
 
 function Home() {
 
     return (
         <main className="home">
+            <AddButton />
             <VideoCards />
         </main>
     )
