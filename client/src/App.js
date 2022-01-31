7a6830708b98939e8f1e7a75af06863d80f1d688
Wiam Nasr <wiamnasr@gmail.com>
2022-01-31 21:55:41 +0000

    finished level 100

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import "./App.css";
+import { useState } from "react";
+import NewVideo from "./components/NewVideo/NewVideo";
 import VideoComponent from "./components/VideoComponent/VideoComponent";
 
 import videoData from "./exampleresponse.json";
 
 function App() {
+  const [currVideos, setCurrVideos] = useState(videoData);
+
+  const videoSubmitHandler = (submittedVideo) => {
+    setCurrVideos((prevVideos) => [submittedVideo, ...prevVideos]);
+  };
+
   return (
     <div className='App'>
       <header className='App-header'>
         <h1 className='pageHeading'>Video Recommendation</h1>
       </header>
+      <NewVideo videoSubmitHandler={videoSubmitHandler} />
       <div>
-        {videoData.map((video, index) => (
+        {currVideos.map((video, index) => (
           <VideoComponent
             key={index}
             videoId={video.url.split("v=")[1].substring(0, 11)}
             videoTitle={video.title}
-            videoRating={video.rating}
+            videoRating={video.rating ? video.rating : ""}
           />
         ))}
       </div>
