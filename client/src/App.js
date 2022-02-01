a8c939775ce5d20c3b5fde152da0e42f35a8a02e
gynaa78 <gynaa78@yahoo.com>
2022-02-01 21:12:55 +0000

    videos showing, delete and vote buttons installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,60 @@
 import "./App.css";
-import React from 'react';
-import data from "./data";
+import React , { useState } from 'react';
+import { BsFillHandThumbsUpFill } from "react-icons/bs";
+import { BsFillHandThumbsDownFill } from "react-icons/bs";
+import data from "./data.json";
 
 
 function App() {
-
-  console.log(data);
+const [videos, setVideos] = useState(data);
+ const [like, setLike] = useState(0);
+ const [disLike, setDisLike] = useState(0);
+ const clickLike = () => {
+   setLike((like) => like + 1);
+ };
+ const clickDisLike = () => {
+   setDisLike((disLike) => disLike + 1);
+ };
+function  clickHandler (ID){
+setVideos(videos.filter((item) => item.id !== ID));
+ }
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
         <div>
           {
-           data.map((video)=> {
+           videos.map((video)=> {
               return (
                 <div key={video.id}>
+                  <div>
+                    <BsFillHandThumbsUpFill
+                      onClick={clickLike}
+                  
+                    />
+                    <span>{like}</span>
+                    <h5>Vote</h5>
+
+                    <BsFillHandThumbsDownFill
+                      onClick={clickDisLike}
+                    
+                    />
+                    <span>{disLike}</span>
+                  </div>
                   <h2>{video.title}</h2>
-                  <iframe width="560" height="315" src={video.url} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <iframe
+                    title={video.title}
+                    width="560"
+                    height="315"
+                    src={video.url.replace("watch?v=", "embed/")}
+                    frameborder="0"
+                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen
+                  ></iframe>
                   <p>{video.rating}</p>
+                  <button onClick={() => clickHandler(video.id)}>Delete</button>
                 </div>
-              )
+              );
            }
            )  
           }
