dd3e40e3f68849cb5eb258e9a9d6dc18743186e4
Maria Chira <chiramariageorgiana@gmail.com>
2022-02-05 00:18:17 +0000

    Level 250 completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,46 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import "./index.js";
 import Jumbotron from "./Jumbotron";
 import AddVideo from "./AddVideo";
 import VideoCard from "./VideoCard";
-import ExampleResponse from "./exampleresponse.json";
 import Footer from "./Footer";
 
-//Ordering the results
-const OrderedData = ExampleResponse.sort((video1, video2) => video2.rating - video1.rating)
+
+
 
 function App() {
   const [id, setId] = useState(0);
-  const [allVideos, setAllVideos] = useState(OrderedData);
+  const [allVideos, setAllVideos] = useState([]);
+
+  //Ordering the results
+  const orderedData = (array) =>
+    array.sort((video1, video2) => video2.rating - video1.rating);
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/")
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          return response.json();
+        }
+        throw `${response.status} ${response.statusText}`;
+      })
+      .then((data) => {
+        console.log(data);
+
+        setAllVideos(orderedData(data));
+      })
+      .catch((error) => console.log(error));
+  }, [])
+  
 
   const decreaseRating = (id) => {
     const updatedVideos = allVideos.map((video) => {
       return {
         ...video,
         rating:
-          video.id === id ? (video.rating = video.rating - 1) : video.rating
+          video.id === id ? (video.rating = video.rating - 1) : video.rating,
       };
     });
     setAllVideos(updatedVideos);
@@ -47,17 +68,19 @@ function App() {
       title: title,
       url: url,
       rating: 0,
-      date: `${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}`
+      date: `${new Date().toLocaleDateString()} - ${new Date().toLocaleTimeString()}`,
     };
 
     //URL validation
-    const isValidUrl = newVideo.url.match(/^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/);
+    const isValidUrl = newVideo.url.match(
+      /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/
+    );
 
-    if (!isValidUrl) { 
+    if (!isValidUrl) {
       alert("PLease enter a valid YouTube URL");
     } else if (!newVideo.title) {
       alert("Please enter a title");
-    } else{
+    } else {
       setId(id + 1);
       allVideos.push(newVideo);
     }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
