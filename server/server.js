eb455a83c028db64ee486881069f3548a6752ba0
tenzyns <86624297+tenzyns@users.noreply.github.com>
2022-02-10 19:38:12 +0000

    Front-end connected to backend

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+
 app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
