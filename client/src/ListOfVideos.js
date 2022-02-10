24ba169af3df56342ab926036100efd6dec11498
Halla-S <86550752+Halla-S@users.noreply.github.com>
2022-02-10 10:55:17 +0000

    Update level 299 after deploying my video API

--- client/src/ListOfVideos.js
+++ client/src/ListOfVideos.js
@@ -1,14 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BsFillHandThumbsUpFill } from "react-icons/bs";
 import { BsFillHandThumbsDownFill } from "react-icons/bs";
 import "./App.css";
-
+import OrderButton from "./OrderButton";
 
 const ListOfVideos = (props) => {
   const [like, setLike] = useState(0);
   const [disLike, setDisLike] = useState(0);
-   const [oneVideo, setOneVideo] = useState(props.ordered);
-
+  const [oneVideo, setOneVideo] = useState([]);
+  const [order, setOrder] = useState([]);
+  const [descOrder, setDescOrder] = useState([]);
+  const [clicked, setClicked] = useState(true);
   const clickLike = () => {
     setLike((like) => like + 1);
   };
@@ -19,45 +21,91 @@ const ListOfVideos = (props) => {
   const deleteHandler = (Id) => {
     setOneVideo(oneVideo.filter((item) => item.id !== Id));
   };
-  
-  return (
-    <div className="video-container">
-      {oneVideo.map((item, index) => {
-        return (
-          <div key={index} className="visible">
-            <div className="vote">
-              <BsFillHandThumbsUpFill onClick={clickLike} className="thumb" />
-              <span>{like}</span>
-              <h5>Vote</h5>
-              <BsFillHandThumbsDownFill
-                onClick={clickDisLike}
-                className="thumb"
-              />
-              <span>{disLike}</span>
-            </div>
-            <h4>Rating:{item.rating}</h4>
 
-            <p>{item.title}</p>
-            <iframe
-              width="300"
-              height="300"
-              src={item.url.replace("watch?v=", "embed/")}
-              // src={item.vurl.replace("watch?v=", "embed/")} when try:https://video-rec.herokuapp.com
-              title={item.title}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-            <button
-              onClick={() => deleteHandler(item.id)}
-              type="button"
-              className="btn btn-primary mt-3"
-            >
-              Delete
-            </button>
-          </div>
-        );
-      })}
+  const videosData = () => {
+    fetch("https://myvideos-api.herokuapp.com/") //I deployed my api app to heroku as I was not able to connect to my local host
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setOneVideo(data);
+        setDescOrder(data);
+      });
+  };
+  useEffect(() => {
+    videosData();
+  }, []);
+
+  const Data = () => {
+    fetch("https://myvideos-api.herokuapp.com/?order=asc") //try https://video-rec.herokuapp.com
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setOrder(data);
+      });
+  };
+  useEffect(() => {
+    Data();
+  }, []);
+  const orderHandler = () => {
+    setClicked(true);
+    if (clicked) {
+      setOneVideo(order);
+      setClicked(false);
+    }
+    if (!clicked) {
+      setOneVideo(descOrder);
+      setClicked(true);
+    }
+  };
+
+  return (
+    <div>
+      <OrderButton order={orderHandler} />
+      <div className="video-container">
+        {oneVideo.map((item, index) => {
+          return (
+            <div key={index} className="visible">
+              <div className="vote">
+                <BsFillHandThumbsUpFill onClick={clickLike} className="thumb" />
+                <span>{like}</span>
+                <h5>Vote</h5>
+                <BsFillHandThumbsDownFill
+                  onClick={clickDisLike}
+                  className="thumb"
+                />
+                <span>{disLike}</span>
+              </div>
+              <h4>Rating:{item.rating}</h4>
+
+              <p>{item.title}</p>
+              <iframe
+                width="300"
+                height="300"
+                src={item.url.replace("watch?v=", "embed/")}
+                title={item.title}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+              <button
+                onClick={() => deleteHandler(item.id)}
+                type="button"
+                className="btn btn-primary mt-3"
+              >
+                Delete
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
