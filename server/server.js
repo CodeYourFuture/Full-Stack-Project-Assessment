0e17381074a2e6c3ef5560f1fc237f82150a9799
Professional-Kate <professionalkateg@gmail.com>
2022-02-11 16:44:01 +0000

    updated the node server, it works now - GET, DELETE and POST all work like beofre but now uses the databse on heroku :abase on heroku. Let's not talk about the react code as it doesn't work, last week I was right, one change and it all broke and honestly I cand honestly I just can't with it right now. I'm going to watch some videos and re-read the material for react as I don't understand it at nderstand it at all. Will do that then make this project working again. Frontend - broken, backend - working.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const { Pool } = require("pg");
+const secrets = require("./secrets.json");
+
+const pool = new Pool({
+  ...secrets,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -18,48 +27,50 @@ app.use((req, res, next) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-const generateRandomNumber = () => Math.floor(Math.random() * 10000000);
-
-const videos = [];
+const basicQuery = "SELECT name, url, date_added, rating FROM videos";
 
 // GET "/" : serve all videos
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  const { rows } = await pool.query(basicQuery);
+
+  return res.json(rows);
+
   res.json({ videos: videos });
 });
 
 // GET "/{id}" : serve one video based on id
-app.get("/:id", (req, res) => {
+app.get("/:id", async (req, res) => {
+  const query = `${basicQuery} WHERE id = $1;`;
   const id = req.params.id;
-  const filteredVideos = videos.filter((video) => video.id === parseInt(id));
 
-  filteredVideos.length === 0
-    ? res.status(500).json({
+  const { rows } = await pool.query(query, [id]);
+
+  rows.length === 0
+    ? res.status(400).json({
         result: "failure",
         message: `Video width id '${id}' could not be found`,
       })
-    : res.json(filteredVideos);
+    : res.json(rows);
 });
 
 // POST "/" : add a video
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
+  const query = `INSERT INTO videos (name, url, date_added) 
+  VALUES ($1, $2, $3)
+  RETURNING *;`;
   const { title, url } = req.body; // deconstruct the body so we only get the keys we want
-  let id;
-
-  // making sure the ID we generate doesn't already exist
-  while (true) {
-    id = generateRandomNumber();
-    if (videos.some((video) => video.id === id)) continue;
-    else break;
-  }
 
   const linkValid = url.match(
-    /^(?:https?:\/\/)?(?:(?:www\.)?youtube.com\/watch\?v=|youtu.be\/)(\w+)$/
+    /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w-_]+)/gim
   );
 
-  const linkExists = videos.every((video) => video.url !== url);
+  const result = await pool.query(
+    "SELECT EXISTS (SELECT 1 FROM videos WHERE url = $1);",
+    [url]
+  );
 
-  if (linkValid === null || !title.trim() || !linkExists)
-    return res.status(500).json({
+  if (linkValid === null || !title.trim() || !result.rows[0].exists)
+    return res.status(400).json({
       result: "failure",
       message: "Video could not be saved",
     });
@@ -69,28 +80,28 @@ app.post("/", (req, res) => {
   const month = date.getMonth() + 1; // January is 0 without the + 1?
   const year = date.getFullYear();
 
-  videos.push({
+  const { rows } = await pool.query(query, [
     title,
-    url,
-    id,
-    rating: 0,
-    dateAdded: `${day}/${month}/${year}`,
-  });
+    url.split("=")[1],
+    `${year}-${month}-${day}`,
+  ]);
 
-  res.json({ id: id });
+  res.json(rows);
 });
 
 // DELETE "/{id}" : remove a video from the array based on an ID
-app.delete("/:id", (req, res) => {
+app.delete("/:id", async (req, res) => {
   const id = req.params.id;
-  const index = videos.findIndex((video) => video.id === parseInt(id));
+  const query = "DELETE FROM videos WHERE id = $1 RETURNING *;";
 
-  if (index === -1)
-    return res.status(500).json({
-      result: "failure",
-      message: "Video could not be deleted",
-    });
+  const { rows } = await pool.query(query, [id]);
 
-  videos.splice(index, 1);
-  res.json({});
+  rows.length === 0
+    ? res
+        .status(400)
+        .json({
+          success: false,
+          message: `No video with id of ${id} could be found`,
+        })
+    : res.json(rows);
 });
