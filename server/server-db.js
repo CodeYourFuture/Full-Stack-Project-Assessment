3960f34121d210f66d1f6cd20b19edf663551f6c
smkamaldar <smkamaldar@gmail.com>
2022-02-24 20:24:28 +0000

    adding ssl e

--- server/server-db.js
+++ server/server-db.js
@@ -15,9 +15,9 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
-    // ssl: {
-    //     rejectUnauthorized: false,
-    // },
+    ssl: {
+        rejectUnauthorized: false,
+    },
 })
 
 // loading front end application
