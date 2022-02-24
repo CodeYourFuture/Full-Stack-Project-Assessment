f34ae26a3dafd59f3e0cf9a562272f7d693d5123
smkamaldar <smkamaldar@gmail.com>
2022-02-24 19:26:10 +0000

    db configuration

--- server/server-db.js
+++ server/server-db.js
@@ -11,9 +11,9 @@ app.use(cors());
 const pool = new Pool({
     user: process.env.DB_USERNAME,
     host: process.env.DB_HOST,
-    database: 'itube',
+    database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
-    port: 5432
+    port: process.env.DB_PORT
 })
 
 // GET "/" , returns all videos 
