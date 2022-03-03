40235a08f7bb5fccac6f9b9a4be37bec48a898d4
Wiam Nasr <wiamnasr@gmail.com>
2022-03-03 14:46:50 +0000

    finalized the full stack project

--- client/src/components/VideoComponent/VideoComponent.js
+++ client/src/components/VideoComponent/VideoComponent.js
@@ -12,14 +12,8 @@ const VideoComponent = ({ videoId, videoTitle, videoRating }) => {
     videoRating,
   });
 
-  const removeButtonClickHandler = () => {
+  const removeButtonClickHandler = (e) => {
     setRemoveVideo(true);
-    fetch(`http://127.0.0.1:5000/${videoId}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((response) => response.json());
   };
 
   const likeButtonClickHandler = () =>
