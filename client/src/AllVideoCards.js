3538985b38578b37e39d6c079dd8c6e9079bbfef
humailhasankhan <h.hasan.khan88@hotmail.com>
2022-03-07 17:47:09 +0000

    change fetch method due to errors

--- client/src/AllVideoCards.js
+++ client/src/AllVideoCards.js
@@ -4,31 +4,21 @@ import AllVideos from "./AllVideos";
 
 function AllVideoCards() {
   const [videos, setVideos] = useState([]);
-  const fetchData = async () => {
-    const url = "https://humailkhan-assessment-project.herokuapp.com/";
+  const url = "https://humailkhan-assessment-project.herokuapp.com/";
 
-    try {
-      const response = await fetch(url, {
-        mode: "cors",
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods":
-            "GET, POST, PATCH, PUT, DELETE, OPTIONS",
-          "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
-        },
-      });
-      if (response.status === 200) {
-        const result = await response.json();
-        console.log(result);
-        setVideos(result);
-      } else {
-        const result = response.json();
-        alert(result.msg);
-      }
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
+  function fetchData() {
+    fetch(url, {
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods":
+          "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setVideos(data));
+  }
   useEffect(() => {
     fetchData();
   }, []);
