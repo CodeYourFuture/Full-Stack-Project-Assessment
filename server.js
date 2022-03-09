82d523a3a453149d7312e8f0436ebea04c3f12e4
humailhasankhan <h.hasan.khan88@hotmail.com>
2022-03-09 12:54:42 +0000

    changes to build

--- server.js
+++ server.js
@@ -7,14 +7,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+app.use(express.static(path.join(__dirname, "client/build")));
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
 }
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 const { Pool } = require("pg");
 require("dotenv").config();
@@ -24,6 +25,9 @@ const devConfig = {
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 };
 
 const proConfig = { connectionString: process.env.DATABASE_URL };
@@ -36,7 +40,7 @@ pool.connect();
 
 // GET all videos:
 
-app.get("/videos", (request, response) => {
+app.get("/", (request, response) => {
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Credentials", true);
   response.header(
@@ -57,7 +61,7 @@ app.get("/videos", (request, response) => {
   });
 });
 
-app.post("/videos", function (request, response) {
+app.post("/", function (request, response) {
   response.header("Access-Control-Allow-Origin", "*");
   response.header("Access-Control-Allow-Credentials", true);
   response.header(
@@ -92,7 +96,7 @@ app.post("/videos", function (request, response) {
   });
 });
 
-app.get("/videos/:videoId", (request, response) => {
+app.get("/:videoId", (request, response) => {
   const videoId = request.params.videoId;
   const query = `SELECT * FROM videos WHERE id =$1`;
   pool.query(query, [videoId]).then((result, error) => {
@@ -108,7 +112,7 @@ app.get("/videos/:videoId", (request, response) => {
   });
 });
 
-app.delete("/videos/:id", function (request, response) {
+app.delete("/:id", function (request, response) {
   const id = request.params.id;
   const query = `SELECT * FROM videos WHERE id=$1`;
   pool.query(query, [id]).then((result) => {
