aaf46718ddc6c62decb7d0fc05caf065f75a56e9
Gulnihal Naldoken <gh.naldoken@gmail.com>
2022-03-23 10:36:45 +0000

    fix: add no-cors to fetch

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Header from './components/Header';
+import Header from "./components/Header";
 // import videosData from "./exampleresponse.json";
-import AllVideoCards from './components/AllVideoCards';
+import AllVideoCards from "./components/AllVideoCards";
 
 function App() {
   const [videos, setVideos] = useState([]);
-// fetch data
+  // fetch data
   const fetchData = () => {
-    fetch('https://fullstack-gulnihal.herokuapp.com/', {
-    headers: {        "Access-Control-Allow-Origin": "*",        "Access-Control-Allow-Methods":          "GET, POST, PATCH, PUT, DELETE, OPTIONS",        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
-        Accept: "application/json",        "Content-type": "application/json" 
-      }
+    fetch("https://fullstack-gulnihal.herokuapp.com/", {
+      mode:  "no-cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods":
+          "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+      },
     })
       .then((res) => res.json())
       .then((data) => {
-        data.sort((a, b) => a.rating - b.rating);
-        setVideos(data);
-      });
-  }
-  useEffect(()=> {
-    fetchData()
-   }, []); 
-   
+        if (data) {
+          setVideos(data);
+        }
+      })
+      .catch((e) => console.log(e));
+  };
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Header videos={videos} setVideos={setVideos} fetchData={fetchData} />
