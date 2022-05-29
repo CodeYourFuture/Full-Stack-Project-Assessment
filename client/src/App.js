e81f21077736e907403cd85bc95cebcd3e575ec2
hachi-ops <elen20@protonmail.com>
2022-05-29 23:26:28 +0100

    Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,47 @@
 import "./App.css";
 import exampleresponse from "./exampleresponse.json";
+import Header from "./Header";
 
 function Video() {
+  // const videoIdArray = [
+  //   "dQw4w9WgXcQ",
+  //   "HerCR8bw_GE",
+  //   "FUeyrEN14Rk",
+  //   "xbs7FT7dXYc",
+  //   "4As0e4de-r",
+  //   "gB1F9G0JXOo",
+  //   "RzWB5jL5RX0",
+  //   "U4ogK0MIzqk",
+  //   "X-iSQQgOd1A",
+  //   "ZacOS8NBK6U",
+  // ];
+
+  // const videoIdMap = videoIdArray.map((id) => {
+  //   console.log(id);
+  //   return id;
+  // });
   const videoData = exampleresponse.map((item) => {
     return (
-      <div className="video-container">
-        <h2>{item.title}</h2>
-        <video src={item.url} alt={""} />
-        <div className="votes">
-          <p>{item.rating}</p>
-          <p>up-vote icon</p>
-          <p>down-vote icon</p>
+      <div className="videos-container">
+        <div className="video-container">
+          <h2>{item.title}</h2>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/{VIDEO_ID_GOES_HERE}"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+          {/* <video src={item.url} alt={""} /> */}
+          <div className="votes">
+            <p>{item.rating}</p>
+            <p>up-vote icon</p>
+            <p>down-vote icon</p>
+          </div>
+          <button>button to remove video</button>
         </div>
-        <button>button to remove video</button>
       </div>
     );
   });
@@ -33,9 +62,7 @@ function AddVideo() {
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
+      <Header />
       <main>
         <AddVideo />
         <Video />
