af2f88422fe6694e9876434a4d1fb55f102c3db2
hachi-ops <elen20@protonmail.com>
2022-05-29 18:22:30 +0100

    initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
+        <div className="video-container">
+          <h2>video's title</h2>
+          <p>embedded video</p>
+          <p>number of votes</p>
+          <button>button to remove video</button>
+        </div>
       </header>
     </div>
   );
