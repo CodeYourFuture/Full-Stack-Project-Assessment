c63423aa5df3d56b0ee8ca45aadf0bf1814faf6b
hachi-ops <elen20@protonmail.com>
2022-05-29 21:03:02 +0100

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,45 @@
 import "./App.css";
+import exampleresponse from "./exampleresponse.json";
+
+function Video() {
+  const videoData = exampleresponse.map((item) => {
+    return (
+      <div className="video-container">
+        <h2>{item.title}</h2>
+        <video src={item.url} alt={""} />
+        <div className="votes">
+          <p>{item.rating}</p>
+          <p>up-vote icon</p>
+          <p>down-vote icon</p>
+        </div>
+        <button>button to remove video</button>
+      </div>
+    );
+  });
+  return <>{videoData}</>;
+}
+
+function AddVideo() {
+  return (
+    <div className="add-video-container">
+      <p>title</p>
+      <p>url</p>
+      <button>cancel</button>
+      <button>add</button>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
-        <div className="video-container">
-          <h2>video's title</h2>
-          <p>embedded video</p>
-          <p>number of votes</p>
-          <button>button to remove video</button>
-        </div>
       </header>
+      <main>
+        <AddVideo />
+        <Video />
+      </main>
     </div>
   );
 }
