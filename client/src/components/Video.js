f659a75456c2bb5baf374f264e2f3abe6582ffc3
hachi-ops <elen20@protonmail.com>
2022-06-01 00:37:25 +0100

    components/styling

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,20 +1,20 @@
-import React from "react";
+// import React from "react";
 
-function Video() {
-  return (
-    <div className="video">
-      <h2>Video title</h2>
+// function Video() {
+//   return (
+//     <div className="video">
+//       <h2>Video title</h2>
 
-      <p>embedded video</p>
+//       <div className="votes">
+//         <button>up</button>
+//         <p>number of votes</p>
+//         <button>down</button>
+//       </div>
 
-      <div className="votes">
-        <button>up</button>
-        <p>number of votes</p>
-        <button>down</button>
-      </div>
-      <button>remove</button>
-    </div>
-  );
-}
+//       <div>embedded video</div>
+//       <button id="remove">remove</button>
+//     </div>
+//   );
+// }
 
-export default Video;
+// export default Video;
--- client/src/components/Videos.js
+++ client/src/components/Videos.js
@@ -1,8 +1,40 @@
 import React from "react";
-import Video from "./Video";
+// import Video from "./Video";
+import data from "../data";
 
 function Videos() {
-  return <Video />;
+  return data.map((item) => {
+    return <Video item={item} />;
+  });
+}
+
+function Video(props) {
+  return (
+    <div className="video">
+      <h2>{props.item.title}</h2>
+      <div className="votes">
+        <button>up</button>
+        <p>{props.item.rating}</p>
+        <button>down</button>
+      </div>
+
+      <div>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+
+      <button id="remove">remove</button>
+
+      <hr />
+    </div>
+  );
 }
 
 export default Videos;
