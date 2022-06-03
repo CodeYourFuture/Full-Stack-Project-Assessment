9e21b398f024873248e746d9e6cfd8f214e6582e
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2022-06-03 13:16:43 +0100

    Full-Stack - Lon8 - Alireza Bagheri

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Video Recommendation</h1>
+        <h1>Test i </h1>
       </header>
     </div>
   );
