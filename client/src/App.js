bad0a795433aa9b0fe87561d5b7968e553b9a762
teniolao <Tennybetty100@gmail.com>
2022-06-06 18:02:18 +0100

    level 100

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,86 @@
 import "./App.css";
+import React, { useState } from "react";
+import { nanoid } from "nanoid";
+import Video from "./components/Video";
+import data from "./exampleresponse.json";
 
 function App() {
+  const [videos, setVideos] = useState(data);
+  const [addFormData, setAddFormData] = useState({
+    title: "",
+    url: "",
+  });
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...addFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setAddFormData(newFormData);
+  };
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newVideo = {
+      id: nanoid(),
+      title: addFormData.title,
+      url: addFormData.url,
+    };
+
+    const newVideos = [...videos, newVideo];
+    console.log(newVideos);
+    setVideos(newVideos);
+  };
+
+  const handleDeleteClick = (videoId) => {
+    const newVideos = [...videos];
+
+    const index = videos.findIndex((video) => video.id === videoId);
+
+    newVideos.splice(index, 1);
+    setVideos(newVideos);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
+
+        <div className="form-container">
+          <h2>Add Video</h2>
+          <form onSubmit={handleAddFormSubmit}>
+            <label>
+              Title
+              <input
+                type="text"
+                name="title"
+                placeholder="Enter title..."
+                onChange={handleAddFormChange}
+                required
+              />
+            </label>
+
+            <label>
+              Url
+              <input
+                type="url"
+                name="url"
+                placeholder="Enter url..."
+                onChange={handleAddFormChange}
+                required
+              />
+            </label>
+            <button type="submit">Add</button>
+          </form>
+        </div>
+        <div className="container">
+          <Video videos={videos} handleDeleteClick={handleDeleteClick} />
+        </div>
       </header>
     </div>
   );
---
+++ client/src/components/Video.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Video = ({ videos, handleDeleteClick }) => {
+  const [count, setCount] = useState(25);
+
+  return videos.map((video) => {
+    const { id, title, url } = video;
+    const base_url = "https://www.youtube.com/embed/";
+    return (
+      <div className="wrapper" key={id}>
+        <h2>{title}</h2>
+        <div>
+          <button onClick={() => setCount(count + 1)}>Up Vote</button> {count}
+          <button onClick={() => setCount(count - 1)}>Down Vote</button>
+        </div>
+        <iframe
+          width="350"
+          height="315"
+          src={base_url + url.slice(-11)}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+        <div>
+          <button type="button" onClick={() => handleDeleteClick(video.id)}>
+            delete
+          </button>
+        </div>
+      </div>
+    );
+  });
+};
+
+export default Video;
