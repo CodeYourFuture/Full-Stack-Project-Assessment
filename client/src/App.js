5aa04e0e4201847a84d5542d183dee5399cd94e7
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-06-06 20:46:00 +0100

    added search input and displaying basic data from object

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
+import React, { useState } from "react";
+import exampleresponse from "./exampleresponse.json";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation Mandeep</h1>
-      </header>
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <h1>Video Recommendation Mandeep</h1>
+        </header>
+      </div>
+      <div>Add video</div>
+      <div>
+        <input
+          className="searchbar"
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />{" "}
+        Search
+      </div>
+      <div>{exampleresponse.map(x => x.title)}</div>
     </div>
   );
 }
