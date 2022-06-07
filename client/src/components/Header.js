f1a23aba481ce623c298af66bae6c7d03bee70ef
Harsheek Thanki <harsheekthanki@gmail.com>
2022-06-07 23:32:37 +0100

    Header styling added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,11 @@ import React from "react";
 function Header() {
   return (
     <header className="App-header">
-      <h1>Video Recommendation</h1>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container d-flex justify-content-center">
+          <h1>Video Recommendation</h1>
+        </div>
+      </nav>
     </header>
   );
 }
