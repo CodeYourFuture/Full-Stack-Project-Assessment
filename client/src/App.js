43a4bfd3dc25a25941743396b219d588cdcda244
mandeepsangha <91365504+mandeepsangha@users.noreply.github.com>
2022-06-10 22:51:17 +0100

    SearchBar Added

--- client/src/App.js
+++ client/src/App.js
@@ -36,59 +36,91 @@ function App() {
         </header>
       </div>
       <div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            setDisplayVideo({
-              title: newVidTitle,
-              url: newVidUrl,
-            });
-            setNewVidTitle("");
-            setNewVidUrl("");
-          }}
-        ></form>
-
+        <div>
+          Search Bar
+          <input
+            className="searchbar"
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <div>
         <div>Add video</div>
       </div>
       <div>
-        <input
-          className="searchbar"
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />{" "}
-        Search
+        <form>
+          <input
+            className="inputBar"
+            type="text"
+            value={newVidTitle}
+            onChange={(e) => setNewVidTitle(e.target.value)}
+          />{" "}
+          Title
+          <input
+            className="inputBar"
+            type="text"
+            value={newVidUrl}
+            onChange={(e) => setNewVidUrl(e.target.value)}
+          />{" "}
+          URL
+        </form>
       </div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          setDisplayVideo({
+            title: newVidTitle,
+            url: newVidUrl,
+          });
+          setNewVidTitle("");
+          setNewVidUrl("");
+        }}
+      >
+        Add a new Video
+      </button>
       <div>
         <ul className="card-container">
-          {displayVideo.map((card) => (
-            <li className="card">
-              <h2>{card.title}</h2>
-              <p>
-                {" "}
-                {
-                  <iframe
-                    src={`https://www.youtube.com/embed/${
-                      card.url.split("v=")[1]
-                    }`}
-                    title="YouTube video player"
-                  ></iframe>
-                }
-              </p>
-              <p>{card.rating}</p>
-              <div className="button-container">
-                <button onClick={() => videoCountPlus(card.id)}>Up Vote</button>
-                <button onClick={() => videoCountMinus(card.id)}>
-                  Down Vote
-                </button>
-              </div>
-              <div>
-                <button onClick={() => handleRemoveItem(card.id)}>
-                  Delete this video
-                </button>
-              </div>
-            </li>
-          ))}
+          {displayVideo
+            .filter((card) => {
+              const { title, id } = card;
+              const searchTermMatches = title
+                .toLowerCase()
+                .includes(searchTerm.toLowerCase());
+              return searchTermMatches;
+            })
+
+            .map((card) => (
+              <li className="card">
+                <h2>{card.title}</h2>
+                <p>
+                  {" "}
+                  {
+                    <iframe
+                      src={`https://www.youtube.com/embed/${
+                        card.url.split("v=")[1]
+                      }`}
+                      title="YouTube video player"
+                    ></iframe>
+                  }
+                </p>
+                <p>{card.rating}</p>
+                <div className="button-container">
+                  <button onClick={() => videoCountPlus(card.id)}>
+                    Up Vote
+                  </button>
+                  <button onClick={() => videoCountMinus(card.id)}>
+                    Down Vote
+                  </button>
+                </div>
+                <div>
+                  <button onClick={() => handleRemoveItem(card.id)}>
+                    Delete this video
+                  </button>
+                </div>
+              </li>
+            ))}
         </ul>
       </div>
     </div>
