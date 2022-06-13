74321d1da51bbbb7f192b6beb5fee6f8845d1d5f
hachi-ops <elen20@protonmail.com>
2022-06-13 20:41:13 +0100

    refactored Video into a separate components

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import smileyFace from "../smiley-face.png";
 import downVoteFace from "../downvote-face.png";
-import data from "../data";
 
-function Video(props) {
-  const [count, setCount] = useState(props.item.rating);
+function Video({ item, onRemove }) {
+  const [count, setCount] = useState(item.rating);
 
   function add() {
     setCount((prevCount) => prevCount + 1);
@@ -14,53 +13,43 @@ function Video(props) {
     setCount((prevCount) => prevCount - 1);
   }
 
-  const [list, updateList] = useState(data);
-
-  const handleRemoveItem = (e) => {
-    const id = e.target.getAttribute("id");
-    updateList(list.filter((item) => item.id !== id));
-  };
-
+  const extractedUrl = item.url.slice(-11);
   return (
-    <div className="video">
-      <hr />
-      <h2>{props.item.title}</h2>
-      <div className="votes">
-        <div className="vote">
-          <img src={smileyFace} alt="up vote" id="smiley-face" />
-          <button id="upvote" onClick={add}>
-            up vote
-          </button>
+    <div>
+      <main>
+        <div className="video">
+          <hr />
+          <h2>{item.title}</h2>
+          <div className="votes">
+            <div className="vote">
+              <img src={smileyFace} alt="up vote" id="smiley-face" />
+              <button id="upvote" onClick={add}>
+                up vote
+              </button>
+            </div>
+
+            <p className="rating">{count}</p>
+            <div className="vote">
+              <img src={downVoteFace} alt="down vote" id="down-vote-face" />
+              <button id="downvote" onClick={subtract}>
+                down vote
+              </button>
+            </div>
+          </div>
+          <iframe
+            width="560"
+            height="315"
+            src={"https://www.youtube.com/embed/" + extractedUrl}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
         </div>
-
-        <p className="rating">{count}</p>
-        <div className="vote">
-          <img src={downVoteFace} alt="down vote" id="down-vote-face" />
-          <button id="downvote" onClick={subtract}>
-            down vote
-          </button>
-        </div>
-      </div>
-
-      <div>
-        <iframe
-          width="560"
-          height="315"
-          src={"https://www.youtube.com/embed/" + props.item.url}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-
-      <button
-        id="remove"
-        type="button"
-        onClick={() => handleRemoveItem(props.item.id)}
-      >
-        remove
-      </button>
+        <button type="button" onClick={() => onRemove(item.id)}>
+          Remove
+        </button>
+      </main>
     </div>
   );
 }
