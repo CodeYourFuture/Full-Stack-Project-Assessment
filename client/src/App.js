8908a91e434539d9836dcc698516bd3b7cbbda9e
Sobia Waqar <sobiawaqar27'gmail.com>
2022-06-14 14:19:02 +0100

    front end work - Full Stack-Project-Assessment- week1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,50 @@
+
+  
+import React, { useState } from "react";
 import "./App.css";
+import RenderVideos from "./RenderVideos";
+import NewVideos from "./NewVideos";
+import data from "./exampleResponse.json";
+
 
 function App() {
+ 
+  const [allVideos, setAllVideos] = useState(data);
+  const [visible, setVisible] = useState(false);
+
+  // A function to delete a video
+  const deleteVideos = (arrVideo) => {
+    setAllVideos((videos) => {
+      return videos.filter((v) => {
+        return v.id !== arrVideo.id;
+      });
+    });
+  };
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Video Recommendation</h1>
+        <h1>Welcome to Video Channel </h1>
       </header>
+      <h2 className="add-video-title" onClick={()=>setVisible(true)}>
+        Add Video
+      </h2>
+<div className="container">
+      { visible && <NewVideos
+        myAllVideos={allVideos}
+        setAllVideos={setAllVideos}
+        setVisible={setVisible}
+      />}
+
+      {allVideos.map((video) => (
+        <RenderVideos
+          key={video.id}
+          video={video}
+          deleteVideos={deleteVideos}
+        />
+      ))}
+      </div>
     </div>
   );
 }
---
+++ client/src/NewVideos.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+const NewVideos = (props) => {
+  const[validationUrl, setValidationUrl ] = useState("")
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+
+  const updateTitle = (event) => {
+    const title = event.target.value;
+    setTitle(title);
+  };
+    function matchYoutubeUrl(url) {
+      let urlType =
+        /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+      if (url.match(urlType)) {
+        return url.match(urlType)[1];
+      }
+      return false;
+    }
+
+  const updateUrl = (event) => {
+    const url = event.target.value;
+    setUrl(url);
+  if (matchYoutubeUrl(url)) return setValidationUrl("valid Url");
+  setValidationUrl("Invalid Youtube URL");
+  };
+
+  const addVideo = (event) => {
+    //prevent all pages from refreshing
+    event.preventDefault();
+
+    const generateId = (e) => {
+      return e++;
+    };
+    const newVideo = { title, url, id: { generateId }, rating: 5 };
+if (matchYoutubeUrl(url)) props.setAllVideos((allVideos) => {
+  //Clear the input after submitting
+  setTitle("");
+  setUrl("");
+  return allVideos.concat(newVideo);
+});
+      
+  };
+
+  return (
+    <div>
+      {validationUrl}
+      <form onSubmit={addVideo}>
+        <label htmlFor="title" name="title">
+          Title:
+        </label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={updateTitle}
+          required
+        />
+        <label htmlFor="url" name="url">
+          url:
+        </label>
+        <input type={"text"} name="url" value={url} onChange={updateUrl} />
+
+        <button type="submit"> Add a Video</button>
+        <button type="submit" onClick={() => props.setVisible(false)}>
+          {" "}
+          Cancel your selection
+        </button>
+      </form>
+    </div>
+  );
+};
+export default NewVideos;
