5bc1ac52becf359a513f7f8412b6d9e9c32d9e79
Shadi <89422699+ShadiSafaee@users.noreply.github.com>
2022-06-16 08:10:56 +0100

    search bar is fixed

--- client/src/components/main/Header.jsx
+++ client/src/components/main/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "../../styles/header.css";
-import Search from "../public/Search";
+// import Search from "../public/Search";
 
 const Header = () => {
   return (
     <header className="header">
       <h2 className="cyf_title">Video Recommendation</h2>
-      <Search />
+      {/* <Search /> */}
     </header>
   );
 };
--- client/src/components/main/Main.jsx
+++ client/src/components/main/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Context from "../../context/Context";
 import VideoCards from "../public/VideoCards";
+import Search from "../public/Search";
 
 const Main = () => {
   return (
     <Context>
+      <Search />
       <VideoCards />
     </Context>
   );
