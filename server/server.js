6d7a28734951e9e95bc238263e114bcead5855ef
Harsheek Thanki <harsheekthanki@gmail.com>
2022-06-17 20:26:48 +0100

    Get endpoint added

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const data = require("../client/src/exampleresponse.json");
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = [...data];
 
-// GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.json(videos);
 });
