6aa6ba663bb873c18dbe53acd76018c01af04d6c
Harsheek Thanki <harsheekthanki@gmail.com>
2022-06-17 21:05:50 +0100

    post endpoint added

--- server/server.js
+++ server/server.js
@@ -3,12 +3,37 @@ const app = express();
 const port = process.env.PORT || 5000;
 const data = require("../client/src/exampleresponse.json");
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// const cors = require("cors");
+// app.use(cors());
+
+app.use(express.json());
 
 // Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
 let videos = [...data];
 
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   res.json(videos);
 });
+
+app.post("/", function (req, res) {
+  const { title, url, rating } = req.body;
+
+  const newVideo = {
+    id: videos.length,
+    title,
+    url,
+    rating: rating ? rating : 0,
+  };
+
+  if (!newVideo.title || !newVideo.url) {
+    return res.status(400).json({
+      result: "failure",
+      message: "Video could not be saved",
+    });
+  } else {
+    videos.push(newVideo);
+    res.json({ id: newVideo["id"] });
+  }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
