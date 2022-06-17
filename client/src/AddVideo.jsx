79c4eb7f8bbe1b347fc447b2b4100ba8c3c67534
Alirezabg <91377914+Alirezabg@users.noreply.github.com>
2022-06-17 12:27:50 +0100

    remove unused useeffect

--- client/src/AddVideo.jsx
+++ client/src/AddVideo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState} from "react";
 import "./App.css";
 const AddVideo = () => {
   const [visibility, setVisibility] = useState("addingOptionHidden");
