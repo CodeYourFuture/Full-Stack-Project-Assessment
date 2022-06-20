02b00c843b64b266fd347798e081288e1971bdb5
DelroyGayle <delroygayle01@gmail.com>
2022-06-20 21:55:33 +0100

    week 2 changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import React from "react";
+// Changed for React 18
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
