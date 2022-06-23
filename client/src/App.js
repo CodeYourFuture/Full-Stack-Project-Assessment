eff9873fe89475cc3827f62d8e9db8081c544b15
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2022-06-23 11:23:30 +0100

    server 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
+import React from "react";
+
 import "./App.css";
-import Homepage from './components/Homepage';
+import Homepage from "./components/Homepage";
 import Video from "./components/Video";
 
 function App() {
   return (
     <div className="App">
-     <Homepage/>
-     <Video/>
+      <Homepage />
+      <Video />
     </div>
   );
 }
--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,8 +1,9 @@
 import React , {useState} from "react";
-
+import mydata from './../exampleresponse.json';
 
 const Video = () => {
     const [votes , setVotes ] = useState (0);
+
     
     const like = () => {
         setVotes(count => count + 1);
@@ -15,8 +16,22 @@ const Video = () => {
         <span>
             <h3>My Video</h3>    
             <button onClick={like}>Like</button>
-            <h5>{votes}</h5>
             <button onClick={dislike}>Dislike</button>
+            <h5>{votes}</h5>
+            {mydata.map( (movie)=>
+             {const {title , id , url , rating } = movie ;
+             return <div key={id}> {title}<iframe
+              width="853"
+              height="480"
+              src={url.replace("watch?v=", "embed/")}
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Embedded youtube"
+            /></div>;
+            })}
+            <button>Delete Video</button>        
+            
         </span>
     )
 };
