403e1a008282dff7b11158b6b55f5615c74f08db
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2022-06-23 11:19:03 +0100

    server port working

--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(cors());
+const port = 8080;
+//evn file or dotenv file is a simple text configuration file
+//for controlling your Application environmentconstans.
+//between local staging and production environments the majority of your Application will not change.
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+//let videos = [];
 
 // GET "/"
-app.get("/", (req, res) => {
+app.get("/task", (req, res) => {
+  const { n } = req.query;
   // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.json(`You hit a video :, ${n}`);
 });
+app.listen(port, () => console.log(`Listening on port ${port}`));
