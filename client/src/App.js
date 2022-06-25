5826ae14d1547ab86d4ecb0be648bb8d31d7de3b
Maryam Mohsenpour <58438858+bita2019@users.noreply.github.com>
2022-06-25 07:45:51 +0100

    Add button

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,24 @@ import Homepage from "./components/Homepage";
 import Video from "./components/Video";
 
 function App() {
+  const [contacts, setContacts] = useState(Video);
+  const [addFormVideo, setAddFormVideo] = useState([]);
   return (
     <div className="App">
+      <div>
+        <h2 className="add-video-bar">Add New Video : </h2>
+        <form>
+          <input
+            type="text"
+            name="title"
+            required="required"
+            placeholder="Enter video ..."
+          />
+        </form>
+        <button type="submit" className="add-button">
+          Add
+        </button>
+      </div>
       <Homepage />
       <Video />
     </div>
