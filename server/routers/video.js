71a29203af40bbb2bf42a31ae4fc083160a31ca4
lalehkoupa <lalehkoupa@gmail.com>
2022-06-27 22:34:44 +0100

    some changes for horeku

--- server/routers/video.js
+++ server/routers/video.js
@@ -2,18 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { Pool } = require("pg");
 
-//const data = fs.readFileSync("exampleresponse.json");
-//const videoData = JSON.parse(data);
-
-// const client = new Client({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
-
-//client.connect();
-
 // user: "postgres",
 //   host: "localhost",
 //   database: "videos",
@@ -26,25 +14,17 @@ const pool = new Pool({
     rejectUnauthorized: false,
   },
 });
+//for setting up the environment variable
+//export PORT=9999
+//for setting up the DatabaseUrl in the connectionString
+//export DATABASE_URL=postgres://postgres:@localhost:5432/videos?sslmode=disable
 
-//export DATABASE_URL=postgres://postgres:@localhost:5432/cyf_ecommerce?sslmode=disable
-//export port=9999
-
-// const pool = new Pool({
-//   user: "postgres",
-//   host: "localhost",
-//   database: "cyf_ecommerce",
-//   password: "",
-//   port: 5432,
-// });
 //from here
 //\i ~/Downloads/cyf_ecommerce.sql
 
 // from terminal
 // heroku pg:psql -a cyf-ecommerce -f ~/Downloads/cyf_ecommerce.sql
 //get product
-
-//psql -h ec2-18-204-142-254.compute-1.amazonaws.com -p 5432 -U vzkfqqxnqszqez -W dfhftjpc14voga
 
 //psql -h ec2-176-34-215-248.eu-west-1.compute.amazonaws.com -p 5432 -U ogspsozpkqzssl -W d838mflm23l9di
 // provide password when prompted
