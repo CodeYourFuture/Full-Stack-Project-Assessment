582b48ecdaabd5427aca1971ebf2486c311156d1
teniolao <Tennybetty100@gmail.com>
2022-06-27 10:26:07 +0100

    uncomment code in server.js

--- server/server.js
+++ server/server.js
@@ -15,14 +15,14 @@ app.use(express.json());
 
 const port = process.env.PORT || 4000;
 
-// function matchYoutubeUrl(url) {
-//   const regex =
-//     /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
-//   if (url.match(regex)) {
-//     return true;
-//   }
-//   return false;
-// }
+function matchYoutubeUrl(url) {
+  const regex =
+    /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+  if (url.match(regex)) {
+    return true;
+  }
+  return false;
+}
 
 //get all videos
 app.get("/", (req, res) => {
@@ -35,47 +35,47 @@ app.get("/", (req, res) => {
     });
 });
 
-// // to create new video
-// app.post("/", (req, res) => {
-//   const { title, url, rating } = req.body;
-//   if (title && matchYoutubeUrl(url))
-//     pool
-//       .query("INSERT INTO videos (title, url, rating) VALUES($1, $2, $3)", [
-//         title,
-//         url,
-//         rating,
-//       ])
-//       .then(() => res.send("New video added"))
-//       .catch((error) => {
-//         console.error(error);
-//         res.status(500).json(error);
-//       });
-// });
+// to create new video
+app.post("/", (req, res) => {
+  const { title, url, rating } = req.body;
+  if (title && matchYoutubeUrl(url))
+    pool
+      .query("INSERT INTO videos (title, url, rating) VALUES($1, $2, $3)", [
+        title,
+        url,
+        rating,
+      ])
+      .then(() => res.send("New video added"))
+      .catch((error) => {
+        console.error(error);
+        res.status(500).json(error);
+      });
+});
 
-// // select a video
-// app.get("/:id", (req, res) => {
-//   const videoId = req.params.id;
+// select a video
+app.get("/:id", (req, res) => {
+  const videoId = req.params.id;
 
-//   pool
-//     .query("SELECT * FROM videos WHERE id = $1", [videoId])
-//     .then((result) => res.send(result.rows))
-//     .catch((error) => {
-//       console.log("an error just occurred");
-//       res.status(500).send("a problem occurred");
-//     });
-// });
+  pool
+    .query("SELECT * FROM videos WHERE id = $1", [videoId])
+    .then((result) => res.send(result.rows))
+    .catch((error) => {
+      console.log("an error just occurred");
+      res.status(500).send("a problem occurred");
+    });
+});
 
-// // delete a video
-// app.delete("/:id", (req, res) => {
-//   const videoId = req.params.id;
+// delete a video
+app.delete("/:id", (req, res) => {
+  const videoId = req.params.id;
 
-//   pool
-//     .query("DELETE FROM videos WHERE id=$1", [videoId])
-//     .then(() => res.send(`Order ${videoId} deleted`))
-//     .catch((error) => {
-//       console.error(error);
-//       res.status(500).json(error);
-//     });
-// });
+  pool
+    .query("DELETE FROM videos WHERE id=$1", [videoId])
+    .then(() => res.send(`Order ${videoId} deleted`))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
