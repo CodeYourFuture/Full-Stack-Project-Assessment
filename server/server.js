82d3ed3fb5de99b51cb766c8ef72d791401ac09e
s-shah-coder <saf.h.shah@gmail.com>
2022-06-28 20:39:14 +0100

    level 200

--- server/server.js
+++ server/server.js
@@ -14,3 +14,39 @@ let videos = videosData;
 app.get("/", (req, res) => {
   res.send(videosData);
 });
+
+app.post("/:id", (req, res) => {
+  const addNewVideo = req.body;
+  addNewVideo.id = Math.floor(Math.random() * 100000);
+  if (!addNewVideo.title || !addNewVideo.url) {
+    res.status(400).send({ msg: "Please add a Title & URL from Youtube !" });
+  } else {
+    videosData.push(addNewVideo);
+    res.status(200).send({ msg: `Video:${addNewVideo.title} has been added.` });
+  }
+});
+
+app.get("/:id", (req, res) => {
+  const videoId = parseInt(req.params.id);
+  const filterVideo = videosData.find((video) => video.id === videoId);
+
+  if (filterVideo.length === 0) {
+    res.status(400).send({ msg: `Video with:${videoId} not found!` });
+  } else {
+    res.status(200).send(filterVideo);
+  }
+});
+
+app.delete("/:id", (req, res) => {
+  const videoId = parseInt(req, params.id);
+  indexVideo = videosData.findIndex((video) => video.id == videoId);
+
+  if (indexVideo >= 0) {
+    videosData.splice(indexVideo, 1);
+    res
+      .status(200)
+      .send({ msg: `Video with id:${videoId} has been deleted. ` });
+  } else {
+    res.status(400).send({ msg: `Video with id:${videoId} not found! ` });
+  }
+});
