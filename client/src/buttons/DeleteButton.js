7511d6df60dd44dae387dc769a5dd3388c52196e
Abadi11 <83921761+Abadi11@users.noreply.github.com>
2022-09-29 00:12:32 +0100

    show the data in the browser

---
+++ client/src/buttons/DeleteButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DeleteButton() {
+  return (
+    <button class="p-2 mb-1 bg-danger text-white delete-button">Delete</button>
+  );
+}
+
+export default DeleteButton
