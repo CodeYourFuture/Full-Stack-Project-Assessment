8fe850ae858b95cee8659f0b401d143c9805fe90
AlexJora <jora.catalinaa@gmail.com>
2022-12-07 14:46:21 +0200

    level 100
    Form for search, add and cancel/ functionality for delete button, like and dislike button, add and cancel button, search/accessibility 100%

--- client/src/components/YouTubeEmbed.js
+++ client/src/components/YouTubeEmbed.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-function YouTubeEmbed({video}) {
- const url = video.url.replace("watch?v=", "embed/");
- console.log(url);
+function YouTubeEmbed({ video }) {
+  const url = video.url.replace("watch?v=", "embed/");
+  //  console.log(url);
   return (
     <iframe
+      title={video.title}
       height={"300"}
       src={url}
       alt={`video ${video.title}`}
