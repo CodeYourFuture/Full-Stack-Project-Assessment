8bb681b1ec4fb3fa907a798b3130dd126f9fda31
Riyaaz Singh <riyaazct1@gmail.com>
2022-12-10 01:08:17 +0200

    first commit

---
+++ client/src/AddVideoButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddVideoButton = () => {
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">Add Video</button>
+    </div>
+  );
+};
+
+export default AddVideoButton;
--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Video from "./Video";
 import dataVideos from "./exampleresponse.json";
+import AddVideoButton from "./AddVideoButton";
 
 function App() {
   return (
@@ -9,8 +10,9 @@ function App() {
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
+      <AddVideoButton />
 
-      <div>
+      <div className="body">
         {dataVideos.map((video, key) => (
           <Video video={video} key={key} />
         ))}
