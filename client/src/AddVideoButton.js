4b1eff128623071fe5496f11bd3ec523a20fe900
Riyaaz Singh <riyaazct1@gmail.com>
2022-12-12 02:59:36 +0200

    add cancel button functionality

--- client/src/AddVideoButton.js
+++ client/src/AddVideoButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 
 const AddVideoButton = ({ videoData, setVideoData }) => {
   const [title, setTitle] = useState("");
@@ -7,6 +7,7 @@ const AddVideoButton = ({ videoData, setVideoData }) => {
   // handle button press to add new video
   const handleOnSubmit = (e) => {
     e.preventDefault();
+    console.log(e.target.id);
     const video = {
       id: new Date().getTime().toString(),
       title,
@@ -20,46 +21,57 @@ const AddVideoButton = ({ videoData, setVideoData }) => {
     setUrl("");
   };
 
+  const handleOnClick = () => {
+    setTitle("");
+    setUrl("");
+  };
+
   return (
-    <form onSubmit={handleOnSubmit}>
-      <div className="formContainer">
-        <button className="titleButton">Add Video</button>
-        <div className="formInputs">
-          <label htmlFor="title">Title</label>
-          <input
-            type="text"
-            className="addVideoInput m-1 p-1"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-          <label htmlFor="url">URL</label>
-          <input
-            type="text"
-            className="addVideoInput m-1 p-1 "
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-          />
+    <Fragment>
+      <form onSubmit={handleOnSubmit}>
+        <div className="formContainer">
+          <button className="titleButton">Add Video</button>
+          <div className="formInputs">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              className="addVideoInput m-1 p-1"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }}
+            />
+            <label htmlFor="url">URL</label>
+            <input
+              type="text"
+              className="addVideoInput m-1 p-1 "
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+            />
+          </div>
+          <div className="addVideoButtons">
+            <button
+              id="test"
+              value="add"
+              type="submit"
+              className="btn btn-primary mx-1 mt-1"
+            >
+              ADD
+            </button>
+            <button
+              value="cancel"
+              className="btn btn-primary mx-1 mt-1"
+              onClick={(e) => {
+                e.preventDefault();
+                handleOnClick();
+              }}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
-        <div className="addVideoButtons">
-          <button
-            value="add"
-            type="submit"
-            className="btn btn-primary mx-1 mt-1"
-          >
-            ADD
-          </button>
-          <button
-            value="cancel"
-            type="submit"
-            className="btn btn-primary mx-1 mt-1"
-          >
-            Cancel
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
+    </Fragment>
   );
 };
 
