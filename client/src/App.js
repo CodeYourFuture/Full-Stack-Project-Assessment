4d7b535b06eaf716ca9da851bb4ce44dd33d4908
AlexJora <jora.catalinaa@gmail.com>
2022-12-14 10:29:17 +0200

    Level 250 - fetching data from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./App.css";
-import data from "./exampleresponse.json";
 import AddAndSearch from "./components/AddAndSearch";
 import AllVideos from './components/AllVideos'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [videos, setVideos] = useState(data);
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const url = `http://localhost:5000/`;
+    fetch(url)
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          return res.json();
+        }
+      })
+      .then((videos) => {
+        console.log(videos);
+        setVideos(videos);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   const handleDelete = (id) => {
     const newData = videos.filter(elem => elem.id !== id)
     setVideos(newData)
