dbc077a5b249fe38be15aa8addcf9253b44cc3df
AlexJora <jora.catalinaa@gmail.com>
2022-12-14 10:29:17 +0200

    Level 250 - fetching data from API

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-const videos = require("../client/src/exampleresponse.json");
+const videos = require("./exampleresponse.json");
+const cors = require("cors");//allowing other domains to make requests against your web API
+app.use(cors());
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -10,8 +12,6 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  // res.send({ express: "Your Backend Service is Running" });
   res.send(videos);
 });
 
