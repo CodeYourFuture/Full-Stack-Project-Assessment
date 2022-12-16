09d5e7f137e0fce89e89d79986e7162ef6232d08
chandredewet <chandredewet@gmail.com>
2022-12-16 20:43:00 +0200

    get all videos and post api

--- server/server.js
+++ server/server.js
@@ -2,14 +2,34 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = require("./../client/src/exampleresponse.json");
 
+//get all example videos
 // GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.send(videos);
+  //res.send({ express: "Your Backend Service is Running" });
 });
+
+// //POST "/"
+// This endpoint is used to add a video to the API.
+// Both fields - title and url - must be included and be valid for this to succeed.
+// Note: When a video is added, you must attach a unique ID to so that it can later be deleted
+
+app.post("/", function (req, res) {
+  console.log("POST / route - video");
+  let newVideo = req.body;
+  console.log(newVideo, videos.length);
+  //checking for an empty object but no need if checking if either fields empty
+  //if (!Object.keys(newChat).length)
+  if (!(req.body.title || req.body.url)) {
+    res.status(400).send("All fields are required to be entered");
+  } else {
+    videos.push(newVideo);
+    res.status(200).json(videos);
+  }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
