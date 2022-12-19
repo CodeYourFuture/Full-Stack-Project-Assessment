a64aa27a70d1b1b4923eb56681c0a136b34cb7e6
Anthony <almogotlane@gmail.com>
2022-12-19 14:05:34 +0200

    Completed level 200

--- server/server.js
+++ server/server.js
@@ -1,15 +1,57 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const data = require("../client/src/exampleresponse.json");
+
+app.use(express.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = data;
 
-// GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.json(videos);
 });
+
+app.get("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (videos.some((v) => v.id === id)) {
+    res.json(videos.find((v) => v.id === id));
+  } else {
+    res.json({
+      result: "failure",
+      message: "Video could not be found",
+    });
+  }
+});
+
+app.post("/", (req, res) => {
+  const video = {
+    id: videos.length,
+    title: req.body.title,
+    url: req.body.url,
+  };
+
+  if (!Object.values(video).every((v) => v)) {
+    return res.json({
+      result: "failure",
+      message: "Video could not be saved",
+    });
+  }
+
+  videos.push(video);
+  res.json({ id: videos.length });
+});
+
+app.delete("/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if(videos.some(v => v.id === id)) {
+    videos = videos.filter(v => v.id !== id);
+    return res.json({});
+  }
+
+  res.json({
+    result: "failure",
+    message: "Video could not be deleted",
+  })
+})
