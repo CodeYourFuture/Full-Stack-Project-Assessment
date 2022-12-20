74e628916c06d79a50b66bb1c7a9c676b7b326e9
Anthony Mogotlane <almogotlane@gmail.com>
2022-12-20 15:31:51 +0200

    Added cors package

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 const data = require("../client/src/exampleresponse.json");
 
 app.use(express.json());
+app.use(cors())
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
