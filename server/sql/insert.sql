8a3a314c8050136a34e254f4fae498c0e62f51ef
Anthony Mogotlane <almogotlane@gmail.com>
2022-12-23 16:28:56 +0200

    Added query scripts

---
+++ server/sql/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO videos (title, link)
+VALUES 
+('Never Gonna Give You Up', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'), 
+('The Coding Train', 'https://www.youtube.com/watch?v=HerCR8bw_GE'), 
+('Mac & Cheese | Basics with Babish', 'https://www.youtube.com/watch?v=FUeyrEN14Rk');
