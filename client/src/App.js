881640867ea131c8810a960744aaba1ab820517b
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2023-01-08 23:55:11 +0200

    ZA2-Christian-Mfuke-Full-Stack-Project-Assessment
    Level 100 completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,39 @@
-import React from "react";
-import "./App.css";
-import Video from "./Video";
-import dataVideos from "./exampleresponse.json";
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  function handleNewTodoChange(event) {
+    setNewTodo(event.target.value);
+  }
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    setTodos([...todos, { text: newTodo, completed: false }]);
+    setNewTodo("");
+  }
+
+  function handleRemoveTodo(index) {
+    setTodos(todos.filter((todo, i) => i !== index));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
-      <body>
-        {dataVideos.map((video, key) => (
-          <Video video={video} key={key}/>
+    <div>
+      <form onSubmit={handleAddTodo}>
+        <input type="text" value={newTodo} onChange={handleNewTodoChange} />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.text}
+            <button onClick={() => handleRemoveTodo(index)}>Remove</button>
+          </li>
         ))}
-      </body>
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
--- client/src/buttons/DeleteButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function DeleteButton() {
-  return (
-    <button class="p-2 mb-1 bg-danger text-white delete-button">Delete</button>
-  );
-}
-
-export default DeleteButton
