d202650882ad8dc50baa3a7c1032812d05ef3711
farzad-nazif <farzadnazif.nz@gmail.com>
2023-01-16 18:01:45 +0000

    Level 100 is finished

--- client/src/Video.js
+++ client/src/Video.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import DeleteButton from './buttons/DeleteButton';
-import LikeIcon from './buttons/LikeIcon';
-import DislikeIcon from './buttons/DislikeIcon';
-import YouTubeEmbed from './YouTubeEmbed';
+import React from "react";
+import DeleteButton from "./buttons/DeleteButton";
+import LikeIcon from "./buttons/LikeIcon";
+import DislikeIcon from "./buttons/DislikeIcon";
+import YouTubeEmbed from "./YouTubeEmbed";
 import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
 
-function Video({video}) {
-  
+function Video({ video, deleteVideo , value }) {
+  const [vote, setVote] = useState(0);
+  const addVote = () => {
+    setVote((vote) => {
+      return vote + 1;
+    });
+  };
+  const reduceVote = () => {
+    setVote((vote) => {
+      return vote - 1;
+    });
+  };
   return (
     <div className="video-container">
       <p>{video.title}</p>
-      <div className='vote-container'>
-        <LikeIcon />
-        <p>0 Vote</p>
-        <DislikeIcon />
+      <div className="vote-container">
+        <LikeIcon onClick={addVote} />
+        <p>{vote > 0 ? `${vote}` : 0}</p>
+        <DislikeIcon onClick={reduceVote} />
       </div>
       <YouTubeEmbed video={video} />
-      <DeleteButton />
+      <DeleteButton deleteVideo={deleteVideo} value={value} />
+      <p>{video.rating}</p>
     </div>
   );
 }
 
-export default Video
+export default Video;
--- client/src/buttons/DislikeIcon.js
+++ client/src/buttons/DislikeIcon.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function DislikeIcon() {
+function DislikeIcon({onClick}) {
   return (
-    <button>
+    <button onClick={onClick} >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="16"
