3edb953efb0fee5b93ce88cc0ee8a216448eafc3
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-17 21:04:38 +0000

    Updated

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+//import CopyrightIcon from "@mui/icons-material/Copyright";
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
+    
       <p>Copyright ⓒ {year}</p>
     </footer>
   );
