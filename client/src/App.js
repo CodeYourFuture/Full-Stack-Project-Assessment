8bf5751842d8e4bbfb22e1efe739c12f39d24ea1
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-17 20:59:03 +0000

    Complated L-100

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import React from "react";
 import "./App.css";
-import Video from "./Video";
-import dataVideos from "./exampleresponse.json";
+import Header from "./components/Header";
+import data from "./exampleresponse.json";
+import Footer from "./components/Footer";
+
+data = data.map(({ url }) => url.replace("watch?v=", "embed/"));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <h1>Video Recommendation</h1>
-      </header>
-      <body>
-        {dataVideos.map((video, key) => (
-          <Video video={video} key={key}/>
+        {data.map((item, i) => (
+          <div key={i}>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/{item.url}"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
         ))}
-      </body>
+      </header>
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <YouTubeIcon />
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
