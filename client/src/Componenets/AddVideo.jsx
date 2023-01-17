00a4a987e83fad27087e9eee3bb3672c3eb1eace
michelle <mindyourphotosynthesis@gmail.com>
2023-01-17 13:03:40 +0000

    initial: setup

---
+++ client/src/Componenets/AddVideo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AddVideo = () => {
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="title">Title: </label>
+        <input type="text" id="input-title" name="title" />
+        <label htmlFor="url">Url:</label>
+        <input type="text" id="input-url" name="url" />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddVideo
---
+++ client/src/Componenets/VideoCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const VideoCard = () => {
+  return (
+    <div>
+      <button>x</button>
+      <h1>title</h1>
+      {/* up vote, down vote icons */}
+    </div>
+  )
+}
+
+export default VideoCard
