765ac17f96e70063dc0fa8203b82ceefc6843334
Hayat <hayatu4islam@yahoo.com>
2023-01-18 13:27:46 +0000

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="my-App-header">
         <h1>Video Recommendation</h1>
       </header>
     </div>
