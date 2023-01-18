177799adfa5221d75e9b5ed310236bb24a733a6f
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-18 23:20:27 +0000

    fixing map keyissue

--- client/src/components/VideosGrid.js
+++ client/src/components/VideosGrid.js
@@ -1,24 +1,20 @@
-import * as React from "react";
+import React from "react";
 import Grid from "@mui/material/Grid";
 import VideoContainer from "./VideoContainer";
-import videos from "../exampleresponse.json";
-videos = videos.map((video) => {
-  video.url = video.url.replace("watch?v=", "embed/");
-  return video;
-});
-function VideosGrid() {
+
+function VideosGrid({ videosData }) {
   return (
     <Grid justifyContent="center" m={{ xs: 1, md: 2 }} container>
-      {videos.map((video, i) => (
-        <Grid m={1}>
-          <VideoContainer
-            key={i}
-            title={video.title}
-            url={video.url}
-            rating={video.rating}
-          />
-        </Grid>
-      ))}
+      {videosData &&
+        videosData.map((video, i) => (
+          <Grid m={1} key={i}>
+            <VideoContainer
+              title={video.title}
+              url={video.url}
+              rating={video.rating}
+            />
+          </Grid>
+        ))}
     </Grid>
   );
 }
