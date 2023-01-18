3245610c1fd295b3892caf67d8e459573b202f6d
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-18 01:32:52 +0000

    grid component

---
+++ client/src/components/VideosGrid.js
@@ -0,0 +1,26 @@
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import VideoContainer from "./VideoContainer";
+import videos from "../exampleresponse.json";
+videos = videos.map((video) => {
+  video.url = video.url.replace("watch?v=", "embed/");
+  return video;
+});
+function VideosGrid() {
+  return (
+    <Grid justifyContent="center" container>
+      {videos.map((video, i) => (
+        <Grid m={1}>
+          <VideoContainer
+            key={i}
+            title={video.title}
+            url={video.url}
+            rating={video.rating}
+          />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+export default VideosGrid;
