6617153d3f3f0a568b6b10bcca553fde6ca7de24
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-18 23:21:23 +0000

    adding functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,64 @@
-import * as React from "react";
+import React, { useState } from "react";
+import videos from "./exampleresponse.json";
 import Box from "@mui/material/Box";
-import "./App.css";
 import VideosGrid from "./components/VideosGrid";
 import Header from "./components/Header";
 import AddVideoForm from "./components/AddVideoForm";
+import "./App.css";
 
 function App() {
+  const [videosData, setVideosData] = useState(formatVideosUrl(videos));
+  const [open, setOpen] = useState(false);
+  const [videoData, setVideoData] = useState({
+    id: null,
+    title: "",
+    url: "",
+    rating: 0,
+  });
+
+  function formatVideosUrl(videos) {
+    const formattedVideosData = videos.map((video) => {
+      video.url = video.url.replace("watch?v=", "embed/");
+      return video;
+    });
+    return formattedVideosData;
+  }
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleInputChange = (e) => {
+    const updatedVideoData = {
+      ...videoData,
+      [e.target.name]: e.target.value,
+    };
+    setVideoData(updatedVideoData);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setVideosData(formatVideosUrl([...videosData, videoData]));
+    setOpen(false);
+    console.log({ videoData, videosData });
+  };
+
   return (
     <div className="App">
       <Box>
         <Header />
-        <AddVideoForm/>
-        <VideosGrid />
+        <AddVideoForm
+          handleClickOpen={handleClickOpen}
+          handleClose={handleClose}
+          handleInputChange={handleInputChange}
+          handleSubmit={handleSubmit}
+          open={open}
+        />
+        <VideosGrid videosData={videosData} />
       </Box>
     </div>
   );
