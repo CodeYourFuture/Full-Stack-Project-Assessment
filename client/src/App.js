ed7569c02e910c2f6faa0fa6f1267576905ce9fa
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-18 01:30:36 +0000

    Adding videos grid

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import "./App.css";
+import VideosGrid from "./components/VideosGrid";
 
 function App() {
   return (
     <div className="App">
-      <Box m={4}>
-        <Typography variant="h3">Video Recommendation</Typography>
+      <Box>
+        <Typography m={5} variant="h3">
+          Video Recommendation
+        </Typography>
+        <VideosGrid />
       </Box>
     </div>
   );
