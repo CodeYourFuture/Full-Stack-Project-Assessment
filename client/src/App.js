8d180a9eb665080ea12c5e38d4387daa4e257a63
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-18 18:20:11 +0000

    added form component

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import Box from "@mui/material/Box";
 import "./App.css";
 import VideosGrid from "./components/VideosGrid";
 import Header from "./components/Header";
+import AddVideoForm from "./components/AddVideoForm";
 
 function App() {
   return (
     <div className="App">
       <Box>
         <Header />
+        <AddVideoForm/>
         <VideosGrid />
       </Box>
     </div>
