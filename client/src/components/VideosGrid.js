43d04ec348cd4cd7ac606ecb0eacecf0063e7831
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-19 01:22:40 +0000

    adding props

--- client/src/components/VideosGrid.js
+++ client/src/components/VideosGrid.js
@@ -2,16 +2,19 @@ import React from "react";
 import Grid from "@mui/material/Grid";
 import VideoContainer from "./VideoContainer";
 
-function VideosGrid({ videosData }) {
+function VideosGrid({ videosData, voteUp, voteDown }) {
   return (
     <Grid justifyContent="center" m={{ xs: 1, md: 2 }} container>
       {videosData &&
         videosData.map((video, i) => (
           <Grid m={1} key={i}>
             <VideoContainer
+              name={video.id}
               title={video.title}
               url={video.url}
               rating={video.rating}
+              voteUp={voteUp}
+              voteDown={voteDown}
             />
           </Grid>
         ))}
