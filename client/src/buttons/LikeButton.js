0ec7891cf8f80493a45640575a27af5135052f6e
jaspreet kaur <kaurjas1928@yahoo.com>
2023-01-20 23:39:17 +0000

    added svg icon but still need to fix some errors

--- client/src/buttons/LikeButton.js
+++ client/src/buttons/LikeButton.js
@@ -4,8 +4,17 @@ function LikeButton({increment}) {
 
     return (
       <button title="likeBtn" onClick={() => increment()}>
-        <svg width="16" height="16">https://icons8.com/icon/FYJ9HNSqf_uK/thumbs-up</svg>
+        <svg
+xmlns="http://www.w3.org/2000/svg"
+x="0px" y="0px"
+width="16" height="16"
+viewBox="0 0 16 16" color="red" fill="currentColor"
+><path d="M21.5,21.5v129h64.5v-32.25v-64.5v-32.25zM86,53.75c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25c-17.7805,0 -32.25,14.4695 -32.25,32.25zM118.25,86c-17.7805,0 -32.25,14.4695 -32.25,32.25c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25z"></path></svg>
+
+
+        
       </button>
     );
 }
 export default LikeButton
+
