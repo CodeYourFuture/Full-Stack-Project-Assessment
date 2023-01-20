a9f2c29aed458ff667158feb45178cbaebb1c85a
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-01-20 09:04:25 +0000

    delete working

--- client/src/Cards.js
+++ client/src/Cards.js
@@ -1,21 +1,30 @@
-import videos from "./exampleresponse.json";
+import {useState} from 'react';
+import Videos from "./exampleresponse.json";
 
 function Cards() {
+  const [videos, setVideos] = useState(Videos);
+  const removeElement = (i) => {
+    let newVideos = [...videos];
+    newVideos.splice(i,1);
+    setVideos(newVideos)
+  
+  }
     return (
       <div className="Cards">
-        {videos.map((item, index) => (
-          <div key={index} className="card">
+        {videos.map((video, index) => (
+          <div index={index} key={video.id} title={video.title}  url={video.url} id={video.id}className="card">
             <div className="card-body">
-              <h4>{item.title}</h4>
-              <h4>{item.rating}</h4>
-              <h5>{item.id}</h5>
+              <h4>{video.title}</h4>
+              <h4>{video.rating}</h4>
+              {/* <h5>{video.id}</h5> */}
               <a
-                href={item.url}
+                href={video.url}
                 rel="noopener noreferrer"
                 className="btn btn-primary"
               >
                 Watch Now
               </a>
+              <button className="delete" onClick={() => removeElement(index)}>Delete</button>
             </div>
           </div>
         ))}
