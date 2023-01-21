90540f79312a16593e4a8c776aeda98e0d36ef0c
jaspreet kaur <kaurjas1928@yahoo.com>
2023-01-21 14:57:51 +0000

    fixed the button errors

--- client/src/Video.js
+++ client/src/Video.js
@@ -3,24 +3,24 @@ import EmbedVideo from "./EmbedVideo";
 import LikeButton from "./buttons/LikeButton";
 import DislikeButton from "./buttons/DisLikeButton";
 
-function Video({video}) {
-    const [count,setCount] = useState(video.rating);
-    const increment = () => {
-        setCount(count + 1);
-    }
-    const decrement = () => {
-        setCount(count - 1);
-    }
-    return (
-<div className="video-container" id={video.id}>
-    <p>{video.title}</p>
-    <div className = 'vote-container'>
-        <LikeButton increment={increment}/>
+function Video({ video }) {
+  const [count, setCount] = useState(video.rating);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div className="video-container" id={video.id}>
+      <p>{video.title}</p>
+      <div className="vote-container">
+        <LikeButton increment={increment} />
         <p>{count}</p>
-        <DislikeButton decrement={decrement}/>
+        <DislikeButton decrement={decrement} />
+      </div>
+      <EmbedVideo video={video} />
     </div>
-    <EmbedVideo video={video}/>
-</div>
-    );
+  );
 }
 export default Video;
--- client/src/buttons/DisLikeButton.js
+++ client/src/buttons/DisLikeButton.js
@@ -1,11 +1,22 @@
 import React from "react";
+import thumb from "../buttons/images/thumbs-down-icon.png";
 
 function DislikeButton({decrement}) {
     return (
       <button title="dislikeBtn" onClick={() => decrement()}>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
+        {
+          <img
+            src={thumb}
+            onClick={decrement}
+            alt=""
+            width="20px"
+            height="20px"
+          /> 
+          
+          /* <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
           https://icons8.com/icon/cPJTvqEzTYvb/thumbs-down
-        </svg>
+        </svg> */
+        }
       </button>
     );
 }
