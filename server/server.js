339bdcd19041d616a35d6dc94720fab472b76e37
irina mihai <irinamihai150@gmail.com>
2023-01-22 18:38:21 +0000

    level200

--- server/server.js
+++ server/server.js
@@ -1,15 +1,111 @@
 const express = require("express");
 const app = express();
+const { v4: uuidv4 } = require("uuid");
+app.use(express.json());
+
+// import { validateUrl } from "youtube-validate";
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = [
+  {
+    id: 523523,
+    title: "Never Gonna Give You Up",
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    rating: 23,
+  },
+  {
+    id: 523427,
+    title: "The Coding Train",
+    url: "https://www.youtube.com/watch?v=HerCR8bw_GE",
+    rating: 230,
+  },
+  {
+    id: 82653,
+    title: "Mac & Cheese | Basics with Babish",
+    url: "https://www.youtube.com/watch?v=FUeyrEN14Rk",
+    rating: 2111,
+  },
+  {
+    id: 858566,
+    title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
+    url: "https://www.youtube.com/watch?v=xbs7FT7dXYc",
+    rating: 11,
+  },
+  {
+    id: 453538,
+    title:
+      "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
+    url: "https://www.youtube.com/watch?v=4As0e4de-rI",
+    rating: 3211,
+  },
+  {
+    id: 283634,
+    title: "Learn Unity - Beginner's Game Development Course",
+    url: "https://www.youtube.com/watch?v=gB1F9G0JXOo",
+    rating: 211,
+  },
+  {
+    id: 562824,
+    title: "Cracking Enigma in 2021 - Computerphile",
+    url: "https://www.youtube.com/watch?v=RzWB5jL5RX0",
+    rating: 111,
+  },
+  {
+    id: 442452,
+    title: "Coding Adventure: Chess AI",
+    url: "https://www.youtube.com/watch?v=U4ogK0MIzqk",
+    rating: 671,
+  },
+  {
+    id: 536363,
+    title: "Coding Adventure: Ant and Slime Simulations",
+    url: "https://www.youtube.com/watch?v=X-iSQQgOd1A",
+    rating: 76,
+  },
+  {
+    id: 323445,
+    title: "Why the Tour de France is so brutal",
+    url: "https://www.youtube.com/watch?v=ZacOS8NBK6U",
+    rating: 73,
+  },
+];
 
-// GET "/"
+const addedVideos = [];
+
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.send(videos);
+});
+app.post("/", (req, res) => {
+  const newVideo = {
+    id: uuidv4(),
+    title: req.body.title,
+    url: req.body.url,
+  };
+  videos.push(newVideo);
+  if (!req.body.title || req.body.url) {
+    res.status(400).send("Video could not be saved");
+    return;
+  } else {
+    res.send(videos);
+  }
+});
+app.get("/:id", (req, res) => {
+  const videoId = parseInt(req.params.id);
+  res.json(videos.find((v) => v.id === videoId));
+});
+app.delete("/:id", (req, res) => {
+  const videoId = parseInt(req.params.id);
+  const videoIndex = videos.findIndex((v) => v.id === videoId);
+
+  if (videoIndex < 0) {
+    res.status(404).send("Video could not be deleted");
+    return;
+  } else {
+    videos.splice(videoIndex, 1);
+    res.send("Video deleted");
+  }
 });
