15fa48836711aeadef6ee219551436f2eac7c8ca
farzad-nazif <farzadnazif.nz@gmail.com>
2023-01-22 13:20:50 +0000

    SQL database is connected in Node

--- server/server.js
+++ server/server.js
@@ -1,15 +1,32 @@
 const express = require("express");
+const { Pool } = require("pg");
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
 let videos = [];
 
-// GET "/"
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_hotels",
+  password: "youtuberfarzad@7633",
+  port: process.env.PORTS,
+});
+
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
   res.send({ express: "Your Backend Service is Running" });
 });
+
+app.get("/hotels/:id", function (req, res) {
+  const id = req.params.id;
+  pool
+    .query("SELECT * FROM clients WHERE id = $1",[id])
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
