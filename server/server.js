fe0a412bb6ddf9b26d712a95b36975d381f29b03
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2023-01-23 14:13:32 +0000

    Finished Server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,72 @@
 const express = require("express");
+let cors = require('cors')
 const app = express();
 const port = process.env.PORT || 5000;
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = require("../client/src/exampleresponse.json");
+
+let fail = {
+  "result": "failure",
+  "message": "Video could not be saved"
+};
+
 
 // GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+app.get("/", (req, res) =>
+{
+  res.send(videos);
+});
+
+app.post("/", (req, res) =>
+{
+  console.log(body);
+  const video = {
+    id: req.body.id,
+    title: req.body.title,
+    url: req.body.url,
+    rating: req.body.id
+  };
+
+
+  if (typeof req.body.title == "string" || typeof req.body.url == "string")
+  {
+    res.status(400).send(fail);
+  }
+
+  else
+  {
+    videos.push(video);
+    res.status(200).json(videos);
+  }
+});
+
+app.get("/:id", function (req, res)
+{
+  let id = parseInt(req.params.id);
+  let filteredVideo = videos.filter(video => video.id === id);
+
+  res.send(filteredVideo);
+});
+
+app.delete("/:id", function (req, res)
+{
+  let id = parseInt(req.params.id);
+  let filterdVideo = videos.filter(video => video.id === id);
+
+  if (filterdVideo.length !== 0)
+  {
+    videos = videos.filter(video => video.id !== id);
+    res.send({});
+  }
+
+  else
+  {
+    res.send(fail);
+  }
 });
