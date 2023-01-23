ab19447d842a125e2f48dd85013202009d874146
SaadiaELF <saadiaelfekak@gmail.com>
2023-01-23 23:11:19 +0000

    adding get all route

--- server/server.js
+++ server/server.js
@@ -3,17 +3,17 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = require("./exampleresponse.json");
 
-// GET "/"
+// GET all videos
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  videos.length > 0
+    ? res.send(videos)
+    : res.status(500).send("No video is available");
 });
 
 app.get("*", (req, res) => {
