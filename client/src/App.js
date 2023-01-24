576a73611152fcf486f71fe831fab0b436f4f946
Nishka Kisten <nishka.crypto@gmail.com>
2023-01-24 22:03:05 +0200

    updated delete endpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
- import Video from "./Video";
- //import dataVideos from "./exampleresponse.json";
-  import AddVideo from "./AddVideo";
+import Video from "./Video";
+//import dataVideos from "./exampleresponse.json";
+import AddVideo from "./AddVideo";
 
 function App() {
   const [del, setDel] = useState([])
--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const pool = new Pool({
 // GET "/"
 app.get("/", (req, res) => {
   // res.send(videos).json;
-  pool.query('SELECT * FROM video')
+  pool.query('SELECT * FROM videos')
   .then((result) => res.send(result.rows).json)
   .catch((error) => {
       console.error(error);
@@ -59,7 +59,7 @@ pool
 //`GET` "/{id}"
 app.get("/:id", (req, res) =>{
 let videoId = req.params.id;
-pool.query('SELECT * FROM video WHERE id=$1', [videoId])
+pool.query('SELECT * FROM videos WHERE id=$1', [videoId])
 .then((result) =>  
 res.json(result.rows))
 .catch((error) => {
@@ -72,27 +72,26 @@ res.json(result.rows))
 // `DELETE` "/{id}"
 app.delete('/:id', (req, res)=> {
 let vidId = req.params.id;
-// let toDel = videos.find(opt => opt.id === id);
-// let notDel = videos.filter(opt => opt.id !== id);
 pool
-.query("DELETE FROM video WHERE id=$1", [vidId])
-.then(() => res.send(`Video ${vidId} deleted!`))
-.catch((error) => {
-  console.error(error);
-  res.status(500).json(error);
+.query("DELETE FROM videos WHERE id=$1", [vidId])
+.then((result) => {
+  if(result.rowCount !== 0){
+    return  pool
+    .query("DELETE FROM videos WHERE id=$1", [vidId])
+    .then(() => res.send(`Video ${vidId} deleted!`))
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+} else{
+  return res
+  .status(400)
+  .send({
+    "result": "failure",
+    "message": "Video could not be deleted"
+  });
+}
 });
-// if(toDel === undefined){
-//   res.send(400).send({
-//     "result": "failure",
-//     "message": "Video could not be deleted"
-//   })
-// }else {
-//   res.send({}).status(200);
-// }
-// videos = notDel;
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-
 
