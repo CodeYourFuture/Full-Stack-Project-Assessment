9c0c1403e5c12c46e263ec80d89b17e1ba623b54
Mathias02 <matthiasm2016@gmail.com>
2023-01-24 23:57:53 +0200

    change made

---
+++ client/src/AddBtn.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddBtn = () => {
+  return (
+  <button className='btnAdd'>Add</button>
+  )
+}
+
+export default AddBtn
--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Video from "./Video";
 import dataVideos from "./exampleresponse.json";
+import NewVideo from "./NewVideo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
+        <NewVideo />
       </header>
       <body>
         {dataVideos.map((video, key) => (
