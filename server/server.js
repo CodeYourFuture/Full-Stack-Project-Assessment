91a50d5169ee4f65e9baf514275b55841c9b0e3f
simeonbikov <bikovsem@gmail.com>
2023-01-24 16:37:15 +0000

    level-200

--- server/server.js
+++ server/server.js
@@ -5,9 +5,6 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-
 let videos = [
   {
     id: 523523,
