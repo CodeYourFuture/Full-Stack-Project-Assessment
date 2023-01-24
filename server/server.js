a73128ad080e2715750af17222559db769290b9d
simeonbikov <bikovsem@gmail.com>
2023-01-24 23:02:36 +0000

    cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 let videos = [
   {
