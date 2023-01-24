331232b272ffc765d7695c811297024f4f43c71e
Gelson Gerson Celestino <103900087+GelsonGerson@users.noreply.github.com>
2023-01-24 13:34:30 +0200

    Update Folders

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
 // Store and retrieve your videos from here
+
+
+
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
 let videos = [];
 
