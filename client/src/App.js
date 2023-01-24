487f2bbbd6036734ebd940a2c3e5d636ed4664a9
Dewayne Sewell <dewaynesewell1@gmail.com>
2023-01-24 21:16:04 +0000

    100

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Video Recommendation</h1>
+        <h1>YouVideos</h1>
       </header>
       <body>
         <div>
