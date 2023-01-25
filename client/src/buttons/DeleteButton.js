ce449d27d3c4c7eb62631453d7da59a8c6ab1628
jaspreet kaur <kaurjas1928@yahoo.com>
2023-01-25 21:57:15 +0000

    still need to work on css

--- client/src/buttons/DeleteButton.js
+++ client/src/buttons/DeleteButton.js
@@ -2,7 +2,13 @@ import React from "react";
 
 function DeleteButton({remove, id}) {
     return (
-        <button title="delBtn"onClick={() => remove(id)} class="text-white delete-button">Delete</button>
+      <button
+        title="delBtn"
+        onClick={() => remove(id)}
+        class="text-white delete-button"
+      >
+        Delete
+      </button>
     );
 }
 export default DeleteButton
