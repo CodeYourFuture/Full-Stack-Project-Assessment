feaf72fe982c9f4dd4e56f26ac6a5d6a7966ce8e
RobCso <robcso@DESKTOP-AA47BN0.localdomain>
2023-01-25 12:09:53 +0000

    add youtube link validation and start work on servr

--- client/src/Form.js
+++ client/src/Form.js
@@ -14,10 +14,19 @@ const Form = ({setVideos}) =>{
         url: url,
         rating: rating
       }
+      function matchYoutubeUrl(url) {
+        var p =
+          /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
+        if (url.match(p)) {
+          return url.match(p)[1];
+        }
+        return false;
+      }
+
       if(newVideo.title==="") {
         alert("Set Valid title")
       }
-      if(newVideo.url===""){
+      if( matchYoutubeUrl(newVideo.url) === false){
         alert("Set valid url")
       } 
            
