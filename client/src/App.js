0e5ac6fafc9e100de96ecf958b768e665108978b
Pakize Bozkurt <pakiozihak@gmail.com>
2023-01-26 19:57:45 +0000

    Spelling errors check

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './companents/Header';
-import Main from './companents/Main';
-import Footer from './companents/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 
 function App() {
