6fade2e94664954b91063c9d17815f57e29eb0bb
Tersia Koetzee <tersiakoetzee@gmail.com>
2023-01-26 16:35:09 +0200

    this is a test

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+;
 const { Pool } = require("pg");
 
-app.use(cors()); 
-app.use(express.json());
+
 // app.use(express.urlencoded({ extended: false }));
 
 const pool = new Pool({
