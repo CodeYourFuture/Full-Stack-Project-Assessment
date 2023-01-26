bd435568bbd3da3894d4d672d140cccd2311bc2c
vanipriya_piramanathan <vpriyaarun23@gmail.com>
2023-01-26 23:17:54 +0000

    level 200-250 completed

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+// let videos = [];
 
 // GET "/"
 app.get("/", (req, res) => {
