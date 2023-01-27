acdc77076f0964516d015b3305bbff8a5a5f1623
Hayat <hayatu4islam@yahoo.com>
2023-01-27 21:00:10 +0000

    Update app and stylings

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import AddNewVideo from "./AddNewVideo";
 
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <AddNewVideo />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
