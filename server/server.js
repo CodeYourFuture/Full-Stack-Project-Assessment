ea295c2ee53eb6f9a57fd0f16e86b8ed168635f1
jaspreet kaur <kaurjas1928@yahoo.com>
2023-01-27 19:37:42 +0000

    Added level 200

--- server/server.js
+++ server/server.js
@@ -10,13 +10,13 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 let videos = [];
 videos.push(dataVideos);
 // GET "/"
-app.get("/", (req, res) => {
+app.get("/videos", (req, res) => {
   
   res.send(videos).json;
 });
 
 //POST "/"
-app.post('/', (req, res) => {
+app.post('/videos', (req, res) => {
   let {title, url } = req.body;
   let newVideos = {
     id: videos.length,
@@ -33,4 +33,31 @@ app.post('/', (req, res) => {
     videos.push(newVideos);
     res.sendStatus(200);
   }
+})
+
+//`GET` "/videos/:id"
+app.get("/videos/:id", (req, res) => {
+  let id = parseInt(req.params.id);
+  console.log(id);
+  let findVideo = videos.find(ID => ID.id === id);
+  if(!findVideo) {
+    res.send("Not Found").status(404)
+  } else {
+    res.send(findVideo);
+  }
+})
+app.delete('/:id', (req, res) => {
+  let id = parseInt(req.params.id);
+  let toDel = videos.find(opt => opt.id === id);
+  let notDel = videos.filter(opt => opt.id !== id);
+
+  if(toDel === undefined) {
+    res.send(400).send({
+      "result": "failure",
+      "message": "Video could not be deleted"
+    })
+  } else {
+    res.send({}).status(200);
+  }
+  videos = notDel;
 })
