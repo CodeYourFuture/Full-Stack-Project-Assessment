7aa6368a0535eb593b9624e3b3988e8eab46a48d
Hayat <hayatu4islam@yahoo.com>
2023-01-27 21:49:34 +0000

    Add SearchVideo component

---
+++ client/src/SearchVideo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SearchVideo() {
+  return (
+    <div className="search-video">
+      <label>Search</label>
+      <input></input>
+    </div>
+  );
+}
