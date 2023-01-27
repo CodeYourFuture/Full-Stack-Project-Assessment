5ba8e4f5ad494e7d3849ced04fffa3c42d912185
kavitappatil <patil.poem@gmail.com>
2023-01-27 21:54:32 +0000

    get videos endpoint

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+const port = process.env.PORT || 5001;
+const videosjson = require("./exampleresponse.json");
+
+app.use(cors({ origin: "*" }));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = videosjson;
 
 // GET "/"
 app.get("/", (req, res) => {
   // Delete this line after you've confirmed your server is running
   res.send({ express: "Your Backend Service is Running" });
+});
+app.get("/videos", (req, res) => {
+  res.send(videos);
 });
