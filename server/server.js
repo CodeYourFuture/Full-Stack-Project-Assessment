c6f16ee5d0d2e730c52c70420fa7e26cbc37a4c2
Chidimma <chidimmamofodum@gmail.com>
2023-01-27 23:41:10 +0000

    updating server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,124 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
+
 const port = process.env.PORT || 5000;
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = [
+  {
+    id: 523523,
+    title: "Never Gonna Give You Up",
+    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    rating: 23,
+  },
+  {
+    id: 523427,
+    title: "The Coding Train",
+    url: "https://www.youtube.com/watch?v=HerCR8bw_GE",
+    rating: 230,
+  },
+  {
+    id: 82653,
+    title: "Mac & Cheese | Basics with Babish",
+    url: "https://www.youtube.com/watch?v=FUeyrEN14Rk",
+    rating: 2111,
+  },
+  {
+    id: 858566,
+    title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
+    url: "https://www.youtube.com/watch?v=xbs7FT7dXYc",
+    rating: 11,
+  },
+  {
+    id: 453538,
+    title:
+      "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
+    url: "https://www.youtube.com/watch?v=4As0e4de-rI",
+    rating: 3211,
+  },
+  {
+    id: 283634,
+    title: "Learn Unity - Beginner's Game Development Course",
+    url: "https://www.youtube.com/watch?v=gB1F9G0JXOo",
+    rating: 211,
+  },
+  {
+    id: 562824,
+    title: "Cracking Enigma in 2021 - Computerphile",
+    url: "https://www.youtube.com/watch?v=RzWB5jL5RX0",
+    rating: 111,
+  },
+  {
+    id: 442452,
+    title: "Coding Adventure: Chess AI",
+    url: "https://www.youtube.com/watch?v=U4ogK0MIzqk",
+    rating: 671,
+  },
+  {
+    id: 536363,
+    title: "Coding Adventure: Ant and Slime Simulations",
+    url: "https://www.youtube.com/watch?v=X-iSQQgOd1A",
+    rating: 76,
+  },
+  {
+    id: 323445,
+    title: "Why the Tour de France is so brutal",
+    url: "https://www.youtube.com/watch?v=ZacOS8NBK6U",
+    rating: 73,
+  },
+];
+
+
+
+function maxId() {
+  let idArray = videos.map((el) => el.id);
+  return Math.max(...idArray);
+}
+
 
 // GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+app.get("/videos", (req, res) => {
+  res.json(videos);
 });
+
+app.get("/videos/:id", (req, res) => {
+  const result = videos.find((el) => el.id === +req.params.id);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send("Video not found");
+  }
+});
+
+app.post("/videos", (req, res) => {
+  let { title, url } = req.body;
+  if (title && url) {
+    let id = maxId() + 1;
+    videos.push({ id: id, ...req.body });
+    res.json({ id: id });
+  } else {
+    res.json({
+      result: "failure",
+      message: "Video could not be saved",
+    });
+  }
+});
+
+app.delete("/videos/:id", (req, res) => {
+  const id = req.params.id
+  videos = videos.filter((el) => el.id !== +id);
+  const deletedVideo = videos.find((el) => el.id === +id)
+  if (deletedVideo) {
+    res.json({
+      'result': 'failure',
+      'message': 'Video could not be deleted'
+    })
+  }
+  res.json(deletedVideo);
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
