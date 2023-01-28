b0474c72a8c223dc50cb3b10868c6a20852d31a4
farzad-nazif <farzadnazif.nz@gmail.com>
2023-01-28 16:30:32 +0000

    cors bug fixed

--- server/server.js
+++ server/server.js
@@ -7,6 +7,12 @@ const port = process.env.PORT || 5000;
 
 // Midlwares
 app.use(express.json());
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // Configing the database
 const pool = new Pool({
