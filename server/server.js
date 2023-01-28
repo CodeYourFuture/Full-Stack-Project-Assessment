57fc8f1704f981e6dc00f82862e1d59646c08fa8
farzad-nazif <farzadnazif.nz@gmail.com>
2023-01-28 15:12:23 +0000

    All CRUD functions are working fine(MVP version)

--- server/server.js
+++ server/server.js
@@ -5,9 +5,10 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Midlwares
 app.use(express.json());
-app.use(cors());
 
+// Configing the database
 const pool = new Pool({
     user: process.env.USER,
     host: process.env.HOST,
@@ -18,8 +19,10 @@ const pool = new Pool({
     ssl: true
 });
 
+// Connecting to the pool using connection string
 pool.connect();
 
+// Reading all videos
 app.get("/videos", (req, res) => {
   // res.send({ express: "Your Backend Service is Running" });
     pool
@@ -31,6 +34,7 @@ app.get("/videos", (req, res) => {
     });
 });
 
+// Reading a video based on ID
 app.get("/videos/:id", function (req, res) {
   const id = req.params.id;
   pool
@@ -42,6 +46,7 @@ app.get("/videos/:id", function (req, res) {
     });
 });
 
+// Creating a video
 app.post("/video",(req,res)=>{
   const title = req.body.title;
   const url = req.body.url;
@@ -56,6 +61,44 @@ app.post("/video",(req,res)=>{
       console.error(error);
       res.status(500).json(error);
     });
+})
+
+// Updating the title of video
+app.put("/video/title",(req,res)=>{
+  const id = Number(req.body.id);
+  const title = req.body.title;
+  pool
+    .query("UPDATE videos SET title=$1 WHERE id=$2", [title, id])
+    .then(() => res.send(`Video ${id} updated!`))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+})
+
+// Updating the url of video
+app.put("/video/url",(req,res)=>{
+  const id = Number(req.body.id);
+  const url = req.body.url;
+  pool
+    .query("UPDATE videos SET url=$1 WHERE id=$2", [url, id])
+    .then(() => res.send(`Video ${id} updated!`))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+})
+
+// Deleting a video
+app.delete("/video/:id",(req,res)=>{
+  const id = Number(req.params.id);
+  pool
+    .query("DELETE FROM videos WHERE id = $1",[id])
+    .then(() => res.json(`Video ${id} deleted!`))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+   });
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
