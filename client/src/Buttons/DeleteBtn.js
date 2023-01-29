2655c96b006a29364633f63eab572b3ccd7da4c7
micheale2021 <andemariammicheale@gmail.com>
2023-01-29 00:37:39 +0000

    create button folder and extract all buttons

---
+++ client/src/Buttons/DeleteBtn.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function DeleteButton({ deleteVideo, value }) {
+  return (
+    <button
+      onClick={deleteVideo}
+      value={value}
+      class="p-2 mb-1 bg-danger text-white delete-button"
+    >
+      Delete
+    </button>
+  );
+}
+
+export default DeleteButton;
