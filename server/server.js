c1ffcbf4b2ddd2bc91d53d8a689d3449e715e353
irina mihai <irinamihai150@gmail.com>
2023-01-29 20:52:04 +0000

    conect to database and changing endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { v4: uuidv4 } = require("uuid");
+const { Pool } = require("pg");
+const pool = require("./db.js");
+const bodyParser = require("body-parser");
+
+// pool.connect();
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -14,90 +19,80 @@ app.use(function (req, res, next) {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // restrict calls to those this address
-    methods: "GET", // only allow GET requests
+    origin: "http://localhost:3000",
+    methods: "GET",
   })
 );
 app.use(express.json());
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [
-  {
-    id: 523523,
-    title: "Never Gonna Give You Up",
-    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    rating: 23,
-  },
-  {
-    id: 523427,
-    title: "The Coding Train",
-    url: "https://www.youtube.com/watch?v=HerCR8bw_GE",
-    rating: 230,
-  },
-  {
-    id: 82653,
-    title: "Mac & Cheese | Basics with Babish",
-    url: "https://www.youtube.com/watch?v=FUeyrEN14Rk",
-    rating: 2111,
-  },
-  {
-    id: 858566,
-    title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
-    url: "https://www.youtube.com/watch?v=xbs7FT7dXYc",
-    rating: 11,
-  },
-  {
-    id: 453538,
-    title:
-      "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
-    url: "https://www.youtube.com/watch?v=4As0e4de-rI",
-    rating: 3211,
-  },
-  {
-    id: 283634,
-    title: "Learn Unity - Beginner's Game Development Course",
-    url: "https://www.youtube.com/watch?v=gB1F9G0JXOo",
-    rating: 211,
-  },
-  {
-    id: 562824,
-    title: "Cracking Enigma in 2021 - Computerphile",
-    url: "https://www.youtube.com/watch?v=RzWB5jL5RX0",
-    rating: 111,
-  },
-  {
-    id: 442452,
-    title: "Coding Adventure: Chess AI",
-    url: "https://www.youtube.com/watch?v=U4ogK0MIzqk",
-    rating: 671,
-  },
-  {
-    id: 536363,
-    title: "Coding Adventure: Ant and Slime Simulations",
-    url: "https://www.youtube.com/watch?v=X-iSQQgOd1A",
-    rating: 76,
-  },
-  {
-    id: 323445,
-    title: "Why the Tour de France is so brutal",
-    url: "https://www.youtube.com/watch?v=ZacOS8NBK6U",
-    rating: 73,
-  },
-];
-
-const addedVideos = [];
-//return all videos
-app.get("/videos", (req, res) => {
-  res.send(videos);
+app.get("/videos", async (req, res) => {
+  try {
+    const allVideos = await pool.query("SELECT * FROM videos");
+    res.json(allVideos.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
 });
-//posting a new video
+
+
+app.get("/videos/:id", async (req, res) => {
+  let videoId = parseInt(req.params.id);
+  try {
+    const video = await pool.query("SELECT * FROM videos WHERE id=$1", [
+      videoId,
+    ]);
+    res.send(video);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+app.post("/videos", async (req, res) => {
+  const video = req.body;
+  let insertQuery = `INSERT INTO videos(id, title, url, rating) 
+   values(${video.id}, '${video.title}', '${video.url}', '${video.rating}')`;
+  pool.query(insertQuery, (err, result) => {
+    if (!err) {
+      res.send("video was successful added");
+    } else {
+      console.log(err.message);
+    }
+  });
+});
+
+app.put("/videos/:id", async (req, res) => {
+  try {
+    const videoId = req.params.id;
+    const videoTitle = req.body.title;
+    const videoUrl = req.body.url;
+    const videoRating = req.body.rating;
+    const updateVideo = await pool.query(
+      "UPDATE videos SET title =$1, url =$2 , rating =$3 WHERE id =$4",
+      [videoId, videoTitle, videoUrl, videoRating]
+    );
+    res.json("Updated video");
+  } catch (err) {
+    res.status(500).json({ error: err });
+  }
+});
+
+app.delete("/videos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteVideo = await pool.query("DELETE FROM videos WHERE id = $1", [
+      id,
+    ]);
+    res.json("Video with ${id} was deleted");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 app.post("/videos", (req, res) => {
-  console.log("post");
   let newVideo = {
     id: parseInt(uuidv4()),
     title: req.body.title,
@@ -122,10 +117,10 @@ app.post("/videos", (req, res) => {
     });
     return;
   }
-  if (id === null) {
+  if (id === null && isNaN(id)) {
     res.status(404);
   }
-  //validate url
+
   const isValidUrl = videoUrl.match(
     /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/
   );
@@ -137,7 +132,7 @@ app.post("/videos", (req, res) => {
     });
   }
 });
-//getting  a video by id
+
 app.get("/videos/:id", (req, res) => {
   const videoId = parseInt(req.params.id);
   if (videoId > 0) {
@@ -147,19 +142,18 @@ app.get("/videos/:id", (req, res) => {
     return res.status(400).json({ msg: "Invalid input" });
   }
 });
-// deleting a video by id
 
-app.delete("/videos/:id", (req, res) => {
-  const videoId = parseInt(req.params.id);
-  const videoIndex = videos.findIndex((v) => v.id === videoId);
+// app.delete("/videos/:id", (req, res) => {
+//   const videoId = parseInt(req.params.id);
+//   const videoIndex = videos.findIndex((v) => v.id === videoId);
 
-  if (videoIndex < 0) {
-    res
-      .status(404)
-      .json({ result: "failure", message: "Video could not be deleted" });
-    return;
-  } else {
-    videos.splice(videoIndex, 1);
-    res.send("video has been deleted");
-  }
-});
+//   if (videoIndex < 0) {
+//     res
+//       .status(404)
+//       .json({ result: "failure", message: "Video could not be deleted" });
+//     return;
+//   } else {
+//     videos.splice(videoIndex, 1);
+//     res.send("video has been deleted");
+//   }
+// });
