5b2c1f8d468b8f77320ebb9a92b0ab31081125bc
Chidimma <chidimmamofodum@gmail.com>
2023-01-31 19:38:00 +0000

    deployment testing2

--- server/postgresClient.js
+++ server/postgresClient.js
@@ -9,8 +9,7 @@ function getPostgresClient() {
   //   password: process.env.PASSWORD,
   // });
   return new Client({
-    connectionString:
-      "postgres://video_recommendation_user:Wne3nYZC6CKKJONp6fuY5GJUhpMN1vKR@dpg-cfcj5h6n6mpierpiluig-a.frankfurt-postgres.render.com/video_recommendation",
+   connectionString: process.env.URI,
     ssl: { rejectUnauthorized: false }
   });
 }
