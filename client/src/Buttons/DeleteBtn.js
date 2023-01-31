522d20d444cf8a9074395ee2491f58a312d2a213
micheale2021 <andemariammicheale@gmail.com>
2023-01-31 10:54:50 +0000

    commented out delete btn component

--- client/src/Buttons/DeleteBtn.js
+++ client/src/Buttons/DeleteBtn.js
@@ -1,15 +1,15 @@
-import React from "react";
+// import React from "react";
 
-function DeleteButton({ deleteVideo, value }) {
-  return (
-    <button
-      onClick={deleteVideo}
-      value={value}
-      class="p-2 mb-1 bg-danger text-white delete-button"
-    >
-      Delete
-    </button>
-  );
-}
+// function DeleteButton({ onClick, value }) {
+//   return (
+//     <button
+//       onClick={onClick}
+//       value={value}
+//       class="p-2 mb-1 bg-danger text-white delete-button"
+//     >
+//       Delete
+//     </button>
+//   );
+// }
 
-export default DeleteButton;
+// export default DeleteButton;
