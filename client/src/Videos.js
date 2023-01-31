8c9313646e302bcb6e9207b57bd79c552ef4a2ec
Arbeits-Sachen <105115862+Arbeits-Sachen@users.noreply.github.com>
2023-01-31 12:38:55 +0000

    Updated Videos and Split into another File

--- client/src/Videos.js
+++ client/src/Videos.js
@@ -1,13 +1,11 @@
-import { useState } from "react";
+import Embed from "./Embed.js";
 
 
-function Video({ data, video, setVideoData })
+function Video({ videoData, video, setVideoData })
 {
-    const deleteFunction = (data, id) => 
+    const deleteFunction = (id) => 
     {
-        //document.getElementById(id).remove();
-
-        fetch('http://192.168.0.15:5000/' + id,
+        fetch('http://192.168.0.15:5000/videos/' + id,
             {
                 method: 'DELETE',
                 headers: {
@@ -17,61 +15,16 @@ function Video({ data, video, setVideoData })
             .then((response) => response.json())
             .then((data) => setVideoData(data))
             .catch((err) => console.log(err.message));
-
-        //setVideoData(data.filter(data => data.id !== id))
     }
 
     return (
         <div className="container" id={video.id}>
             <h1>{video.title}</h1>
-            <button id="delete" onClick={() => deleteFunction(data, video.id)}>Delete</button>
+            <button id="delete" onClick={() => deleteFunction(video.id)}>Delete</button>
 
-            <Embed video={video} data={data} />
+            <Embed video={video} />
         </div>
     );
-}
-
-
-
-
-
-
-
-
-const Embed = ({ video, data, sortData }) => 
-{
-    const url = video.url.replace("watch?v=", "embed/");
-    const [rating, setRating] = useState(video.rating);
-    const [hasVoted, setHasVoted] = useState(false);
-
-    const upVote = () => 
-    {
-        if (!hasVoted)
-        {
-            setRating(rating + 1);
-            setHasVoted(true);
-        }
-    }
-
-    const downVote = () => 
-    {
-        if (!hasVoted)
-        {
-            setRating(rating - 1);
-            setHasVoted(true);
-        }
-    }
-
-
-    return (
-        <div>
-            <iframe width="auto" height="auto" src={url} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
-
-            </iframe>
-            <p>Rating: {video.rating}</p>
-            <button id="up" onClick={upVote} disabled={hasVoted}>Like</button>
-            <button id="down" onClick={downVote} disabled={hasVoted}>Dislike</button>
-        </div>);
 }
 
 export default Video
