026c8be27d229dab6521b73c09d4d841024fa3a3
Chidimma <chidimmamofodum@gmail.com>
2023-01-31 23:07:17 +0000

    ready for final deployment

--- server/postgresClient.js
+++ server/postgresClient.js
@@ -1,17 +1,12 @@
 const { Client } = require("pg");
 
-function getPostgresClient() {
-  // return new Client({
-  //   host: "localhost",
-  //   database: "videos",
-  //   port: 5432,
-  //   user: "postgres",
-  //   password: process.env.PASSWORD,
-  // });
-  return new Client({
+async function getPostgresClient() {
+  const client = new Client({
    connectionString: process.env.URI,
     ssl: { rejectUnauthorized: false }
   });
+  await client.connect()
+  return client
 }
 
 module.exports = getPostgresClient;
