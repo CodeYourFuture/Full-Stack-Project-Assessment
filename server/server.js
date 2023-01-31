eff504c1b0c0c0373468fd96d359aabfea68ac93
irina mihai <irinamihai150@gmail.com>
2023-01-31 19:39:55 +0000

    level 300

--- server/server.js
+++ server/server.js
@@ -6,8 +6,6 @@ const { Pool } = require("pg");
 const pool = require("./db.js");
 const bodyParser = require("body-parser");
 
-// pool.connect();
-
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -30,57 +28,8 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.get("/videos", async (req, res) => {
-  try {
-    const allVideos = await pool.query("SELECT * FROM videos");
-    res.json(allVideos.rows);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-
-
-app.get("/videos/:id", async (req, res) => {
-  let videoId = parseInt(req.params.id);
-  try {
-    const video = await pool.query("SELECT * FROM videos WHERE id=$1", [
-      videoId,
-    ]);
-    res.send(video);
-  } catch (error) {
-    console.error(error.message);
-  }
-});
-app.post("/videos", async (req, res) => {
-  const video = req.body;
-  let insertQuery = `INSERT INTO videos(id, title, url, rating) 
-   values(${video.id}, '${video.title}', '${video.url}', '${video.rating}')`;
-  pool.query(insertQuery, (err, result) => {
-    if (!err) {
-      res.send("video was successful added");
-    } else {
-      console.log(err.message);
-    }
-  });
-});
-
-app.put("/videos/:id", async (req, res) => {
-  try {
-    const videoId = req.params.id;
-    const videoTitle = req.body.title;
-    const videoUrl = req.body.url;
-    const videoRating = req.body.rating;
-    const updateVideo = await pool.query(
-      "UPDATE videos SET title =$1, url =$2 , rating =$3 WHERE id =$4",
-      [videoId, videoTitle, videoUrl, videoRating]
-    );
-    res.json("Updated video");
-  } catch (err) {
-    res.status(500).json({ error: err });
-  }
-});
-
-app.delete("/videos/:id", async (req, res) => {
+//deleting videos
+app.get("/delete-videos/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const deleteVideo = await pool.query("DELETE FROM videos WHERE id = $1", [
@@ -88,18 +37,38 @@ app.delete("/videos/:id", async (req, res) => {
     ]);
     res.json("Video with ${id} was deleted");
   } catch (error) {
-    console.error(error.message);
+  res.send(err);
   }
 });
 
-app.post("/videos", (req, res) => {
-  let newVideo = {
+app.get("/videos", async (req, res) => {
+  try {
+    const allVideos = await pool.query("SELECT * FROM videos");
+    res.json(allVideos.rows);
+  } catch (error) {
+    res.send(err);
+  }
+});
+
+app.get("/videos/:id", async (req, res) => {
+  let videoId = parseInt(req.params.id);
+
+  try {
+    const video = await pool.query("SELECT * FROM videos WHERE id=$1", [
+      videoId,
+    ]);
+    res.send(video);
+  } catch (error) {
+    res.send(err);
+  }
+});
+app.post("/post-videos", async (req, res) => {
+  const video = {
     id: parseInt(uuidv4()),
     title: req.body.title,
     url: req.body.url,
     rating: 0,
   };
-
   const videoTitle = req.body.title;
   const videoUrl = req.body.url;
   let id = uuidv4();
@@ -121,39 +90,25 @@ app.post("/videos", (req, res) => {
     res.status(404);
   }
 
-  const isValidUrl = videoUrl.match(
+  let validUrl = videoUrl.match(
     /^(?:https?:\/\/)?(?:m\.|www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/
   );
 
-  if (newVideo.title && isValidUrl) {
-    videos.push(newVideo);
-    res.status(200).json({
-      message: ` new video with id ${newVideo.id} has been posted`,
+  const insertQuery = `INSERT INTO videos(id, title, url, rating)
+   values(${video.id}, '${video.title}', '${video.url}', '${video.rating}')`;
+   
+  if (validUrl) {
+    pool.query(insertQuery, (err, result) => {
+      if (!err) {
+        res.json({
+          message: ` new video with has been posted`,
+        });
+      } else {
+        res.status(500).json({
+          result: "failure",
+          msg: "An url is required",
+        });
+      }
     });
   }
 });
-
-app.get("/videos/:id", (req, res) => {
-  const videoId = parseInt(req.params.id);
-  if (videoId > 0) {
-    res.json(videos.find((v) => v.id === videoId));
-  }
-  if (!videoId) {
-    return res.status(400).json({ msg: "Invalid input" });
-  }
-});
-
-// app.delete("/videos/:id", (req, res) => {
-//   const videoId = parseInt(req.params.id);
-//   const videoIndex = videos.findIndex((v) => v.id === videoId);
-
-//   if (videoIndex < 0) {
-//     res
-//       .status(404)
-//       .json({ result: "failure", message: "Video could not be deleted" });
-//     return;
-//   } else {
-//     videos.splice(videoIndex, 1);
-//     res.send("video has been deleted");
-//   }
-// });
