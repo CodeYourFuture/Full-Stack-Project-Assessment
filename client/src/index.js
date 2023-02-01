e4088cf9d3dc879f2f1d3df3ebee9705f675adef
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-02-01 10:29:08 +0000

    added packages

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import {disableReactDevTools} from '@fvilers/disable-react-devtools';
 
+if (process.env.NODE_ENV === 'production')disableReactDevTools()
 ReactDOM.render(
   <React.StrictMode>
     <App />
