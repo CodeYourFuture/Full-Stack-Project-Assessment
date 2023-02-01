b0ae346c5f487fe4c91bbd632cfb00b1ac44938b
irina mihai <irinamihai150@gmail.com>
2023-02-01 19:22:17 +0000

    1

---
+++ client/src/Components/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Searching for: ${searchTerm}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleChange}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
