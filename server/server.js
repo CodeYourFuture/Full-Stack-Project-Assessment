6d5d205cc99f88a606426a02d4d682c5bf10f565
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-02-01 20:17:28 +0000

    Get_and_post_query_added

--- server/server.js
+++ server/server.js
@@ -41,71 +41,69 @@ if (existingId) {
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [
-    {
-      id: 523523,
-      title: "Never Gonna Give You Up",
-      url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-      rating: 23,
-    },
-    {
-      id: 523427,
-      title: "The Coding Train",
-      url: "https://www.youtube.com/watch?v=HerCR8bw_GE",
-      rating: 230,
-    },
-    {
-      id: 82653,
-      title: "Mac & Cheese | Basics with Babish",
-      url: "https://www.youtube.com/watch?v=FUeyrEN14Rk",
-      rating: 2111,
-    },
-    {
-      id: 858566,
-      title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
-      url: "https://www.youtube.com/watch?v=xbs7FT7dXYc",
-      rating: 11,
-    },
-    {
-      id: 453538,
-      title:
-        "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
-      url: "https://www.youtube.com/watch?v=4As0e4de-rI",
-      rating: 3211,
-    },
-    {
-      id: 283634,
-      title: "Learn Unity - Beginner's Game Development Course",
-      url: "https://www.youtube.com/watch?v=gB1F9G0JXOo",
-      rating: 211,
-    },
-    {
-      id: 562824,
-      title: "Cracking Enigma in 2021 - Computerphile",
-      url: "https://www.youtube.com/watch?v=RzWB5jL5RX0",
-      rating: 111,
-    },
-    {
-      id: 442452,
-      title: "Coding Adventure: Chess AI",
-      url: "https://www.youtube.com/watch?v=U4ogK0MIzqk",
-      rating: 671,
-    },
-    {
-      id: 536363,
-      title: "Coding Adventure: Ant and Slime Simulations",
-      url: "https://www.youtube.com/watch?v=X-iSQQgOd1A",
-      rating: 76,
-    },
-    {
-      id: 323445,
-      title: "Why the Tour de France is so brutal",
-      url: "https://www.youtube.com/watch?v=ZacOS8NBK6U",
-      rating: 73,
-    },
-  ];
-
-
+// let videos = [
+//     {
+//       id: 523523,
+//       title: "Never Gonna Give You Up",
+//       url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+//       rating: 23,
+//     },
+//     {
+//       id: 523427,
+//       title: "The Coding Train",
+//       url: "https://www.youtube.com/watch?v=HerCR8bw_GE",
+//       rating: 230,
+//     },
+//     {
+//       id: 82653,
+//       title: "Mac & Cheese | Basics with Babish",
+//       url: "https://www.youtube.com/watch?v=FUeyrEN14Rk",
+//       rating: 2111,
+//     },
+//     {
+//       id: 858566,
+//       title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
+//       url: "https://www.youtube.com/watch?v=xbs7FT7dXYc",
+//       rating: 11,
+//     },
+//     {
+//       id: 453538,
+//       title:
+//         "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
+//       url: "https://www.youtube.com/watch?v=4As0e4de-rI",
+//       rating: 3211,
+//     },
+//     {
+//       id: 283634,
+//       title: "Learn Unity - Beginner's Game Development Course",
+//       url: "https://www.youtube.com/watch?v=gB1F9G0JXOo",
+//       rating: 211,
+//     },
+//     {
+//       id: 562824,
+//       title: "Cracking Enigma in 2021 - Computerphile",
+//       url: "https://www.youtube.com/watch?v=RzWB5jL5RX0",
+//       rating: 111,
+//     },
+//     {
+//       id: 442452,
+//       title: "Coding Adventure: Chess AI",
+//       url: "https://www.youtube.com/watch?v=U4ogK0MIzqk",
+//       rating: 671,
+//     },
+//     {
+//       id: 536363,
+//       title: "Coding Adventure: Ant and Slime Simulations",
+//       url: "https://www.youtube.com/watch?v=X-iSQQgOd1A",
+//       rating: 76,
+//     },
+//     {
+//       id: 323445,
+//       title: "Why the Tour de France is so brutal",
+//       url: "https://www.youtube.com/watch?v=ZacOS8NBK6U",
+//       rating: 73,
+//     },
+//   ];
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -119,28 +117,42 @@ const pool = new Pool({
 });
 
 
-//query here a
 // GET "/"
 app.get("/", (req, res) => {
-  res.send(videos);
+  pool
+ .query("SELECT * FROM videos")
+ .then((result) => res.json(result.rows))
+ .catch((error) => {
+  console.error(error);
+  res.status(500).json(error);
+ });
 });
 
 app.post("/", (req, res) => {
   const { title, url } = req.body;
-  const id = createRandomId(videos)
+ 
   if (title && youtubeUrl(url)) {
-    videos.push( { title, url, id });
-    return res.sendStatus(201);
+     pool
+       .query("INSERT INTO videos(title, url) values ($1, $2)", [title, url])
+       .then(() => {
+         res.sendStatus(201);
+       })
+       .catch(() => {
+         res.sendStatus(500);
+       });
   }
-
-  return res.sendStatus(403);
 });
 
 app.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const item = videos.find((v) => v.id === Number(id));
-  if (item) return res.send(item);
-  return res.status(404).send("ID is not found");
+  const videoId = req.params.id;
+  pool
+    .query("SELECT * FROM videos WHERE id=$1", [videoId])
+    .then((result) => res.json(result.rows))
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json(error);
+    });
+
 });
 
 //delete
@@ -157,3 +169,5 @@ app.delete("/:id", (req, res) => {
     message: "Video could not be deleted",
   });
 });
+
+// delete from the client side
