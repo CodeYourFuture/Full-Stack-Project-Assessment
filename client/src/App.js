43e64c55ae74bec4a0edd6a261d545363378aaef
Miladepour <epour.milad@gmail.com>
2023-02-02 15:07:10 +0000

    Fixing some bugs from buttuns.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import VideoScreen from "./main/VideosScreen";
+
 function App() {
   return (
     <div className="App">
--- client/src/main/VideosScreen.js
+++ client/src/main/VideosScreen.js
@@ -6,27 +6,41 @@ const VideoScreen = () => {
   const [like, setLike] = useState(0);
   const [unLike, setUnLike] = useState(0);
   const [videos, setVideos] = useState(data);
-  function likeHandler() {
-    setLike(like + 1);
+  const [vote, setVote] = useState(0);
+  function likeHandler(index) {
+    setVideos(
+      videos.map((video, i) => {
+        if (i === index) {
+          return { ...video, rating: video.rating + 1 };
+        }
+        return video;
+      })
+    );
   }
-  function unLikeHandler() {
-    setUnLike(unLike + 1);
+  function unLikeHandler(index) {
+    setVideos(
+      videos.map((video, i) => {
+        if (i === index) {
+          return { ...video, rating: video.rating - 1 };
+        }
+        return video;
+      })
+    );
   }
   function deleteHandler(index) {
     console.log(index);
     setVideos(videos.filter((video, i) => i !== index));
   }
   const addVideo = (title, url) => {
-    setVideos([...videos, { title: title, url: url }]);
+    setVideos([...videos, { title: title, url: url, rating: 0 }]);
   };
   return (
-    <div>
+    <div className="card-item">
       <AddVideo addVideo={addVideo} />
       <div>
         {videos.map((video, index) => {
           return (
-            <>
-              <h3 key={index}>{video.title}</h3>
+            <div>
               <iframe
                 width="400"
                 height="280"
@@ -35,13 +49,15 @@ const VideoScreen = () => {
                 allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 title={video.title}
+                className="card-img-top"
               />
-              <h4 key={index}>Rating:{video.rating}</h4>
-              <h5>Voted:{like - unLike}</h5>
-              <button onClick={likeHandler}>👏🏼 Up Vote {like}</button>
-              <button onClick={unLikeHandler}>🍅 Down Vote{unLike}</button>
+              <h4 key={index}>{video.title}</h4>
+              <h5 key={index}>Rating:{video.rating}</h5>
+              {/* <h5>Voted:{video.rating}</h5> */}
+              <button onClick={() => likeHandler(index)}>👏🏼 Up Vote</button>
+              <button onClick={() => unLikeHandler(index)}>🍅 Down Vote</button>
               <button onClick={() => deleteHandler(index)}>Delete video</button>
-            </>
+            </div>
           );
         })}
       </div>
