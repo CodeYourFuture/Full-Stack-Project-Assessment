4a22a1bb42dd8b392a7fda1e87af089584b033b3
FranklinPTorres <105072367+FranklinPTorres@users.noreply.github.com>
2023-02-02 18:44:32 +0000

    fetching/ linking back en with front end 250 done

--- client/src/components/Videos.jsx
+++ client/src/components/Videos.jsx
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import data from "./exampleresponse.json";
 import VideoCards from "./VideoCards";
+
 function Videos() {
-  const [videos, setVideos] = useState(data);
+  const [videos, setVideos] = useState([]);
   const [filter, setFilter] = useState(videos);
+  useEffect(() => {
+    fetch(`http://localhost:5000/videos`)
+      .then((res) => res.json())
+      .then((data) => {
+        setVideos(data);
+        setFilter(videos);
+        // console.log(data);
+      });},[])
+    console.log()
   const handleSearch = (e) => {
     if (e.target.value === "") {
       setFilter(videos);
