81fb7c748fffec8eac9b1fad33b11db62c3fa918
Miladepour <epour.milad@gmail.com>
2023-02-02 11:51:10 +0000

    Level 100 - frond end
    -Add functionality to delete btn

---
+++ client/src/main/AddVideo.js
@@ -0,0 +1,15 @@
+const AddVideo = () => {
+  return (
+    <>
+      <form>
+        <h4>Add your favorite video</h4>
+        <label>Title</label>
+        <input></input>
+        <label>Url</label>
+        <input></input>
+        <input type="submit"></input>
+      </form>
+    </>
+  );
+};
+export default AddVideo;
--- client/src/main/VideosScreen.js
+++ client/src/main/VideosScreen.js
@@ -1,38 +1,46 @@
-import data from "../exampleresponse.json";
 import { useState } from "react";
+import data from "../exampleresponse.json";
+import AddVideo from "./AddVideo";
 
 const VideoScreen = () => {
   const [like, setLike] = useState(0);
   const [unLike, setUnLike] = useState(0);
+  const [videos, setVideos] = useState(data);
   function likeHandler() {
     setLike(like + 1);
   }
   function unLikeHandler() {
     setUnLike(unLike + 1);
   }
+  function deleteHandler(index) {
+    setVideos(videos.filter((video, i) => i !== index));
+  }
   return (
     <div>
-      {data.map((video, index) => {
-        return (
-          <>
-            <h3 key={index}>{video.title}</h3>
-            <iframe
-              width="400"
-              height="280"
-              src={video.url}
-              frameBorder="0"
-              allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title={video.title}
-            />
-            <h4 key={index}>Rating:{video.rating}</h4>
-            <h5>Voted:{like - unLike}</h5>
-            <button onClick={likeHandler}>👏🏼 Up Vote {like}</button>
-            <button onClick={unLikeHandler}>🍅 Down Vote{unLike}</button>
-            <button>Delete video</button>
-          </>
-        );
-      })}
+      <AddVideo />
+      <div>
+        {videos.map((video, index) => {
+          return (
+            <>
+              <h3 key={index}>{video.title}</h3>
+              <iframe
+                width="400"
+                height="280"
+                src={video.url}
+                frameBorder="0"
+                allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title={video.title}
+              />
+              <h4 key={index}>Rating:{video.rating}</h4>
+              <h5>Voted:{like - unLike}</h5>
+              <button onClick={likeHandler}>👏🏼 Up Vote {like}</button>
+              <button onClick={unLikeHandler}>🍅 Down Vote{unLike}</button>
+              <button onClick={() => deleteHandler(index)}>Delete video</button>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 };
