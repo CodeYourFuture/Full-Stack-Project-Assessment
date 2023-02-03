ed5791e4f9a80dd7c242066dd46d8916e23d2ca3
RobCso <robcso@DESKTOP-AA47BN0.localdomain>
2023-02-03 17:16:59 +0000

    create database and connect to back and frontend

--- server/server.js
+++ server/server.js
@@ -2,36 +2,37 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3001;
-const fs = require("fs");
+//const fs = require("fs");
 const path = require("path");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv")
-dotenv.config()
+const dotenv = require("dotenv");
+dotenv.config();
 app.use(bodyParser.json());
+
+const { Pool } = require("pg");
 
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 // const cors = require("cors");
 // app.use(cors);
 
-// DATABASE connection
+//DATABASE connection
 // const isProduction = process.env.NODE_ENV === "production";
 // const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
-// const pool = new Pool({
-//   connectionString: isProduction? process.env.DATABASE_URL : connectionString,
-//   connectionTimeoutMillis: 6000,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// })
-
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  connectionTimeoutMillis: 6000,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 //let videos = JSON.parse(fs.readFileSync("videos.json", "utf-8"));
-let videos = require("./videos.json");
+// let videos = require("./videos.json");
 
-const save = () => {
-  fs.writeFileSync("videos.json", JSON.stringify(videos, null, 2));
-};
+// const save = () => {
+//   fs.writeFileSync("videos.json", JSON.stringify(videos, null, 2));
+// };
 
 function matchYoutubeUrl(url) {
   var p =
@@ -49,40 +50,59 @@ app.get("/", (req, res) => {
   //res.json(videos);
 });
 
-app.get("/videos", (req, res) => {
-  res.json(videos);
+app.get("/videos", async (req, res) => {
+  const query = "select * from videos";
+  try {
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
-app.get("/videos/:id", (req, res) => {
+app.get("/videos/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   if (isNaN(id)) {
     res.sendStatus(400);
     return;
   }
-  const findVideo = videos.find((a) => a.id === id);
+  //const findVideo = videos.find((a) => a.id === id);
 
-  if (!findVideo) {
-    res.sendStatus(404);
-    return;
+  // if (!findVideo) {
+  //   res.sendStatus(404);
+  //   return;
+  // }
+  // res.send(findVideo);
+  const query = `select * from videos where id = ${id}`;
+  try {
+    const result = await pool.query(query);
+    if (!result.rowCount) {
+      res.sendStatus(404);
+      return;
+    }
+    res.json(result.rows);
+  } catch (error) {
+    res.status(500).send(error);
   }
-  res.send(findVideo);
 });
 
 // POST
 
-const compulsoryFields = ["title", "url", "rating"];
-
 app.post("/videos", (req, res) => {
+  const compulsoryFields = ["title", "url", "rating"];
   if (!compulsoryFields.every((cf) => req.body.hasOwnProperty(cf))) {
     res.status(401).send("not all compulsory fields supplied");
   }
 
-  let newVideo = {
-    id: req.body.id,
-    title: req.body.title,
-    url: req.body.url,
-    rating: req.body.rating,
-  };
+  // let newVideo = {
+  //   id: req.body.id,
+  //   title: req.body.title,
+  //   url: req.body.url,
+  //   rating: req.body.rating,
+  // };
+  let title = req.body.title;
+  let url = req.body.url;
+  let rating = req.body.rating;
 
   if (!req.body.title) {
     res.status(400).send("Please enter a valid title");
@@ -94,14 +114,22 @@ app.post("/videos", (req, res) => {
     return;
   }
 
-  if ((isNaN(req.body.rating) || !req.body.rating)) {
+  if (isNaN(req.body.rating) || !req.body.rating) {
     res.status(400).send("Please enter a valid number as rating");
     return;
   }
 
-  videos.push(newVideo);
-  save();
-  res.status(201).json({"message": "Your video is successfully uploaded"});
+  const insertQuery =
+    "INSERT INTO videos(title, url, rating) VALUES($1, $2, $3)";
+  // you can use async try catch or below code
+  pool
+    .query(insertQuery, [title, url, rating])
+    .then(() => {
+      res.status(201).json({
+        message: "Your video is successfully uploaded",
+      });
+    })
+    .catch((error) => console.error(error));
 });
 
 // Delete
@@ -112,15 +140,20 @@ app.delete("/videos/:id", (req, res) => {
     res.sendStatus(400);
     return;
   }
-  const findVideo = videos.find((v) => v.id === videoId);
-  if (!findVideo) {
+
+  const deleteQuery = `delete from videos where id=${videoId}`;
+  const findVideo = `select form videos where id=${videoId}`;
+  const result = pool.query(findVideo);
+  if (!result.rowCount) {
     res.sendStatus(404);
     return;
   }
-  const index = videos.findIndex((v) => v.id === videoId);
-  videos.splice(index, 1);
-  save(videos);
-  res.status(200).json({ message: "Your video is successfully deleted" });
+  pool
+    .query(deleteQuery)
+    .then(() => {
+      res.json({ message: `Video by ${videoId} has been deleted` });
+    })
+    .catch((error) => console.error(error));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
