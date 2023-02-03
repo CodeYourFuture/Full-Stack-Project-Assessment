eac6fc2a8306d724bc953f218f09f54857a815c0
michelle <mindyourphotosynthesis@gmail.com>
2023-02-03 21:51:20 +0000

    videos: links fixed

--- client/src/Componenets/Embeded.jsx
+++ client/src/Componenets/Embeded.jsx
@@ -1,15 +1,14 @@
 const Embeded = ({ url }) => {
   //   console.log(typeof url)
-  let url0
   if (url.includes('watch?v=')) {
-    url0 = url.replace('watch?v=', 'embed/')
+    url = url.replace('watch?v=', 'embed/')
   }
   return (
     <div>
       <iframe
         width="560"
         height="315"
-        src={url0}
+        src={url}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
