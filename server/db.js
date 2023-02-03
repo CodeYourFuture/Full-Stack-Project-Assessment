53a9a0b1771616d0d5831130efece054c03c784e
Tsione Temesgen Abraham <tsionetemesgenabraham@gmail.com>
2023-02-03 22:19:41 +0000

    Added proxy

--- server/db.js
+++ server/db.js
@@ -5,8 +5,9 @@ dotenv.config();
 const pool = new Pool({
   user: "postgres",
   connectionString:
+    process.env.DATABASE_URL ||
     "postgresql://postgres:CYFStudent123@localhost:5432/video_lists",
-  ssl: false,
+  ssl: process.env.DATABASE_URL ? true : false,
 });
 
 module.exports = pool;
