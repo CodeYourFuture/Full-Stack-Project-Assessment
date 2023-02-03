c6352fc7ac0bb290bae3a7dc56ff2b7aaf696df1
Dewayne Sewell <dewaynesewell1@gmail.com>
2023-02-03 05:51:30 +0000

    get from render_db

---
+++ server/database.js
@@ -0,0 +1,12 @@
+const Pool = require("pg").Pool;
+const dotenv = require("dotevn");
+
+dotenv.config();
+const pool = new Pool ({  
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  user: 'dewayne',
+});    
+
+
+module.exports = pool;
--- server/server.js
+++ server/server.js
@@ -3,79 +3,37 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+// const pool = require("./database");
 
 app.use(cors());
 app.use(express.json());
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: 'postgres://dewayne:QWLHeEPQ2fCSL58qpqVL6o2JmV8ROx2Z@dpg-cfdos482i3mmlo3gli7g-a.frankfurt-postgres.render.com/videos_nwpu',
+  ssl: { rejectUnauthorized: false },
+  user: 'dewayne',
+  host: 'postgres://dewayne:QWLHeEPQ2fCSL58qpqVL6o2JmV8ROx2Z@dpg-cfdos482i3mmlo3gli7g-a.frankfurt-postgres.render.com/videos_nwpu',
+  database: 'videos_nwpu',
+  password: 'QWLHeEPQ2fCSL58qpqVL6o2JmV8ROx2Z',
+  port: 5432
+});
+
+app.get("/video", function(req, res) {
+    pool.query('SELECT * FROM videos')
+        .then((result) => res.json(result.rows))
+        .catch((error) => {
+            console.error(error);
+            res.status(500).json(error);
+        });
+});
 
 
-let videos = [
-  {
-    id: 523523,
-    title: "Never Gonna Give You Up",
-    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    rating: 23,
-  },
-  {
-    id: 523427,
-    title: "The Coding Train",
-    url: "https://www.youtube.com/watch?v=HerCR8bw_GE",
-    rating: 230,
-  },
-  {
-    id: 82653,
-    title: "Mac & Cheese | Basics with Babish",
-    url: "https://www.youtube.com/watch?v=FUeyrEN14Rk",
-    rating: 2111,
-  },
-  {
-    id: 858566,
-    title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
-    url: "https://www.youtube.com/watch?v=xbs7FT7dXYc",
-    rating: 11,
-  },
-  {
-    id: 453538,
-    title:
-      "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
-    url: "https://www.youtube.com/watch?v=4As0e4de-rI",
-    rating: 3211,
-  },
-  {
-    id: 283634,
-    title: "Learn Unity - Beginner's Game Development Course",
-    url: "https://www.youtube.com/watch?v=gB1F9G0JXOo",
-    rating: 211,
-  },
-  {
-    id: 562824,
-    title: "Cracking Enigma in 2021 - Computerphile",
-    url: "https://www.youtube.com/watch?v=RzWB5jL5RX0",
-    rating: 111,
-  },
-  {
-    id: 442452,
-    title: "Coding Adventure: Chess AI",
-    url: "https://www.youtube.com/watch?v=U4ogK0MIzqk",
-    rating: 671,
-  },
-  {
-    id: 536363,
-    title: "Coding Adventure: Ant and Slime Simulations",
-    url: "https://www.youtube.com/watch?v=X-iSQQgOd1A",
-    rating: 76,
-  },
-  {
-    id: 323445,
-    title: "Why the Tour de France is so brutal",
-    url: "https://www.youtube.com/watch?v=ZacOS8NBK6U",
-    rating: 73,
-  }
-];
 
-// const maxID = uuid.v4();
-// console.log(maxID);
+
+/* 
 let maxID = Math.max(...videos.map((video) => video.id));
 
 app.get("/video", (req, res) => {
@@ -132,3 +90,4 @@ app.delete("/video/:id", (req, res) => {
   res.send({ id: vidId, message: "Video deleted" });
 });
 
+ */
