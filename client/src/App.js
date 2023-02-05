5221c9fc8803d32b8a3b2f2f0ae3295d551a5413
leilafaez <leila.faez@gmail.com>
2023-02-05 23:53:47 +0000

    Complete Level 100 and task 1 & 2 of Level 199

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,53 @@
+import React,{useState} from "react";
 import "./App.css";
+import exampleresponse from "./exampleresponse.json";
+import VideoList from "./component/VideoList";
+import AddVideo from "./component/AddVideo";
 
 function App() {
+  const [videos,setVideos]=useState(exampleresponse)
+
+  const upVote=(id)=>{
+    setVideos(prevVideos=>{
+      prevVideos.map(video=>{
+        if(video.id===id){
+          return {...video,rating: video.rating+1}
+        }
+        return video;
+      })
+    })
+  }
+
+  const downVote=(id)=>{
+    setVideos(prevVideos=>{
+      prevVideos.map(video=>{
+        if(video.id===id){
+          return{...video,rating:video.rating-1}
+        }
+        return video;
+      })
+    })
+  }
+
+  const removeVideo=id=>{
+    setVideos(prevVideos=>prevVideos.filter(video=>video.id!==id));
+  }
+
+  const addVideo=video=>{
+    setVideos(prevVideos=>[...prevVideos,video])
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
+      <AddVideo addVideo={addVideo} />
+      <VideoList
+        videos={videos}
+        upVote={upVote}
+        downVote={downVote}
+        removeVideo={removeVideo}
+      />
     </div>
   );
 }
---
+++ client/src/component/AddVideo.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+
+const AddVideo=({addVideo})=>{
+    const [title,setTitle]=useState("");
+    const[url,setUrl]=useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const handleSubmit = event=>{
+        event.preventDefault();
+         if (!title || !url) {
+           setErrorMessage("Please enter a Title and URL");
+           return;
+         }
+        addVideo({title,url});
+        setTitle("");
+        setUrl("");
+    }
+
+    return (
+      <div className="video-adder">
+        <form onSubmit={handleSubmit}>
+          <label for="title">Add Title </label>
+          <input
+            id="title"
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+          <label for="url">Add Url</label>
+          <input
+            id="url"
+            type="text"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+          {errorMessage && <div className="error-message">{errorMessage}</div>}
+          <button type="submit">Add Video</button>
+        </form>
+      </div>
+    );
+
+}
+
+
+
+
+
+
+
+export default AddVideo; 
