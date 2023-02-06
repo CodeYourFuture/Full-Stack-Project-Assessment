6004677064bc5f9f69048467960a3f4db9073c46
Jihad Omer <jhjahony@gmail.com>
2023-02-06 14:31:03 +0000

    Error when rendering the addvieo component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
-// import addVideo from "./components/addVideo";
+// import React from "react";
+// import addVideo from "./client/components/addVideo"; // This is the line that is causing the error
 
 function App() {
   return (
@@ -7,7 +8,11 @@ function App() {
       <header className="App-header">
         <h1>Video Recommendation</h1>
         </header>
+        {/* <addVideo/> // This is the line that is causing the error */}
+        
+        
     </div>
+  
     
   );
 }
