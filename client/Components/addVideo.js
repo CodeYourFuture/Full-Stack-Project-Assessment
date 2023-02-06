26b9287491e5068da2b195e8259d962a289dd2fd
Jihad Omer <jhjahony@gmail.com>
2023-02-06 14:20:30 +0000

    add form to 'add video component'

--- client/Components/addVideo.js
+++ client/Components/addVideo.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 function addVideo() {
   return (
-    <div>addVideo</div>
+    <div>
+    <h1>addVideo</h1>
+    <form>  
+        <label>Video Title</label>
+        <input type="text" name="title" />
+        <label>Video URL</label>
+        <input type="text" name="url" />
+        <label>Video Description</label>
+        <input type="text" name="description" />
+        <label>Video Category</label>
+        <input type="text" name="category" />
+        <label>Video Rating</label>
+        <input type="text" name="rating" />
+        <button type="submit">Submit</button>
+            
+    
+    
+    
+    </form>
+    </div>
   )
 }
 
