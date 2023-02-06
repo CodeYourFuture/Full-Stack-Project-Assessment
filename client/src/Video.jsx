4456c9d6fec0d8ef017f6e29805573206e5430d9
ShimenAfshar <afsharshimen@gmail.com>
2023-02-06 21:02:05 +0000

    level 100

---
+++ client/src/Video.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+
+const Video = ({ video, upVote, downVote, removeVideo }) => {
+    console.log(video);
+    if (!video) {
+        return null;
+    }
+    return (
+        <div className="video-card">
+            <h2 className="video-title">{video.title}</h2>
+            <iframe
+                className="video-frame"
+                title={video.title}
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${video.url.split("v=")[1]}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+            ></iframe>{" "}
+            <section className="vote-section">
+                <p className="vote-score">Votes :{video.rating}</p>
+
+                <button className="vote-button" onClick={upVote}>
+                    UpVote
+                </button>
+                <button className="vote-button" onClick={downVote}>
+                    DownVote
+                </button>
+                <button className="remove-button" onClick={removeVideo}>
+                    Rmove
+                </button>
+            </section>
+        </div>
+    );
+}
+
+
+export default Video;
---
+++ client/src/VideoList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Video from "./Video";
+
+const VideoList = ({ videos, upVote, downVote, removeVideo }) => {
+    videos.sort((a, b) => b.rating - a.rating);
+
+    return videos ? (
+        <div className="video-list">
+            {videos.map((video) => (
+                <Video
+                    key={video.id}
+                    video={video}
+                    upVote={() => upVote(video.id)}
+                    downVote={() => downVote(video.id)}
+                    removeVideo={() => removeVideo(video.id)}
+                />
+            ))}
+        </div>
+    ) : (
+        <p>No videos found</p>
+    );;
+
+}
+
+export default VideoList;
+
