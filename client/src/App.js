2961467fabac1389339928a383e9c6f971e184ad
Jihad7898 <107761291+Barryeasy90@users.noreply.github.com>
2023-02-08 16:08:51 +0000

    content created level 100
    Need to familiarised with react

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,22 @@ function App() {
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
+      <div className="video-content">
+        
+        <form>
+          <h5>Add video</h5>
+          <label for="title">Title </label>
+          <input type="text" id="title" name="title" /><br></br><br></br>
+          <label for= "url">URL</label>
+          <input type="text" id="url" name="title"/><br></br><br></br>
+          <input type="submit" value="Cancel"/> 
+          <input type="submit" value="ADD" />
+        </form>
+        <div className="search_button">
+          <button>Search </button> 
+          <input type="text" placeholder="search" />
+        </div>
+      </div>
     </div>
   );
 }
