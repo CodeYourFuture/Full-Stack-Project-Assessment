240fecd6534ce3fda77e05334bcf6e6d7a94f650
4maziar <53979524+8maziar@users.noreply.github.com>
2023-02-12 11:46:56 +0000

    level 200 finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import AddVideo from "./components/AddVideo";
 import Search from "./components/Search";
 import SingleVideo from "./components/SingleVideo";
-import data from "./exampleresponse.json";
+
+let fetchedList;
 
 function App() {
-  const [list, setList] = useState(data.sort((a, b) => b.rating - a.rating));
+  const [list, setList] = useState(undefined);
+
+  useEffect(() => {
+    fetch("/videos")
+      .then((response) => response.json())
+      .then((data) => {
+        fetchedList = data.sort((a, b) => b.rating - a.rating);
+        setList(fetchedList);
+      });
+  }, []);
 
   function handleRemove(id) {
     const newList = list.filter((video) => video.id !== id);
     setList(newList);
+    fetch("/videos/" + id, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
   }
 
   function searchVideo(text) {
-    const newList = data.filter((video) => video.title.toLowerCase().includes(text.toLowerCase()));
+    const newList = fetchedList.filter((video) => video.title.toLowerCase().includes(text.toLowerCase()));
     if (text === "") {
-      setList(data);
+      setList(fetchedList);
     } else {
       setList(newList);
     }
@@ -25,6 +40,16 @@ function App() {
   function addNewVideo(obj) {
     const newList = [...list, obj];
     setList(newList);
+    fetch("/videos", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
   }
 
   return (
@@ -37,9 +62,13 @@ function App() {
         <Search searchVideo={searchVideo} />
       </div>
       <div className="video-list">
-        {list.map((video) => {
-          return <SingleVideo key={video.id} {...video} handleRemove={handleRemove} />;
-        })}
+        {list === undefined ? (
+          <p>loadind...</p>
+        ) : (
+          list.map((video) => {
+            return <SingleVideo key={video.id} {...video} handleRemove={handleRemove} />;
+          })
+        )}
       </div>
     </div>
   );
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/videos", function (req, res) {
-  res.send(videos);
+  res.json(videos);
 });
 
 app.get("/videos/:id", function (req, res) {
