44e9a73e4d155abdc51042e5dc98b896036844d5
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-02-12 21:41:19 +0000

    level 200-250

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,37 @@
 import "./App.css";
 import Search from "./components/Search";
 import Add from "./components/Add";
-import data from "./exampleresponse.json";
+// import data from "./exampleresponse.json";
 import Singlevideo from "./components/SingleVideo";
-import { useState } from "react";
- 
+import { useState, useEffect } from "react";
+
+let data;
+
 function App() {
   //   199-1  Ordering Results
-  data.sort((a, b) => b.rating - a.rating);
+  // data.sort((a, b) => b.rating - a.rating);
   //   199-1  Ordering Results
-  
-  const [list, setList] = useState(data);
+
+  const [list, setList] = useState(undefined);
+
+  useEffect(() => {
+    fetch("/videos")
+      .then((response) => response.json())
+      .then((videos) => {
+        data = videos.sort((a, b) => b.rating - a.rating);
+        setList(data);
+      });
+  }, []);
 
   const handleRemove = (Id) => {
     const newList = list.filter((movie) => movie.id !== Id);
     setList(newList);
+    // level 250- fetch delete video in server
+    fetch("/videos/" + Id, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res));
   };
 
   const handelSearch = (value) => {
@@ -26,17 +43,27 @@ function App() {
   };
 
   const handelSave = (title, url) => {
-    data = data.concat([
-      {
-        id: Math.random() * (10000 - 1000) + 1000,
-        title: title,
-        url: url,
-        rating: 0,
-        uploadedDate : new Date().toLocaleString('en-US', { hour12: false })
-      },
-    ]);
+    const obj = {
+      // id: Math.random() * (10000 - 1000) + 1000,
+      title: title,
+      url: url,
+      rating: 0,
+      uploadedDate: new Date().toLocaleString("en-US", { hour12: false }),
+    };
+    data = data.concat([obj]);
 
     setList(data);
+    //  level 250- fetch in add new video to server
+    fetch("/videos", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    })
+      .then((res) => res.json(obj))
+      .then((res) => console.log(res));
   };
 
   return (
@@ -54,17 +81,21 @@ function App() {
           <Add handelSave={handelSave} />
         </div>
         <div className="video">
-          {list.map((video) => {
-            return (
-              <Singlevideo
-                key={video.id}
-                {...video}
-                handleRemove={handleRemove}
-              />
-            );
-          })}
+          {list === undefined ? (
+            <p>Loading</p>
+          ) : (
+            list.map((video) => {
+              return (
+                <Singlevideo
+                  key={video.id}
+                  {...video}
+                  handleRemove={handleRemove}
+                />
+              );
+            })
+          )}
         </div>
-      </section> 
+      </section>
     </main>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,51 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const videos = require("./exampleresponse.json");
+
+const port = 5000;
+app.use(express.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
-
-// GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+//  level200-start
+app.get("/videos", (req, res) => {
+  res.json(videos);
 });
+
+// for convert string to number add + before request
+app.get("/videos/:id", (req, res) => {
+  const newId = +req.params.id;
+  const oneVideo = videos.find((video) => video.id === newId);
+  if (oneVideo) {
+    res.json(oneVideo);
+  }
+});
+
+app.post("/videos", (req, res) => {
+  let data = req.body;
+  // new data and get time methods are js inbuild function which are use to create a new id in this project
+  data.id = new Date().getTime();
+  const valid = data.url !== "" && data.title !== "";
+
+  if (valid) {
+    videos.push(data);
+    res.json({ id: data.id });
+  } else {
+    res.status(400).json({
+      result: "failure",
+      message: "Video could not be saved",
+    });
+  }
+});
+
+app.delete("/videos/:id", (req, res) => {
+  const newId = +req.params.id;
+  const index = videos.findIndex((video) => video.id === newId);
+  if (index === -1)
+    return res.status(404).json({
+      result: "failure",
+      message: "Video could not be deleted",
+    });
+  videos.splice(index, 1);
+  res.json({});
+});
+// level 200-end
