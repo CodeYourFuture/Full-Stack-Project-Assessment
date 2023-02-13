3e730d0edbfd54db54b708002cf0103220e2d41c
Ekrem <ekremteke.uk@gmail.com>
2023-02-13 23:51:16 +0000

    video card component was created

--- client/src/compenents/Forms.js
+++ client/src/compenents/Forms.js
@@ -5,15 +5,17 @@ export const Forms = () => {
         <div>
             <form>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" className="form-label col-sm-3">Email address</label>
+                    <label for="exampleInputEmail1" className="form-label col-sm-3">Title</label>
                     <input type="email" className="form-control col-sm-3" id="exampleInputEmail1" aria-describedby="emailHelp" />
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label col-sm-3">Password</label>
-                    <input type="password" class="form-control col-sm-3" id="exampleInputPassword1" />
+                    <label for="exampleInputPassword1" className="form-label col-sm-3">URL</label>
+                    <input type="url" class="form-control col-sm-3" id="url" />
                 </div>
-                <button type="submit" className="btn btn-primary">Add</button>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <div><button type="submit" className="btn btn-primary">Cancel</button>
+                    <p> </p>
+                    <button type="submit" className="btn btn-primary">Add</button> </div>
+
             </form>
         </div>
     )
---
+++ client/src/compenents/VideoCards.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const VideoCards = (props) => {
+    return
+}
