5d6aa96743eceed9a96bfb5e0d9a7fd3f7d4980e
Ekrem <ekremteke.uk@gmail.com>
2023-02-13 23:06:08 +0000

    React, forms and Header components were created

---
+++ client/src/compenents/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Video Recommendation</h1>
+            </header>
+        </div>
+    )
+}
