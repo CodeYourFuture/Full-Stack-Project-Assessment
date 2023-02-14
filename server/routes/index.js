2259f8f24f8696ea09502b4a1df32f0fdb24294e
Miladepour <epour.milad@gmail.com>
2023-02-14 12:29:44 +0000

    Level 200 - Add delete to index router

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,4 +7,30 @@ router.get("/", (req, res) => {
   res.json(videos);
 });
 
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const video = videos.find((v) => v.id === id);
+  if (!video) {
+    return res.status(404).json({
+      result: "Failure",
+      message: "Video not found",
+    });
+  }
+  res.json(video);
+});
+
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const index = videos.findIndex((v) => v.id === id);
+  console.log(index);
+  if (index === -1) {
+    return res.status(404).json({
+      result: "Failure",
+      message: "Video not found",
+    });
+  }
+  const deleted = videos.splice(index, 1);
+  res.json({ message: `${id} has been deleted` });
+});
+
 module.exports = router;
