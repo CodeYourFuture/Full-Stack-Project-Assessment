6c9d8fd76477e21c83114eca41c5ebda4965e0b9
migue <miguelanjo2@hotmail.com>
2023-02-14 12:43:09 +0000

    display title of video

---
+++ client/src/components/AddVideo.js
@@ -0,0 +1,37 @@
+import React,{useState} from 'react'
+
+
+const AddVideo = () => {
+  const [title, setTitle]= useState("");
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(title === ""){
+      alert("please fill out this field");
+    }
+
+  }
+
+
+  return (
+    <div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Add Video Title</label>
+          <input type="text" id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
+
+          <label htmlFor="url">Add Video Url</label>
+          <input type="text" id='url' value={url} onChange={(e) => setUrl(e.target.value)}/>
+
+          <button className='add-video' onClick={handleSubmit}>Add Video</button>
+          <button className='cancel-video'  >Cancel</button>
+          
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default AddVideo
