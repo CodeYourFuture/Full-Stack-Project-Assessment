a6cc401c31230a1b2c9e42c120c73743cb53fbfc
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2023-02-14 22:01:03 +0000

    complete level 100 and 199

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,46 @@
 import "./App.css";
+import Header from "./Componets/Header/Header";
+import Main from "./Componets/Main/Main";
+import ShowVidoes from "./Componets/ShowVideos/ShowVidoes";
+import data from "./data/exampleresponse.json";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    setVideos(data);
+  }, []);
+  function addVideos(video) {
+    setVideos([...videos, video]);
+  }
+  function decRating(id, rating) {
+    if (rating > 0) {
+      setVideos(
+        videos.map((vd) =>
+          vd.id === id ? { ...vd, rating: vd.rating - 1 } : vd
+        )
+      );
+    }
+  }
+  function incRating(id) {
+    setVideos(
+      videos.map((vd) => (vd.id === id ? { ...vd, rating: vd.rating + 1 } : vd))
+    );
+  }
+  function deletev(id) {
+    setVideos(videos.filter((vd) => vd.id !== id));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
+      <Header />
+      <Main addVideos={addVideos} />
+      <ShowVidoes
+        videos={videos}
+        decRating={decRating}
+        incRating={incRating}
+        deletev={deletev}
+      />
     </div>
   );
 }
---
+++ client/src/Componets/Main/Main.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import "./Main.css";
+
+function Main({ addVideos }) {
+  const [input, setinput] = useState({
+    title: "",
+    url: "",
+    rating: 0,
+  });
+  const handleChange = (event) => {
+    setinput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+  };
+  function handleSubmit(e) {
+    e.preventDefault();
+    addVideos(input);
+    // console.log(addVideo);
+  }
+  return (
+    <div className="left">
+      <div>
+        <h1>Add video</h1>
+        <div className="contenar">
+          <form onSubmit={handleSubmit}>
+            <label>Title </label>
+            <input
+              type="text"
+              name="title"
+              value={input.title}
+              onChange={handleChange}
+            />
+            <label>URL </label>
+            <input
+              type="text"
+              name="url"
+              value={input.url}
+              onChange={handleChange}
+            />
+            <div className="left">
+              <button type="Submit">Add</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div>
+        <label className="search">Search </label>
+        <input type={"text"} />
+      </div>
+    </div>
+  );
+}
+
+export default Main;
