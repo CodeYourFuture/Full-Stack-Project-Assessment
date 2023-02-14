1bd410198b209f36835ba2cb923b7e030528105b
khmdagal <khmdagal@gmail.com>
2023-02-14 21:25:35 +0000

    Level 250 -- and enable corse() inorder F-end & B-end connect smoothly

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,17 @@ import VideoList from "./components/VideoList";
 import InputForm from "./components/InputForm";
 import AddVideoButton from "./components/AddVideoButton";
 import { useState, useEffect } from "react";
-import data from "./data/exampleresponse.json";
+//import data from "./data/exampleresponse.json";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [videos, setVideos] = useState([""]);
 
   useEffect(() => {
-    setVideos(data);
+    fetch("http://127.0.0.1:5000/")
+      .then(res => res.json())
+    .then(data => setVideos(data))
+    //setVideos(data);
   }, []);
 
   const addVideo = (video) => {
--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 
+const cors = require('cors')
+
+app.use(cors())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
