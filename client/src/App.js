b1f19d18f4f1dbdb55f4eafac9104f6c1a61c53e
sasarkarimi <sahar.karimi90@outlook.com>
2023-02-18 16:40:32 +0000

    ACTION ONE

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,28 @@
 import "./App.css";
+import React, {useState} from "react";
+import Header from './components/Header';
+import Search from './components/Search';
+import Videos from './components/Videos';
+import data from "./exampleresponse.json";
+import AddVideo from "./components/AddVideo";
+
+const allData = data;
 
 function App() {
+
+  const [data , setData]= useState(allData)
+
+  console.log(data,'<<<<<<<<<<<<<<<<')
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
+      <Header/>
+      <div className="inputData">
+        <AddVideo data={data} setData={setData} />
+        <Search/>
+        <Videos data={data} setData={setData} /> 
+      </div>
     </div>
   );
 }
-
 export default App;
