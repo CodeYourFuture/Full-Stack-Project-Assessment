ac7b4a2aba4a59f1c87fb80f9ff7426f878fc506
rebwar <rebwar1.azizi@gmail.com>
2023-02-19 22:03:59 +0000

    level 300 DONE

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 const cors = require("cors");
 const express = require("express");
+const { Pool } = require("pg");
 const app = express();
 const port = process.env.PORT || 5001;
 let videos = require("./data/exampleresponse.json");
 
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  user: "rebwar",
+  password: "",
+  database: "cyf_videos_project",
+});
+
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json(videos);
+app.get("/", async (req, res) => {
+  const rs = await pool.query("SELECT * FROM videos");
+  res.json(rs.rows);
 });
 
 app.get("/:id", (req, res) => {
@@ -16,18 +26,22 @@ app.get("/:id", (req, res) => {
   res.json(video);
 });
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
   const video = {
-    id: Math.round(Math.random() * 1000),
     title: req.body.title,
     url: req.body.url,
-    rating: 0,
   };
 
   try {
-    videos.push(video);
+    await pool.query("INSERT INTO videos (title, url) VALUES($1, $2)", [
+      video.title,
+      video.url,
+    ]);
+    const rs = await pool.query(
+      "SELECT id from videos ORDER BY id DESC LIMIT 1"
+    );
 
-    res.json({ id: video.id });
+    res.json({ id: rs.rows[0].id });
   } catch (error) {
     res.json({
       result: "failure",
@@ -36,48 +50,13 @@ app.post("/", (req, res) => {
   }
 });
 
-// app.put("/:id", (req, res) => {
-//   const videoId = +req.params.id;
-//   const video = videos.find(video => video.id === videoId);
-
-//   try {
-//     video.title = req.body.title;
-//     video.url = req.body.url;
-//     video.rating = req.body.rating;
-
-//     res.json({});
-//   } catch (error) {
-//     res.json({
-//       result: "failure",
-//       message: "Video could not be updated",
-//     });
-//   }
-// });
-
-// app.patch("/:id", (req, res) => {
-//   const videoId = +req.params.id;
-//   const video = videos.find(video => video.id === videoId);
-
-//   try {
-//     video.rating = req.body.rating;
-
-//     res.json({});
-//   } catch (error) {
-//     res.json({
-//       result: "failure",
-//       message: "Video could not be updated",
-//     });
-//   }
-// });
-
-app.patch("/:id/inc-rating", (req, res) => {
+app.patch("/:id/inc-rating", async (req, res) => {
   const videoId = +req.params.id;
 
   try {
-    videos = videos.map(video =>
-      video.id !== videoId ? video : { ...video, rating: video.rating + 1 }
-    );
-    console.log(videos[0]);
+    await pool.query("UPDATE videos SET rating = (rating + 1) WHERE id = $1", [
+      videoId,
+    ]);
 
     res.json({});
   } catch (error) {
@@ -88,14 +67,13 @@ app.patch("/:id/inc-rating", (req, res) => {
   }
 });
 
-app.patch("/:id/dec-rating", (req, res) => {
+app.patch("/:id/dec-rating", async (req, res) => {
   const videoId = +req.params.id;
 
   try {
-    videos = videos.map(video =>
-      video.id !== videoId ? video : { ...video, rating: video.rating - 1 }
-    );
-    console.log(videos[0]);
+    await pool.query("UPDATE videos SET rating = (rating - 1) WHERE id = $1", [
+      videoId,
+    ]);
 
     res.json({});
   } catch (error) {
@@ -106,12 +84,11 @@ app.patch("/:id/dec-rating", (req, res) => {
   }
 });
 
-app.delete("/:id", (req, res) => {
+app.delete("/:id", async (req, res) => {
   const videoId = +req.params.id;
 
   try {
-    videos = videos.filter(video => video.id !== videoId);
-    console.log(videos[0]);
+    await pool.query("DELETE FROM videos WHERE id = $1", [videoId]);
 
     res.json({});
   } catch (error) {
