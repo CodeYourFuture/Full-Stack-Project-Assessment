5575e828093dc3336c58c848929bd3ea28e1d563
Jihad Omer <jhjahony@gmail.com>
2023-02-19 13:28:10 +0000

    add gitignore

--- client/Components/addVideo.js
+++ client/Components/addVideo.js
@@ -1,28 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./Main.css";
 
-function addVideo() {
+function Main({ addVideos }) {
+  const [input, setinput] = useState({
+    title: "",
+    url: "",
+    rating: 0,
+  });
+  const handleChange = (event) => {
+    setinput({
+      ...input,
+      [event.target.name]: event.target.value,
+    });
+  };
+  function handleSubmit(e) {
+    e.preventDefault();
+    addVideos(input);
+  }
   return (
-    <div>
-    <h1>addVideo</h1>
-    <form>  
-        <label>Video Title</label>
-        <input type="text" name="title" />
-        <label>Video URL</label>
-        <input type="text" name="url" />
-        <label>Video Description</label>
-        <input type="text" name="description" />
-        <label>Video Category</label>
-        <input type="text" name="category" />
-        <label>Video Rating</label>
-        <input type="text" name="rating" />
-        <button type="submit">Submit</button>
-            
-    
-    
-    
-    </form>
+    <div className="left">
+      <div>
+        <h1>Add video</h1>
+        <div className="contenar">
+          <form onSubmit={handleSubmit}>
+            <label>Title </label>
+            <input
+              type="text"
+              name="title"
+              value={input.title}
+              onChange={handleChange}
+            />
+            <label>URL </label>
+            <input
+              type="text"
+              name="url"
+              value={input.url}
+              onChange={handleChange}
+            />
+            <div className="left">
+              <button type="Submit">Add</button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div>
+        <label className="search">Search </label>
+        <input type={"text"} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default addVideo
+export default Main;
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 // import React from "react";
-// import addVideo from "./client/components/addVideo"; // This is the line that is causing the error
+import addVideo from "./Componets/AddVideos/AddVidoes";
 
 function App() {
   return (
@@ -9,6 +9,76 @@ function App() {
         <h1>Video Recommendation</h1>
         </header>
         {/* <addVideo/> // This is the line that is causing the error */}
+
+         <div className="container">
+           <div className="row">
+             <div className="col-md-4">
+              <div className="card card-body">
+                 <h4>Create a new video</h4>
+               <div className="form-group">
+                 <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Title"
+                    name="title"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Description"
+                    name="description"
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="URL"
+                    name="url"
+                  />
+                </div>
+                <form action="/upload" method="post" enctype="multipart/form-data">
+                  <input type="file" name="file" />
+                  <input type="submit" />
+                </form>
+                <button type="submit" className="btn btn-primary">
+                  Save
+                </button>
+              </div>
+            </div>
+            <div className="col-md-8">
+              <ul className="list-group">
+                <li className="list-group-item list-group-item-action">
+                  <a href="https://www.youtube.com/watch?v=7WwtzsSHdpI" target="_blank">
+                    <h4>Video 1</h4>
+                  </a>
+                  <p>Description</p>
+                  <button className="btn btn-secondary">Edit</button>
+                  <button className="btn btn-danger">Delete</button>
+                </li>
+                <li className="list-group-item list-group-item-action">
+                  <a href="https://www.youtube.com/watch?v=7WwtzsSHdpI" target="_blank">
+                    <h4>Video 2</h4>
+                  </a>
+                  <p>Description</p>
+                  <button className="btn btn-secondary">Edit</button>
+                  <button className="btn btn-danger">Delete</button>
+                </li>
+                <li className="list-group-item list-group-item-action">
+                  <a href="https://www.youtube.com/watch?v=7WwtzsSHdpI" target="_blank">
+                    <h4>Video 3</h4>
+                  </a>
+                  <p>Description</p>
+                  <button className="btn btn-secondary">Edit</button>
+                  <button className="btn btn-danger">Delete</button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+          
         
         
     </div>
