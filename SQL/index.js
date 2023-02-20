7d2b227c7c5f7ef631b54063bd90622d1b82fcdb
Ekrem <ekremteke.uk@gmail.com>
2023-02-20 22:19:40 +0000

    node get query was written

--- SQL/index.js
+++ SQL/index.js
@@ -1,33 +1,32 @@
-import express from "express";
-
-
+const express = require("express");
 const app = express();
 
-// const { Pool } = require("pg");
+const { Pool } = require("pg");
 
-
-
-
-// const db = new Pool({
-//     user: "tekre",
-//     host: "localhost",
-//     database: "fsp_videos",
-//     password: "185606",
-//     port: 5432,
-// });
-
-
-
-
-
+const db = new Pool({
+    host: "localhost",
+    user: "tekre",
+    password: "185606",
+    database: "videos",
+    port: 5432
+});
 
 const port = 6000 || process.env.PORT;
+app.get('/', (req, res) => {
+    res.json("hello this is the backend")
+});
+app.get("/videos", (req, res) => {
+    db.query("select * from videos", (err, result) => {
+        res.json(result.rows);
+    });
+});
 
-
-
-
+app.post("/videos", (req, res) => {
+    const query = "INSERT INTO videos (`id`,`title`,`url`,`rating`) VALUES(?)"
+    const values = ["id from SQL", "title", "url", "rating"];
+    db.query(query, [values], (err, data) => {
+        res.json(data);
+    })
+})
 
 app.listen(port, () => console.log(`Connected to backend ${port}`));
-
-postgres://postgres:[185606]@db.vdbnhqozmlzdsaejdxwr.supabase.co:5432/postgres
-
