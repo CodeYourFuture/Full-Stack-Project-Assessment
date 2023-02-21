178e4d70f9a6b8287c319051c027d1f88378ae21
azadehroshan <105980239+azadehroshan@users.noreply.github.com>
2023-02-21 23:10:50 +0000

    level 300-database replace by json file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const videos = require("./exampleresponse.json");
+// const videos = require("./exampleresponse.json");
 const { Pool } = require("pg");
 
 const port = 5000;
-app.use(express.json()); 
+app.use(express.json());
 
-const db = new Pool({
+const pool = new Pool({
   host: "localhost",
   port: 5432,
   user: "postgres",
@@ -14,45 +14,47 @@ const db = new Pool({
   database: "videos_list_project",
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-
 });
 
 //  level200-start
-app.get("/videos", (req, res) => { 
-  let { order } = req.query; 
-  order = order === "asc" ? "ASC" : "DESC";
+app.get("/videos", (req, res) => {
+  // let { order } = req.query;
+  // order = order === "asc" ? "ASC" : "DESC";
   //  level299-Ordered Data - Back End/start
   // send url: http://localhost:5000/videos?order=desc or asc for test in postman
- 
-  db.query("SELECT * FROM youtube_videos ORDER BY rating "+ order )
-    .then((result) => res.json(result.rows))
-    .catch((error) => res.status(500).json(error));
+  pool.query("SELECT * FROM videos", (error, result) => {
+    res.json(result.rows);
+  });
+  // db.query("SELECT * FROM youtube_videos ORDER BY rating "+ order )
+  //   .then((result) => res.json(result.rows))
+  //   .catch((error) => res.status(500).json(error));
 });
 
 // for convert string to number add + before request
-app.get("/videos/:id", (req, res) => {  
-  const newId =+ req.params.id; 
+app.get("/videos/:id", (req, res) => {
+  const newId = +req.params.id;
   const oneVideo = videos.find((video) => video.id === newId);
-  if (oneVideo) { 
-    res.json(oneVideo); 
-  }   
-  res.json({"result": "not Found"});
+  if (oneVideo) {
+    res.json(oneVideo);
+  }
+  res.json({ result: "not Found" });
 });
 
-app.post("/videos", (req, res) => {
-  // everything from users send in body
-  let data = req.body; 
-
-  // new data and get time methods are js inbuild function which are use to create a new id in this project
-  data.id = new Date().getTime();
- 
-  const valid = data.url !== "" && data.title !== "";
-
-  if (valid) {
-    videos.push(data);
-    res.json({ id: data.id });
+// insert into the database
+app.post("/videos", function (req, res) {
+  const { title, url } = req.body;
+  if (title && url) {
+    pool.query(
+      "INSERT INTO videos (title, url, rating) VALUES ($1, $2, $3)",
+      [title, url, 0],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+      }
+    );
   } else {
     res.status(400).json({
       result: "failure",
@@ -60,20 +62,16 @@ app.post("/videos", (req, res) => {
     });
   }
 });
-
+// delete from database
 app.delete("/videos/:id", (req, res) => {
-  const newId =+ req.params.id;
-  const index = videos.findIndex((video) => video.id === newId);
-  // -1 is not exist result from findindex in array.
-  if (index === -1)
-    return res.status(404).json({
-      result: "failure",
-      message: "Video could not be deleted",
-    });
-  videos.splice(index, 1);
-  res.json({});
+  const newId = +req.params.id;
+  pool.query("DELETE FROM videos WHERE id = $1", [newId], (error, results) => {
+    if (error) {
+      res.status(404).json({
+        result: "failure",
+        message: "Video could not be deleted",
+      });
+      throw error;
+    }
+  });
 });
-// level 200-end
-
-
-
