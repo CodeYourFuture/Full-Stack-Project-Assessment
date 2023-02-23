db670000e7b41837e32005bb29499481cf4a185e
clav00cl <clav00cl@hotmail.com>
2023-02-23 16:45:00 +0000

    Action 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,12 @@
-import "./App.css";
-import React, {useState} from "react";
-import Header from './components/Header';
-import Search from './components/Search';
-import Videos from './components/Videos';
-import data from "./exampleresponse.json";
-import AddVideo from "./components/AddVideo";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
 
-const allData = data;
-
-function App() {
-
-  const [data , setData]= useState(allData)
-
-  console.log(data,'<<<<<<<<<<<<<<<<')
-  
-  return (
-    <div className="App">
-      <Header/>
-      <div className="inputData">
-        <AddVideo data={data} setData={setData} />
-        <Search/>
-        <Videos data={data} setData={setData} /> 
-      </div>
-    </div>
-  );
-}
-export default App;
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
