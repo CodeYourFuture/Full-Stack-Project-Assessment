c05f7cc686b38f616b4a4e923acae475f21701d7
migue <miguelanjo2@hotmail.com>
2023-02-23 13:17:52 +0000

    add video list

--- client/src/components/VideoList.jsx
+++ client/src/components/VideoList.jsx
@@ -6,10 +6,10 @@ const VideoList = () => {
 
   return (
     <div>
-     {videos.map(video => <div><h2>{video.title}
-     </h2>
-     <iframe width="560" height="315" src={video.url.replace("watch?v=", "embed/")} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div> )}
-     
+      {videos.map(video => <div><h4>{video.title}</h4>
+        <iframe width="560" height="315" src={video.url.replace("watch?v=", "embed/")} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>)}
+      
+
     </div>
   )
 }
