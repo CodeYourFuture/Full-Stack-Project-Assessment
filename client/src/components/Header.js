c65691d5bbb878c460df6065f877aef851dc841c
sasarkarimi <sahar.karimi90@outlook.com>
2023-02-23 16:21:45 +0000

    remove node_mudulse

---
+++ client/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className="App-header">
+        <h1>Video Recommendation</h1>
+    </div>
+  );
+}
