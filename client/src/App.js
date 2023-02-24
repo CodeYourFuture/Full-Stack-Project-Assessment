f366a5c925f17f28a726a92b47cbd315a977b25e
mhallaq <malhallaq@gmail.com>
2023-02-24 12:33:44 +0000

    adding sorting feature on the NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,10 @@ const App = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [error, setError] = useState(null);
   const [originalVideos, setOriginalVideos] = useState([]);
-
+  const [sortVideos, setSortVideos] = useState({
+    sortBy: "rating",
+    sortDirection: "desc",
+  });
   // Helper function to get the Video Url Link
   const getVideoUrl = function (video) {
     const regex = /v=([^&]*)/;
@@ -123,6 +126,30 @@ const App = () => {
     }
   };
 
+  const handleSort = (sortBy) => {
+    const sortedVideos = [...videos].sort((a, b) => {
+      if (sortBy === "title") {
+        if (sortVideos.sortDirection === "asc") {
+          return a.title.localeCompare(b.title);
+        } else {
+          return b.title.localeCompare(a.title);
+        }
+      } else {
+        if (sortVideos.sortDirection === "asc") {
+          return a.rating - b.rating;
+        } else {
+          return b.rating - a.rating;
+        }
+      }
+    });
+
+    setVideos(sortedVideos);
+    setSortVideos({
+      sortBy: sortBy,
+      sortDirection: sortVideos.sortDirection === "asc" ? "desc" : "asc",
+    });
+  };
+
   const onClose = () => {
     setIsOpen(false);
   };
@@ -130,7 +157,14 @@ const App = () => {
   return (
     <div className="flex flex-col items-center">
       {error && <p>Failed to load resource: net::ERR_CONNECTION_REFUSED</p>}
-      <NavBar onSearch={onSearch} onAdd={onAdd} setIsOpen={setIsOpen} />
+      <NavBar
+        onSearch={onSearch}
+        onAdd={onAdd}
+        setIsOpen={setIsOpen}
+        sortingVideos={handleSort}
+        sortVideos={sortVideos}
+        setSortVideos={setSortVideos}
+      />
       <VideoAdder
         isOpen={isOpen}
         setIsOpen={setIsOpen}
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,13 +1,52 @@
 import React, { useState } from "react";
 import { HiPlus } from "react-icons/hi";
-import { FaSearch } from "react-icons/fa";
+import {
+  FaSearch,
+  FaSortAlphaDown,
+  FaSortAlphaUp,
+  FaSortNumericDown,
+  FaSortNumericUp,
+} from "react-icons/fa";
+import { TiSortAlphabetically, TiSortNumerically } from "react-icons/ti";
 
-const NavBar = ({ onSearch, setIsOpen }) => {
+const NavBar = ({
+  onSearch,
+  setIsOpen,
+  sortVideos,
+  setSortVideos,
+  sortingVideos,
+}) => {
   const [searchTerm, setSearchTerm] = useState("");
+  // const [sortVideos, setSortVideos] = useState({
+  //   sortBy: "rating",
+  //   sortDirection: "desc",
+  // });
+  // const [sortDirection, setSortDirection] = useState("desc");
 
   const handleSearch = (event) => {
     event.preventDefault();
     onSearch(searchTerm);
+  };
+
+  const handleSort = (event) => {
+    // event.preventDefault();
+
+    const field = event.currentTarget.getAttribute("data-sort-by");
+    console.log("sort By: ", sortVideos.sortBy);
+    console.log("clicked icon: ", field);
+    console.log("sorting direction:", sortVideos.sortDirection);
+    if (sortVideos.sortBy === field) {
+      setSortVideos({
+        sortBy: field,
+        sortDirection: sortVideos.sortDirection === "asc" ? "desc" : "asc",
+      });
+      //setSortDirection(sortVideos.sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      setSortVideos({ sortBy: field, sortDirection: "desc" });
+      // setSortDirection("desc");
+    }
+    // onSort(field, sortDirection === "asc" ? "desc" : "asc");
+    sortingVideos(field);
   };
 
   return (
@@ -32,6 +71,39 @@ const NavBar = ({ onSearch, setIsOpen }) => {
             </div>
           </form>
         </div>
+
+        <button
+          className="mx-4 text-white text-2xl"
+          data-sort-by="title"
+          onClick={handleSort}
+        >
+          {sortVideos.sortBy === "title" &&
+          sortVideos.sortDirection === "asc" ? (
+            <FaSortAlphaUp />
+          ) : sortVideos.sortBy === "title" &&
+            sortVideos.sortDirection === "desc" ? (
+            <FaSortAlphaDown />
+          ) : (
+            <TiSortAlphabetically />
+          )}
+        </button>
+
+        <button
+          className="mx-4 text-gray-50 text-2xl"
+          data-sort-by="rating"
+          onClick={handleSort}
+        >
+          {sortVideos.sortBy === "rating" &&
+          sortVideos.sortDirection === "asc" ? (
+            <FaSortNumericUp />
+          ) : sortVideos.sortBy === "rating" &&
+            sortVideos.sortDirection === "desc" ? (
+            <FaSortNumericDown />
+          ) : (
+            <TiSortNumerically />
+          )}
+        </button>
+
         <div className="flex items-center flex-shrink-0 text-white">
           <button
             className="bg-transparent hover:bg-white flex items-center text-white font-semibold hover:text-blue-500 py-2 px-4 border border-white hover:border-transparent rounded h-10"
