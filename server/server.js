09ad4d7a54c867488ea06dfd84acec995c9e2068
Anosh Malik <anoshmalik.am@gmail.com>
2023-02-27 18:08:52 +0000

    Level 200 - post/get/delete functions working

--- server/server.js
+++ server/server.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const localVideosFile = require("../exampleresponse.json");
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(express.json());
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
 
 // GET "/"
-app.get("/", (req, res) => {
+app.get("/", (request, response) => {
   // Delete this line after you've confirmed your server is running
-  console.log(req.body);
-  res.send({ express: "Your Backend Service is Running" });
+  console.log(request.body);
+  response.send({ express: "Your Backend Service is Running" });
 });
 
 app.post("/", (request, response) => {
-  // Delete this line after you've confirmed your server is running
     console.log(request.body);
 
   const body = request.body;
 
-  if (request.body["title"] && request.body["url"]) {
+  if (body["title"] && body["url"]) {
+    localVideosFile.push({ id: Math.floor(Math.random()*100000), title: body["title"], url: body["url"] });
+    console.log(localVideosFile[localVideosFile.length-1]);
     response.send("Your POST was succesful!");
 
   } else { 
