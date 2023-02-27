29468d5559c58f84cf64238b6620b6e0192c46c1
khmdagal <khmdagal@gmail.com>
2023-02-27 21:45:50 +0000

    Take the submit handler function up to the app level

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import { useState, useEffect } from "react";
 function App() {
   const [showForm, setShowForm] = useState(false);
   const [videos, setVideos] = useState([]);
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
 
   useEffect(() => {
     fetch("http://127.0.0.1:5000/")
@@ -14,7 +16,60 @@ function App() {
       .then((data) => setVideos(data));
   }, []);
 
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleUrlChange = (e) => {
+    setUrl(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isYoutubeUrlValid(url)) {
+      const newVideo = {
+        id: Number(new Date().getTime()),
+        title: title,
+        url: extractVideoKey(url),
+        rating: 1,
+      };
+      fetch("http://127.0.0.1:5000/addVideo", {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newVideo),
+      })
+        .then((res) => res.json())
+        .then((data) => data);
+      setVideos([...videos, newVideo]);
+      //addVideo(newVideo);
+      // setTitle("");
+      // setUrl("");
+    } else {
+      alert("Invalid URL");
+      console.log("Invalid URL");
+    }
+  };
+
+  function isYoutubeUrlValid(url) {
+    try {
+      const { hostname } = new URL(url);
+
+      return hostname === "www.youtube.com" || hostname === "youtu.be";
+    } catch {
+      return false;
+    }
+  }
+
+  function extractVideoKey(url) {
+    const regex = /watch\?v=(\w+)/;
+    const match = url.match(regex);
+    const videoId = match[1];
+    return `https://www.youtube.com/embed/${videoId}`;
+  }
+
   const handleDeleteVideo = (key) => {
+    setVideos(videos.filter((video) => video.v_id !== key));
     fetch(`http://127.0.0.1:5000/delete/${key}`, {
       method: "delete",
       body: JSON.stringify(key),
@@ -34,7 +89,15 @@ function App() {
 
       <div className="addVideoForm">
         <AddVideoButton displayForm={displayForm} hideForm={hideForm} />
-        {showForm ? <InputForm /> : null}
+        {showForm ? (
+          <InputForm
+            title={title}
+            url={url}
+            handleTitleChange={handleTitleChange}
+            handleUrlChange={handleUrlChange}
+            handleSubmit={handleSubmit}
+          />
+        ) : null}
       </div>
 
       <VideoList handleDeleteVideo={handleDeleteVideo} videos={videos} />
--- client/src/components/InputForm.jsx
+++ client/src/components/InputForm.jsx
@@ -3,63 +3,12 @@ import { CDBBtn } from "cdbreact";
 import Form from "react-bootstrap/Form";
 import { FormGroup } from "react-bootstrap";
 
-function InputForm({ addVideo }) {
-  const [title, setTitle] = useState("");
-  const [url, setUrl] = useState("");
-
-  const handleTitleChange = (e) => { 
-      setTitle(e.target.value);
-  };
-
-  const handleUrlChange = (e) => {
-    setUrl(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (isYoutubeUrlValid(url)) {
-      const newVideo = {
-        id: Number(new Date().getTime()),
-        title: title,
-        url: extractVideoKey(url),
-        rating: 1,
-      };
-      fetch("http://127.0.0.1:5000/addVideo", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(newVideo),
-      })
-        .then((res) => res.json())
-        .then((data) => data);
-
-      addVideo(newVideo);
-      setTitle("");
-      setUrl("");
-    } else {
-      alert("Invalid URL");
-      console.log("Invalid URL");
-    }
-  };
-
-  function isYoutubeUrlValid(url) {
-    try {
-      const { hostname } = new URL(url);
-
-      return hostname === "www.youtube.com" || hostname === "youtu.be";
-    } catch {
-      return false;
-    }
-  }
-
-  function extractVideoKey(url) {
-    const regex = /watch\?v=(\w+)/;
-    const match = url.match(regex);
-    const videoId = match[1];
-    return `https://www.youtube.com/embed/${videoId}`;
-  }
-
+function InputForm({
+  url,
+ title,
+  handleTitleChange,handleUrlChange,
+  handleSubmit,
+}) {
   return (
     <Form className="form" onSubmit={handleSubmit}>
       <FormGroup>
