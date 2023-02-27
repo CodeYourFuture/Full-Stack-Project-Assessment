2260ba2c4309f7b290d2b4f39e6afbfc08bdcffe
Anosh Malik <anoshmalik.am@gmail.com>
2023-02-27 13:04:05 +0000

    First commit. post method added in server.js. Bootstrap dependencies added in frontend

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 ReactDOM.render(
   <React.StrictMode>
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.json());
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
@@ -11,5 +12,22 @@ let videos = [];
 // GET "/"
 app.get("/", (req, res) => {
   // Delete this line after you've confirmed your server is running
+  console.log(req.body);
   res.send({ express: "Your Backend Service is Running" });
+});
+
+app.post("/", (request, response) => {
+  // Delete this line after you've confirmed your server is running
+    console.log(request.body);
+
+  const body = request.body;
+
+  if (request.body["title"] && request.body["url"]) {
+    response.send("Your POST was succesful!");
+
+  } else { 
+    response.send("Data missing");
+  }
+    // response.json("body");
+
 });
