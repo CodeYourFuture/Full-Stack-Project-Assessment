68f408f7e8007b5538963c86eb0670244ef60ea9
Anosh Malik <anoshmalik.am@gmail.com>
2023-02-27 18:09:25 +0000

    recommitting

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 const localVideosFile = require("../exampleresponse.json");
 
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(express.json());
 
@@ -17,19 +16,53 @@ app.get("/", (request, response) => {
   response.send({ express: "Your Backend Service is Running" });
 });
 
-app.post("/", (request, response) => {
-    console.log(request.body);
 
+app.get("/:id", (request, response) => {
+  // Delete this line after you've confirmed your server is running
+  console.log(request.body);
+  const returnVideo = localVideosFile.filter(video => video.id == request.params.id)
+  response.json(returnVideo);
+});
+
+app.post("/", (request, response) => {
   const body = request.body;
 
   if (body["title"] && body["url"]) {
-    localVideosFile.push({ id: Math.floor(Math.random()*100000), title: body["title"], url: body["url"] });
-    console.log(localVideosFile[localVideosFile.length-1]);
-    response.send("Your POST was succesful!");
-
-  } else { 
-    response.send("Data missing");
+    localVideosFile.push({
+      id: Math.floor(Math.random() * 100000),
+      title: body["title"],
+      url: body["url"],
+    });
+    console.log(localVideosFile[localVideosFile.length - 1]);
+    response.json(localVideosFile[localVideosFile.length - 1].id);
+  } else {
+    response.json({
+      result: "failure",
+      message: "Video could not be saved",
+    });
   }
-    // response.json("body");
+  // response.json("body");
+});
 
+
+app.delete("/:id", (request, response) => {
+  // Delete this line after you've confirmed your server is running
+  console.log(request.body);
+  let videoArrayLengthBefore = localVideosFile.length;
+  const returnVideo = localVideosFile.filter((video, index) => {
+    if (video.id == request.params.id) { 
+      localVideosFile.splice(index,1);
+    }
+  }
+  );
+  if (videoArrayLengthBefore === localVideosFile.length) {
+    response.json({
+      result: "failure",
+      message: "Video could not be deleted",
+    });
+
+  } else {
+    console.log(localVideosFile);
+    response.json("DELETED");
+  }
 });
