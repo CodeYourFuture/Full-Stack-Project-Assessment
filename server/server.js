f1685b1fcf65779adb8621ba2fcff8264959c475
Anosh Malik <anoshmalik.am@gmail.com>
2023-03-01 13:18:18 +0000

    Level 300 functioning in postman. Errors in browser

--- server/server.js
+++ server/server.js
@@ -2,67 +2,123 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const localVideosFile = require("../exampleresponse.json");
+const { Pool } = require("pg");
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(express.json());
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-
-// GET "/"
-app.get("/", (request, response) => {
-  // Delete this line after you've confirmed your server is running
-  console.log(request.body);
-  response.json(localVideosFile);
+// POSTGRES CONNECTOR
+const db = new Pool({
+  user: "codeyourfuture",
+  host: "localhost",
+  database: "fullstackproject",
+  password: "cyf123",
+  port: 5432,
 });
 
+app.get("/", (request, response) => {
+  // console.log(request.body);
+  db.query("SELECT * FROM videos", (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      response.json(results.rows);
+    }
+  });
+});
 
 app.get("/:id", (request, response) => {
-  // Delete this line after you've confirmed your server is running
-  // console.log(request.body);
-  const returnVideo = localVideosFile.filter(video => video.id == request.params.id)
-  response.json(returnVideo);
+    const body = request.body;
+
+  // const returnVideo = localVideosFile.filter(video => video.id == request.params.id)
+  // response.json(returnVideo);
+  db.query(
+    "SELECT * FROM videos WHERE id=$1",
+    [request.params.id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      } else {
+        response.json(results.rows);
+      }
+    }
+  );
 });
 
 app.post("/", (request, response) => {
   const body = request.body;
 
   if (body["title"] && body["url"]) {
-    localVideosFile.push({
-      id: Math.floor(Math.random() * 100000),
-      title: body["title"],
-      url: body["url"],
-    });
-    console.log(localVideosFile[localVideosFile.length - 1]);
-    response.json(localVideosFile[localVideosFile.length - 1].id);
+    // localVideosFile.push({
+    //   id: Math.floor(Math.random() * 100000),
+    //   title: body["title"],
+    //   url: body["url"],
+    // });
+    if (!body["id"] && !body["rating"]) {
+      db.query(
+        "INSERT INTO videos (id, title, url, rating) VALUES ($1, $2, $3, $4)",
+        [Math.floor(Math.random() * 100000), body["title"], body["url"], 0],
+        (error, results) => {
+          if (error) {
+            throw error;
+          } else {
+            response.json("SUCCESS : Added a random id and rating of 0");
+          }
+        }
+      );
+    } else {
+      db.query(
+        "INSERT INTO videos (id, title, url, rating) VALUES ($1, $2, $3, $4)",
+        [body["id"], body["title"], body["url"], body["rating"]],
+        (error, results) => {
+          if (error) {
+            throw error;
+          } else {
+            response.json("SUCCESS : ALL GOOD");
+          }
+        }
+      );
+    }
+
+    // console.log(localVideosFile[localVideosFile.length - 1]);
+    // response.json(localVideosFile[localVideosFile.length - 1].id);
   } else {
     response.json({
       result: "failure",
       message: "Video could not be saved",
     });
   }
-  // response.json("body");
 });
 
-
 app.delete("/:id", (request, response) => {
-  // Delete this line after you've confirmed your server is running
   console.log(request.body);
-  let videoArrayLengthBefore = localVideosFile.length;
-  const returnVideo = localVideosFile.filter((video, index) => {
-    if (video.id == request.params.id) { 
-      localVideosFile.splice(index,1);
-    }
-  }
-  );
-  if (videoArrayLengthBefore === localVideosFile.length) {
-    response.json({
-      result: "failure",
-      message: "Video could not be deleted",
-    });
+  // let videoArrayLengthBefore = localVideosFile.length;
+  // const returnVideo = localVideosFile.filter((video, index) => {
+  //   if (video.id == request.params.id) {
+  //     localVideosFile.splice(index,1);
+  //   }
+  // }
+  // );
+  // if (videoArrayLengthBefore === localVideosFile.length) {
+  //   response.json({
+  //     result: "failure",
+  //     message: "Video could not be deleted",
+  //   });
 
-  } else {
-    console.log(localVideosFile);
-    response.json("DELETED");
-  }
+  // } else {
+  //   console.log(localVideosFile);
+  //   response.json("DELETED");
+  // }
+
+  db.query(
+    "DELETE FROM videos WHERE id=$1",
+    [request.params.id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      } else {
+        response.json(results.rows);
+      }
+    }
+  );
 });
