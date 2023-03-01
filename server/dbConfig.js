3f4c6beeb7e3d615f5776f586ed5d230b891bf9e
khmdagal <khmdagal@gmail.com>
2023-03-01 16:40:51 +0000

    Change DATABASE_URL

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-const isProduction = process.env.NODE_env === "production";
+//const isProduction = process.env.NODE_env === "production";
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
+// const pool = new Pool({
+//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+// });
+
 const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = { pool };
