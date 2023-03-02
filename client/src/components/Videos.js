9ca4d217e2efcf8c84a5a0a8976de8fd29751c50
Marryggk <99277162+Meronggk@users.noreply.github.com>
2023-03-02 10:30:56 +0000

    level 100 completed

--- client/src/components/Videos.js
+++ client/src/components/Videos.js
@@ -1,31 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Form from './Form';
+
+
 
 function Videos() {
-  const [searchTerm, setSearchTerm] = React.useState("");
-
+  const [searchTerm, setSearchTerm] = useState("");
+  
+  
   const handleChange = event => {
     setSearchTerm(event.target.value);
   };
-  
+ 
   return (
+    
     <Container>
       <Row>
         <Col>
         <a href=' '>Add Video</a>
-        <form>
-          <label>TITLE</label>
-          <input type='text'></input><br />
-          <label>URL</label>
-          <input type='text'></input>
-        </form>
-        <button className='cancel-button'>Cancel</button>
-        <button className='add-button'>Add</button>
-
+        <Form />
         </Col>
         <Col>
+        <label>Search</label>
         <input
         type="text"
         placeholder="Search"
@@ -34,7 +32,10 @@ function Videos() {
       />
         </Col>
       </Row>
+      
     </Container>
+    
+    
   );
 }
 
