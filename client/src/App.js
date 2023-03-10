a26cea68e818c625d112bf47f1ce6397aaaf89c6
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2023-03-10 22:58:33 +0200

    ZA2-Christian-Mfuke-Full-Stack-Project-Assessment
    level 100 update

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,89 @@
-import React from "react";
+// import React from "react";
+// import videos from "./exampleresponse.json";
+// import Video from "./Video";
+// import "./App.css";
+
+// const App = () => {
+//   return (
+//     <div className="main-container">
+//       <h1>video Recomendation</h1>
+
+//       <div className="video-container">
+//         {videos.map((video) => {
+//           let url = video.url.split("v=")[1];
+//           return <Video title={video.title} rating={video.rating} url={url} />;
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React, { useState } from "react";
 import videos from "./exampleresponse.json";
 import Video from "./Video";
 import "./App.css";
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [videoList, setVideoList] = useState(videos);
+
+  const handleThumbUp = (index) => {
+    const newVideos = [...videoList];
+    newVideos[index].rating += 1;
+    setVideoList(newVideos);
+  };
+
+  const handleThumbDown = (index) => {
+    const newVideos = [...videoList];
+    if (newVideos[index].rating > 0) {
+      newVideos[index].rating -= 1;
+      setVideoList(newVideos);
+    }
+  };
+
+  const handleDelete = (index) => {
+    const newVideos = [...videoList];
+    newVideos.splice(index, 1);
+    setVideoList(newVideos);
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredVideos = videoList.filter((video) =>
+    video.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="main-container">
-      <h1>video Recomendation</h1>
+      <h1>Video Recommendations</h1>
+
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search for a video..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
 
       <div className="video-container">
-        {videos.map((video) => {
+        {filteredVideos.map((video, index) => {
           let url = video.url.split("v=")[1];
-          return <Video title={video.title} rating={video.rating} url={url} />;
+          return (
+            <Video
+              key={video.id}
+              title={video.title}
+              rating={video.rating}
+              url={url}
+              onThumbUp={() => handleThumbUp(index)}
+              onThumbDown={() => handleThumbDown(index)}
+              onDelete={() => handleDelete(index)}
+            />
+          );
         })}
       </div>
     </div>
--- client/src/Video.jsx
+++ client/src/Video.jsx
@@ -1,25 +1,53 @@
+// import React from "react";
+
+// const Video = ({ title, rating, url }) => {
+//   return (
+//     <div className="video">
+//       <h5>{title}</h5>
+//       <div>
+//         <span>&#x1F44D;</span>
+//         {rating}
+//         <span>&#x1F44E;</span>
+//       </div>
+//       <iframe
+//         width="300"
+//         height="205"
+//         src={`https://www.youtube.com/embed/${url}`}
+//         title="YouTube video player"
+//         frameborder="0"
+//         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+//         allowfullscreen
+//       ></iframe>
+//       <br />
+//       <button>Delete</button>
+//     </div>
+//   );
+// };
+
+// export default Video;
+
 import React from "react";
 
-const Video = ({ title, rating, url }) => {
+const Video = ({ title, rating, url, onThumbUp, onThumbDown, onDelete }) => {
   return (
     <div className="video">
       <h5>{title}</h5>
       <div>
-        <span>&#x1F44D;</span>
+        <span onClick={onThumbUp}>&#x1F44D;</span>
         {rating}
-        <span>&#x1F44E;</span>
+        <span onClick={onThumbDown}>&#x1F44E;</span>
       </div>
       <iframe
         width="300"
         height="205"
         src={`https://www.youtube.com/embed/${url}`}
         title="YouTube video player"
-        frameborder="0"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        allowfullscreen
+        allowFullScreen
       ></iframe>
       <br />
-      <button>Delete</button>
+      <button onClick={onDelete}>Delete</button>
     </div>
   );
 };
