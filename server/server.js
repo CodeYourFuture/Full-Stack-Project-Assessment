e347ca03bdfca253a645e28d7b7f5129132f8174
mfuke christian kambulu <60177462+mfukechristian@users.noreply.github.com>
2023-03-16 22:00:24 +0200

    ZA2-Christian-Mfuke-Full-Stack-Project-Assessment
    level 300

--- server/server.js
+++ server/server.js
@@ -3,8 +3,77 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const videos = require("./exampleresponse.json");
+const { Pool } = require("pg");
+const config = require("./db.js");
+const pool = new Pool(config);
+
+// Test the connection
+pool.query("SELECT NOW()", (err, res) => {
+  console.log(err, res);
+  pool.end();
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+//DATABASE CODE level 300
+app.get("/videos", (req, res) => {
+  pool.query("SELECT * FROM videos", (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error getting videos from database");
+    } else {
+      res.send(result.rows);
+    }
+  });
+});
+app.post("/videos", (req, res) => {
+  const { title, url, rating } = req.body;
+  pool.query(
+    "INSERT INTO videos (title, url, rating) VALUES ($1, $2, $3) RETURNING *",
+    [title, url, rating],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error adding video to database");
+      } else {
+        res.status(201).send(result.rows[0]);
+      }
+    }
+  );
+});
+app.put("/videos/:id", (req, res) => {
+  const id = req.params.id;
+  const { title, url, rating } = req.body;
+  pool.query(
+    "UPDATE videos SET title = $1, url = $2, rating = $3 WHERE id = $4 RETURNING *",
+    [title, url, rating, id],
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error updating video in database");
+      } else if (result.rowCount === 0) {
+        res.status(404).send("Video not found");
+      } else {
+        res.send(result.rows[0]);
+      }
+    }
+  );
+});
+
+//SERVER CODE level 250
+app.delete("/videos/:id", (req, res) => {
+  const id = req.params.id;
+  pool.query("DELETE FROM videos WHERE id = $1", [id], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error deleting video from database");
+    } else if (result.rowCount === 0) {
+      res.status(404).send("Video not found");
+    } else {
+      res.send("Video deleted");
+    }
+  });
+});
 
 // GET "/"
 app.get("/", (req, res) => {
