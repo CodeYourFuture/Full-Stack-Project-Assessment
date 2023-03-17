65fcff41d703de666e13289aa41d83d9017f073b
Patou Malongi Mabando <patoumabando@gmail.com>
2023-03-18 01:03:46 +0200

    Za2-Patou-Mabando-Full-Stack-project-Assesssment-Week2

---
+++ client/src/components/AddVideo.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react'
+import VideoList from './VideoList';
+const AddVideo = () => {
+  const [title, setTitle]= useState("");
+  const [url, setUrl] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="title">Title</label>
+          <input type="text" id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
+
+          <label htmlFor="url">Url</label>
+          <input type="text" id='url' value={url} onChange={(e) => setUrl(e.target.value)}/>
+
+          <button className='add-video' onClick={handleSubmit}>Add Video</button>
+
+        </form>
+        <VideoList/>
+      </div>
+
+  )
+}
+
+export default AddVideo
---
+++ client/src/components/VideoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import videos from "../data/exampleresponse.json"
+const VideoList = () => {
+
+  return (
+    <div>
+      {videos.map(video => <div><h4>{video.title}</h4>
+        <iframe width="560" height="315" src={video.url.replace("watch?v=", "embed/")} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>)}
+      
+
+    </div>
+  )
+}   
+  
+export default VideoList
