4e01f4a0562fe9b4d61d90302259a33406aee229
TresorBipa <tresorbipa3@gmail.com>
2023-03-17 13:28:55 +0200

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,81 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import AddVideo from "./AddVideo";
+import AddVideoForm from "./AddVideoForm";
+import Search from "./Search";
+import Video from "./Video";
 
 function App() {
+  const [addVideo, setAddVideo] = useState(false);
+  const [videoData, setVideoData] = useState([]);
+  const [videosFilter, setVideosFilter] = useState("");
+  const [filteredVideoData, setFilteredVideoData] = useState([]);
+
+
+  const addVideoHandler = () => {
+    setAddVideo((prevVideo) => !prevVideo);
+  };
+
+
+  useEffect(() => {
+  fetch("http://localhost:5000/video/")
+    .then((res) => res.json())
+    .then((data) => {
+      setVideoData(data);
+      setFilteredVideoData(data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}, []);
+
+useEffect(() => {
+  setFilteredVideoData(
+    videoData?.filter((video) =>
+      video.title.toLowerCase().includes(videosFilter.toLowerCase())
+    ) || []
+  );
+}, [videosFilter, videoData]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
+    <div className="App bg-secondary.bg-gradient">
+      <header className="shadow-sm p-4 mb-1 bg-success alert-dark">
+        <h1 className="text-light active fst-italic">Video Recommendation</h1>
       </header>
+      <Container className=" mt-3">
+        <Row>
+          <Col md>
+            <AddVideo onClick={addVideoHandler}>Add Video</AddVideo>
+            {addVideo && <AddVideoForm setVideoData={setVideoData} />}
+             <AddVideoForm videoData={videoData} setVideoData={setVideoData} />
+          </Col>
+          <Col md>
+            <Search handler={(e) => setVideosFilter(e.target.value)} />
+          </Col>
+        </Row>
+      </Container>
+      <div>
+        <Row xs={1} sm={2} md={3} className="mt-3">
+          {filteredVideoData.map((video, key) => (
+            <Video
+              video={video}
+              key={key}
+              data={videoData}
+              setData={setVideoData}
+            />
+          ))}
+        </Row>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
--- server/server.js
+++ server/server.js
@@ -1,15 +1,113 @@
 const express = require("express");
 const app = express();
+const { Pool } = require("pg");
+const cors = require("cors");
+
+require('dotenv').config();   // Dotenv to load environment variables
+
+app.use(express.json());
+app.use(cors()); 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// hard-coded values with the variables from your .env file
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+// Get all videos endpoint
+app.get("/video", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM video");
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
-// GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+// Search videos endpoint
+app.get("/video/search", async (req, res) => {
+  const videoSearch = req.query.term;
+  try {
+    const result = await pool.query(
+      "SELECT * FROM video WHERE title LIKE $1",
+      [`%${videoSearch}%`]
+    );
+    res.json(result.rows);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Create video endpoint
+app.post("/video", async (req, res) => {
+  const { title, url } = req.body;
+
+  try {
+    const result = await pool.query("SELECT * FROM video WHERE title = $1", [
+      title,
+    ]);
+    if (result.rows.length > 0) {
+      return res.status(400).json({ error: "Video already exists" });
+    }
+    // Check if video already exists by URL
+    const urlResult = await pool.query("SELECT * FROM video WHERE url = $1", [
+      url,
+    ]);
+    if (urlResult.rows.length > 0) {
+      return res.status(400).json({ error: "Video already exists" });
+    }
+    await pool.query("INSERT INTO video (title, url) VALUES ($1, $2)", [
+      title,
+      url,
+    ]);
+    res.json({ message: "Video created" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Get video by id endpoint
+app.get("/video/:id", async (req, res) => {
+  const videoId = req.params.id;
+  try {
+    const result = await pool.query("SELECT * FROM video WHERE id=$1", [
+      videoId,
+    ]);
+    if (result.rows.length === 0) {
+      return res.status(404).send("Video not found!");
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// DELETE //
+app.delete("/video/:id", async (req, res) => {
+  const vidId = req.params.id;
+  try {
+    const result = await pool.query("DELETE FROM video WHERE id=$1", [vidId]);
+    if (result.rowCount === 0) {
+      return res.status(404).send(`Video with id ${vidId} not found`);
+    }
+    res.send(`Video ${vidId} deleted!`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json(error);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is Listening in http://localhost:${port}`);
 });
