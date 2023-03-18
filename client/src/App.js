687fe8021764f2c315252b1a17dc447536e120b4
Patou Malongi Mabando <patoumabando@gmail.com>
2023-03-18 02:47:36 +0200

    Za2-Patou-Mabando-Full-Stack-project-Assesssment-Week2

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,36 @@
-import "./App.css";
-import AddVideo from "./components/AddVideo";
+// import "./App.css";
+// import AddVideo from "./components/AddVideo";
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <h1>Video Recommendation</h1>
+//       </header>
+//       { <AddVideo/> }
+//     </div>
+//   );
+// }
+// export default App;
+
+import React from "react";
+import videos from "./exampleresponse.json";
+import Video from "./Video";
+import "./App.css";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
-      { <AddVideo/> }
+    <div className="main-container">
+      <h1>video Recomendation</h1>
+
+      <div className="video-container">
+        {videos.map((video) => {
+          let url = video.url.split("v=")[1];
+          return <Video title={video.title} rating={video.rating} url={url} />;
+        })}
+      </div>
     </div>
   );
-}
+};
+
 export default App;
