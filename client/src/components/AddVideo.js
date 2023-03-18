2ee799809bbb5640a2ef0a3dddc16cad2b5335bf
Patou Malongi Mabando <patoumabando@gmail.com>
2023-03-18 02:07:33 +0200

    Za2-Patou-Mabando-Full-Stack-project-Assesssment-Week2

--- client/src/components/AddVideo.js
+++ client/src/components/AddVideo.js
@@ -1,30 +1,49 @@
-import React,{useState} from 'react'
-import VideoList from './VideoList';
+import React, { useState } from "react";
 
 const AddVideo = () => {
-  const [title, setTitle]= useState("");
+  const [title, setTitle] = useState("");
+
   const [url, setUrl] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-  }
+
+    if (title === "") {
+      alert("please fill out this field");
+    }
+  };
 
   return (
     <div>
       <div>
         <form onSubmit={handleSubmit}>
           <label htmlFor="title">Add Video Title</label>
-          <input type="text" id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
-          <label htmlFor="url">Add Video Url</label>
-          <input type="text" id='url' value={url} onChange={(e) => setUrl(e.target.value)}/>
-          <button className='add-video' onClick={handleSubmit}>Add Video</button>
-          <button className='cancel-video'  >Cancel</button>
 
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+          <label htmlFor="url">Add Video Url</label>
+
+          <input
+            type="text"
+            id="url"
+            value={url}
+            onChange={(e) => setUrl(e.target.value)}
+          />
+
+          <button className="add-video" onClick={handleSubmit}>
+            Add Video
+          </button>
+
+          <button className="cancel-video">Cancel</button>
         </form>
-        <VideoList/>
       </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default AddVideo
+export default AddVideo;
