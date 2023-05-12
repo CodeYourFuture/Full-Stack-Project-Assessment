6766b85ab56b1f8c9bfe17a63b516f27fa981887
Lorena <lorenacapraru@gmail.com>
2023-05-12 12:20:07 +0100

    header and cards container

--- client/src/VideosCards.jsx
+++ client/src/VideosCards.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import data from "./exampleresponse.json";
+import "./VideosCards.css";
 
 export const VideosCards = () => {
   const Card = (v) => {
     return (
       <div className="cardContainer" key={v.id}>
         <iframe
+          height="240"
           src={v.url.replace("watch?v=", "embed/")}
           title={v.title}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
         ></iframe>
-
-        <h1>{v.title}</h1>
+        <h6>{v.title}</h6>
         <p>{v.rating}</p>
       </div>
     );
