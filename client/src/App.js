a20e6f8fdc44bcd057bd89abccf349c22cc9eb06
Karleen Richards <karleenmsrichards@gmail.com>
2023-05-12 22:47:28 +0100

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
