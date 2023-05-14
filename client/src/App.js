4003f3afe828e070a2bca22cb6d162bc91ede4cb
nataliizab <nataliia.zab@gmail.com>
2023-05-14 16:12:50 +0100

    added Tailwind CSS

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
