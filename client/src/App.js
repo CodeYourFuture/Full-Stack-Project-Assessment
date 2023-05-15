1dfe7955d42206138b37a819d0ff64d0c712b933
Hengameh M.Khani <afsooneday@gmail.com>
2023-05-15 22:46:35 +0100

    bootstrap added

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,81 @@
-import AddVideo from "./AddVideo";
 import "./App.css";
 import Video from "./Video";
-import data from "./exampleresponse.json"
+import data from "../src/exampleresponse.json"
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { useState } from "react";
 
 function App() {
+  const [isOpen , setIsOpen]= useState(false)
+
+  const clickHandler =() => {
+    setIsOpen (true)
+  }
   return (
     <div className="App">
       <header className="App-header">
-
-
         <h1>Video Recommendation</h1>
-
-
-
       </header>
-      <AddVideo />
+      <Container>
+      <Row>
+        <Col><Button variant="link" onClick={clickHandler}> Add Video </Button>
+        { isOpen && (
+          <>
+
+        <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">Title</InputGroup.Text>
+        <Form.Control
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+      <InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">URL</InputGroup.Text>
+        <Form.Control
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+      <br></br>
+      <Button variant="Cancle">Warning</Button>{' '}
+      <Button variant="Add">Danger</Button>{' '}
+
+        </>)}      
 
 
+</Col>
+        <Col><InputGroup className="mb-3">
+        <InputGroup.Text id="basic-addon1">Search</InputGroup.Text>
+        <Form.Control
+          placeholder="Username"
+          aria-label="Username"
+          aria-describedby="basic-addon1"
+        />
+      </InputGroup>
+      </Col>
+      </Row>
+      
+      </Container>
+      <div className="video-container">
 
       {data.map(item =>(
-        <Video info = {item}>
+              <Video info = {item}>
+              </Video>
+            ))}
+        </div>
+           
+     
 
-
-        </Video>
-        
-
-      ))}
       
-    </div>
 
+     
+    </div>
   );
 }
 
--- client/src/Video.jsx
+++ client/src/Video.jsx
@@ -1,39 +1,58 @@
-import React from 'react'
 
-function Video({info}) {
-  return (
-    <div>
+
+    import React from "react"
+    function Video({info}) {
+      return (
         <div>
+            <div>
+            <h1>
+            {info.title}
+            </h1>
+            <iframe src={`https://www.youtube.com/embed/${info.url}`}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            title="Embedded youtube"
+            >
+            </iframe>
+            <p>
+            {info.rating}
+            </p>
             <button>
-                like
-
+                👍
             </button>
-        <h1>
-        {info.title}
-        </h1>
-        <button>
-            dislike
-
-        </button>
-        <iframe src={`https://www.youtube.com/embed/${info.url}`}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="Embedded youtube"
-        
-        >
-
-        </iframe>
-        <p>
-        {info.rating}
-        </p>
-       <button>
-        Delete
-       </button>
-
+            <button>
+               👎
+            </button>
+            <button>
+                Delete
+            </button>
+            </div>
         </div>
-       
-    </div>
-  )
-}
+      )
+    }
+    export default Video
 
-export default Video
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
