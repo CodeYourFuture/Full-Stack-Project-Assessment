8f0eca830084c325df60e34e1086ca07586ac162
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-05-15 23:57:42 +0100

    start

---
+++ client/src/Video.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Video() {
+  return (
+    <>
+      <div>Video</div>
+      <button>Up vote</button>
+      <button>Down vote</button>
+    </>
+  );
+}
+
+export default Video;
