ca76d2c0a1fa12518c5bcfc7ca229a64f1d758cd
Andriana <andrianasaffo@gmail.com>
2023-05-15 20:25:26 +0100

    Server side level 200 complete

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,27 @@ import VideoInfo from "./VideoInfo";
 function App() {
   const [videos, setVideos] = useState(initialVideos);
 
+  const [toggleArea, setToggleArea] = useState(false);
+
+  const toggleShow = () => setToggleArea((s) => !s);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
       <AddVideos setVideos={setVideos} />
-      {videos.map((video) => (
-        <VideoInfo
-          key={video.id}
-          video={video}
-          videos={videos}
-          setVideos={setVideos}
-          initialVideos={initialVideos}
-        />
-      ))}
+      <button onClick={toggleShow}>Show Videos</button>
+      {toggleArea &&
+        videos.map((video) => (
+          <VideoInfo
+            key={video.id}
+            video={video}
+            videos={videos}
+            setVideos={setVideos}
+            initialVideos={initialVideos}
+          />
+        ))}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,68 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const cors = require("cors");
+const port = process.env.PORT || 3005;
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+let videos = require("../exampleresponse.json");
+
+// GET ALL VIDEOS
+app.get("/", (req, res) => {
+  res.json(videos);
+});
+
+// POST VIDEO
+app.post("/", (req, res) => {
+  const { title, url } = req.body;
+  let randomID = Math.floor(100000 + Math.random() * 900000);
+  let randomRating = Math.floor(100 + Math.random() * 900);
+
+  const newVideo = {
+    id: randomID,
+    title: title,
+    url: url,
+    rating: randomRating,
+  };
+
+  if (!newVideo.title || !newVideo.url) {
+    return res
+      .status(400)
+      .json({ result: "failure", message: "Video could not be saved" });
+  }
+
+  videos.push(newVideo);
+  res.status(200).json({ message: "New video added", videos });
+});
+
+// GET BY ID
+app.get("/:id", (req, res) => {
+  const foundVideo = videos.find(
+    (video) => video.id === parseInt(req.params.id)
+  );
+
+  foundVideo
+    ? res.json(foundVideo)
+    : res.status(400).json({ message: `Video ${req.params.id} not found` });
+});
+
+// DELETE BY ID
+app.delete("/:id", (req, res) => {
+  const foundVideo = videos.find(
+    (video) => video.id === parseInt(req.params.id)
+  );
+
+  if (foundVideo) {
+    videos = videos.filter((video) => video.id !== parseInt(req.params.id));
+    res.json({ message: `Video ${req.params.id} deleted` });
+  } else {
+    res
+      .status(400)
+      .json({ result: "failure", message: "Video could not be found" });
+  }
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
-
-// GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
-});
