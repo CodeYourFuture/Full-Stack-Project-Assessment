ac62b4114ef084a1589c71bd7bd9bb7dcfe0ac74
Bahare <itxbb09@gmail.com>
2023-05-16 00:26:28 +0100

    Rating.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import VCard from "./components/VCard";
 import videos from "./exampleresponse.json";
+import AddVideo from "./components/AddVideo";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <AddVideo videoData={videos} />
       <VCard data={videos} />
     </div>
   );
--- client/src/components/VCard.js
+++ client/src/components/VCard.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Rating from "./Rating";
 import "./VCard.css";
 const VCard = ({ data }) => {
+  const [list, setList] = useState(data);
+
+  const handleDelete = (id) => {
+    setList(list.filter((video) => video.id !== id));
+  };
+
   return (
     <div>
-      {data.map((video, index) => (
+      {list.map((video, index) => (
         <div key={index} className="video-card">
           <h3>{video.title}</h3>
           <iframe
             src={video.url.replace("watch?v=", "embed/")}
             title={video.title}
           ></iframe>
-          <p>Votes: {video.rating}</p>
-          <button>Remove</button>
+
+          <Rating rating={video.rating} />
+          <button onClick={() => handleDelete(video.id)}>Remove</button>
         </div>
       ))}
     </div>
