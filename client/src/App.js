e6b67e60d245001e129bc29c3b59e52b2886bd7b
elahemortazavi <emor5680@gmail.com>
2023-05-16 08:58:00 +0100

    level 100-step1

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import "./App.css";
+import React, { useState } from "react";
+import videosData from "./exampleresponse.json";
+import VideoList from "./component/VideoList";
 
-function App() {
+
+const App = () => {
+  const [videos, setVideos] = useState(videosData);
+  const handleRemove = (id) => {
+    setVideos(videos.filter((video) => video.id !== id));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
-      </header>
+    <div className="app">
+      <h1>Video List</h1>
+      <VideoList videos={videos} onRemove={handleRemove} />
     </div>
   );
-}
-
+};
 export default App;
