3ba99f19e78d7a6886982f51c7b63a57c391c4d2
elahemortazavi <emor5680@gmail.com>
2023-05-18 00:49:33 +0100

    app.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import videosData from "./exampleresponse.json";
 import VideoList from "./component/VideoList";
+import AddVideoForm from "./component/AddVideoForm";
+
+import "./App.css";
 
 
 const App = () => {
@@ -8,10 +11,27 @@ const App = () => {
   const handleRemove = (id) => {
     setVideos(videos.filter((video) => video.id !== id));
   };
+
+  const handleVote = (id, increment) => {
+    setVideos((prevVideos) =>
+      prevVideos.map((video) =>
+        video.id === id ? { ...video, rating: video.rating + increment } : video
+      )
+    );
+  };
+  const handleAddVideo = (newVideo) => {
+    setVideos((prevVideos) => [...prevVideos, newVideo]);
+  };
+
   return (
+    // <div className="app">
+    //   <h1>Video List</h1>
+    //   <VideoList videos={videos} onRemove={handleRemove} />
+    // </div>
     <div className="app">
-      <h1>Video List</h1>
-      <VideoList videos={videos} onRemove={handleRemove} />
+      <h1 className="app-title">Video List</h1>
+      <AddVideoForm onAddVideo={handleAddVideo} />
+      <VideoList videos={videos} onRemove={handleRemove} onVote={handleVote} />
     </div>
   );
 };
