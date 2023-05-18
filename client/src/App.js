8cec29b1d9970383a27860c62e0d56fbc7daf180
Saghar Hosseinmardi <saghar.h.mardi@gmail.com>
2023-05-18 11:38:02 +0100

    level 100 & 199 done!

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
+import React from "react";
+
 import "./App.css";
+import VideoList from "./VideoList";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
+       <VideoList />
       </header>
     </div>
   );
