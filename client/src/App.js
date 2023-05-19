85af7e45a461853b964d35ae7389acfc5556ebec
tony-dev-wm <ta.devopsuk@gmail.com>
2023-05-19 14:42:45 +0100

    lvl 100 ongoing

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,55 @@
+import React, {useState} from 'react';
+import videoData from '../../exampleresponse.json';
+import NewVideoForm from './NewVideoForm';
 import "./App.css";
 
 function App() {
+  const [videos, setVideos] = useState(videoData);
+
+  function newVideo(video) {
+    const newAddedVideos = videos.concat(video);
+    setVideos(newAddedVideos);
+  }
+
+  function voteDown(videoId) {
+    const updatedVideos = videos.map((video) => {
+      if (video.id === videoId) {
+        return {
+          ...video, rating:video.rating - 1,
+        };
+      }
+      return video;
+    });
+    setVideos(updatedVideos);
+  }
+
+  function voteUp(videoId) {
+    const updatedVideos = videos.map((video) => {
+      if (video.id === videoId) {
+        return {
+          ...video, rating:video.rating + 1,
+        };
+      }
+      return video;
+    });
+    setVideos(updatedVideos);
+  }
+  
+  function deleteVideo(videoId) {
+    const updatedVideos = video.filter((video) => video.id !== videoId);
+    setVideos(updatedVideos);
+  }
+
+  const sortedVideos = videos.concat().sort((b,a) => {
+    if (a.rating > b.rating) {
+      return 1;
+    } else if (a.rating === b.rating) {
+      return 0;
+    } else if (a.rating < b.rating) {
+      return -1;
+    }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
---
+++ client/src/NewVideoForm.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+
+function NewVideoForm ({newVideoAdded}) {
+    const [title, setTitle] = useState("");
+    const [url, setUrl]= useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!title || !url) return;
+    const newVideo= {
+        id: Math.floor(Math.random()*1000000),
+        title: title,
+        url: url,
+        rating: 0,
+    };
+    newVideoAdded(newVideo);
+    setTitle("");
+    setUrl ("");
+  }
+  
+  return (
+    <form onSubmit={handleSubmit}>
+        <label>
+            Title:
+            <input type='text' value={title} onChange={(event)=> setTitle(event.target.value)} />
+        </label>
+        <label>
+            URL:
+            <input  type='text' value={url} onChange={(event)=> setUrl(event.target.value)} />
+        </label>
+        
+        <div className ='submit'>
+            <button type='submit'>
+                Add Video
+            </button>
+        </div>    
+        
+    </form>
+  );
+}
+
+export default NewVideoForm;
