720fd099f7f47980d1683ffda18791e78066d214
Lorena <lorenacapraru@gmail.com>
2023-05-26 14:53:41 +0100

    no changes

--- server/server.js
+++ server/server.js
@@ -3,21 +3,13 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const { v4: uuidv4 } = require("uuid");
-const { Pool } = require("pg");
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 app.use(cors());
 app.use(express.json());
 
 let videos = require("../exampleresponse.json");
-
-const pool = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+// let allVideos = [videos];
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
