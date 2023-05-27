3d3f110362b48361dc808d0359007f3a17152fd4
Lorena <lorenacapraru@gmail.com>
2023-05-27 18:57:56 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,11 +12,10 @@ app.use(bodyParser.json());
 const { Pool } = require("pg");
 
 const db = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 app.use((req, res, next) => {
