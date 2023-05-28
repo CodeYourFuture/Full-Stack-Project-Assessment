4c569dfb658a223bd4abed795ddcb07bdb459615
Lorena <lorenacapraru@gmail.com>
2023-05-28 11:38:11 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv"); // Module for loading environment variables from a .env file
+const dotenv = require("dotenv");
 
 dotenv.config();
 app.listen(port, () => console.log(`Listening on port ${port}`));
