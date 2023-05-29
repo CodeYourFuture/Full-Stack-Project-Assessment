ec1fcfa297b3b4a5ee732bb3203c2e4dfd8bb9e7
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-05-29 23:32:14 +0100

    Run the server

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(cors());
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
 let videos = [];
 
 // GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
-});
+// app.get("/", (req, res) => {
+// Delete this line after you've confirmed your server is running
+// res.send({ express: "Your Backend Service is Running" });
+// });
+app.listen(port, () => console.log(`Listening on port ${port}`));
