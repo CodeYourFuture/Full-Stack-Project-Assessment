7fcb9794cf796202689d6f76de03f7f768588b67
MacOS <macos@blessing.local>
2023-05-29 22:15:49 +0100

    feat: added buttons

--- client/src/VideoCards.js
+++ client/src/VideoCards.js
@@ -2,6 +2,15 @@
 import { useState } from "react";
 
 function VideoCards({video, videos, setVideos}) {
+  const handleUp = () => {
+
+  }
+  const handleDown = () => {
+
+  }
+  const handleDelete = () => {
+
+  }
 
   return (
     <>
@@ -12,10 +21,16 @@ function VideoCards({video, videos, setVideos}) {
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
         ></iframe>
         <div className="card-body">
-          <h5 className="card-title">Title: {video.title}</h5>
+          <h5 className="card-title">{video.title}</h5>
           <p className="card-text"> Rating: {video.rating}</p>
         </div>
+        <div className="button-group">
+        <button type="button" class="btn btn-success" onClick={handleUp}>Up</button>
+        <button type="button" class="btn btn-warning" onClick={handleDown}>Down</button>
+        <button type="button" class="btn btn-danger" onClick={handleDelete}>Delete</button>
       </div>
+      </div>
+
     </>
   );
 }
