801ed6e86ce194bc4244f45fc1b8daa23df31ccd
Oleh <Oleh@progressive-id.co.uk>
2023-05-30 15:20:46 +0100

    feat: 100 adding new videos to the current list

--- client/src/App.js
+++ client/src/App.js
@@ -8,21 +8,20 @@ function App() {
   const [videos, setVideos] = useState(listOfVideos);
   
   const handleListOfVideos = (id) => {
-     const index = videos.findIndex((video) => video.id == id)
+     const index = videos.findIndex((video) => video.id === id)
      if (index !== -1) {
-      // Create a new array without the deleted card
       const updatedVideos = [...videos];
       updatedVideos.splice(index, 1);
       setVideos(updatedVideos);
      }
   }
-
+  
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
-      <Header setVideos={setVideos} />
+      <Header videos={videos} setVideos={setVideos} />
       <div className="cards">
         {videos.map((video) => (
           <VideoCards
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,15 +1,58 @@
+import { useState } from "react";
 
-function Header () {
-    return (
-        <form >
-        <label>
-          <input type="text" name="title" placeholder="Title"/>
-          <input type="text" name="link" placeholder="Link"/>
-        </label>
-        <input type="submit" value="Add" />
-      </form>
-    )
+const defaultState = {
+  id: 0,
+  title: "",
+  url: "",
+  rating: 0,
+  videoID: ""
 }
 
-export default Header
+function Header(props) {
 
+  const [formData, setFormData] = useState(defaultState);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.setVideos([...props.videos, {...formData, 
+    id: props.videos.length +1,
+    videoID: formData.url.split("v=")[1]}])
+    console.log(formData)
+    setFormData(defaultState)
+  }
+
+  function handleChange (event){
+    event.preventDefault();
+    const newState = {...formData}
+    newState[event.target.name] = event.target.value 
+    setFormData(newState)
+  }
+
+//  const videoID = link.split("v=")[1];
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={formData.title}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="url"
+          placeholder="Link"
+          value={formData.url}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <button>ADD</button>
+    </form>
+  );
+}
+
+export default Header;
