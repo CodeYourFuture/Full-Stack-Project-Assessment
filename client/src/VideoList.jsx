c93082fb39e8d15362e2352c0b5301fb9a353991
Meysam-Arshadi <meysamarshadi@gmail.com>
2023-06-01 20:24:22 +0100

    Level 300 Done

--- client/src/VideoList.jsx
+++ client/src/VideoList.jsx
@@ -1,106 +1,118 @@
-import React, { useState, useEffect } from "react";
-import "./VideoList.css";
-import Input from "./Input";
+import React, { useState, useEffect } from 'react'
+import './VideoList.css'
+import Input from './Input'
 
 const VideoList = () => {
-  const [videos, setVideos] = useState([]);
+	const [videos, setVideos] = useState([])
 
-  useEffect(() => {
-    fetch(
-      `https://full-stack-project-assessment-server-kus1.onrender.com/videos`
-    )
-      .then((res) => res.json())
-      .then((data) => setVideos(data))
-      .catch((error) => console.log(error));
-  }, []);
+	useEffect(() => {
+		fetch(`https://full-stack-project-assessment-server-kus1.onrender.com/videos`)
+			.then((res) => res.json())
+			.then((data) => setVideos(data))
+			.catch((error) => console.log(error))
+	}, [])
 
-  const handleVoteUp = (id) => {
-    const updateVideos = videos.map((video) => {
-      if (video.id === id) {
-        return { ...video, rating: video.rating + 1 };
-      }
-      return video;
-    });
-    setVideos(updateVideos);
-  };
+	const handleVoteUp = (id) => {
+		const updateVideos = videos.map((video) => {
+			if (video.id === id) {
+				return { ...video, rating: video.rating + 1 }
+			}
+			return video
+		})
+		setVideos(updateVideos)
+		updateRating(id, updateVideos.find((video) => video.id === id).rating)
+	}
 
-  const handleVoteDown = (id) => {
-    const updateVideos = videos.map((video) => {
-      if (video.id === id) {
-        const newRating = Math.max(0, video.rating - 1);
-        // Making sure the rating doesn't go below 0
-        return { ...video, rating: newRating };
-      }
-      return video;
-    });
-    setVideos(updateVideos);
-  };
+	const handleVoteDown = (id) => {
+		const updateVideos = videos.map((video) => {
+			if (video.id === id) {
+				const newRating = Math.max(0, video.rating - 1)
+				// Making sure the rating doesn't go below 0
+				return { ...video, rating: newRating }
+			}
+			return video
+		})
+		setVideos(updateVideos)
+		updateRating(id, updateVideos.find((video) => video.id === id).rating)
+	}
 
-  const handleDelete = async (id) => {
-    const response = await fetch(
-      `https://full-stack-project-assessment-server-kus1.onrender.com/videos/${id}`,
-      {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    );
-    if (response.ok) {
-      const deletedVideoList = await response.json();
-      setVideos(deletedVideoList);
-    }
-  };
+	const handleDelete = async (id) => {
+		const response = await fetch(
+			`https://full-stack-project-assessment-server-kus1.onrender.com/videos/${id}`,
+			{
+				method: 'DELETE',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		)
+		if (response.ok) {
+			setVideos(videos.filter((video) => video.id !== id))
+		}
+	}
 
-  const handleAddVideo = async (title, url) => {
-    const response = await fetch(
-      "https://full-stack-project-assessment-server-kus1.onrender.com/videos",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ title, url }),
-      }
-    );
-    if (response.ok) {
-      const newVideo = await response.json();
-      setVideos([...videos, newVideo]);
-    } else {
-      // Handle error
-    }
-  };
+	const handleAddVideo = async (title, url) => {
+		const response = await fetch(
+			'https://full-stack-project-assessment-server-kus1.onrender.com/videos',
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ title, url }),
+			}
+		)
+		if (response.ok) {
+			const newVideo = await response.json()
+			setVideos([...videos, newVideo])
+		} else {
+			console.log('Error')
+		}
+	}
 
-  return (
-    <>
-      <Input onAddVideo={handleAddVideo} />
-      <div className="box">
-        {videos.map((video) => (
-          <div className="card" key={video.id}>
-            <h2>{video.title}</h2>
-            <div>
-              <iframe
-                src={video.url}
-                title="YouTube video player"
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowFullScreen
-              ></iframe>
-            </div>
-            <p>vote: {video.rating}</p>
-            <p>Uploaded At: {video.postedAt}</p>
-            <div className="group-button">
-              <button onClick={() => handleVoteUp(video.id)}>Vote Up</button>
-              <button onClick={() => handleVoteDown(video.id)}>
-                Vote Down
-              </button>
-              <button onClick={() => handleDelete(video.id)}>Delete</button>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
-  );
-};
+	const updateRating = (id, newRating) => {
+		fetch(
+			`https://full-stack-project-assessment-server-kus1.onrender.com/videos/${id}/rating`,
+			{
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ rating: newRating }),
+			}
+		)
+			.then((res) => res.json)
+			.catch((error) => console.log(error))
+	}
 
-export default VideoList;
+	return (
+		<>
+			<Input onAddVideo={handleAddVideo} />
+			<div className='box'>
+				{videos.map((video) => (
+					<div className='card' key={video.id}>
+						<h2>{video.title}</h2>
+						<div>
+							<iframe
+								src={video.url}
+								title='YouTube video player'
+								frameBorder='0'
+								allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+								allowFullScreen
+							></iframe>
+						</div>
+						<p>vote: {video.rating}</p>
+						<p>Uploaded At: {video.postedAt}</p>
+						<div className='group-button'>
+							<button onClick={() => handleVoteUp(video.id)}>Vote Up</button>
+							<button onClick={() => handleVoteDown(video.id)}>Vote Down</button>
+							<button onClick={() => handleDelete(video.id)}>Delete</button>
+						</div>
+					</div>
+				))}
+			</div>
+		</>
+	)
+}
+
+export default VideoList
--- server/server.js
+++ server/server.js
@@ -1,121 +1,154 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
-app.use(cors());
-app.use(express.json());
-const port = process.env.PORT || 8080;
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const dotenv = require('dotenv')
+const { Pool } = require('pg')
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+dotenv.config()
+app.use(cors())
+app.use(express.json())
+
+const port = process.env.PORT || 8080
+const pool = new Pool({
+	connectionString: process.env.DB_URL,
+	ssl: { rejectUnauthorized: false },
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
 let videos = [
-  {
-    id: 523523,
-    title: "Never Gonna Give You Up",
-    url: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
-    rating: 23,
-  },
-  {
-    id: 523427,
-    title: "The Coding Train",
-    url: "https://www.youtube.com/watch?v=HerCR8bw_GE",
-    rating: 230,
-  },
-  {
-    id: 82653,
-    title: "Mac & Cheese | Basics with Babish",
-    url: "https://www.youtube.com/watch?v=FUeyrEN14Rk",
-    rating: 1112,
-  },
-  {
-    id: 858566,
-    title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
-    url: "https://www.youtube.com/watch?v=xbs7FT7dXYc",
-    rating: 11,
-  },
-  {
-    id: 453538,
-    title:
-      "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
-    url: "https://www.youtube.com/watch?v=4As0e4de-rI",
-    rating: 3211,
-  },
-  {
-    id: 283634,
-    title: "Learn Unity - Beginner's Game Development Course",
-    url: "https://www.youtube.com/watch?v=gB1F9G0JXOo",
-    rating: 211,
-  },
-  {
-    id: 562824,
-    title: "Cracking Enigma in 2021 - Computerphile",
-    url: "https://www.youtube.com/watch?v=RzWB5jL5RX0",
-    rating: 111,
-  },
-  {
-    id: 442452,
-    title: "Coding Adventure: Chess AI",
-    url: "https://www.youtube.com/watch?v=U4ogK0MIzqk",
-    rating: 671,
-  },
-  {
-    id: 536363,
-    title: "Coding Adventure: Ant and Slime Simulations",
-    url: "https://www.youtube.com/watch?v=X-iSQQgOd1A",
-    rating: 76,
-  },
-  {
-    id: 323445,
-    title: "Why the Tour de France is so brutal",
-    url: "https://www.youtube.com/watch?v=ZacOS8NBK6U",
-    rating: 73,
-  },
-];
+	{
+		id: 523523,
+		title: 'Never Gonna Give You Up',
+		url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+		rating: 23,
+	},
+	{
+		id: 523427,
+		title: 'The Coding Train',
+		url: 'https://www.youtube.com/watch?v=HerCR8bw_GE',
+		rating: 230,
+	},
+	{
+		id: 82653,
+		title: 'Mac & Cheese | Basics with Babish',
+		url: 'https://www.youtube.com/watch?v=FUeyrEN14Rk',
+		rating: 1112,
+	},
+	{
+		id: 858566,
+		title: 'Videos for Cats to Watch - 8 Hour Bird Bonanza',
+		url: 'https://www.youtube.com/watch?v=xbs7FT7dXYc',
+		rating: 11,
+	},
+	{
+		id: 453538,
+		title:
+			'The Complete London 2012 Opening Ceremony | London 2012 Olympic Games',
+		url: 'https://www.youtube.com/watch?v=4As0e4de-rI',
+		rating: 3211,
+	},
+	{
+		id: 283634,
+		title: "Learn Unity - Beginner's Game Development Course",
+		url: 'https://www.youtube.com/watch?v=gB1F9G0JXOo',
+		rating: 211,
+	},
+	{
+		id: 562824,
+		title: 'Cracking Enigma in 2021 - Computerphile',
+		url: 'https://www.youtube.com/watch?v=RzWB5jL5RX0',
+		rating: 111,
+	},
+	{
+		id: 442452,
+		title: 'Coding Adventure: Chess AI',
+		url: 'https://www.youtube.com/watch?v=U4ogK0MIzqk',
+		rating: 671,
+	},
+	{
+		id: 536363,
+		title: 'Coding Adventure: Ant and Slime Simulations',
+		url: 'https://www.youtube.com/watch?v=X-iSQQgOd1A',
+		rating: 76,
+	},
+	{
+		id: 323445,
+		title: 'Why the Tour de France is so brutal',
+		url: 'https://www.youtube.com/watch?v=ZacOS8NBK6U',
+		rating: 73,
+	},
+]
 
 // GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you'vea confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
-});
+app.get('/', (req, res) => {
+	// Delete this line after you'vea confirmed your server is running
+	res.send({ express: 'Your Backend Service is Running' })
+})
 
-app.get("/videos", (req, res) => {
-  res.json(videos);
-});
+app.get('/videos', async (req, res) => {
+	try {
+		const query = 'SELECT * FROM videoss'
+		const result = await pool.query(query)
+		const orderedVideos = result.rows
+		res.status(200).json(orderedVideos)
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ error: 'Error Message' })
+	}
+})
 
-const isValidYouTubeURL = (url) => {
-  // Regular expression to match YouTube URL pattern
-  const youtubeRegex = /^(https?:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/;
-  return youtubeRegex.test(url);
-};
+// const isValidYouTubeURL = (url) => {
+// 	// Regular expression to match YouTube URL pattern
+// 	const youtubeRegex = /^(https?:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+$/
+// 	return youtubeRegex.test(url)
+// }
 
-app.post("/videos", (req, res) => {
-  const { title, url } = req.body;
+app.post('/videos', async (req, res) => {
+	try {
+		const { title, url } = req.body
+		const rating = 0
+		const query =
+			'INSERT INTO videoss (title, url, rating) VALUES ($1,$2,$3) RETURNING *'
+		const values = [title, url, rating]
+		const result = await pool.query(query, values)
+		const addVideos = result.rows[0]
+		res.status(201).json(addVideos)
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({ error: 'Error Message' })
+	}
+})
 
-  if (title !== "" && isValidYouTubeURL(url)) {
-    const newVideo = {
-      id: Date.now(),
-      title,
-      url,
-      rating: 0,
-      postedAt: new Date().toLocaleString(),
-    };
+app.delete('/videos/:id', async (req, res) => {
+	try {
+		const videoID = Number(req.params.id)
+		const query = 'DELETE FROM videoss WHERE id = $1 RETURNING *'
+		const values = [videoID]
+		const result = await pool.query(query, values)
+		const deleteVideos = result.rows[0]
 
-    videos.push(newVideo);
-    res.status(201).json(newVideo);
-  } else {
-    res.status(400).json({ error: "Invalid video title or URL" });
-  }
-});
+		if (deleteVideos) {
+			res.status(200).json(deleteVideos)
+		} else {
+			res.status(404).json({ message: 'Video Not Found' })
+		}
+	} catch (error) {
+		res.status(500).json({ error: 'Error Message' })
+	}
+})
 
-app.delete("/videos/:id", (req, res) => {
-  const videoID = Number(req.params.id);
-  const deletedVideo = videos.find((video) => videoID === video.id);
-
-  if (deletedVideo) {
-    videos = videos.filter((video) => videoID !== video.id);
-    res.json(videos);
-  } else {
-    res.status(404).json({ error: "Video not found" });
-  }
-});
+app.put('/videos/:id/rating', async (req, res) => {
+	try {
+		const videoID = Number(req.params.id)
+		const { rating } = req.body
+		const query = 'UPDATE videoss SET rating = $1 WHERE id = $2'
+		const values = [rating, videoID]
+		await pool.query(query, values)
+		res.json('Rating Updated Successfully')
+	} catch (error) {
+		res.status(500).json({ error: 'Error Message' })
+	}
+})
