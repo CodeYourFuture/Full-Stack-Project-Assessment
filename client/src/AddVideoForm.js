8ebab60ddbed81fcbef8511a7dff750580140287
Gayle Ngozi <gaylengozi@gmail.com>
2023-06-01 02:01:47 +0100

    Update AddVideoForm.js
    updated adding new video form

--- client/src/AddVideoForm.js
+++ client/src/AddVideoForm.js
@@ -1,11 +1,100 @@
 import React, { useState } from "react";
 
-function AddVideoForm({ onAddVideo }) {
+// function AddVideoForm({ onAddVideo, fetchVideos }) {
+//   const [title, setTitle] = useState("");
+//   const [url, setUrl] = useState("");
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     fetch("http://localhost:5000/videos/", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({
+//         title: title,
+//         url: url,
+//       }),
+//     })
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         console.log(data);
+//         fetchVideos();
+//         setTitle("");
+//         setUrl("");
+//       })
+//       .catch((error) => {
+//         console.log("error:", error);
+//       });
+//     if (!title || !url) return;
+//     const newVideo = {
+//       id: Math.floor(Math.random() * 1000000),
+//       title: title,
+//       url: url,
+//       rating: 0,
+//     };
+//     onAddVideo(newVideo);
+//     setTitle("");
+//     setUrl("");
+//   };
+//   function handleInputChange(event) {
+//     if (event.target.title === "title") {
+//       setTitle(event.target.value);
+//       console.log(event);
+//     } else if (event.target.url === "url") {
+//       setUrl(event.target.value);
+//     }
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label>
+//         Title:
+//         <input type="text" onChange={handleInputChange} />
+//       </label>
+//       <br></br>
+//       <label>
+//         URL:
+//         <input type="text" onChange={handleInputChange} />
+//       </label>
+//       <div className="submit">
+//         <button type="submit">Add Video</button>
+//       </div>
+//     </form>
+//   );
+// }
+
+// export default AddVideoForm;
+
+
+function AddVideoForm({ onAddVideo, fetchVideos }) {
   const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
 
-  function handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
+    fetch("http://localhost:5000/videos/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: title,
+        url: url,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        fetchVideos();
+        setTitle("");
+        setUrl("");
+      })
+      .catch((error) => {
+        console.log("error:", error);
+      });
     if (!title || !url) return;
     const newVideo = {
       id: Math.floor(Math.random() * 1000000),
@@ -16,6 +105,14 @@ function AddVideoForm({ onAddVideo }) {
     onAddVideo(newVideo);
     setTitle("");
     setUrl("");
+  };
+
+  function handleInputChange(event) {
+    if (event.target.name === "title") {
+      setTitle(event.target.value);
+    } else if (event.target.name === "url") {
+      setUrl(event.target.value);
+    }
   }
 
   return (
@@ -24,23 +121,22 @@ function AddVideoForm({ onAddVideo }) {
         Title:
         <input
           type="text"
+          name="title"
           value={title}
-          onChange={(event) => setTitle(event.target.value)}
+          onChange={handleInputChange}
         />
-      </label> <br></br>
+      </label>{" "}
+      <br></br>
       <label>
         URL:
         <input
           type="text"
+          name="url"
           value={url}
-          onChange={(event) => setUrl(event.target.value)}
+          onChange={handleInputChange}
         />
       </label>
-      <div className="submit">
-        <button type="submit">
-          Add Video
-        </button>
-      </div>
+      <button type="submit" className="submit">Add Video</button>
     </form>
   );
 }
