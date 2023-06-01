f1efcd6db51b82fde1bd8f0e2eedcd8412bb8619
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-06-01 08:16:14 +0100

    Adding 2 select menu to VideoPicker and styling them

--- client/src/components/VideoPicker/VideoPicker.js
+++ client/src/components/VideoPicker/VideoPicker.js
@@ -1,9 +1,67 @@
 import "./VideoPicker.css";
+import ReactSelect from "react-select";
+import { useState } from "react";
 
-const VideoPicker = () => {
+const VideoPicker = ({ categories }) => {
+  const [isCategoryMenuOpen, setIsCategoryMenuOpen] = useState(false);
+  const [isOrderMenuOpen, setIsOrderMenuOpen] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [selectedOrder, setSelectedOrder] = useState(null);
+  const orderOptions = [
+    { value: "Ascending", label: "Ascending" },
+    { value: "Descending", label: "Descending" },
+  ];
+  let categoryOptions = [];
+
+  //adding values to the options array
+  categories.forEach((category) => {
+    const newOption = {};
+    newOption.value = category;
+    newOption.label = category;
+    categoryOptions.push(newOption);
+  });
+
+  const toggleCategoryMenu = () => {
+    setIsCategoryMenuOpen(!isCategoryMenuOpen);
+  };
+
+  const toggleOrderMenu = () => {
+    setIsOrderMenuOpen(!isOrderMenuOpen);
+  };
+
+  const handleCategoryChange = (selectedCategory) => {
+    setSelectedCategory(selectedCategory);
+  };
+
+  const handleOrderChange = (selectedOrder) => {
+    setSelectedOrder(selectedOrder);
+  };
+
   return (
-    <div className="video-picker">
-      <h1>Video Picker</h1>
+    <div className="video_picker">
+      <h1>Watch videos</h1>
+      <div className="selectors">
+        <div className="selector_field" onClick={toggleCategoryMenu}>
+          <ReactSelect
+            options={categoryOptions}
+            value={selectedCategory}
+            onChange={handleCategoryChange}
+            className="custom_select"
+            isSearchable={false}
+            menuIsOpen={isCategoryMenuOpen} // Set the menu visibility based on isMenuOpen state
+          />
+        </div>
+        <div className="selector_field" onClick={toggleOrderMenu}>
+          <ReactSelect
+            options={orderOptions}
+            value={selectedOrder}
+            onChange={handleOrderChange}
+            className="custom_select"
+            isSearchable={false}
+            menuIsOpen={isOrderMenuOpen} // Set the menu visibility based on isMenuOpen state
+          />
+        </div>
+      </div>
     </div>
   );
 };
