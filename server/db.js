1efe37a4f1b976ee4eb3064fa54bcbc62543395d
Mohamed Abdalla <mohamed.e.abdalla1@gmail.com>
2023-06-02 19:12:53 +0100

    changed db file

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
+const ssl = process.env.DEV ? false : true;
 const pool = new Pool({
   user: process.env.USERNAME,
   password: process.env.PASSWORD,
   host: process.env.HOST,
   port: process.env.DBPORT,
-  ssl: process.env.DEV ? false : true,
+  ssl: ssl,
   database: "videosapp",
 });
 
