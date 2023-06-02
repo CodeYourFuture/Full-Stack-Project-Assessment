325bf040fa53c2a2e60842fb0a8ff5fd36d9b12e
Hengameh M.Khani <afsooneday@gmail.com>
2023-06-02 03:28:36 +0100

    like &dislike

--- client/src/Video.jsx
+++ client/src/Video.jsx
@@ -1,58 +1,106 @@
-  import React from "react";
+  // import React from "react";
+  // import Button from "react-bootstrap/Button";
+  // import axios from "axios";
+
+  // function Video({ info }) {
+  //   const handleLike = () => {
+  //     // Handle like button click
+  //   };
+  
+  //   const handleDislike = () => {
+  //     // Handle dislike button click
+  //   };
+  
+  //   const handleDelete = (id) => {
+  //     axios.delete(`https://full-stack-assessment.onrender.com/video/${id}`)
+  //     alert ("video has been deleted")
+  //     window.location.reload()
+
+  //     // Handle delete button click
+  //   };
+  
+  //   return (
+    
+  //       <div>
+  //         <h6 className="title" >{info.title}</h6>
+
+  //         <iframe
+  //           src={`https://www.youtube.com/embed/${info.url}`}
+  //           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  //           allowFullScreen
+  //           title="Embedded youtube"
+  //         ></iframe>
+  //         <p>{info.rating}</p>
+  //         <div className="d-flex justify-content-evenly align-items-center">
+  //           <div className="d-flex">
+  //             <Button variant="primary" onClick={handleLike} className="mr-2">
+  //               👍
+  //             </Button>
+  //             <Button variant="secondary" onClick={handleDislike} className="mr-2">
+  //               👎
+  //             </Button>
+  //           </div>
+  //           <Button variant="outline-danger" onClick={() => handleDelete(info.id)}>
+  //             Delete
+  //           </Button>
+  //         </div>
+  //       </div>
+      
+  //   );
+  // }
+  
+  
+  // export default Video;
+  
+  import React, { useState } from "react";
   import Button from "react-bootstrap/Button";
   import axios from "axios";
-
+  
   function Video({ info }) {
+    const [likeCount, setLikeCount] = useState(0);
+    const [dislikeCount, setDislikeCount] = useState(0);
+  
     const handleLike = () => {
-      // Handle like button click
+      setLikeCount(likeCount + 1);
     };
   
     const handleDislike = () => {
-      // Handle dislike button click
+      setDislikeCount(dislikeCount + 1);
     };
   
     const handleDelete = (id) => {
-      axios.delete(`https://full-stack-assessment.onrender.com/video/${id}`)
-      alert ("video has been deleted")
-      window.location.reload()
-
-      // Handle delete button click
+      axios.delete(`https://full-stack-assessment.onrender.com/video/${id}`);
+      alert("video has been deleted");
+      window.location.reload();
     };
   
     return (
-    
-        <div>
-          <h6 className="title" >{info.title}</h6>
-
-          <iframe
-            src={`https://www.youtube.com/embed/${info.url}`}
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube"
-          ></iframe>
-          <p>{info.rating}</p>
-          <div className="d-flex justify-content-evenly align-items-center">
-            <div className="d-flex">
-              <Button variant="primary" onClick={handleLike} className="mr-2">
-                👍
-              </Button>
-              <Button variant="secondary" onClick={handleDislike} className="mr-2">
-                👎
-              </Button>
-            </div>
-            <Button variant="outline-danger" onClick={() => handleDelete(info.id)}>
-              Delete
+      <div>
+        <h6 className="title">{info.title}</h6>
+        <iframe
+          src={`https://www.youtube.com/embed/${info.url}`}
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded youtube"
+        ></iframe>
+        <p>Like: {likeCount}</p>
+        <p>Dislike: {dislikeCount}</p>
+        <div className="d-flex justify-content-evenly align-items-center">
+          <div className="d-flex">
+            <Button variant="primary" onClick={handleLike} className="mr-2">
+              👍
+            </Button>
+            <Button variant="secondary" onClick={handleDislike} className="mr-2">
+              👎
             </Button>
           </div>
+          <Button variant="outline-danger" onClick={() => handleDelete(info.id)}>
+            Delete
+          </Button>
         </div>
-      
+      </div>
     );
   }
   
-
-  
   export default Video;
-  
-  
-  
   
