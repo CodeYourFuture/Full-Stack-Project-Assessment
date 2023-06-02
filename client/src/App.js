f29040d99b8f460fd747faf03ccfe6a42595b947
Mari Azh <Marzi.aghchay@gmail.com>
2023-06-02 01:00:25 +0100

    need db and rating

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,15 @@
-import React, { useState } from "react";
-import VideoList from "./component/VideoList";
-import videosData from "./exampleresponse.json";
-import AddVideoForm from "./component/AddVideoForm";
 import "./App.css";
+import AppHeader from "./components/AppHeader";
+import CardList from "./components/CardList";
 
-const App = () => {
-  const [videos, setVideos] = useState(videosData);
 
-  const handleRemove = (id) => {
-    setVideos(videos.filter((video) => video.id !== id));
-  };
-
-   const handleVote = (id, increment) => {
-     setVideos((prevVideos) =>
-       prevVideos.map((video) =>
-         video.id === id
-           ? { ...video, rating: video.rating + increment }
-           : video
-       )
-     );
-   };
-     const handleAddVideo = (newVideo) => {
-       setVideos((prevVideos) => [...prevVideos, newVideo]);
-     };
-
+function App() {
   return (
-    <div className="app">
-      <h1 className="app-title">Video List</h1>
-      <AddVideoForm onAddVideo={handleAddVideo} />
-      <VideoList videos={videos} onRemove={handleRemove} onVote={handleVote} />
-    </div>
+    <>
+      <AppHeader/>
+      <CardList/>
+    </>
   );
-};
+}
 
 export default App;
