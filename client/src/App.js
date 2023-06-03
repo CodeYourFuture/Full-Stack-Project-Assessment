9e6e764d299abd68df926f1c0135ca6b0083a6dc
ZahraaTayyar <zahraatayyar@gmail.com>
2023-06-03 12:31:30 +0100

    app.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Navbar from "./comps/Navbar";
 import VideosList from "./comps/VideosList";
-import AddVideo from "./comps/AddVideo";
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
