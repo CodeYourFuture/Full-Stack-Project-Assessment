e3251fd07b31c5b135ba63ead6aba366ceb268c2
zobeir-rigi <rigi.zobir@yahoo.com>
2023-06-03 15:18:09 +0100

    installed express & cors and also get videos- 200

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+var cors = require('cors');
+app.use(cors())
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const videos = require ("./data.json")
+console.log(videos);
+
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+// let videos = [];
 
 // GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.send(videos);
 });
+
+app.post("/", (req, res) => {
+  const{title, url} = req.body;
+  
+  res.json(videos);
+});
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
