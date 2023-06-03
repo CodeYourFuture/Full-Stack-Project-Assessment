b69550362f3d887d6dfe0ef640e79b5b6175e942
zobeir-rigi <rigi.zobir@yahoo.com>
2023-06-03 16:05:17 +0100

    find video id & delete Video

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 var cors = require('cors');
 app.use(cors())
+app.use(express.json())
 
 const videos = require ("./data.json")
 console.log(videos);
@@ -16,11 +17,41 @@ console.log(videos);
 app.get("/", (req, res) => {
   res.send(videos);
 });
-
+/*************** */
 app.post("/", (req, res) => {
-  const{title, url} = req.body;
-  
-  res.json(videos);
+  const newVideo = {
+    id: 1,
+    title: req.body.title,
+    url: req.body.url,
+    rating: 0
+  };
+
+  console.log(newVideo)
+
+  if (!newVideo.title || !newVideo.url) {
+    res.status(404).json({message: "Fill in all fields"})
+  } else {
+    videos.push(newVideo);
+    res.status(200).json({message: "New video added", videos})
+  }
+});
+/******************* */
+app.get("/:id", (req, res) => {
+  const newId = Number(req.params.id);
+  const foundVideo = videos.find(el => el.id === newId);
+  console.log(foundVideo);
+  res.status(200).json(foundVideo);
+
+});
+
+app.delete("/:id", (req, res) => {
+  const newId = Number(req.params.id);
+  const foundVideo = videos.findIndex(el => el.id === newId);
+  const videoToDelete = videos.find(el => el.id === newId)
+  console.log(videoToDelete);
+  videos.splice(videoToDelete,1);
+  res.status(200).json(videos);
+
 });
 
 
