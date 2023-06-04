4cddf243885c7d80f1c175f9828ca44635170ac0
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-06-05 00:35:19 +0100

    Connecting front-end and back-end
    Retrieve the data from the API

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,93 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddVideoForm from "./components/AddVideoForm";
-import videoData from "./exampleresponse.json";
 import Video from "./components/Video";
 import Header from "./components/Header";
 import "./App.css";
 import "./components/AddVideoForm.css";
 
 const App = () => {
-  const [videos, setVideos] = useState(() => {
-    const sortedVideos = videoData.sort((a, b) => b.rating - a.rating);
-    return sortedVideos.map((video) => ({
-      ...video,
-      votes: video.rating || 0,
-    }))
-});
-  const handleUpVote = (videoId) => {
-    setVideos((oldVideos) =>
-      oldVideos.map((video) =>
-        video.id === videoId ? { ...video, votes: video.votes + 1 } : video
-      )
-    );
-  };
-  const handleDownVote = (videoId) => {
-    setVideos((oldVideos) =>
-      oldVideos.map((video) =>
-        video.id === videoId ? { ...video, votes: video.votes - 1 } : video
-      )
-    );
-  };
-  const handleRemoveVideo = (videoId) => {
-    setVideos((oldVideos) => oldVideos.filter((video) => video.id !== videoId));
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+  const fetchVideos = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/videos");
+      const data = await res.json();
+      setVideos(data);
+    } catch (err) {
+      console.error("Error fetching videos:", err);
+    }
   };
 
-  const handleAddVideo = (newVideo) => {
-    setVideos((oldVideos) => [...oldVideos, { ...newVideo, votes: 0 }]);
+  const handleAddVideo = async (newVideo) => {
+    try {
+      const res = await fetch("http://localhost:5000/videos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newVideo),
+      });
+      const data = await res.json();
+      setVideos((prevVideos) => [...prevVideos, { ...newVideo, id: data.id }]);
+    } catch (err) {
+      console.log("Error adding video:", err);
+    }
   };
+
+  const handleUpVote = async (videoId) => {
+    try {
+      const res = await fetch(`http://localhost:5000/videos/${videoId}`);
+      const data = await res.json();
+      const updatedVideo = { ...data, votes: data.votes + 1 };
+      await fetch(`http://localhost:5000/videos/${videoId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedVideo),
+      });
+      setVideos((prevVideos) =>
+        prevVideos.map((video) => (video.id === videoId ? updatedVideo : video))
+      );
+    } catch (err) {
+      console.log("Error upvoting video:", err);
+    }
+  };
+  const handleDownVote = async (videoId) => {
+    try {
+      const res = await fetch(`http://localhost:5000/videos/${videoId}`);
+      const data = await res.json();
+      const updatedVideo = { ...data, votes: data.votes - 1 };
+      await fetch(`http://localhost:5000/videos/${videoId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedVideo),
+      });
+      setVideos((prevVideos) =>
+        prevVideos.map((video) => (video.id === videoId ? updatedVideo : video))
+      );
+    } catch (err) {
+      console.log("Error downvoting video:", err);
+    }
+  };
+  const handleRemoveVideo = async (videoId) => {
+    try {
+      await fetch(`http://localhost:5000/videos/${videoId}`, {
+        method: "DELETE",
+      });
+      setVideos((prevVideos) =>
+        prevVideos.filter((video) => video.id !== videoId)
+      );
+    } catch (err) {
+      console.log("Error removing video:", err);
+    }
+  };
+
   return (
     <div className="app">
       <Header />
