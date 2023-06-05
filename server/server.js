e9b5b4bb66e4cdd66ac64eaeed9c4993774e7070
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-06-05 20:12:21 +0100

    Changing endpoint get id - adding thumbs-up and thumbs-down

--- server/server.js
+++ server/server.js
@@ -7,9 +7,12 @@ const { Pool } = require("pg");
 // Create a PostgreSQL connection pool using the DATABASE_URL environment variable
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: false,
   // connectionString:
   //   "postgres://videos_recommendations_user:x4lYTYQsPAllyyOFEEWMmnPI4MTFu0Fm@dpg-chv05v67avj345ftl870-a.frankfurt-postgres.render.com/videos_recommendations",
-  ssl: false,
+  // ssl: {
+  //   rejectUnauthorized: true, // Enable SSL validation
+  },
 });
 
 app.use(express.json());
@@ -86,14 +89,14 @@ app.post("/", async (req, res) => {
   }
 });
 
-// GET "/{id}"
+// GET "/{id}?action=thumbs-up"
 app.get("/:id", async (req, res) => {
   const { id } = req.params;
 
   try {
     const query = "SELECT * FROM videos WHERE id = $1";
     const result = await db.query(query, [id]);
-    const video = result.rows[0];
+    let video = result.rows[0];
 
     if (!video) {
       return res.status(404).json({
@@ -101,6 +104,17 @@ app.get("/:id", async (req, res) => {
         message: "Video not found",
       });
     }
+
+    const action = req.query.action;
+
+    if (action === "thumbs-up") {
+      video.rating = parseInt(video.rating) + 1;
+    } else if (action === "thumbs-down") {
+      video.rating = parseInt(video.rating) - 1;
+    }
+
+    const updateQuery = "UPDATE videos SET rating = $1 WHERE id = $2";
+    await db.query(updateQuery, [video.rating, id]);
 
     res.json(video);
   } catch (error) {
