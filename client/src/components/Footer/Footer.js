4645bd6bac6f8fb345598a8a7df80a895e218648
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2023-06-05 22:08:56 +0100

    Modifying, styling Footer, changing video-card min-height

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,8 +2,8 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p className="footer__copyright">Made by Vitalina Kuzmenko</p>
+    <footer>
+      <p>Created by Vitalina Kuzmenko</p>
     </footer>
   );
 };
