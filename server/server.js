a97b47c3fad2f0febbc919432519cabf240300c4
Gayle Ngozi <gaylengozi@gmail.com>
2023-06-06 00:04:07 +0100

    .env and dbConfig set up. database deployed on render

--- server/server.js
+++ server/server.js
@@ -1,32 +1,53 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const app = express();
+
 const port = process.env.PORT || 5000;
-const initialData = require("./exampleresponse.json");
+
+const database = require("./dbConfig");
+
 app.use(express.json());
 app.use(
   cors({
-    origin: "*"
+    origin: "*",
   })
 );
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = initialData;
-
 // GET "/"
-app.get("/videos", (request, response) => {
-  response.send(videos);
+app.get("/videos", async function (request, response) {
+  const sqlQuery = "SELECT * FROM videos;"
+  const getAllVideos = await database.query(sqlQuery);
+  // console.log(getAllVideos);
+  const allVideos = getAllVideos.rows;
+  return response.status(200).json(allVideos);
 });
 
-app.get("/videos/:id", function (request, response) {
-  const videosId = request.params.id;
-  const eachVideo = videos.find(
-    (eachVideo) => eachVideo.id === parseInt(videosId)
+app.get("/videos/:id", async function (request, response) {
+  
+  const videoId = request.params.id;
+
+  // const sqlQuery = "SELECT * FROM videos WHERE id = $1"
+  const getVideoById = await database.query(
+    "SELECT * FROM videos WHERE id = $1",
+    [videoId], 
   );
-  eachVideo
-    ? response.json(eachVideo)
-    : response.json({message: "There're no videos matching your request" });
+
+  if (getVideoById.rows.length === 0) {
+   return response.status(400).json({error: `The video with ${videoId} doesnt exist`})
+  }
+
+  const videoWithThatId = getVideoById.rows[0]
+  return response.status(200).json({ videoWithThatId });
+
+  
+  // const eachVideo = videos.find(
+  //   (eachVideo) => eachVideo.id === parseInt(videoId)
+  // );
+  // eachVideo
+  //   ? response.json(eachVideo)
+  //   : response.json({ message: "There're no videos matching your request" });
 });
 
 app.delete("/videos/:id", function (request, response) {
@@ -36,9 +57,7 @@ app.delete("/videos/:id", function (request, response) {
   );
 
   if (eachVideo) {
-    videos = videos.filter(
-      (eachVideo) => eachVideo.id !== parseInt(videosId)
-    );
+    videos = videos.filter((eachVideo) => eachVideo.id !== parseInt(videosId));
     response.json({ message: "Video Succesfully deleted" });
   } else {
     response.json({ message: "Unable to find or delete video" });
@@ -50,21 +69,17 @@ app.post("/videos/addnew", function (request, response) {
     id: videos.length + 1,
     title: request.body.title,
     url: request.body.url,
-    rating: 0
+    rating: 0,
   };
 
-  if (
-    !addNewVideo.title ||
-     !addNewVideo.url
-  ) {
+  if (!addNewVideo.title || !addNewVideo.url) {
     return response
       .status(400)
       .json({ message: "Please fill out the required areas" });
   }
- 
+
   videos.push(addNewVideo);
   response.json(videos);
 });
-
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
