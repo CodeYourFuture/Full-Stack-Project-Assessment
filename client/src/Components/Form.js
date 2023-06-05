16372342f780a660f5494622a48aea4ef1ddd743
Olena Reukova <kronverk01@gmail.com>
2023-06-05 02:12:55 +0100

    Data Validation

--- client/src/Components/Form.js
+++ client/src/Components/Form.js
@@ -1,67 +1,90 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
+function Form({ onAddVideo }) {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+  const [error, setError] = useState("");
 
-
-function Form({onAddVideo, }) {
-  const [title, setTitle] = useState('');
-  const [url, setUrl] = useState('');
- 
-
-  const handleTitleChange =(event) => {
+  const handleTitleChange = (event) => {
     setTitle(event.target.value);
   };
 
-  const handleUrlChamge = (event)=>{
+  const handleUrlChange = (event) => {
     setUrl(event.target.value);
   };
 
-  const handleSubmit = (event) =>{
-    event.preventDefault();
-    const currentDate = new Date();
-  const id = currentDate.getTime(); // Use the timestamp as the ID
-    const newVideo= {
-        id: id,
-        title: title,
-        url: url,
-        rating: 0,
-    };
-    onAddVideo(newVideo);
-    setTitle('');
-    setUrl ('');
+  const validateUrl = (inputUrl) => {
+    // Regular expression pattern for matching YouTube URLs
+    const youtubeUrlPattern =
+      /^(https?:\/\/)?(www\.)?youtube\.com\/watch\?v=([a-zA-Z0-9_-]{11})$/;
+
+    return youtubeUrlPattern.test(inputUrl);
   };
 
-  
-    return(
-      
-        <div className="form" >
-        <a href="#">Add Videos</a>
-        <div id="form">
-          <form action="#" method="get">
-            <label htmlFor="title">Title</label>
-            <input type="text" 
-            id="title" 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (title.trim() === "") {
+      setError("Title cannot be empty");
+      return;
+    }
+
+    if (!validateUrl(url)) {
+      setError("Invalid YouTube URL");
+      return;
+    }
+
+    const currentDate = new Date();
+    const id = currentDate.getTime(); // Use the timestamp as the ID
+    const newVideo = {
+      id: id,
+      title: title,
+      url: url,
+      rating: 0,
+    };
+
+    onAddVideo(newVideo);
+    setTitle("");
+    setUrl("");
+    setError("");
+  };
+
+  return (
+    <div className="form">
+      <a href="#">Add Videos</a>
+      <div id="form">
+        <form action="#" method="get">
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
             name="title"
-            placeholder="Title" 
-            value = {title} 
-            onChange={handleTitleChange} 
-            required/><br />
-            <label htmlFor="url">URL</label>
-            <input type="text" 
-            id="url" 
-            name="url" 
-            placeholder="URL" 
-            value={url}  
-            onChange={handleUrlChamge} 
-            required/><br />
-          </form>
-        </div>
-        <button className="add-button" onClick={handleSubmit} >
+            placeholder="Title"
+            value={title}
+            onChange={handleTitleChange}
+            required
+          />
+          <br />
+          <label htmlFor="url">URL</label>
+          <input
+            type="text"
+            id="url"
+            name="url"
+            placeholder="URL"
+            value={url}
+            onChange={handleUrlChange}
+            required
+          />
+          <br />
+        </form>
+      </div>
+      {error && <p className="error">{error}</p>}
+      <button className="add-button" onClick={handleSubmit}>
         ADD
       </button>
-      
-      </div>
-    );
+    </div>
+  );
 }
 
 export default Form;
