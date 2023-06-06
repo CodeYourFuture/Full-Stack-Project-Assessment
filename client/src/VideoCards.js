d2267167abbf4a8fa2e62923bf9fa409c4d6fb90
laxmi <lkc712628@gmail.com>
2023-06-06 23:20:59 +0100

    full stack done

--- client/src/VideoCards.js
+++ client/src/VideoCards.js
@@ -11,7 +11,8 @@ const VideoCards = ({ videos, setVideos }) => {
         })
         .then((response) => response.json())
         .then((data) =>{
-            let newData = videos.filter((video) => video.id !== data.id);
+            console.log("deleted", data)
+            let newData = videos.filter((video) => video.id !== id);
             setVideos(newData);
         })
         .catch((error) => {
@@ -23,33 +24,53 @@ const increaseRating = (id) => {
     let newData = [];
     newData = videos.map((video) => 
     video.id === id ? { ...video, rating: video.rating + 1 }: video);
+    fetch(`http://localhost:5000/rating`,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ id, increment: 1 }),
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
     return setVideos(newData);
 }
 
 const decreaseRating = (id) => {
     let newData = [];
     newData = videos.map((video) => 
-    video.id === id ? {...video, rating: video.rating - 1}: video);
+    video.id === id ? {...video, rating: video.rating - 1 }: video);
+    fetch(`http://localhost:5000/rating`,{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id, increment: -1 }),
+    })
+    .catch((error) => {
+        console.error("Error:", error);
+    });
     return setVideos(newData);
 }; 
 
-const Card = (video) => {
+const Card = ({ video }) => {
   return (
     <div className='cardContainer' key={video.id}>
         <iframe
          height="315" 
          src={video.url.replace("watch?v=", "embed/")}
          title={video.title} 
-         frameborder="0" 
+         frameBorder="0" 
          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
         allowFullScreen>
         </iframe>
 
         <h6>{video.title}</h6>
         <span className='UpDownVote'>
-            <i className='fa fa-thumbs-up' onClick={() => increaseRating(video.id)}></i>
+            <i className='fa fa-thumbs-up' onClick={() => increaseRating(video.id)}>👍</i>
             {video.rating}
-            <i className='fa fa-thumbs-down' onClick={() => decreaseRating(video.id)}></i>
+            <i className='fa fa-thumbs-down' onClick={() => decreaseRating(video.id)}>👎</i>
         </span>
 
         <button className='deleteButton' onClick={() => handleDeleteClick(video.id)}>
@@ -59,13 +80,16 @@ const Card = (video) => {
   );
 };
 
-console.log(videos);
+console.log("videos", videos);
 
 return(
-    <div className="allCardsContainer">
-       {videos.map((video) => Card(video))}
-    </div>
+        <div className="allCardsContainer"> 
+          {videos
+          .filter((video) => video.url)
+          .map((video) => (
+            <Card key={video.id} video={video} /> 
+          ))}
+        </div>
   );
-
 }
 export default VideoCards;
