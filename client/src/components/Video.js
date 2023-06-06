40d815fbe7c7760ec3daa8ef1d435a87c38a42d0
Farnoosh <91014204+Farnooshmo@users.noreply.github.com>
2023-06-06 18:58:11 +0100

    Fixed the videos url
    the Youtube did not play

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -6,15 +6,16 @@ const Video = ({ title, url, rating, onUpVote, onDownVote, onRemove }) => {
   return (
     <div className="video_wrapper">
       <h2 className="video_title">Title:{title}</h2>
-      <Youtube
+      <iframe
         width="560"
         height="315"
-        src={url}
-        title={title}
+        src={url.replace("watch?v=", "embed/")}
+        title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></Youtube>
+        allowfullscreen
+      ></iframe>
+
       <div>Votes:{rating}</div>
       <div className="votes_btn_wrap">
         <button onClick={onUpVote} className="up_vote">
