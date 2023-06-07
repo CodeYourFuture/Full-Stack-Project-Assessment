ba04d955db31f6c1874b6054d2cbdf7cb2cb1043
tony-devops <ta.devopsuk@gmail.com>
2023-06-07 21:24:23 +0100

    updates

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-function Header ({orderChange}) {
-    return (
-        <header>
-            <h1>Music and Fun</h1>
-            <nav>
-                <a href="/">
-                    <h3>Home</h3>
-                </a>
-            </nav>
-        </header>
-    );
+function Header({ orderChange }) {
+  return (
+    <header>
+      <h1>Welcome to Video Server</h1>
+      <nav>
+        <a href="/">
+          <h3>Home</h3>
+        </a>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
