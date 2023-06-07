991b6ccbf09a74d88221478152b7a784a0f12ab6
Gayle Ngozi <gaylengozi@gmail.com>
2023-06-07 17:57:54 +0100

    databases updated with new columns

--- server/ProjectDataBase.sql
+++ server/ProjectDataBase.sql
@@ -1,23 +1,24 @@
 DROP TABLE IF EXISTS videos;
 CREATE TABLE videos(
   id     SERIAL PRIMARY KEY, 
+  genre   VARCHAR(10) NOT NULL,
   title  VARCHAR(500) NOT NULL,
   url    VARCHAR(500) NOT NULL,
   rating INTEGER  NOT NULL
 );
-INSERT INTO videos(title,url,rating) VALUES ('Never Gonna Give You Up','https://www.youtube.com/watch?v=dQw4w9WgXcQ',23);
-INSERT INTO videos(title,url,rating) VALUES ('The Coding Train','https://www.youtube.com/watch?v=HerCR8bw_GE',230);
-INSERT INTO videos(title,url,rating) VALUES ('Mac & Cheese | Basics with Babish','https://www.youtube.com/watch?v=FUeyrEN14Rk',2111);
-INSERT INTO videos(title,url,rating) VALUES ('Videos for Cats to Watch - 8 Hour Bird Bonanza','https://www.youtube.com/watch?v=xbs7FT7dXYc',11);
-INSERT INTO videos(title,url,rating) VALUES ('Learn Unity - Beginner''s Game Development Course','https://www.youtube.com/watch?v=gB1F9G0JXOo',211);
-INSERT INTO videos(title,url,rating) VALUES ('Cracking Enigma in 2021 - Computerphile','https://www.youtube.com/watch?v=RzWB5jL5RX0',111);
-INSERT INTO videos(title,url,rating) VALUES ('Coding Adventure: Chess AI','https://www.youtube.com/watch?v=U4ogK0MIzqk',671);
-INSERT INTO videos(title,url,rating) VALUES ('Coding Adventure: Ant and Slime Simulations','https://www.youtube.com/watch?v=X-iSQQgOd1A',76);
-INSERT INTO videos(title,url,rating) VALUES ('Coding Interview 101','https://www.youtube.com/watch?v=K5qbSQm1Dr4&t=1347s',20345);
-INSERT INTO videos(title,url,rating) VALUES ('Brandy Song','https://www.youtube.com/watch?v=gCVo2sLIndU',9730);
-INSERT INTO videos(title,url,rating) VALUES ('SQL Tutorial - Full Database Course for Beginners','https://www.youtube.com/watch?v=HXV3zeQKqGY&t=4724s',77779);
-INSERT INTO videos(title,url,rating) VALUES ('Codility - Binary Gap (JavaScript)','https://www.youtube.com/watch?v=PWADsooDNMg&list=PLqDHFR_FjG5-rH6Bg5xYMR-HTmSOqyF-L',9800);
-INSERT INTO videos(title,url,rating) VALUES ('Learn Basic SQL in 15 Minutes','https://www.youtube.com/watch?v=kbKty5ZVKMY',10450);
-INSERT INTO videos(title,url,rating) VALUES ('Studying Skills for the Tech Interview','https://www.youtube.com/watch?v=X-jUp2O_3M8',10420);
-INSERT INTO videos(title,url,rating) VALUES ('How to Ace Your First Entry-Level Technical Interview','https://www.youtube.com/watch?v=D');
-
+-- INSERT INTO videos(title,url,rating) VALUES ('Never Gonna Give You Up','https://www.youtube.com/watch?v=dQw4w9WgXcQ',23);
+INSERT INTO videos(genre,title,url,rating) VALUES ('coding','The Coding Train','https://www.youtube.com/watch?v=HerCR8bw_GE',230);
+INSERT INTO videos(genre,title,url,rating) VALUES ('coding','Cracking Enigma in 2021 - Computerphile','https://www.youtube.com/watch?v=RzWB5jL5RX0',111);
+INSERT INTO videos(genre,title,url,rating) VALUES ('coding','Coding Adventure: Ant and Slime Simulations','https://www.youtube.com/watch?v=X-iSQQgOd1A',76);
+INSERT INTO videos(genre,title,url,rating) VALUES ('coding','Coding Interview 101','https://www.youtube.com/watch?v=K5qbSQm1Dr4&t=1347s',20345);
+INSERT INTO videos(genre,title,url,rating) VALUES ('coding','SQL Tutorial - Full Database Course for Beginners','https://www.youtube.com/watch?v=HXV3zeQKqGY&t=4724s',77779);
+INSERT INTO videos(genre,title,url,rating) VALUES ('coding','Codility - Binary Gap (JavaScript)','https://www.youtube.com/watch?v=PWADsooDNMg&list=PLqDHFR_FjG5-rH6Bg5xYMR-HTmSOqyF-L',9800);
+INSERT INTO videos(genre,title,url,rating) VALUES ('coding','Learn Basic SQL in 15 Minutes','https://www.youtube.com/watch?v=kbKty5ZVKMY',10450);
+INSERT INTO videos(genre,title,url,rating) VALUES ('coding','Studying Skills for the Tech Interview','https://www.youtube.com/watch?v=X-jUp2O_3M8',10420);
+INSERT INTO videos(genre,title,url,rating) VALUES ('coding','How to Ace Your First Entry-Level Technical Interview','https://www.youtube.com/watch?v=D');
+INSERT INTO videos(genre,title,url,rating) VALUES ('beauty','How to apply full face makeup', 'https://www.youtube.com/watch?v=zS3d3A_lcUc', 57000);
+INSERT INTO videos(genre,title,url,rating) VALUES ('fashion','Dress in plus-size fabulosity','https://www.youtube.com/watch?v=Tu4dk5xgj0Q', 3700);
+INSERT INTO videos(genre,title,url,rating) VALUES ('fashion','Look Super-glam on a budget','https://www.youtube.com/watch?v=UcD8nTMzO0k', 4700);
+INSERT INTO videos(genre,title,url,rating) VALUES ('beauty','Naomi Campbell-Beauty Routine','https://www.youtube.com/watch?v=oCJFJMsZRRo', 26000);
+INSERT INTO videos(genre,title,url,rating) VALUES ('fashion','Top-5 Wardrobe Staples','https://www.youtube.com/watch?v=a7v0HpIeMDU', 10000);
+INSERT INTO videos(genre,title,url,rating) VALUES ('beauty', 'How to Perfect Brows', 'https://www.youtube.com/watch?v=kesCKoaCNkE')
