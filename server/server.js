efdb3c1b21f54f6985a7085a0609b3849ed6dae6
Oleh <Oleh@progressive-id.co.uk>
2023-06-08 09:03:53 +0100

    feat: Sam comments fix

--- server/server.js
+++ server/server.js
@@ -1,20 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const dotenv = require('dotenv')
-const { Pool } = require('pg')
-dotenv.config()
+const dotenv = require("dotenv");
+const { Pool } = require("pg");
+dotenv.config();
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 3001;
 const pool = new Pool({
-	connectionString: process.env.DB_URL,
-	ssl: { rejectUnauthorized: false },
-})
+  connectionString: process.env.DB_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
@@ -26,49 +25,45 @@ app.get("/running", (req, res) => {
   res.send({ express: "Your Backend Service is Running" });
 });
 
-app.get('/', async (req, res) => {
-	try {
-		const result = await pool.query('SELECT * FROM videos')
-		res.status(200).json(result.rows)
-	} catch (error) {
-		console.log(error)
-		res.status(500).json({ error: 'Error Message' })
-	}
-})
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM videos");
+    res.status(200).json(result.rows);
+  } catch (error) {
+    res.status(500).json({ error: "Error Message" });
+  }
+});
 
 // `POST` This endpoint is used to add a video to the API.
 app.post("/", async (req, res) => {
   try {
-    const rating = 0
-		const query =
-			'INSERT INTO videos (title, url, rating) VALUES ($1,$2,$3) RETURNING *'
-		const values = [title, url, rating]
-		const result = await pool.query(query, values)
-		res.status(201).json(result.rows[0])
-    }
-   catch (error) {
-		console.log(error)
-		res.status(500).json({ error: 'Error Message' })
-	}
+    const rating = 0;
+    const query =
+      "INSERT INTO videos (title, url, rating) VALUES ($1,$2,$3) RETURNING *";
+    const values = [title, url, rating];
+    const result = await pool.query(query, values);
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    res.status(500).json({ error: "Error Message" });
+  }
 });
-
 
 //`DELETE` "/{id}"
 
-app.delete('/:id', async (req, res) => {
-	try {
-		const videoID = Number(req.params.id)
-		const query = 'DELETE FROM videos WHERE id = $1 RETURNING *'
-		const values = [videoID]
-		const result = await pool.query(query, values)
-		const deleteVideos = result.rows[0]
+app.delete("/:id", async (req, res) => {
+  try {
+    const videoID = Number(req.params.id);
+    const query = "DELETE FROM videos WHERE id = $1 RETURNING *";
+    const values = [videoID];
+    const result = await pool.query(query, values);
+    const deleteVideos = result.rows[0];
 
-		if (deleteVideos) {
-			res.status(200).json(deleteVideos)
-		} else {
-			res.status(404).json({ message: 'Video Not Found' })
-		}
-	} catch (error) {
-		res.status(500).json({ error: 'Error Message' })
-	}
-})
+    if (deleteVideos) {
+      res.status(200).json(deleteVideos);
+    } else {
+      res.status(404).json({ message: "Video Not Found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error Message" });
+  }
+});
