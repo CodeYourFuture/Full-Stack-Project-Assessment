0e5be2e7d3cd82be948ba29ab21488ec74b85397
leilafarsani <108967493+leilafarsani@users.noreply.github.com>
2023-06-09 12:28:28 +0100

    up to 300 done and also deployed

--- server/routes/video.js
+++ server/routes/video.js
@@ -1,39 +1,94 @@
 const express = require("express");
 const { v4: uuid } = require("uuid");
+const { Client } = require("pg");
+
+const client = new Client({
+  host: "localhost",
+  port: 5432,
+  database: "cyf_videos",
+});
 
 const videoData = require("../../client/src/video-data.json");
 
 const router = express.Router();
 
+async function startApp() {
+  try {
+    await client.connect();
+    console.log("Connected to the database");
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    process.exit(1);
+  }
+}
+
+startApp();
+
 router.get("/", (req, res) => {
   res.send(videoData);
 });
 
-router.get("/:id", (req, res) => {
-  const video = videoData.find((video) => video.id === parseInt(req.params.id));
-  if (!video) {
-    res.status(404).json({ error: "Video not found" });
-  } else {
-    res.send(video);
+router.get("/", async (req, res) => {
+  try {
+    const result = await client.query("SELECT * FROM cyf_videos");
+    const videos = result.rows;
+    res.send(videos);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Error retrieving videos from the database" });
   }
 });
 
-router.post("/", (req, res) => {
+router.get("/:id", async (req, res) => {
+  const videoId = parseInt(req.params.id);
+  try {
+    const result = await client.query(
+      "SELECT * FROM cyf_videos WHERE id = $1",
+      [videoId]
+    );
+    const video = result.rows[0];
+    if (!video) {
+      res.status(404).json({ error: "Video not found" });
+    } else {
+      res.send(video);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error retrieving video from the database" });
+  }
+});
+
+router.post("/", async (req, res) => {
   const { title, url, rating } = req.body;
 
   if (!title || !url) {
-    res
-      .status(400)
-      .json({
-        error: "Please make sure you have entered a title and a valid URL.",
-      });
+    res.status(400).json({
+      error: "Please make sure you have entered a title and a valid URL.",
+    });
   } else {
-    const newVideo = { id: uuid(), title, url, rating };
-    videoData.push(newVideo);
-    res.send(videoData);
+    try {
+      const result = await client.query(
+        "INSERT INTO cyf_videos (id, title, url_link, rating) VALUES ($1, $2, $3, $4) RETURNING *",
+        [uuid(), title, url, rating]
+      );
+      const newVideo = result.rows[0];
+      res.send(newVideo);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Error inserting video into the database" });
+    }
   }
 });
-router.delete("/:id", (req, res) => {
-  res.send(videoData.filter((video) => video.id !== parseInt(req.params.id)));
+
+router.delete("/:id", async (req, res) => {
+  const videoId = parseInt(req.params.id);
+  try {
+    await client.query("DELETE FROM cyf_videos WHERE id = $1", [videoId]);
+    res.send({ message: "Video deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Error deleting video from the database" });
+  }
 });
+
 module.exports = router;
