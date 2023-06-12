2c4b9ef6b7be1b380b5cffbadd4b5d69a936a317
OleksChep <aachep@gmail.com>
2023-06-12 20:19:07 +0100

    add AddVideoform component
    Add content inside

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
-
+import AddVideoForm from "./components/AddVideoForm";
+import VideoCard from "./components/VideoCard";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
-      </header>
+       </header>
+       <AddVideoForm />
+       <VideoCard/>
     </div>
+    
+      
   );
 }
 
 export default App;
-  
+  <div>audio</div>
