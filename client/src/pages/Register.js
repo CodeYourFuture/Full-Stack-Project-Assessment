ec9617e355c8744c9ad788f0341ef40745327b91
Haroon <haroon.ali.1104@gmail.com>
2023-06-13 21:09:26 +0100

    create register form

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,30 @@
-export default function Register() {
-  
+import { AppContext } from "../App";
+import { useState, useContext } from "react";
 
-  return (
-    <div></div>
-  );
+export default function Register() {
+    const apiURL = useContext(AppContext);
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label>
+                    Title
+                    <br />
+                    <input id="title" type="text" name="title" value={input.title} onChange={handleChange} required />
+                </label>
+            </div>
+
+            <div>
+                <label>
+                    Url
+                    <br />
+                    <input id="url" type="text" name="url" value={input.url} onChange={handleChange} required />
+                </label>
+            </div>
+
+            <div>
+                <button className="btn-submit" type="submit">Add</button>
+            </div>
+        </form>
+    );
 }
