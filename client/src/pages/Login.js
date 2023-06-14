ece82a61fad7ea41bbbc62da8af0e8132a495f96
Haroon <haroon.ali.1104@gmail.com>
2023-06-14 11:02:27 +0100

    create login page component

---
+++ client/src/pages/Login.js
@@ -0,0 +1,85 @@
+import { AppContext } from "../App";
+import { useState, useContext } from "react";
+import Notification from "../components/Notification";
+
+export default function Register() {
+    const apiURL = useContext(AppContext);
+
+    const [notification, setNotification] = useState({
+        message: "",
+        display: false,
+        bgColor: ""
+    });
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await fetch(`${apiURL}/api/login`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password })
+            });
+
+            const data = await res.json();
+
+            if (res.status === 200) {
+                localStorage.setItem("token", data.token);
+
+                navigate("/videos");
+            } else {
+                setNotification({
+                    message: data.message,
+                    display: true,
+                    bgColor: "#E2412E"
+                });
+            }
+        } catch (error) {
+            setNotification({
+                message: error.message,
+                display: true,
+                bgColor: "#E2412E"
+            });
+        } finally {
+            setTimeout(() => {
+                setNotification({
+                    message: "",
+                    display: false,
+                    bgColor: "#E2412E"
+                });
+            }, 3000);
+        }
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <div>
+                <label>
+                    Email
+                    <br />
+                    <input id="email" type="text" value={email} onChange={(e) => { setEmail(e.target.value); }} required />
+                </label>
+            </div>
+
+            <div>
+                <label>
+                    Password
+                    <br />
+                    <input id="password" type="password" value={password} onChange={(e) => { setPassword(e.target.value); }} required />
+                </label>
+            </div>
+
+            <div>
+                <button className="btn-submit" type="submit">Login</button>
+            </div>
+            {notification.display && (
+                <Notification
+                    message={notification.message}
+                    bgColor={notification.bgColor}
+                />
+            )}
+        </form>
+    );
+}
