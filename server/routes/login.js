b7a8ed58ae39413c69bb70d7e4009150c7c2d966
Haroon <haroon.ali.1104@gmail.com>
2023-06-14 11:19:14 +0100

    add password check and token generation to login API route

--- server/routes/login.js
+++ server/routes/login.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const pool = require("../db/db");
 const validate = require("../validations/users");
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const config = require("config");
 
 router.post("/", async (req, res) => {
     const { error } = validate(req.body);
@@ -10,6 +13,13 @@ router.post("/", async (req, res) => {
 
     const rs = await pool.query("SELECT * FROM users WHERE email = $1", [req.body.email]);
     if (rs.rowCount <= 0) return res.status(400).json({ message: "User already registered." });
+
+    const validPassword = await bcrypt.compare(req.body.password, rs.rows[0].password);
+    if (!validPassword) return res.status(400).send({ message: "Invalid email or password." });
+
+    const token = jwt.sign({ uId: rs.rows[0].id, email: rs.rows[0].email }, config.get("jwtPrivateKey"));
+
+    res.json({ token });
 });
 
 module.exports = router;
