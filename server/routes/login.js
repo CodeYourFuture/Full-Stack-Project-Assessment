71e676ec1865b35f48c32896610be0e7b6b77e39
Haroon <haroon.ali.1104@gmail.com>
2023-06-14 11:13:00 +0100

    add validation and user check in login API route

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,7 +5,11 @@ const validate = require("../validations/users");
 const bcrypt = require('bcrypt');
 
 router.post("/", async (req, res) => {
-    
+    const { error } = validate(req.body);
+    if (error) return res.status(400).json({ message: error.message });
+
+    const rs = await pool.query("SELECT * FROM users WHERE email = $1", [req.body.email]);
+    if (rs.rowCount <= 0) return res.status(400).json({ message: "User already registered." });
 });
 
 module.exports = router;
