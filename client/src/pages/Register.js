c1acc7dfd55c734121f5d9174790251c0eec729c
Haroon <haroon.ali.1104@gmail.com>
2023-06-15 21:26:21 +0100

    animate register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,6 @@
 import { AppContext } from "../App";
 import { useState, useContext } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import Notification from "../components/Notification";
 
 export default function Register() {
@@ -58,32 +59,47 @@ export default function Register() {
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <div>
-                <label>
-                    Email
-                    <br />
-                    <input id="email" type="text" value={email} onChange={(e) => { setEmail(e.target.value); }} required />
-                </label>
-            </div>
+        <motion.div
+            initial={{ opacity: 0, y: +50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ ease: "easeOut", duration: 1.5 }}
+        >
+            <form onSubmit={onSubmit}>
+                <div>
+                    <label>
+                        Email
+                        <br />
+                        <input id="email" type="text" value={email} onChange={(e) => { setEmail(e.target.value); }} required />
+                    </label>
+                </div>
 
-            <div>
-                <label>
-                    Password
-                    <br />
-                    <input id="password" type="password" value={password} onChange={(e) => { setPassword(e.target.value); }} required />
-                </label>
-            </div>
+                <div>
+                    <label>
+                        Password
+                        <br />
+                        <input id="password" type="password" value={password} onChange={(e) => { setPassword(e.target.value); }} required />
+                    </label>
+                </div>
 
-            <div>
-                <button className="btn-submit" type="submit">Register</button>
-            </div>
-            {notification.display && (
-                <Notification
-                    message={notification.message}
-                    bgColor={notification.bgColor}
-                />
-            )}
-        </form>
+                <div>
+                    <button className="btn-submit" type="submit">Register</button>
+                </div>
+                <AnimatePresence>
+                    {notification.display && (
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ ease: "easeOut", duration: 1.5 }}
+                            exit={{ opacity: 0 }}
+                        >
+                            <Notification
+                                message={notification.message}
+                                bgColor={notification.bgColor}
+                            />
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+            </form>
+        </motion.div>
     );
 }
