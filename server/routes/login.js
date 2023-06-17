ef84da8e508bf00c14189d8b7f90cf6e0c34f04a
Haroon <haroon.ali.1104@gmail.com>
2023-06-17 13:01:04 +0100

    add error handling to register and login API routes

--- server/routes/login.js
+++ server/routes/login.js
@@ -11,15 +11,19 @@ router.post("/", async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).json({ message: error.message });
 
-    const rs = await pool.query("SELECT * FROM users WHERE email = $1", [req.body.email]);
-    if (rs.rowCount <= 0) return res.status(400).json({ message: "User already registered." });
+    try {
+        const rs = await pool.query("SELECT * FROM users WHERE email = $1", [req.body.email]);
+        if (rs.rowCount <= 0) return res.status(400).json({ message: "User already registered." });
 
-    const validPassword = await bcrypt.compare(req.body.password, rs.rows[0].password);
-    if (!validPassword) return res.status(400).send({ message: "Invalid email or password." });
+        const validPassword = await bcrypt.compare(req.body.password, rs.rows[0].password);
+        if (!validPassword) return res.status(400).send({ message: "Invalid email or password." });
 
-    const token = jwt.sign({ uId: rs.rows[0].id, email: rs.rows[0].email }, config.get("jwtPrivateKey"));
+        const token = jwt.sign({ uId: rs.rows[0].id, email: rs.rows[0].email }, config.get("jwtPrivateKey"));
 
-    res.json({ token });
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 });
 
 module.exports = router;
--- server/routes/register.js
+++ server/routes/register.js
@@ -8,15 +8,19 @@ router.post("/", async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).json({ message: error.message });
 
-    const rs = await pool.query("SELECT * FROM users WHERE email = $1", [req.body.email]);
-    if (rs.rowCount > 0) return res.status(400).json({ message: "User already registered." });
-
-    const salt = await bcrypt.genSalt(10);
-    const password = await bcrypt.hash(req.body.password, salt);
-
-    await pool.query("INSERT INTO users (email, password) VALUES($1, $2)", [req.body.email, password]);
-
-    res.json({ message: "User registered." });
+    try {
+        const rs = await pool.query("SELECT * FROM users WHERE email = $1", [req.body.email]);
+        if (rs.rowCount > 0) return res.status(400).json({ message: "User already registered." });
+    
+        const salt = await bcrypt.genSalt(10);
+        const password = await bcrypt.hash(req.body.password, salt);
+    
+        await pool.query("INSERT INTO users (email, password) VALUES($1, $2)", [req.body.email, password]);
+    
+        res.json({ message: "User registered." });
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 });
 
 module.exports = router;
