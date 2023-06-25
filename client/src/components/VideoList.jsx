422928d4569fe05f713354b7c0d68afbb395b4b4
elahemortazavi <emor5680@gmail.com>
2023-06-25 18:44:57 +0100

    VideoList.js

--- client/src/components/VideoList.jsx
+++ client/src/components/VideoList.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import Video from "./Video";
+import "./VideoList.css";
+import Data from "./exampleresponse.json";
 
 const VideoList = () => {
-    return (
-        <div>
-            videolist
-        </div>
-    );
+  return (
+    <div className="VideoList">
+      {Data.map((item) => (
+        <Video
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          url={item.url}
+          rating={item.rating}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default VideoList;
