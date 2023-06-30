a79d66037b02d083fc5709c78109013278a87a9b
elahemortazavi <emor5680@gmail.com>
2023-06-30 15:29:14 +0100

    db add

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const { Pool } = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
 const port = process.env.PORT || 5000;
 const app = express();
+
+const pool = new Pool({ 
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
+
 app.use(cors());
 app.use(express.json());
 
@@ -9,57 +20,57 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [
-  {
-    id: 82653,
-    title: "Mac & Cheese | Basics with Babish",
-    url: "https://www.youtube.com/watch?v=FUeyrEN14Rk",
-    rating: 2111,
-  },
-  {
-    id: 858566,
-    title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
-    url: "https://www.youtube.com/watch?v=xbs7FT7dXYc",
-    rating: 11,
-  },
-  {
-    id: 453538,
-    title:
-      "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
-    url: "https://www.youtube.com/watch?v=4As0e4de-rI",
-    rating: 3211,
-  },
-  {
-    id: 283634,
-    title: "Learn Unity - Beginner's Game Development Course",
-    url: "https://www.youtube.com/watch?v=gB1F9G0JXOo",
-    rating: 211,
-  },
-  {
-    id: 562824,
-    title: "Cracking Enigma in 2021 - Computerphile",
-    url: "https://www.youtube.com/watch?v=RzWB5jL5RX0",
-    rating: 111,
-  },
-  {
-    id: 442452,
-    title: "Coding Adventure: Chess AI",
-    url: "https://www.youtube.com/watch?v=U4ogK0MIzqk",
-    rating: 671,
-  },
-  {
-    id: 536363,
-    title: "Coding Adventure: Ant and Slime Simulations",
-    url: "https://www.youtube.com/watch?v=X-iSQQgOd1A",
-    rating: 76,
-  },
-  {
-    id: 323445,
-    title: "Why the Tour de France is so brutal",
-    url: "https://www.youtube.com/watch?v=ZacOS8NBK6U",
-    rating: 73,
-  },
-];
+// let videos = [
+//   {
+//     id: 82653,
+//     title: "Mac & Cheese | Basics with Babish",
+//     url: "https://www.youtube.com/watch?v=FUeyrEN14Rk",
+//     rating: 2111,
+//   },
+//   {
+//     id: 858566,
+//     title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
+//     url: "https://www.youtube.com/watch?v=xbs7FT7dXYc",
+//     rating: 11,
+//   },
+//   {
+//     id: 453538,
+//     title:
+//       "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
+//     url: "https://www.youtube.com/watch?v=4As0e4de-rI",
+//     rating: 3211,
+//   },
+//   {
+//     id: 283634,
+//     title: "Learn Unity - Beginner's Game Development Course",
+//     url: "https://www.youtube.com/watch?v=gB1F9G0JXOo",
+//     rating: 211,
+//   },
+//   {
+//     id: 562824,
+//     title: "Cracking Enigma in 2021 - Computerphile",
+//     url: "https://www.youtube.com/watch?v=RzWB5jL5RX0",
+//     rating: 111,
+//   },
+//   {
+//     id: 442452,
+//     title: "Coding Adventure: Chess AI",
+//     url: "https://www.youtube.com/watch?v=U4ogK0MIzqk",
+//     rating: 671,
+//   },
+//   {
+//     id: 536363,
+//     title: "Coding Adventure: Ant and Slime Simulations",
+//     url: "https://www.youtube.com/watch?v=X-iSQQgOd1A",
+//     rating: 76,
+//   },
+//   {
+//     id: 323445,
+//     title: "Why the Tour de France is so brutal",
+//     url: "https://www.youtube.com/watch?v=ZacOS8NBK6U",
+//     rating: 73,
+//   },
+// ];
 
 // GET root
 app.get("/", (req, res) => {
@@ -68,25 +79,71 @@ app.get("/", (req, res) => {
 });
 
   // GET 
-app.get("/videos", (req, res) => {
-  res.json(videos);
+app.get("/videos", async (req, res) => {
+  try {
+    const query = `SELECT * FROM videos`;
+    const result = await pool.query(query);
+    const videos = result.rows;
+    res.json(videos);
+  } catch (error) {
+    res.status(500).json({err: "fetching error"})
+  }
+  
 });
 
 // POST 
-app.post("/videos", (req, res) => {
-  const newVideo = req.body;
-  videos.push(newVideo);
-  res.status(200).json(newVideo);
+app.post("/videos", async(req, res) => {
+  try {
+    const { title, url } = req.body;
+    const rating = 0;
+    const query ="INSERT INTO videos (title, url, rating) VALUES ($1, $2, $3) RETURNING *";
+    const values = [title, url, rating]; 
+    const result = await pool.query(query, values); 
+    const newVideo = result.rows[0]; 
+
+    res.status(201).json(newVideo); 
+  } catch (error) {
+    console.error("Error creating video:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the video" }); 
+  
+  }
+  
 });
 
 // DELETE 
-app.delete("/videos/:id", (req, res) => {
-  const videoId = req.params.id;
-  const index = videos.findIndex((video) => video.id === parseInt(videoId));
-  if (index !== -1) {
-    const removedVideo = videos.splice(index, 1);
-    res.json(removedVideo);
-  } else {
-    res.status(404).json({ message: "Video does not exist" });
+app.delete("/videos/:id", async (req, res) => {
+  try {
+    const videoId = req.params.id; 
+
+    const query = "DELETE FROM videos WHERE id = $1 RETURNING *"; 
+    const values = [videoId]; 
+    const result = await pool.query(query, values); 
+    const removedVideo = result.rows[0]; 
+
+    if (removedVideo) {
+      res.json(removedVideo); 
+    } else {
+      res.status(404).json({ message: "Video not found" }); 
+    }
+  } catch (error) {
+    console.error("Error deleting video:", error);
+    res
+      .status(500)
+      .json({ error: "deleting error" }); 
   }
 });
+
+//   // PUT (Update Rating)
+// app.put("/videos/:id/rating", (req, res) => {
+//   const videoId = req.params.id;
+//   const { rating } = req.body;
+//   const VideoIndex = videos.findIndex((video) => video.id === parseInt(videoId));
+//   if (VideoIndex !== -1) {
+//     videos[VideoIndex].rating = rating;
+//     res.json({ message: "Rating updated" });
+//   } else {
+//     res.status(404).json({ message: "err" });
+//   }
+//})
