57a11bcb2f357527cbab1d23ba194eec6856a7d9
Olha Danylevska <odanilevskaya2@gmail.com>
2023-08-03 12:25:48 +0100

    1

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
@@ -13,3 +14,7 @@ app.get("/", (req, res) => {
   // Delete this line after you've confirmed your server is running
   res.send({ express: "Your Backend Service is Running" });
 });
+
+app.get("/videos", (req, res) => {
+  res.send()
+})
