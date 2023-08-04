aa7fcf5d0a83ad7e7c9aa2d61cb7aad8d4d21196
Olha Danylevska <odanilevskaya2@gmail.com>
2023-08-04 18:24:40 +0100

    level 200 done

---
+++ server/node_modules/cours/module.js
@@ -0,0 +1,9 @@
+var direBonjour = function() {
+    console.log('bonjour');
+}
+
+var direBonsoir = function() {
+    console<;<log('bonsoir');
+}
+//module.exports = direBonjour;
+module.exports= {sayHello : direBonjour};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const AllVideos = require("../client/src/exampleresponse.json")
+const cors = require("cors");
+const short = require('short-uuid');
+const translator = short("12345")
 
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = AllVideos;
 
 // GET "/"
 app.get("/", (req, res) => {
@@ -15,6 +22,55 @@ app.get("/", (req, res) => {
   res.send({ express: "Your Backend Service is Running" });
 });
 
-app.get("/videos", (req, res) => {
-  res.send()
+
+
+app.post("/videos", (req, res) => {
+  let newVideo = req.body
+  console.log(req.body)
+
+  if (!newVideo.title || !newVideo.url || newVideo.title === "" || newVideo.url === "") {
+    res.status(400).send("Fill in the missing fields")
+  } else {
+    newVideo.title = req.body.title
+    newVideo.url = req.body.url
+    newVideo.id = (translator.generate()).slice(4, 10)
+    videos.push(newVideo)
+    res.send(videos)
+  }
+
+});
+
+
+app.delete("/videos/:id", (req, res) => {
+  let videoId = req.params.id
+
+  res.send(deleteVideoByID(videos, videoId))
 })
+
+
+app.get("/videos", (req, res) => {
+  res.send(videos)
+})
+
+app.get("/videos/:id", (req, res) => {
+  let id = req.params.id
+  res.send(findVideoByID(id))
+})
+
+
+
+const findVideoByID = (id) => {
+  return videos.filter(video => {
+    return video.id == id
+  })
+}
+
+
+const deleteVideoByID = (videos, id) => {
+  let videoI = videos.findIndex((video) => video.id == id);
+  if (videoI > -1) {
+    videos.splice(videoI, 1)
+  }
+  return videos
+
+}
