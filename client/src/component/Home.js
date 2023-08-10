cb461efa0c97b9deb0083a1e0c50bab787138538
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-08-10 21:00:44 +0100

    finish display videos and styling

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="home-div">
       <h1>Home Component</h1>
-      <div class="carousel-caption d-none d-md-block">
+      <div className="carousel-caption d-none d-md-block">
         <h5>Slider One Item</h5>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime,
--- client/src/component/Videos.js
+++ client/src/component/Videos.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from "react";
+import videoData from "../exampleresponse.json";
+
 
 const Videos = () => {
+
   return (
-    <div className='videos-div'>
+    <div className="videos-div">
       <h1>Videos Component</h1>
+      <div className="vid-div">
+        {videoData.map((video) => (
+          <div key={video.id} className="video-card">
+            <video controls className="vid-size">
+              <source src={video.url} type="video/mp4" />
+            </video>
+            <div>
+              <h4>{video.title}</h4>
+              <h4>{`Rating: ${video.rating}`}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
-export default Videos
+export default Videos;
+
+// <div class="card">
+//     <h2>{video.title}</h2>
+//     <div className="video">
+//       <ReactPlayer url={video.url} controls={true} />
+//     </div>
+//   </div>
