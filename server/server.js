c5c8a5c4fd621b738cb5bd9a078a7c6cd64ed6c2
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-08-13 22:14:29 +0100

    set up server to get data and get by id

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
-const videosData = require('./exampleresponse.json');
+const videosData = JSON.parse(fs.readFileSync('./exampleresponse.json'));
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.use(express.json());
+
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = videosData;
 
 // GET "/"
 app.get("/", (req, res) => {
@@ -16,5 +18,16 @@ app.get("/", (req, res) => {
 });
 
 app.get('/videos/data', (req, res) => {
-  res.send(videosData);
+  res.send(videos);
 })
+
+app.get('/videos/data/:id', (req, res) => {
+  const videoID = Number(req.params.id)
+  const getVideoByID = videos.find((video) => video.id === videoID)
+  res.status(200).send(getVideoByID);
+})
+
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
