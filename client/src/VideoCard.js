744bd1534c8d2f67f68b28cb49945795a0dc33c0
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-08-16 19:33:25 +0100

    Added Searchbar component file

--- client/src/VideoCard.js
+++ client/src/VideoCard.js
@@ -1,8 +1,9 @@
 import React from "react";
 
+// add onClick for upvote and downvote
 export default function VideoCard(props) {
   return (
-    <div>
+    <div className="video--card">
       <h1>{props.title}</h1>
       <p>{props.url}</p>
       <p>{props.rating}</p>
