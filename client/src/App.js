c3a97923498df8d623aecc5d5ef5b8db069f400d
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-08-16 20:39:33 +0100

    Rendered videos

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import videoData from "./exampleresponse.json";
 import VideoList from "./VideoList";
 
 function App() {
-  // const [videos, setVideos] = useState(videoData);
+  const [videos, setVideos] = useState(videoData);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
-      <VideoList videos={videoData} />
+      <VideoList videos={videos} />
     </div>
   );
 }
--- client/src/VideoCard.js
+++ client/src/VideoCard.js
@@ -2,10 +2,22 @@ import React from "react";
 
 // add onClick for upvote and downvote
 export default function VideoCard(props) {
+  const videoId = props.url.split("watch?v=")[1];
+
+  console.log(props.url.split("watch?v=")[1]);
+
   return (
     <div className="video--card">
-      <h1>{props.title}</h1>
-      <p>{props.url}</p>
+      <h2>{props.title}</h2>
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${videoId}`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
       <p>{props.rating}</p>
     </div>
   );
