dfc5c901ff7edf460bc5e2f4ff66041bff7c79b5
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-08-17 18:29:49 +0100

    Edited AddVideoForm

--- client/src/components/AddVideoForm.js
+++ client/src/components/AddVideoForm.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 export default function AddVideoForm() {
+  function handleAddVideo() {
+    console.log("video added");
+  }
+
   return (
     <div className="form">
       <input type="text" placeholder="Title" className="form--input" />
       <input type="text" placeholder="url" className="form--input" />
-      <button className="form--button">Add New Video</button>
+      <button onClick={handleAddVideo} className="form--button">
+        Add New Video
+      </button>
     </div>
   );
 }
