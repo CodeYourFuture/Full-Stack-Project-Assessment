64a49107e4ae47f69db467e04162c26b5dded06e
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-08-17 18:15:45 +0100

    Created AddVideoForm component file

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import React, { useState } from "react";
 import videoData from "./exampleresponse.json";
 import VideoList from "./VideoList";
+import AddVideoForm from "./AddVideoForm";
+import Searchbar from "./Searchbar";
 
 function App() {
   const [videos, setVideos] = useState(videoData);
@@ -11,6 +13,10 @@ function App() {
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
+      <div>
+        <AddVideoForm />
+        <Searchbar />
+      </div>
       <div className="video--container">
         <VideoList videos={videos} />
       </div>
---
+++ client/src/Searchbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Searchbar() {
+  return (
+    <>
+      <input type="text" placeholder="Search..."></input>
+    </>
+  );
+}
