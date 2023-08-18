e06dcb3ba84d744dc402f09c9f91a8e3ea062da9
malkitbenning <malkitbenning@gmail.com>
2023-08-18 21:33:23 +0100

    video changes now work on API

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
+let cors = require("cors");
+app.use(cors());
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
@@ -13,6 +17,11 @@ let videos = require("./exampleresponse.json");
 
 // GET "/"
 app.get("/", (req, res) => {
+  res.json(videos);
+});
+
+app.get("/info", (req, res) => {
+  console.log("got called info");
   res.json(videos);
 });
 
@@ -42,6 +51,21 @@ app.delete("/:id", function (req, res) {
   }
 });
 
+app.put("/:id", function (req, res) {
+  console.log("put body", req.body);
+  let searchId = Number(req.params.id);
+  let putIndex = videos.findIndex((video) => video.id === searchId);
+  if (putIndex >= 0) {
+    videos[putIndex].rating = Number(req.body.rating);
+    res.status(200).json({});
+  } else {
+    res.status(404).json({
+      result: "failure",
+      message: "Video could not be updated",
+    });
+  }
+});
+
 app.post("/", (req, res) => {
   console.log(req.body);
   if (!req.body.title || !req.body.url) {
@@ -53,10 +77,11 @@ app.post("/", (req, res) => {
   let maxId = Math.max(...videos.map((video) => video.id));
   let newRec = {
     id: maxId + 1,
-    title: req.title,
-    url: req.url,
+    title: req.body.title,
+    url: req.body.url,
     rating: 0,
   };
+  console.log("new rec is ", newRec);
   videos.push(newRec);
   res.status(201).json({
     id: newRec.id,
