88fdbd23a6204af9362f24939a712434fd07dead
Afsha10 <unityofbeing1@gmail.com>
2023-08-18 12:20:45 +0100

    Set up gitignore, tailwind
    Tested: Tailwind and it works

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-right App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
