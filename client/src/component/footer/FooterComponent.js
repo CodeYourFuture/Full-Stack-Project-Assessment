73c5291399cde33564d18609a6831da5540a67cd
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-08-18 18:30:25 +0100

    update

--- client/src/component/footer/FooterComponent.js
+++ client/src/component/footer/FooterComponent.js
@@ -11,7 +11,7 @@ const FooterComponent = () => {
                 
                 <li><a href="http://localhost:3000/"><i className="fa fa-twitter"></i></a></li>
                 
-                <li><a href="http://localhost:3000/"><i className="fa fa-linkedin"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/abubakar-meigag-0a625a17a/"><i className="fa fa-linkedin"></i></a></li>
                 
                 <li><a href="http://localhost:3000/"><i className="fa fa-codepen"></i></a></li>
             </ul>
