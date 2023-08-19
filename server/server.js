fcb81d3fa772b6ca84eb1d23700b7ce1db55d328
mandy <109825538+m4ndycheung@users.noreply.github.com>
2023-08-19 21:42:01 +0100

    Set up GET
    Returns all videos

--- server/server.js
+++ server/server.js
@@ -6,10 +6,10 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+const videos = require("./exampleresponse.json");
 
 // GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+// Returns all the videos
+app.get("/", (request, response) => {
+  response.json(videos);
 });
