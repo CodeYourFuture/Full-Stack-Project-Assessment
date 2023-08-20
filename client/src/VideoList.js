5ef222dc63a9a122b6c3e6d3dd879cb4866c037c
Delnia Alipour <delniaalipur@gmail.com>
2023-08-20 13:01:15 +0100

    add client side

---
+++ client/src/VideoList.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Video from "./Video";
+
+const VideoList = ({ videos, onVote, onDelete }) => {
+  return (
+    <div className="video-list">
+      {videos.map((video) => (
+        <Video
+          key={video.id}
+          video={video}
+          onVote={onVote}
+          onDelete={onDelete}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default VideoList;
