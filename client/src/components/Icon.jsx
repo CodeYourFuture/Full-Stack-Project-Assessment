ed9d8c5b2af7c5aaa4c6e0e9ec2f173bde04c100
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-08-21 19:38:12 +0100

    level 100 wip

---
+++ client/src/components/Icon.jsx
@@ -0,0 +1,9 @@
+// const Icon = ({props}) => {
+//     return(
+//         <div>
+//             {props=<i class="bi bi-hand-thumbs-up"></i>}
+//             {props<i class="bi bi-hand-thumbs-down"></i>}
+//         </div>
+//     )
+
+
--- client/src/components/MainCard.jsx
+++ client/src/components/MainCard.jsx
@@ -1,5 +1,6 @@
 import data from "../exampleresponse.json";
 
+
 const MainCard = () => {
     return(
         <div>
@@ -7,10 +8,15 @@ const MainCard = () => {
              {data.map((video) => {
                 return(
              <div>
-            <li key = {video.id} ></li>
-            <li>{video.title}</li>
-            <li>{<iframe width="560" height="315" src={video.url.replace("watch?v=", "embed/")} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>}</li>
-            <li>{video.rating}</li>
+            <li key = {video.id} >
+            <p>{video.title}</p>
+            {<iframe width="560" height="315" src={video.url.replace("watch?v=", "embed/")} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>}
+           <p>{video.rating}</p>
+           <p>
+            <button><i class='fa fa-thumbs-up'></i></button>
+            <button><i class='fa fa-thumbs-down'></i></button></p>
+           
+          </li>
              
             
              </div> 
