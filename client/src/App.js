8eeb95405f7979eb16fdb9d00e943555874480fa
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-08-21 17:56:37 +0100

    level 100

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import MainCard from "./components/MainCard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
+        <MainCard />
       </header>
     </div>
   );
