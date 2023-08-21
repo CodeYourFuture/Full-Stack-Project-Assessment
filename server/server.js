3f79f1bdc7c16a882d63e46de0986725a536da5f
nasir ali <a.nasirali@outlook.com>
2023-08-21 13:35:30 +0100

    server still under progress

--- server/server.js
+++ server/server.js
@@ -6,10 +6,53 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
 
-// GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+app.use(express.json());
+
+const videos = []; // in-memory array to store videos
+
+// Get all videos
+app.get("/videos", (req, res) => {
+  res.json(videos);
+});
+
+// Get single video
+app.get("/videos/:id", (req, res) => {
+  const video = videos.find((v) => v.id === parseInt(req.params.id));
+  if (!video) return res.status(404).send("Video not found");
+  res.json(video);
+});
+
+// Add new video
+app.post("/videos", (req, res) => {
+  const { title, url } = req.body;
+
+  if (!title || !url) {
+    return res.status(400).send("Title and url are required");
+  }
+
+  const newVideo = {
+    id: videos.length + 1,
+    title,
+    url,
+  };
+
+  videos.push(newVideo);
+  res.status(201).json(newVideo);
+});
+
+// Delete video
+app.delete("/videos/:id", (req, res) => {
+  videos = videos.filter((v) => v.id !== parseInt(req.params.id));
+  res.status(204).end();
+});
+
+// Update video
+app.put("/videos/:id", (req, res) => {
+  // TODO: update video by id
+  res.status(200).end();
+});
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
 });
