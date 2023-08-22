fee5852080536372a1e2b415a243b04e5d352c87
Saliha Popal <salihapopal@gmail.com>
2023-08-22 23:03:57 +0100

    Done level 200.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,87 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(express.json()); // before our routes definition
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = [ 
+{
+  "id": 523523,
+  "title": "Never Gonna Give You Up",
+  "url": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+  "rating": 23
+},
+{
+  "id": 523427,
+  "title": "The Coding Train",
+  "url": "https://www.youtube.com/watch?v=HerCR8bw_GE",
+  "rating": 230
+},
+{
+  "id": 82653,
+  "title": "Mac & Cheese | Basics with Babish",
+  "url": "https://www.youtube.com/watch?v=FUeyrEN14Rk",
+  "rating": 2111
+},
+{
+  "id": 858566,
+  "title": "Videos for Cats to Watch - 8 Hour Bird Bonanza",
+  "url": "https://www.youtube.com/watch?v=xbs7FT7dXYc",
+  "rating": 11
+}
+];
 
 // GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  console.log("Get <----Getting all videos");
+  res.status(200).json({videos});
+  });
+
+// Post "/:id"
+app.post("/:id", (req, res) => {
+  console.log("POST <---- add a new video route", req.body);
+  const {title, url} = req.body;
+  if(!title || !url){
+  return res.status(404).json({
+  result: "failure",
+  message: "Video could not be saved"
+  })
+  }
+  const id = Date.now();
+  const video = {id, title, url, rating: 0 };
+  videos.push(video);
+  res.status(201).json({id});
+  })
+
+
+// GET "/:id"
+app.get("/:id", (req, res) => {
+  console.log("GET /:id", req.params.id)
+  const videoId = req.params.id; 
+  const video = videos.find(video => video.id == videoId);
+  
+  if (!video) {
+    return res.status(404).json({ 
+      message: "Failed to find the video." 
+    });
+  }
+  res.status(200).json({ video });
 });
+
+
+// Delete the video
+app.delete("/:id", (req, res) =>{
+  console.log("Delete /:id", req.params.id);
+  const idToDelete = req.params.id;
+  const video = videos.filter(video => video.id == idToDelete);
+   
+  if(!video){
+    res.status(404).json({message: "Video not deleted"});
+  }
+  return res.status(204).json({video});
+})
