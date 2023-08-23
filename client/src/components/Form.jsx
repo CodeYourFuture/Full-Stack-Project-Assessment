e970c84bec5053fdf42384dd93159b52a1cbd2e2
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-08-24 00:43:30 +0100

    create form componenents

---
+++ client/src/components/Form.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const Form = ({ onAdd }) => {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && url) {
+      onAdd({ title, url });
+      setTitle("");
+      setUrl("");
+    }
+  };
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="url"
+        placeholder="YouTube URL"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <button type="submit">Add Video</button>
+    </form>
+  );
+};
+
+export default Form;
