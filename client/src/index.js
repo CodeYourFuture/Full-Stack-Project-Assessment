afaec74f76ff0ccbfa933658fd7d5202c424a697
softacoder <113185323+softacoder@users.noreply.github.com>
2023-08-23 08:30:01 +0100

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,6 @@
 // update 1
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// original
 // import React from "react";
 // import ReactDOM from "react-dom";
-// import "./index.css";
 // import App from "./App";
 
 // ReactDOM.render(
@@ -22,3 +9,16 @@ ReactDOM.render(
 //   </React.StrictMode>,
 //   document.getElementById("root")
 // );
+
+// original
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
