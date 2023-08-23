bc7504ea02c68e398107de62c5cbd8c093a5fa63
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-08-23 23:33:04 +0100

    finishing level 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; // Import Routes instead of Switch
+import React, {useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"; 
 
 import "./App.css";
 import Videos from "./Videos.js";
 
 function App() {
+  
   const [showVideos, setShowVideos] = useState(false);
+ 
+  
+
+
+     
+  
   return (
     <Router>
       <div className="App">
@@ -13,6 +20,7 @@ function App() {
           <h1>Video Recommendation</h1>
         </header>
         <nav className="nav">
+          
           <Link to="/videos" onClick={() => setShowVideos(true)}>
             Videos
           </Link>
--- client/src/Videos.js
+++ client/src/Videos.js
@@ -1,14 +1,18 @@
-import { useEffect, useState } from "react";
-import  React from "react";
-import ShowVideos from "./ShowVideos";
+import React, { useEffect, useState } from "react";
 
 function Videos(props) {
+  const [loadData, setLoadData] = useState([]);
+  const [titleData, setTitleData] = useState("");
+  const [urlData, setUrlData] = useState("");
 
-   const [loadData,setLoadData] = useState([]);
-    const[titleData,setTitleData] = useState();
-    const [urlData, setUrlData] = useState();
+  // fetch("http://localhost:3000/videos")
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     return setLoadData(data);
+  //   });
 
-    useEffect(()=>{const getData = async () => {
+  useEffect(() => {
+    const getData = async () => {
       try {
         const response = await fetch("http://localhost:3000/videos");
         if (!response.ok) {
@@ -19,37 +23,44 @@ function Videos(props) {
       } catch (error) {
         console.error("Error fetching data:", error);
       }
-    };getData()},[setLoadData])
+    };
+    getData();
+  },[setLoadData]);
 
+  function cancelBtnHandler(e) {
+   
+    props.setShow(false);
+  }
 
-    function cancelBtnHandler(e){
-      e.preventDefault();
-      props.setShow(false);
-    }
-
-
-    function addClickHandeler(e) {
-
-      e.preventDefault();
-      const newVideo = {  title: titleData, url: urlData };
-fetch("http://localhost:3000/videos", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify(newVideo),
-})
-  .then((response) => response.json())
-  .then((data) => {
-    setLoadData((prevLoadData) => [...prevLoadData, newVideo]);
-    setTitleData("");
-    setUrlData("");
-  })
-  .catch((error) => {
-    console.error("Error adding video:", error);
-  });
-
-    }
+  function addClickHandeler(e) {
+    e.preventDefault();
+    const newVideo = { title: titleData, url: urlData };
+    
+    fetch("http://localhost:3000/videos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newVideo),
+      
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setLoadData(data);
+        
+        setTitleData("");
+        setUrlData("");
+      })
+    .catch((error) => {
+      console.error("Error adding video:", error);
+    });
+  }
+function deleteBtnHandler() {
+  const deletData = 
+  
+}
+  
   return (
     <>
       {props.show && (
@@ -58,7 +69,6 @@ fetch("http://localhost:3000/videos", {
             <label htmlFor="title">Title</label>
             <input
               id="title"
-              value={titleData}
               onChange={(e) => setTitleData(e.target.value)}
               required
             />
@@ -67,23 +77,41 @@ fetch("http://localhost:3000/videos", {
             <label htmlFor="url">Url</label>
             <input
               id="url"
-              value={urlData}
               onChange={(e) => setUrlData(e.target.value)}
               required
             />
           </div>
-        
+
           <div className="addCancelBtn">
-            <button onClick={cancelBtnHandler}>
+            <button type="button" onClick={cancelBtnHandler}>
               Cancel
             </button>
-            <button type="submit">
-              Add
-            </button>
+            <button type="submit">Add</button>
           </div>
         </form>
       )}
+      {loadData.length > 0 ? (
+        loadData.map((item) => (
+          <div key={item.id}>
+            <p style={{ margin: 5 }}>{item.title}</p>
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/{VIDEO_ID_GOES_HERE}"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+
+            {/* <div className="showVideo">{item.url}</div> */}
+            <button onClick={deleteBtnHandler}>Delete</button>
+          </div>
+        ))
+      ) : (
+        <p>No video yet.</p>
+      )}
     </>
-  );  
+  );
 }
 export default Videos;
