5f97c3ff6aeafa2c48c2378d31cf443cf70346a0
fowobi <109865505+fowobi@users.noreply.github.com>
2023-08-23 22:26:49 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
