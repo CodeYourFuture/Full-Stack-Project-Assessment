c881f2fe38480f59dec91ff923323664d380a3c4
AppolinFotso <appolin.s.fotso@gmail.com>
2023-08-24 10:24:24 +0100

    Added database query for GET request

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const CORS = require("cors");
 const urid = require("urid");
+const { Pool } = require("pg");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -11,75 +12,30 @@ app.use(express.json());
 app.use(CORS());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [
-  {
-    id: 523523,
-    title: "Never Gonna Give You Up",
-    url: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-    rating: 23,
-  },
-  {
-    id: 523427,
-    title: "The Coding Train",
-    url: "https://www.youtube.com/embed/HerCR8bw_GE",
-    rating: 230,
-  },
-  {
-    id: 82653,
-    title: "Mac & Cheese | Basics with Babish",
-    url: "https://www.youtube.com/embed/FUeyrEN14Rk",
-    rating: 2111,
-  },
-  {
-    id: 858566,
-    title: "Videos for Cats to Watch - 8 Hour Bird Bonanza",
-    url: "https://www.youtube.com/embed/xbs7FT7dXYc",
-    rating: 11,
-  },
-  {
-    id: 453538,
-    title:
-      "The Complete London 2012 Opening Ceremony | London 2012 Olympic Games",
-    url: "https://www.youtube.com/embed/4As0e4de-rI",
-    rating: 3211,
-  },
-  {
-    id: 283634,
-    title: "Learn Unity - Beginner's Game Development Course",
-    url: "https://www.youtube.com/embed/gB1F9G0JXOo",
-    rating: 211,
-  },
-  {
-    id: 562824,
-    title: "Cracking Enigma in 2021 - Computerphile",
-    url: "https://www.youtube.com/embed/RzWB5jL5RX0",
-    rating: 111,
-  },
-  {
-    id: 442452,
-    title: "Coding Adventure: Chess AI",
-    url: "https://www.youtube.com/embed/U4ogK0MIzqk",
-    rating: 671,
-  },
-  {
-    id: 536363,
-    title: "Coding Adventure: Ant and Slime Simulations",
-    url: "https://www.youtube.com/embed/X-iSQQgOd1A",
-    rating: 76,
-  },
-  {
-    id: 323445,
-    title: "Why the Tour de France is so brutal",
-    url: "https://www.youtube.com/embed/ZacOS8NBK6U",
-    rating: 73,
-  },
-];
+// database connection
+const pool = new Pool({
+  user: "coder",
+  password: "glasgow321!",
+  host: "localhost",
+  database: "videos_storage",
+  port: 5432,
+});
+//
 
 // GET "/"
 app.get("/", (req, res) => {
-  res.json(videos);
+  const getVideos = async () => {
+    try {
+      const videos = await pool.query("SELECT * from videos");
+      console.log(videos.rows);
+      res.json(videos.rows);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      pool.end();
+    }
+  };
+  getVideos();
 });
 
 // POST "/"
