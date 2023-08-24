d2f4a1d758c0d71c12863a35d23de70d934ced87
Haroon <haroon.ali.1104@gmail.com>
2023-08-24 23:03:08 +0100

    improve video validation in API

--- server/validations/videos.js
+++ server/validations/videos.js
@@ -4,6 +4,13 @@ function validate(video) {
     const schema = Joi.object({
         userId: Joi.number().required(),
         url: Joi.string().min(3).max(100).required()
+            .custom((value, helpers) => {
+                if (value.includes('https://www.youtube.com/watch?v=') && !value.includes('&')) {
+                    return value;
+                }
+                
+                return helpers.error('any.invalid');
+            }, 'Custom validation')
     });
 
     return schema.validate(video);
