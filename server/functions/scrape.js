edf89311870f8ae9f474700333ea08683bd0ce24
Haroon <haroon.ali.1104@gmail.com>
2023-08-24 17:31:38 +0100

    get video view count

--- server/functions/scrape.js
+++ server/functions/scrape.js
@@ -3,11 +3,16 @@ const cheerio = require('cheerio');
 async function scrape(videoId) {
     const fetchRes = await fetch(`https://www.youtube.com/watch?v=${videoId}`);
     const html = await fetchRes.text();
-
     const $ = cheerio.load(html);
 
+    const infoText = $('script:contains("viewCount")').first().text();
+
     const title = $('meta[name=title]').attr('content');
-    console.log(title);
+
+    const viewsString = infoText.split('"viewCount":"')[1].split('"')[0];
+    const views = +viewsString;
+    
+    console.log(number.toLocaleString('en-US'));
 }
 
 scrape('iuarghqmi-4');
