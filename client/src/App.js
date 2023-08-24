baf788b33d656c72db91481362711bb9c457cd34
Saliha Popal <salihapopal@gmail.com>
2023-08-24 18:56:33 +0100

    Level 250

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,38 @@
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Handlers from "./Handlers";
 
 
 
 function App() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchVideosData  = async () => {
+      try {
+       const res = await fetch('https://full-stack-front-end.onrender.com');
+       const data = await res.json();
+       setVideos(data); 
+      }
+      catch (error) {
+       console.log(error);
+      }
+   }
+   fetchVideosData()
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <h4>Video Recommendation</h4>
       </header>
+      {videos.map(video => (
+      <div key={video.id}>
+        <h5>{video.title}</h5>
+        <p>{video.url}</p>
+        <p>{`rating: video.rating`}</p>
+      </div>
+    ))}
       <Handlers />
     </div>
   );
