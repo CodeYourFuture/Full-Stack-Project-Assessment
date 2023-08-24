85fbfee7763794442d1632bf2e343b9bec913451
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-08-24 21:10:26 +0100

    adding some styling

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
-        <VideoCards />
+        <div className="card-container">
+          <VideoCards />
+        </div>
       </header>
     </div>
   );
