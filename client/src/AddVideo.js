93002023000f6241892fcb51adf2289898b67c48
fowobi <109865505+fowobi@users.noreply.github.com>
2023-08-24 21:06:26 +0100

    level 100 updated

---
+++ client/src/AddVideo.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import "./AddVideo.css";
+
+const AddVideo = ({ onAddVideo }) => {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && url) {
+      onAddVideo({ title, url });
+      setTitle("");
+      setUrl("");
+    }
+  };
+
+  return (
+    <div className="add-video">
+      <h2>Add Video</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="URL"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          required
+        />
+        <button type="submit">Add Video</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddVideo;
