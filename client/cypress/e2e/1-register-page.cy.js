619701ccffad2efa2d4478525d2d2f0face042fa
Haroon <haroon.ali.1104@gmail.com>
2023-08-25 19:23:05 +0100

    add e2e tests for form validation on register page

--- client/cypress/e2e/1-register-page.cy.js
+++ client/cypress/e2e/1-register-page.cy.js
@@ -9,38 +9,38 @@ describe('Form validation', () => {
       cy.contains('Email is a required field').should('exist');
   })
 
-  // it('Email must be at least 3 characters', () => {
-  //     cy.get('#email').type('h');
-  //     cy.get('[type=submit]').click();
-  //     cy.contains('Email must be a valid email').should('exist');
-  // })
+  it('Email must be at least 3 characters', () => {
+      cy.get('#email').type('h');
+      cy.get('[type=submit]').click();
+      cy.contains('Email must be a valid email').should('exist');
+  })
 
-  // it('Email must be a valid email', () => {
-  //     cy.get('#email').type('bobemail');
-  //     cy.get('[type=submit]').click();
-  //     cy.contains('Email must be a valid email').should('exist');
-  // })
+  it('Email must be a valid email', () => {
+      cy.get('#email').type('bobemail');
+      cy.get('[type=submit]').click();
+      cy.contains('Email must be a valid email').should('exist');
+  })
 
-  // it('Email cannot be longer than 256 characters', () => {
-  //     let longEmail = 'bobemail';
-  //     longEmail += Cypress._.repeat('l', 250);
-  //     longEmail += '@gmail.com';
+  it('Email cannot be longer than 256 characters', () => {
+      let longEmail = 'bobemail';
+      longEmail += Cypress._.repeat('l', 250);
+      longEmail += '@gmail.com';
 
-  //     cy.get('#email').invoke('val', longEmail);
-  //     cy.get('#email').type('l');
-  //     cy.get('[type=submit]').click();
-  //     cy.contains('Email must be at most 256 characters').should('exist');
-  // })
+      cy.get('#email').invoke('val', longEmail);
+      cy.get('#email').type('l');
+      cy.get('[type=submit]').click();
+      cy.contains('Email must be at most 256 characters').should('exist');
+  })
 
-  // it('Password must be at least 3 characters', () => {
-  //     cy.get('#password').type('h');
-  //     cy.get('[type=submit]').click();
-  //     cy.contains('Password must be at least 3 characters').should('exist');
-  // })
+  it('Password must be at least 3 characters', () => {
+      cy.get('#password').type('h');
+      cy.get('[type=submit]').click();
+      cy.contains('Password must be at least 3 characters').should('exist');
+  })
 
-  // it('Password cannot be longer than 15 characters', () => {
-  //     cy.get('#password').type('password32111111');
-  //     cy.get('[type=submit]').click();
-  //     cy.contains('Password must be at most 15 characters').should('exist');
-  // })
+  it('Password cannot be longer than 15 characters', () => {
+      cy.get('#password').type('password32111111');
+      cy.get('[type=submit]').click();
+      cy.contains('Password must be at most 15 characters').should('exist');
+  })
 })
