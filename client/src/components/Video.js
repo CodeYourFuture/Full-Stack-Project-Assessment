d3f6d9099a53757cf4610b98c9ba257659db9b39
Haroon <haroon.ali.1104@gmail.com>
2023-08-25 16:32:00 +0100

    implement react-hook-form validation on register page

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -5,7 +5,7 @@ export default function Video({ video, deleteVideo, incRating, decRating }) {
   return (
     <div className="container-video">
       <span className="video-title">{video.title}</span>
-      <iframe className="youtube-frame" width="100%" height="150" src={`https://www.youtube.com/embed/${video.url.split("watch?v=")[1]}`} title={video.title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      
       <div className="container-buttons">
         <div className="container-ratings">
           <FontAwesomeIcon icon={faStar} />
