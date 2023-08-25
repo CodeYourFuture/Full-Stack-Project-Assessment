0d2d67dbd5d0208d55323e70c0234e423d0a38cb
Haroon <haroon.ali.1104@gmail.com>
2023-08-25 20:08:02 +0100

    add e2e tests for login page

---
+++ client/cypress/e2e/2-login-page.cy.js
@@ -0,0 +1,67 @@
+describe('Form validation', () => {
+    beforeEach(() => {
+        cy.task('seedDB');
+        cy.visit('/login');
+    });
+
+    it('Fields cannot be empty', () => {
+        cy.get('[type=submit]').click();
+        cy.contains('Email is a required field').should('exist');
+    })
+
+    it('Email must be at least 3 characters', () => {
+        cy.get('#email').type('h');
+        cy.get('[type=submit]').click();
+        cy.contains('Email must be a valid email').should('exist');
+    })
+
+    it('Email must be a valid email', () => {
+        cy.get('#email').type('bobemail');
+        cy.get('[type=submit]').click();
+        cy.contains('Email must be a valid email').should('exist');
+    })
+
+    it('Email cannot be longer than 256 characters', () => {
+        let longEmail = 'bobemail';
+        longEmail += Cypress._.repeat('l', 250);
+        longEmail += '@gmail.com';
+
+        cy.get('#email').invoke('val', longEmail);
+        cy.get('#email').type('l');
+        cy.get('[type=submit]').click();
+        cy.contains('Email must be at most 256 characters').should('exist');
+    })
+
+    it('Password must be at least 3 characters', () => {
+        cy.get('#password').type('h');
+        cy.get('[type=submit]').click();
+        cy.contains('Password must be at least 3 characters').should('exist');
+    })
+
+    it('Password cannot be longer than 15 characters', () => {
+        cy.get('#password').type('password32111111');
+        cy.get('[type=submit]').click();
+        cy.contains('Password must be at most 15 characters').should('exist');
+    })
+})
+
+describe('User login', () => {
+    beforeEach(() => {
+        cy.task('seedDB');
+        cy.visit('/login');
+    });
+
+    it('Wrong password or email', () => {
+        cy.get('#email').type('haroon@gmail.co');
+        cy.get('#password').type('password321');
+        cy.get('[type=submit]').click();
+        cy.contains('Invalid email or password.').should('exist');
+    })
+
+    it('Successful login', () => {
+        cy.get('#email').type('haroon@gmail.com');
+        cy.get('#password').type('password321');
+        cy.get('[type=submit]').click();
+        cy.contains('Logout').should('exist');
+    })
+})
