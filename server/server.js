ace94d713ead686b7ae12f9b74b699eacfcfff76
Irianni Munoz <82213740+munozirianni1988@users.noreply.github.com>
2023-08-26 00:41:31 +0100

    get and post done

--- server/server.js
+++ server/server.js
@@ -9,7 +9,33 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
 const videos = require("./exampleresponse.json");
 
-// GET "/"
+// GET "/" to get all the videos
 app.get("/", (req, res) => {
   res.json(videos);
+});
+
+//POST "/" to post a new video
+
+app.post("/", (req, res) => {
+  const { title, url } = request.body;
+  if (!title || !url) {
+    return res.status(400).json({
+      result: "failure",
+      message: "Video could not be saved.",
+    });
+  } else {
+    const videoId = videos.url.match(
+      /(?:\/|%3D|v=|vi=)([0-9A-Za-z_-]{11})(?:[%#?&]|$)/
+    )[1];
+    const newVideo = {
+      id: videoId,
+      title: title,
+      url: url,
+      rating: 0,
+    };
+    videos.push(newVideo);
+    res.status(201).json({
+      id: newVideo.id,
+    });
+  }
 });
