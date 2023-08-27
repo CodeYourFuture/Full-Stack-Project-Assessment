58fcadf1d3c7f939eecef878b9fa9ce7748caf2e
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-08-28 00:56:02 +0100

    update

--- client/src/VideoComponent.js
+++ client/src/VideoComponent.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 // className = "video";
 
 const VideoComponent = ({ video, onRemove }) => {
-  const [votes, setVotes] = useState(0);
+  const [votes, setVotes] = useState(video.rating);
 
   function handleUpVotes() {
     setVotes(votes + 1);
@@ -17,11 +17,23 @@ const VideoComponent = ({ video, onRemove }) => {
         <div className="row justify-content-center">
           <div className="col-md-8"> */}
       <h1 className="text-center">{video.title}</h1>
-      {/* <div className="embed-responsive embed-responsive-16by9"> */}
+      {/* {/* <div className="embed-responsive embed-responsive-16by9"> */}
       <video className="embed-responsive-item" controls>
         <source src={video.url} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
+      {/* <iframe
+        width="560"
+        height="315"
+        src={video.url}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe> */}
+      {/* Error when trying to use iframe - video is not showing - youtube refused to connect */}
+
       <p>Votes {votes}</p>
 
       <button className="btn btn-outline-dark" onClick={handleUpVotes}>
@@ -37,16 +49,6 @@ const VideoComponent = ({ video, onRemove }) => {
       >
         Remove Video
       </button>
-
-      {/* <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/watch?v=qSCdXiNSfeA"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe> */}
 
       {/* </div>
           </div>
