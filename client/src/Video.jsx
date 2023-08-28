187a86df4b1cd35fa4c776147e35cbe285b27db9
BEKIRKSU <bekirkscu@gmail.com>
2023-08-28 13:05:52 +0100

    Update Video.jsx

--- client/src/Video.jsx
+++ client/src/Video.jsx
@@ -7,3 +7,14 @@ function Video({ video, onRemove}) {
 const handleVote = () => {
     setVotes(prevVotes => prevVotes + 1);
   };
+
+  return (
+    <div className="video">
+      <h2>{video.title}</h2>
+      <iframe width="560" height="315" src={video.url} frameBorder="0" allowFullScreen></iframe>
+      <p>Votes: {votes}</p>
+      <button onClick={handleVote}>Vote</button>
+      <button onClick={() => onRemove(video.id)}>Remove</button>
+    </div>
+  );
+}
