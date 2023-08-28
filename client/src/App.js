ab9a08b7c7a91f01553cf6026bf6bc47b7976949
Yesna-Omar <109865751+Yesna-Omar@users.noreply.github.com>
2023-08-28 05:03:35 +0100

    level-100

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
 import "./App.css";
-
+import Container from "./Container";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Video Recommendation</h1>
       </header>
+      <Container />
     </div>
   );
 }
---
+++ client/src/VideoContainer.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class VideoContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "",
+      url: "",
+    };
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.title && this.state.url) {
+      this.props.onAdd({ title: this.state.title, url: this.state.url });
+      this.setState({
+        title: "",
+        url: "",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          value={this.state.title}
+          onChange={(e) => this.setState({ title: e.target.value })}
+        />
+        <input
+          type="url"
+          placeholder="YouTube URL"
+          value={this.state.url}
+          onChange={(e) => this.setState({ url: e.target.value })}
+        />
+        <button type="submit">Add Video</button>
+      </form>
+    );
+  }
+}
+
+export default VideoContainer;
