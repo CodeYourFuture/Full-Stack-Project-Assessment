085e8468c5d6f4fd6bfe026b39de7355b8cde58b
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-08-28 20:51:11 +0100

    level_100_3: up vote and down vote

--- client/src/components/VideoCard.jsx
+++ client/src/components/VideoCard.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownOffAltIcon from "@mui/icons-material/ThumbDownOffAlt";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
 
 function VideoCard(props) {
   const deleteIcon = <DeleteForeverIcon />;
+  const up = <ThumbUpIcon />;
+  const upA = <ThumbUpOffAltIcon />;
+  const down = <ThumbDownIcon />;
+  const downA = <ThumbDownOffAltIcon />;
+
+  const [like, setLike] = useState(0);
+  const [dislike, setDislike] = useState(0);
+  const [rate, setRate] = useState(props.rating)
 
   return (
     <div className="card" style={{ width: "330px", height: "350px" }}>
@@ -21,9 +33,37 @@ function VideoCard(props) {
       </div>
       <ul className="list-group list-group-flush">
         <li className="list-group-item">
+          <button
+            onClick={() => {
+              if (like === 1) {
+                setLike(0);
+                setRate(rate - 1);
+              }
+              if (like === 0) {
+                setLike(1);
+                setRate(rate + 1);
+              }
+            }}
+          >
+            {like === 1 ? up : upA} 
+          </button>
+          <button
+            onClick={() => {
+              if (dislike === 1) {
+                setDislike(0);
+                setRate(rate + 1);
+              }
+              if (dislike === 0) {
+                setDislike(1);
+                setRate(rate - 1);
+              }
+            }}
+          >
+            {dislike === 1 ? down : downA}
+          </button>
           <button onClick={props.delete}>{deleteIcon}</button>
         </li>
-        <li className="list-group-item">{props.rating}</li>
+        <li className="list-group-item">{rate}</li>
       </ul>
     </div>
   );
