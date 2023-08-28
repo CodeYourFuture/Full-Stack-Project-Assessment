2118cfcae76883d57941202f353192af63a75a5f
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-08-28 11:17:54 +0100

    level_100_1: data loaded from a local js variable

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
+import { useState } from "react";
+import exampleresponse from "./exampleresponse.json";
 
 function App() {
+  const [data, setData] = useState(exampleresponse)
   return (
     <div className="App">
       <header className="App-header">
