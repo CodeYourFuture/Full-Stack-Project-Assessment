094ddfe693020539fa2f30d95da65135e1d7c15b
Afsha10 <unityofbeing1@gmail.com>
2023-08-28 18:07:08 +0100

    Simplified Import for GET response
    Displayed the data from the exampleresponse.json by assigning a variable to the data and using the variable inside the get request.

--- server/server.js
+++ server/server.js
@@ -4,26 +4,22 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
-const fs = require("fs").promises;
+const videos = require("./exampleresponse.json")
 
 app.use(express.json());
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
 
 // GET "/"
 app.get("/", (req, res) => {
   // Delete this line after you've confirmed your server is running
-  const videos = fs
-    .readFile("./exampleresponse.json", "utf8")
-    .then((videos) => {
-      res.status(200).send({ data: JSON.parse(videos) })
-    });
+  res.status(200).json(videos);
 });
 
 app.post("/", (req, res) => {
   res.send("success You have made a post request");
 });
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
