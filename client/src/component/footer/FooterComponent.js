503c55ee79bcc725fc12b0417be312ad55d6112c
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-08-29 12:19:23 +0100

    update

--- client/src/component/footer/FooterComponent.js
+++ client/src/component/footer/FooterComponent.js
@@ -7,13 +7,13 @@ const FooterComponent = () => {
         <div className="social-container">
             <ul className="social-icons">
               
-                <li><a href="http://localhost:3000/"><i className="fa fa-instagram"></i></a></li>
+                <li><a href="https://www.instagram.com/"><i className="fa fa-instagram"></i></a></li>
                 
-                <li><a href="http://localhost:3000/"><i className="fa fa-twitter"></i></a></li>
+                <li><a href="https://twitter.com/"><i className="fa fa-twitter"></i></a></li>
                 
                 <li><a href="https://www.linkedin.com/in/abubakar-meigag-0a625a17a/"><i className="fa fa-linkedin"></i></a></li>
                 
-                <li><a href="http://localhost:3000/"><i className="fa fa-codepen"></i></a></li>
+                <li><a href="https://codepen.io/"><i className="fa fa-codepen"></i></a></li>
             </ul>
         </div>
   )
