7ee9d4eabcc270ba18fa812d6411e19f3f3a3a8b
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-08-31 11:31:07 +0100

    make some changes in server and db

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: "videos_recommendation",
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
