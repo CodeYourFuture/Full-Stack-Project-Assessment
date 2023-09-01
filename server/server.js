6c2b5352e823778d7d8c6d7ce4c8ff7f44b970e5
christina-mifsud <christina.mifsud@gmail.com>
2023-09-01 21:13:10 +0100

    Run React on port 5005

--- server/server.js
+++ server/server.js
@@ -3,7 +3,12 @@ require("dotenv").config();
 const cors = require("cors");
 const app = express();
 
-// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
+
+console.log(`DB_NAME = ${process.env.DB_NAME}`);
+if (!process.env.DB_NAME) {
+  throw new Error("DB_NAME not defined");
+}
 
 app.use(cors());
 app.use(express.json()); // needed to parse JSON data
@@ -15,7 +20,7 @@ const db = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5000,
+  port: process.env.DB_PORT,
   ssl: true,
 });
 
