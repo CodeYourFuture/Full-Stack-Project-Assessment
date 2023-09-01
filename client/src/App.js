c8e53a7cc297c61996fd62171eea34f5b7a28913
Artem Filkovskyi <avfilkovsky@gmail.com>
2023-09-01 16:27:31 +0100

    Lvl 100 completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,39 @@
 import "./App.css";
+import { useState } from "react";
+import AddVideo from "./components/AddVideo";
+import VideosList from "./components/VideosList";
+import data from "./exampleresponse.json";
 
 function App() {
+  const [videos, setVideos] = useState(data);
+
+  function handleDelete(id) {
+    const updatedVideos = videos.filter((video) => video.id !== id);
+    setVideos(updatedVideos);
+  }
+
+  function addVideo(title, url) {
+    const newVideo = {
+      title: title,
+      url: url,
+      id: Math.random().toString(),
+      rating: 0,
+    };
+    setVideos([...videos, newVideo]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Video Recommendation</h1>
+    <div className='App'>
+      <header className='App-header'>
+        <h1 className='mb-3 text-3xl font-bold'>Video Recommendation</h1>
       </header>
+      <main className='container mx-auto'>
+        <AddVideo addVideo={addVideo} />
+        <VideosList
+          videos={videos}
+          handleDelete={handleDelete}
+        />
+      </main>
     </div>
   );
 }
