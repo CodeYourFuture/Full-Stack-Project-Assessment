fa51e965afcffef2debbbd63a45506f884c28b6c
Artem Filkovskyi <avfilkovsky@gmail.com>
2023-09-05 10:11:57 +0100

    add GET and POST request handlers

--- server/server.js
+++ server/server.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const randomIntFromInterval = require("./utils/randomIntFromInterval");
+
+app.use(bodyParser.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = require("../exampleresponse.json");
 
-// GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.json(videos);
+});
+
+app.post("/", (req, res) => {
+  if (!req.body || !req.body.title || !req.body.url) {
+    res.status(400).send("Bad Request: Missing title or url field");
+    return;
+  }
+
+  const newVideo = {
+    id: randomIntFromInterval(10, videos[videos.length - 1].id),
+    title: req.body.title,
+    url: req.body.url,
+    rating: 0,
+  };
+  videos.push(newVideo);
+  res.json(videos);
 });
