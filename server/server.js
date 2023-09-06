b95df2cdc06bda6f5216d829fb3935452897bc74
Artem Filkovskyi <avfilkovsky@gmail.com>
2023-09-06 10:37:26 +0100

    updated exampleresponse.json import

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = require("../exampleresponse.json");
+let videos = require("../client/src/exampleresponse.json");
 
 app.get("/", (req, res) => {
   res.json(videos);
