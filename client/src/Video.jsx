d2802536ece04bd747358b72969974fbfa43cd1b
BEKIRKSU <bekirkscu@gmail.com>
2023-09-07 13:39:20 +0100

    Update Video.jsx

--- client/src/Video.jsx
+++ client/src/Video.jsx
@@ -45,7 +45,14 @@ function Video({ video, onRemove }) {
           title={video.title}
           allowFullScreen
         /> */}
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/${new URL(video.url)" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <iframe
+  width="560"
+  height="315"
+  src={`https://www.youtube.com/embed/${new URL(video.url).searchParams.get('v')}`}
+  title={video.title}
+  allowFullScreen
+/>
+
       </div>
       
       <p>Rating: {rating}</p> 
