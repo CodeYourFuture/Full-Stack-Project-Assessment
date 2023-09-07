fd14196fd9931520b6e526534314136060ff2df1
BEKIRKSU <bekirkscu@gmail.com>
2023-09-07 14:07:24 +0100

    -

--- client/src/Video.jsx
+++ client/src/Video.jsx
@@ -1,22 +1,4 @@
-// import React, { useState } from 'react';
 
-// function Video({ video, onRemove }) {
-//   const [votes, setVotes] = useState(video.rating);
-
-//   const handleVote = () => {
-//     setVotes(prevVotes => prevVotes + 1);
-//   };
-
-//   return (
-//     <div className="video">
-//       <h2>{video.title}</h2>
-//       <iframe width="560" height="315" src={video.url} frameBorder="0" allowFullScreen></iframe>
-//       <p>Votes: {votes}</p>
-//       <button onClick={handleVote}>Vote</button>
-//       <button onClick={() => onRemove(video.id)}>Remove</button>
-//     </div>
-//   );
-// }
 
 import React, { useState } from 'react';
 
@@ -37,14 +19,6 @@ function Video({ video, onRemove }) {
       <h2>{video.title}</h2>
       
       <div className="video-embed">
-        {/* <iframe
-          width="100%"
-          height="315"
-          // src={`https://www.youtube.com/embed/${new URL(video.url).searchParams.get('v')}`}
-          
-          title={video.title}
-          allowFullScreen
-        /> */}
        <iframe
   width="560"
   height="315"
