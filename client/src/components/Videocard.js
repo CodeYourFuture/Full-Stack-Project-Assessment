a1ab0b4aa577309daa4e5fae157ec10957c92029
Joemwa <109852314+Joemwa@users.noreply.github.com>
2023-09-08 21:58:12 +0100

    Updated video card Id to match

--- client/src/components/Videocard.js
+++ client/src/components/Videocard.js
@@ -1,9 +1,12 @@
 import React from "react";
 function Videocard (props) {
+    const videoId = props.link;
+    const modifiedVideoId = videoId.replace("https://www.youtube.com/watch?v=", "")
 return (
     <div>
     <h1>{props.name}</h1>
     <p>{props.link}</p>
+    <iframe width="560" height="315" src= "https://www.youtube.com/embed/{modifiedVideoId}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
 )
 }
