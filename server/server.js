d4c18ba2642ccce16439e3191c3ca5ddaa9a9a16
Najah Farah <najahfarah27@gmail.com>
2023-09-08 17:11:32 +0100

    glasgow_6|najah_farah|fullstack-level300

--- server/server.js
+++ server/server.js
@@ -6,19 +6,29 @@ const cors = require('cors');
 app.use(cors()); 
 app.use(express.json()); 
 
-let videos = [];
+const dotenv = require('dotenv');
+dotenv.config();
 
-function generateVideoId() {
-  return Math.floor(Math.random() * 1000000000000); 
-}
+const itemsPool = require('./DBConfig');
 
-// GET 
-app.get("/", (req, res) => {
-  res.json(videos);
+
+
+// GET
+app.get("/api/videos", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM videos");
+    const videos = result.rows;
+    client.release();
+    res.json(videos);
+  } catch (error) {
+    console.error("Error fetching videos:", error);
+    res.status(500).json({ error: "An error occurred while fetching videos" });
+  }
 });
 
 // POST 
-app.post("/", (req, res) => {
+app.post("/api/videos",async (req, res) => {
   const { title, url } = req.body;
  if (!title || !url) {
     return res.status(400).json({
@@ -27,26 +37,30 @@ app.post("/", (req, res) => {
     });
   }
 
-
-  const id = generateVideoId();
-
- 
-  const newVideo = {
-    id,
-    title,
-    url,
-    rating: 0, 
-  };
-  videos.push(newVideo);
-  res.status(201).json({ id });
+  try {
+    const client = await pool.connect();
+    const result = await client.query(
+      "INSERT INTO videos (title, url, rating) VALUES ($1, $2, $3) RETURNING id",
+      [title, url, 0] 
+    );
+    const videoId = result.rows[0].id;
+    client.release();
+    res.status(201).json({ id: videoId });
+  } catch (error) {
+    console.error("Error adding a video:", error);
+    res.status(500).json({ error: "An error occurred while adding the video" });
+  }
 });
 
+
 // GET "/:id"
-app.get("/:id", (req, res) => {
+app.get("/api/videos/:id",async (req, res) => {
   const id = parseInt(req.params.id);
 
- 
-  const video = videos.find((v) => v.id === id);
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM videos WHERE id = $1", [id]);
+    const video = result.rows[0];
 
   if (!video) {
     return res.status(404).json({
@@ -54,27 +68,35 @@ app.get("/:id", (req, res) => {
       message: "Video not found",
     });
   }
-
+  client.release();
   res.json(video);
+} catch (error) {
+  console.error("Error fetching a video:", error);
+  res.status(500).json({ error: "An error occurred while fetching the video" });
+}
 });
 
 // DELETE
-app.delete("/:id", (req, res) => {
+app.delete("/api/videos/:id", async (req, res) => {
   const id = parseInt(req.params.id);
 
-  const index = videos.findIndex((v) => v.id === id);
+  try {
+    const client = await pool.connect();
+    const result = await client.query("DELETE FROM videos WHERE id = $1", [id]);
 
-  if (index === -1) {
+  if (result.rowCount === -1) {
     return res.status(404).json({
       result: "failure",
       message: "Video not found",
     });
   }
 
-  
-  videos.splice(index, 1);
-
+  client.release();
   res.json({});
+} catch (error) {
+  console.error("Error deleting a video:", error);
+  res.status(500).json({ error: "An error occurred while deleting the video" });
+}
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
