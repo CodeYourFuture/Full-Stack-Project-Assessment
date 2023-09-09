d582bcc5a785d3e5703268ee6fb7b7ee02df1244
fowobi <109865505+fowobi@users.noreply.github.com>
2023-09-09 20:46:00 +0100

    update

--- client/src/App.js
+++ client/src/App.js
@@ -60,33 +60,6 @@ function App() {
   };
 
 
-  const handleAddVideo = (newVideo) => {
-    // Send a POST request to add the new video
-    fetch("https://full-stack-server-3nzy.onrender.com/videos", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newVideo),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        // Include the generated ID in the new video
-        const updatedVideo = {
-          ...newVideo,
-          id: data.id,
-          votes: 0,
-          uploadDate: data.uploadDate,
-        };
-
-        // Add the new video to the list of videos
-        // setVideos([...videos, updatedVideo]);
-        // Clear the existing videos state and add the new video
-        setVideos([updatedVideo]);
-      })
-      .catch((error) => console.error("Error adding video:", error));
-  };
-
   // const handleRemove = (id) => {
   //   setVideos((prevVideos) => prevVideos.filter((video) => video.id !== id));
   // };
@@ -125,10 +98,34 @@ function App() {
   //     { ...newVideo, id, votes: 0 },
   //   ]);
   // };
+
+   const handleAddVideo = (newVideo) => {
+     // Send a POST request to add the new video
+     fetch("https://full-stack-server-3nzy.onrender.com/videos", {
+       method: "POST",
+       headers: {
+         "Content-Type": "application/json",
+       },
+       body: JSON.stringify(newVideo),
+     })
+       .then((response) => response.json())
+       .then((data) => {
+         // Include the generated ID in the new video
+         const updatedVideo = {
+           ...newVideo,
+           id: data.id,
+           votes: 0,
+           uploadDate: data.uploadDate,
+         };
+
+         // Add the new video to the list of videos
+         // setVideos([...videos, updatedVideo]);
+         // Clear the existing videos state and add the new video
+         setVideos([updatedVideo]);
+       })
+       .catch((error) => console.error("Error adding video:", error));
+   };
     
-
-
-
 
   return (
     <div className="App">
