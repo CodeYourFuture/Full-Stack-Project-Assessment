709575124136a27eccb18ef97421018bb461dcf6
ManSangSin <109699416+ManSangSin@users.noreply.github.com>
2023-09-09 16:11:40 +0100

    updated src url to fix video not showing

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -9,7 +9,7 @@ export default function Video(props) {
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/{videoId}"
+        src={`https://www.youtube.com/embed/${videoId}`}
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
