6c95d6eaa3c0de3fe35aa0848b727bbe6efdd441
nsaimk <103388334+nsaimk@users.noreply.github.com>
2023-09-11 18:13:56 +0100

    level_200_1: GET

--- server/server.js
+++ server/server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
+let videos = require("./exampleresponse.json");
 
 // GET "/"
 app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+  res.json(videos)
 });
