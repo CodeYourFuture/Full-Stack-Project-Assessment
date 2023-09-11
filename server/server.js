993acc4bdc2be7a331063a2d701218343b77da00
Olha Danylevska <odanilevskaya2@gmail.com>
2023-09-11 14:04:34 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -61,7 +61,8 @@ app.delete("/videos/:id", (req, res) => {
 app.get("/videos", (req, res) => {
   db.query("SELECT * FROM videos")
     .then((result) => {
-      res.send(200).json({ videos: result.rows });
+      res.status(200).json({ videos: result.rows });
+      res.send(result.rows)
     })
     .catch((error) => {
       console.log(error);
