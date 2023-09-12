c4fc8dfa119a5f37473173f8f2747449bf8b55f1
saqibjvd <70890519+saqibjvd@users.noreply.github.com>
2023-09-12 18:22:20 +0100

    Level 200 - Week 2 - Back End
    Level 200 - Week 2 - Back End - completed

--- server/server.js
+++ server/server.js
@@ -3,11 +3,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
+const cors = require("cors");
+app.use(cors());
 
 // app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
+
 let videos = [
   {
     id: 523523,
@@ -72,42 +75,87 @@ let videos = [
   },
 ];
 
-// GET "/"
-// app.get("/", (req, res) => {
-//   // Delete this line after you've confirmed your server is running
-//   res.send({ express: "Your Backend Service is Running" });
-// });
-
+// GET "/" ....confirms server is running
 app.get("/", (req, res) => {
-  res.send({ videos });
+  // Delete this line after you've confirmed your server is running
+  res.send({ express: "Your Backend Service is Running" });
 });
 
-var id = videos.length;
 
-app.post("/", (req, res) => {
-  // console.log("this is data1", res)
-  console.log("this is data2", req);
+// This endpoint is used to return all of the videos
+app.get("/videos", (req, res) => {
+  res.status(200).json({ videos });
+});
+
+
+// This endpoint is used to add a video to the API.
+app.post("/videos", (req, res) => {
   try {
-    let data = {
-      id: id++,
+    let newVideo = {
+      id: 0,
       title: req.body.title,
       url: req.body.url,
+      rating: 0
     };
-    console.log("this is data", data);
-    res.status(200).send({
-      message: "Video saved successfully",
-      item: data,
-    });
+
+    if (!newVideo.title || !newVideo.url || !newVideo.url.startsWith("https://www.youtube.com")) {
+      res.status(404).send({
+        result: "failure",
+        message: "Video could not be saved",
+      });
+    } else {
+      const videoid = videos.map((video) => video.id);
+      const id = Math.max(...videoid) + 1;
+      
+      videos.push(newVideo);
+      res
+        .status(201)
+        .send({
+          message: "Video saved successfully",
+        })
+        .json({ newVideo });
+    }
+
   } catch (error) {
-    res.status(404).send({
-      result: "failure",
-      message: "Video could not be saved",
-    });
   }
 });
 
-app.get("/:id", (res, req) =>{
+// Returns the video with the ID contained within the {id} parameter.
 
+app.get("/videos/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const matchingVideo = videos.find((video) => {
+    return video.id === id;
+  });
+  if (!matchingVideo) {
+    res.status(400).send("No matching video with this ID exists.");
+  } else {
+    res.status(200).json({ matchingVideo });
+  }
+});
+
+//Deletes the video with the ID container within the {id} parameter
+
+app.delete("/videos/:id", (req, res) => {
+  let id = Number(req.params.id);
+  const matchingVideo = videos.find((video) => {
+    return video.id === id;
+  });
+
+  if (!matchingVideo) {
+    res.status(400).json({
+      result: "failure",
+      message: "Video could not be deleted",
+      id: id
+    })
+  } else {
+    const videoIndexToBeDeleted = videos.indexOf(matchingVideo);
+    videos.splice(videoIndexToBeDeleted, 1);
+    res.status(200).json({
+      title: matchingVideo.title,
+      message: "Video deleted successfully!",
+    });
+  }
 });
 
 
