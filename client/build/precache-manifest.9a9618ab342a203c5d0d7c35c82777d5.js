8ba1aae58a25c253056f6b78c2f2ee5d7868d6f7
Yesna-Omar <109865751+Yesna-Omar@users.noreply.github.com>
2023-09-12 16:58:10 +0100

    300

---
+++ client/build/precache-manifest.9a9618ab342a203c5d0d7c35c82777d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcf51640fdee38c5b4a0",
+    "url": "/static/css/main.c5191df2.chunk.css"
+  },
+  {
+    "revision": "dcf51640fdee38c5b4a0",
+    "url": "/static/js/main.34fb29ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "91bfd4a6fb451e9360ec",
+    "url": "/static/js/2.41f6ce85.chunk.js"
+  },
+  {
+    "revision": "6e9b8d3ac6344004235f88c2ab57e313",
+    "url": "/index.html"
+  }
+];
