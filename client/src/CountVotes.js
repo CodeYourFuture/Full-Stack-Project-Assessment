a94da9ed149df74c7ec6f9e37159c0a4105fb5d9
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-09-13 03:43:13 +0100

    installed "@material-ui/icons", updated CountVotes.js, added CSS

--- client/src/CountVotes.js
+++ client/src/CountVotes.js
@@ -1,22 +1,68 @@
 import { useState } from "react";
+import ThumbUp from "@material-ui/icons/ThumbUp";
+import ThumbDown from "@material-ui/icons/ThumbDown";
+
 const CountVotes = () => {
-  const [upVote, setUpVote] = useState(0);
-  const [downVote, setDownVote] = useState(0);
-  const handleUpVote = () => {
-    setUpVote(upVote + 1);
+  const [likeCount, setLikeCount] = useState(0);
+  const [dislikeCount, setDislikeCount] = useState(0);
+  const [activeButton, setActiveButton] = useState("none");
+
+  const handleLikeClick = () => {
+    if (activeButton === "none") {
+      setLikeCount(likeCount + 1);
+      setActiveButton("like");
+      return;
+    }
+
+    if (activeButton === "like") {
+      setLikeCount(likeCount - 1);
+      setActiveButton("none");
+      return;
+    }
+
+    if (activeButton === "dislike") {
+      setLikeCount(likeCount + 1);
+      setDislikeCount(dislikeCount - 1);
+      setActiveButton("like");
+    }
   };
-  const handleDownVote = () => {
-    setDownVote(upVote - 1);
+  const handleDislikeClick = () => {
+    if (activeButton === "none") {
+      setDislikeCount(dislikeCount + 1);
+      setActiveButton("dislike");
+      return;
+    }
+    if (activeButton === "dislike") {
+      setDislikeCount(dislikeCount - 1);
+      setActiveButton("none");
+      return;
+    }
+    if (activeButton === "like") {
+      setDislikeCount(dislikeCount + 1);
+      setLikeCount(likeCount - 1);
+      setActiveButton("dislike");
+    }
   };
   return (
-    <div className="mainConteiner">
-      <div >
-        <span>{upVote}</span>
-        <button onClick={handleUpVote}>Up Vote</button>
-      </div>
-      <div>
-        <span>{downVote}</span>
-        <button onClick={handleDownVote}>Down Vote</button>
+    <div className="main-container">
+      <div className="button-container">
+        <button
+          className={`button ${activeButton === "like" ? "like-active" : ""}`}
+          onClick={handleLikeClick}
+        >
+          <ThumbUp />
+          Like {likeCount}
+        </button>
+
+        <button
+          className={`button ${
+            activeButton === "dislike" ? "dislike-active" : ""
+          }`}
+          onClick={handleDislikeClick}
+        >
+          <ThumbDown />
+          Dislike {dislikeCount}
+        </button>
       </div>
     </div>
   );
