27c947986a2de5977c5a211dedb4b79a1842c3d7
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-09-14 07:09:06 +0100

    .

--- server/DBConfig.js
+++ server/DBConfig.js
@@ -1,8 +1,26 @@
+// const { Pool } = require("pg");
+// const itemsPool = new Pool({
+//   connectionString: process.env.DBConfigLink,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
+// module.exports = itemsPool;
 const { Pool } = require("pg");
-const itemsPool = new Pool({
-  connectionString: process.env.DBConfigLink,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+
+const pool = new Pool({
+  user: "ramo",
+  host: "dpg-cjmvldsdfrcc73cqdgj0-a",
+  database: "full_stack_assessment_database",
+  password: "QM8CiEFShmAod1u3wlJxKFzv0OPDmNaP",
+  port: "5432",
 });
-module.exports = itemsPool;
+
+// Example query
+pool.query("SELECT * FROM your_table", (err, res) => {
+  if (err) {
+    console.error("Error executing query:", err);
+  } else {
+    console.log("Query result:", res.rows);
+  }
+});
