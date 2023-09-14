e004e72d31c75be7b9d3f60f537e3e63d351f54b
Afsha10 <unityofbeing1@gmail.com>
2023-09-14 12:45:41 +0100

    Working on local build
    - Commented out ssl:  true
    - Added sql with the pg library
    - Added endpoints with express library
    - Created a pool that connects the server to the SQL database
    - Wrote SQL queries and sent to database

--- server/server.js
+++ server/server.js
@@ -4,16 +4,81 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 app.use(cors());
+const { Pool } = require("pg");
+require("dotenv").config();
 
 const videos = require("./exampleresponse.json");
 
 app.use(express.json());
 
+const db = new Pool({
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.POSTGRES_PORT,
+  // ssl: true,
+});
+
 // GET "/"
+// This endpoint is used to get all the videos
 app.get("/videos", (req, res) => {
   res.status(200).json(videos);
 });
 
+db.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+// friend's idea to get all the videos:
+
+app.get("/", (req, res) => {
+  db.query(`SELECT * FROM videos ORDER BY title`, (error, result) => {
+    // using callback function to catch the error; later on we can use .then promises
+    if (!error) {
+      res.json(result.rows);
+    } else {
+      console.log(error.message);
+    }
+    db.end;
+  });
+});
+
+// get one particular video using the ID
+app.get("/:id", function (req, res) {
+  const searchId = Number(req.params.id);
+
+  db.query("SELECT * FROM videos WHERE id = $1", [searchId])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        res.json(result.rows);
+      } else {
+        res.status(404).json({
+          result: "failure",
+          message: "Video could not be found",
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error.message);
+      res.status(404).json({
+        result: "failure",
+        message: "Video could not be found",
+      });
+    });
+});
+
+// app.get("/videos", (req, res) => {
+//   db.query("SELECT * FROM videos").then((result) => {
+//   response.json( result.rows );
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
+// });
+
+// This endpoint is used to create new videos
 app.post("/videos", (req, res) => {
   const { title, url } = req.body;
   if (!title || !url || !url.startsWith("https://www.youtube.com")) {
@@ -35,6 +100,7 @@ app.post("/videos", (req, res) => {
   }
 });
 
+// This endpoint is used to get a single video with a given ID
 app.get("/videos/:id", (req, res) => {
   const id = Number(req.params.id);
   const matchingVideo = videos.find((video) => {
@@ -47,6 +113,7 @@ app.get("/videos/:id", (req, res) => {
   }
 });
 
+// This endpoint is used to delete a single video with a given ID
 app.delete("/videos/:id", (req, res) => {
   let id = Number(req.params.id);
   const matchingVideo = videos.find((video) => {
@@ -66,6 +133,7 @@ app.delete("/videos/:id", (req, res) => {
   }
 });
 
+// This endpoint is used to update an existing video with a given ID
 app.put("/videos/:id", (req, res) => {
   const newVideo = req.body;
   let id = Number(req.params.id);
