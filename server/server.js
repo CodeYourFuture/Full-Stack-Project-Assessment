4a6a7bfa4044b2a690f35ae13791d99891307a67
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-09-14 13:52:35 +0100

    update

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-require("dotenv").config({path: './.env'});
+require("dotenv").config();
 const express = require("express");
-// const path = require("path");
+const path = require("path");
 const { body, validationResult } = require("express-validator");
 const fs = require("fs");
 const bodyParser = require("body-parser");
@@ -11,7 +11,7 @@ const pool = require("./db");
 const app = express();
 const port = process.env.PORT || 5000;
 
-// app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(bodyParser.json());
 app.use(cors());
 pool.connect();
@@ -115,8 +115,8 @@ app.delete("/videos/data/:id", async (req, res) => {
   // });
 });
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
