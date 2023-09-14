dee2318291946f03904f77ff484a06a7b7647cdb
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-09-14 22:12:38 +0100

    installed CORS

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
