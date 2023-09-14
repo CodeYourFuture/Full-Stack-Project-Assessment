a2d855f1703db6449d08bf59a5b2327fb3cc8969
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-09-14 21:26:08 +0100

    200 =>Done

--- server/server.js
+++ server/server.js
@@ -1,15 +1,65 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const videos = require("../client/src/Data/exampleresponse.json");
+
+app.use(express.json());
+
+// <-------------------get------------------->
+app.get("/", (req, res) => {
+  res.json(videos);
+});
+// <----------------------------------------->
+// <---------------get By ID----------------->
+
+app.get("/:id", (req, res) => {
+  const id = req.params.id * 1;
+  const video = videos.find((ele) => ele.id === id);
+
+  if (!video) {
+    return res
+      .status(404)
+      .json({ result: "failure", message: "Video not found" });
+  }
+
+  res.json(video);
+});
+// <----------------------------------------->
+
+// <---------------------Edit---------------->
+app.post("/", (req, res) => {
+  const { title, url } = req.body;
+
+  if (!title || !url) {
+    return res
+      .status(400)
+      .json({ result: "failure", message: "Video could not be saved" });
+  }
+  const id = Math.max(...videos.map((video) => video.id), 0) + 1;
+  const newVideo = { id, title, url, rating: 0 };
+  videos.push(newVideo);
+
+  res.status(201).json({ id });
+});
+// <----------------------------------------->
+
+// <-------------------Delete---------------->
+app.delete("/:id", (req, res) => {
+  const id = req.params.id * 1;
+
+  const index = videos.findIndex((ele) => ele.id === id);
+
+  if (index === -1) {
+    return res
+      .status(404)
+      .json({ result: "failure", message: "Video not found" });
+  }
+
+  videos.splice(index, 1);
+
+  res.json({ message: "The item has been deleted" });
+});
+// <----------------------------------------->
+// app starting ----------->
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
-
-// GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
-});
