617033c7a3c96818f57cbd77edd43f79d81a7c27
Haroun Alarabi <alarabiharoun@gmail.com>
2023-09-16 12:37:36 +0100

    updated data base link

--- server/DBConfig.js
+++ server/DBConfig.js
@@ -1,6 +1,6 @@
 const { Pool } = require("pg");
 const videosPool = new Pool({
-  connectionString: "postgres://project_data_eqrf_user:MldQZenHLTFNPhwOUA6Nx6a9zrKRMfXc@dpg-ck1o4bnhdsdc739osv00-a.oregon-postgres.render.com/project_data_eqrf",
+  connectionString: "postgres://full_stack_database_jp62_user:JfOA8mzl7maxtJEnTL28nmslJ5JSqVfe@dpg-ck2p2hvqj8ts73fm2m5g-a.oregon-postgres.render.com/full_stack_database_jp62",
   ssl: {
     rejectUnauthorized: false,
   },
