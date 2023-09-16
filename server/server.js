a15f7e85e888dd3bb794f6d805f4ad808766ff7c
Iryna Lypnyk <iryna.lypnyk@gmail.com>
2023-09-16 23:31:00 +0300

    level 200-299

--- server/server.js
+++ server/server.js
@@ -1,15 +1,63 @@
-const express = require("express");
+import cors from "cors";
+import express from "express";
+import {videos} from './data.js';
+
+const port = 3001;
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
+app.use(cors());
+app.use(express.json());
 // Store and retrieve your videos from here
 // If you want, you can copy "exampleresponse.json" into here to have some data to work with
-let videos = [];
 
-// GET "/"
-app.get("/", (req, res) => {
-  // Delete this line after you've confirmed your server is running
-  res.send({ express: "Your Backend Service is Running" });
+app.get("/", function (req, res) {
+    res.status(200).json({videos});
 });
+
+app.get('/:id', function (req, res) {
+    const id = req.params.id;
+    res.status(200).json({videos: videos.filter(video => String(video.id) === id)});
+});
+
+app.post('/', function (req, res) {
+    if (req.body.title && req.body.url) {
+        const newVideo = {
+            id: Date.now(),
+            title: req.body.title,
+            url: req.body.url,
+            rating: 0,
+            timestamp: new Date(),
+        }
+        videos.push(newVideo);
+        res.status(201).json({newVideo})
+    } else {
+        res.status(400).json({
+            error: {
+                "result": "failure",
+                "message": "Video could not be saved"
+            }
+        });
+    }
+});
+
+app.delete('/:id', function (req, res) {
+    const id = parseInt(req.params.id);
+    // Find the item index in the array
+    const videoIndex = videos.findIndex((video) => video.id === id);
+
+    if (videoIndex === -1) {
+        // Item not found
+        res.status(404).json({
+            error: {
+                "result": "failure",
+                "message": "Video could not be deleted"
+            }
+        });
+    } else {
+        // Remove the item from the array
+        videos.splice(videoIndex, 1);
+        res.status(200).json({success: true});
+    }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
