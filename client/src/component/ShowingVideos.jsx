b6cfb308653e9db6a5c3972ed4cb3919e9afdcbe
Muath Alawadhi <109815858+Muath-Alawadhi@users.noreply.github.com>
2023-09-16 10:17:50 +0100

    300 fixed

--- client/src/component/ShowingVideos.jsx
+++ client/src/component/ShowingVideos.jsx
@@ -1,114 +1,3 @@
-// import React, { useState, useEffect } from "react";
-
-// function ShowingVideos() {
-//   const [videos, setVideos] = useState([]);
-//   const [orderBy, setOrderBy] = useState("desc");
-
-//   useEffect(() => {
-//     fetch(`http://127.0.0.1:5000/?order=${orderBy}`)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setVideos(data);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching data:", error);
-//       });
-//   }, [orderBy]);
-
-//   const toggleOrder = () => {
-//     setOrderBy(orderBy === "asc" ? "desc" : "asc");
-//   };
-
-//   const convertToEmbedUrl = (url) => {
-//     const videoId = url.split("v=")[1];
-//     return `https://www.youtube.com/embed/${videoId}`;
-//   };
-
-//   const deleteVideo = (id) => {
-//     fetch(`http://127.0.0.1:5000/${id}`, {
-//       method: "DELETE",
-//     })
-//       .then((response) => {
-//         response.json();
-//         setVideos((prevVideos) =>
-//           prevVideos.filter((video) => video.id !== id)
-//         );
-//       })
-//       .catch((error) => {
-//         console.error("Error deleting video:", error);
-//       });
-//   };
-
-//   const handleRating = (id, type) => {
-//     fetch(`http://127.0.0.1:5000/rate/${id}/${type}`, {
-//       method: "PUT",
-//     })
-//       .then((response) => {
-//         return response.json();
-//       })
-//       .then((updatedVideo) => {
-//         setVideos((prevVideos) =>
-//           prevVideos.map((video) => {
-//             if (video.id === id) {
-//               return { ...video, rating: updatedVideo.rating };
-//             }
-//             return video;
-//           })
-//         );
-//       })
-//       .catch((error) => {
-//         console.error("Error updating rating:", error);
-//       });
-//   };
-
-//   const sortedVideos = [...videos].sort((a, b) => {
-//     if (orderBy === "asc") {
-//       return a.rating - b.rating;
-//     } else {
-//       return b.rating - a.rating;
-//     }
-//   });
-
-//   return (
-//     <div className="ShowingVideos">
-//       <button onClick={toggleOrder} className="Ordes">
-//         {orderBy === "asc" ? "Order Ascending" : "Order Descending"}
-//       </button>
-
-//       {sortedVideos.map((video) => (
-//         <div className="videos" key={video.id}>
-//           <p>{video.title}</p>
-//           <div className="buttons">
-//             <i
-//               className="fa-solid fa-thumbs-up"
-//               onClick={() => handleRating(video.id)}
-//             ></i>
-//             <h4>{video.rating}</h4>
-//             <i
-//               className="fa-solid fa-thumbs-down"
-//               onClick={() => handleRating(video.id)}
-//             ></i>
-//           </div>
-
-//           <iframe
-//             className="allVideos"
-//             width="300"
-//             height="220"
-//             src={convertToEmbedUrl(video.url)}
-//             title={video.title}
-//             frameBorder="0"
-//             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-//             allowFullScreen
-//           ></iframe>
-//           <button onClick={() => deleteVideo(video.id)}>Delete</button>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default ShowingVideos;
-
 import React, { useState, useEffect } from "react";
 
 function ShowingVideos() {
@@ -116,7 +5,7 @@ function ShowingVideos() {
   const [orderBy, setOrderBy] = useState("desc");
 
   useEffect(() => {
-    fetch("http://127.0.0.1:5000/videos")
+    fetch("https://node-js-full-stack-project-assessment.onrender.com/videos")
       .then((response) => response.json())
       .then((data) => {
         setVideos(data);
@@ -136,9 +25,12 @@ function ShowingVideos() {
   };
 
   const deleteVideo = (id) => {
-    fetch(`http://127.0.0.1:5000/videos/${id}`, {
-      method: "DELETE",
-    })
+    fetch(
+      `https://node-js-full-stack-project-assessment.onrender.com/videos/${id}`,
+      {
+        method: "DELETE",
+      }
+    )
       .then((response) => {
         return response.json();
       })
@@ -153,14 +45,16 @@ function ShowingVideos() {
   };
 
   const handleRating = (id, type) => {
-    fetch(`http://127.0.0.1:5000/rate/${id}/${type}`, {
-      method: "PUT",
-    })
+    fetch(
+      `https://node-js-full-stack-project-assessment.onrender.com/rate/${id}/${type}`,
+      {
+        method: "PUT",
+      }
+    )
       .then((response) => {
         return response.json();
       })
       .then((updatedRating) => {
-        // Update the state with the new rating
         setVideos((prevVideos) =>
           prevVideos.map((video) => {
             if (video.id === id) {
--- server/server.js
+++ server/server.js
@@ -1,83 +1,7 @@
-// const express = require("express");
-// const app = express();
-// const port = process.env.PORT || 5000;
-// const videos = require("../client/src/Data/exampleresponse.json");
-// const cors = require("cors");
-
-// app.use(express.json());
-// app.use(cors());
-
-// // <-------------------get------------------->
-// app.get("/", (req, res) => {
-//   let orderedVideos = [...videos];
-//   const order = req.query.order;
-
-//   if (order === "asc") {
-//     orderedVideos.sort((a, b) => a.rating - b.rating);
-//   } else {
-//     orderedVideos.sort((a, b) => b.rating - a.rating);
-//   }
-
-//   res.json(orderedVideos);
-// });
-// // <----------------------------------------->
-// // <---------------get By ID----------------->
-
-// app.get("/:id", (req, res) => {
-//   const id = req.params.id * 1;
-//   const video = videos.find((ele) => ele.id === id);
-
-//   if (!video) {
-//     return res
-//       .status(404)
-//       .json({ result: "failure", message: "Video not found" });
-//   }
-
-//   res.json(video);
-// });
-// // <----------------------------------------->
-
-// // <---------------------Edit---------------->
-// app.post("/", (req, res) => {
-//   const { title, url } = req.body;
-
-//   if (!title || !url) {
-//     return res
-//       .status(400)
-//       .json({ result: "failure", message: "Video could not be saved" });
-//   }
-//   const id = Math.max(...videos.map((video) => video.id), 0) + 1;
-//   const newVideo = { id, title, url, rating: 0 };
-//   videos.push(newVideo);
-
-//   res.status(201).json({ id });
-// });
-// // <----------------------------------------->
-
-// // <-------------------Delete---------------->
-// app.delete("/:id", (req, res) => {
-//   const id = req.params.id * 1;
-
-//   const index = videos.findIndex((ele) => ele.id === id);
-
-//   if (index === -1) {
-//     return res
-//       .status(404)
-//       .json({ result: "failure", message: "Video not found" });
-//   }
-
-//   videos.splice(index, 1);
-
-//   res.json({ message: "The item has been deleted" });
-// });
-// // <------------------ app starting ----------->
-
-// app.listen(port, () => console.log(`Listening on port ${port}`));
-
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const videosPool = require("./DBConfig"); // Assuming you have a database configuration
+const videosPool = require("./DBConfig");
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -189,7 +113,7 @@ app.delete("/videos/:id", async (req, res) => {
 
 app.put("/rate/:id/:type", async (req, res) => {
   const videoId = req.params.id;
-  const ratingType = req.params.type; // 'up' or 'down'
+  const ratingType = req.params.type;
 
   if (ratingType !== "up" && ratingType !== "down") {
     res.status(400).json({ error: "Invalid rating type" });
