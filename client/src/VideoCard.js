0c85088819af63c16553dc1a41262672fe2c0134
Najah Farah <najahfarah27@gmail.com>
2023-09-18 09:40:37 +0100

    tiyding

--- client/src/VideoCard.js
+++ client/src/VideoCard.js
@@ -1,56 +1,33 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 
 const Videocard = ({ video, removeVideo, upVote, downVote }) => {
-  const { id, title, url, rating } = video;
+  const { id, title, url } = video;
+  const [rating, setRating] = useState(video.rating)
   
-  const embedVideos = (url) => {
+  function embedVideos(url) {
     if (url.includes("watch?v=")) {
-     const videoId = url.split("watch?v=")[1];
-      
+      const videoId = url.split("watch?v=")[1];
+
       return `https://www.youtube.com/embed/${videoId}`;
     } else {
-      
+
       return url;
     }
-  };
+  }
     
+   function handleUpVote() {
+    setRating(rating + 1);
+  }
+  
+    function handleDownVote() {
+    setRating(rating - 1);
+  }
 
-  const handleUpVote = () => {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/videos/${video.id}/upvote`, {
-      method: 'POST',
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Failed to upvote video');
-        }
-        upVote(video.id);
-      })
-      .catch((error) => {
-        console.error('Error upvoting video:', error);
-      });
-  };
-
-  const handleDownVote = () => {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/videos/${video.id}/downvote`, {
-      method: 'POST',
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Failed to downvote video');
-        }
-        downVote(video.id);
-      })
-      .catch((error) => {
-        console.error('Error downvoting video:', error);
-      });
-  };
-
-  return (
+   return (
     <div className="video"> 
      
         <h2>{title}</h2>
-        <iframe width="100" height="100" src={embedVideos(url)} title={title} allowFullScreen></iframe>
+        <iframe width="250" height="250" src={embedVideos(url)} title={title} allowFullScreen></iframe>
         <p className="video-rating">Votes: {rating}</p>
         <button className="upvote-button" onClick={handleUpVote}></button>
         <button className="downvote-button" onClick={handleDownVote}></button>
