bf46c8bedb5924c6c2f66a999bb6e2e2acdb1661
Afsha10 <unityofbeing1@gmail.com>
2023-09-19 23:35:52 +0100

    Added a loading message and animation to indicate data being loaded
    Used a Tailwind's built-in class for loading animation to indicated the data is being loaded from the server

--- client/src/components/MainContainer.jsx
+++ client/src/components/MainContainer.jsx
@@ -4,17 +4,25 @@ import VideoForm from "./VideoForm";
 import { baseUrl } from "../config";
 
 function MainContainer() {
+  const [loading, setLoading] = useState(true);
   const [videoData, setVideoData] = useState();
   const [fetchData, setFetchData] = useState(true);
 
   useEffect(() => {
+    setLoading(true); // set setLoading to true before fetching data
     if (fetchData) {
       fetch(`${baseUrl}/videos`) // prod
         .then((response) => response.json())
-        .then((data) => setVideoData(data));
+        .then((data) => {
+          setVideoData(data);
+          setLoading(false); // set setLoading to false after data is fetched
+        })
+        .catch((error) => {
+          console.log("Error fetching data:", error); // log any errors
+        });
     }
     setFetchData(false);
-  }, [fetchData, videoData]);
+  }, [fetchData]);
 
   return (
     <div>
@@ -23,11 +31,18 @@ function MainContainer() {
         videoData={videoData}
         setVideoData={setVideoData}
       />
-      <CardsContainer
-        setFetchData={setFetchData}
-        videoData={videoData}
-        setVideoData={setVideoData}
-      />
+      {loading ? (
+        <div className="justify-center text-center flex items-center  text-lg sm:text-4xl xl:m-9 xl:p-9 xl:text-5xl ">
+          <h2>Page loading...</h2>
+          <div className="h-12 w-12 animate-spin rounded-full border-b-4 border-t-4 border-gray-600 mx-9"></div>
+        </div>
+      ) : (
+        <CardsContainer
+          setFetchData={setFetchData}
+          videoData={videoData}
+          setVideoData={setVideoData}
+        />
+      )}
     </div>
   );
 }
