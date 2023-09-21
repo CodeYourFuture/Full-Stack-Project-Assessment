cf043d0ae2b25d5f8c7939685fc8a8ea04becc36
Afsha10 <unityofbeing1@gmail.com>
2023-09-21 01:35:29 +0100

    Added a Header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="flex flex-row pb-8 pt-8 text-3xl sm:px-8 sm:pt-8 sm:text-4xl sm:font-bold sm:tracking-wider lg:text-6xl">
+      <img
+        src={youtubeLogo}
+        alt="Logo"
+        className="w-13 mx-4 h-9 sm:mx-7 sm:h-16 sm:w-24"
+      />
+      <h1>YouTube Vault</h1>
+    </header>
+  );
+}
+
+export default Header;
