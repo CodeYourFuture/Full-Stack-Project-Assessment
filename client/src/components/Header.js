7f3446626db63d96960f25e828f25a413ef7cbf1
Yuliya110692 <122671093+Yuliya110692@users.noreply.github.com>
2023-09-23 12:04:32 +0100

    Added Header + updated css

---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <h1>Video Recommendation</h1>
+    </header>
+  );
+};
+
+export default Header;
