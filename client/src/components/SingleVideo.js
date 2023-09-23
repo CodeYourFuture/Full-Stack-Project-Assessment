cc17094b118ffe6f0f3952165a617765c58500d0
Yuliya110692 <122671093+Yuliya110692@users.noreply.github.com>
2023-09-23 12:05:16 +0100

    Updated SingleVideo

---
+++ client/src/components/SingleVideo.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
+import "./SingleVideo.css";
+
+const SingleVideo = ({ video, handleDeleteVideoClick }) => {
+  const { title, url, rating, id } = video;
+  const [votes, setVotes] = useState(0); // can be 0, 1 or -1 => to add to rating when thumbsUp or ThumbsDown is clicked
+  const iconSize = 30;
+
+  const handleUpVote = () => {
+    setVotes(1);
+  };
+
+  const handleDownVote = () => {
+    setVotes(-1);
+  };
+
+  return (
+    <div className="video-card">
+      <h3>{title}</h3>
+      <div className="iframe-container">
+        <iframe
+          className="responsive-iframe"
+          width="560"
+          height="310"
+          src={`https://www.youtube.com/embed/${url.split("v=")[1]}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="votes-container">
+        <div>
+          <FaThumbsUp
+            onClick={handleUpVote}
+            className={votes === 1 ? "voted thumb-icon" : "thumb-icon"}
+            size={iconSize}
+          />
+        </div>
+        <div>
+          <p>{rating + votes} votes</p>
+        </div>
+        <div>
+          <FaThumbsDown
+            onClick={handleDownVote}
+            className={votes === -1 ? "voted thumb-icon" : "thumb-icon"}
+            size={iconSize}
+          />
+        </div>
+      </div>
+      <button
+        onClick={() => handleDeleteVideoClick(id)}
+        className="delete-video-btn"
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+export default SingleVideo;
