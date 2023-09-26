2ee135f6b182ae7079dcb66af1e54f1fbd5de7ed
softacoder <113185323+softacoder@users.noreply.github.com>
2023-09-26 12:34:24 +0100

    commit add pg and dotenv

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 // update8.this version has databse connection and in videotable.sql sql code.
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
-require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
--- server/videotable.sql
+++ server/videotable.sql
@@ -4,4 +4,4 @@ CREATE TABLE videos
         title VARCHAR(50) NOT NULL,
         url VARCHAR(120) NOT NULL,
         rating integer not null
-)
+);
