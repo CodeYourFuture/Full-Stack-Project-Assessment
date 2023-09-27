a5f075f379d3ccd8950f0ee3208166cc9ca05daf
Kristina <kristina.dudnyk20@gmail.com>
2023-09-27 22:43:22 +0100

    +

--- client/src/CardVideo.jsx
+++ client/src/CardVideo.jsx
@@ -4,7 +4,12 @@ import UpdateRating from "./UpdateRating";
 export default function CardVideo({ video, setRefreshVideos }) {
   return (
     <div key={video.id} className="video-container">
-      <iframe title={video.title} src={video.url} />
+      <iframe
+        title={video.title}
+        src={video.url}
+        allow="autoplay; encrypted-media"
+        allowFullScreen
+      />
       <div className="videos-functions">
         <UpdateRating video={video} setRefreshVideos={setRefreshVideos} />
       </div>
