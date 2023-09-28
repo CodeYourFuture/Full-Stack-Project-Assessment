8752c40f62fb9bed8e982ff08dda012855e06e91
Adrian Ilovan <adrian88ilovan@yahoo.com>
2023-09-28 21:34:53 +0100

    fixed

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,26 +1,26 @@
 import React from 'react';
 
-
 function Video (props) {
   const videoId = props.link;
-  const changedVideoId = videoId.replace("https://www.youtube.com/watch?v=", "")
+  const changedVideoId = videoId.replace("https://www.youtube.com/watch?v=", "");
 
   const handleAddRating = () => {
     props.AddRating(props.name, props.rating); 
-
+  };
 
   const handleDownRating = () => {
     props.downRating(props.name);
+  };
 
   return (
     <div>
       <h1>{props.name}</h1>
       <p>{props.link}</p>
-      <iframe width="460" height="250" src= {`https://www.youtube.com/embed/${changedVideoId}`} title="YouTube player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="460" height="250" src={`https://www.youtube.com/embed/${changedVideoId}`} title="YouTube player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <div>
-        <button onClick={props.handleAddRating}>Plus</button>
+        <button onClick={handleAddRating}>Plus</button>
         <span> Rating: {props.rating} </span>
-        <button onClick={props.handleDownRating}>Minus</button>
+        <button onClick={handleDownRating}>Minus</button>
       </div>
     </div>
   );
