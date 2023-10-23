c40c58474e342b0e7cd840bedef6c6a9156683a6
BoshraM <gbboshra@gmail.com>
2023-10-23 11:13:43 +0100

    change database from localy to render

--- server/server.js
+++ server/server.js
@@ -13,11 +13,10 @@ app.use(bodyParser.json());
 
 
 const db = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 //getting all the data
