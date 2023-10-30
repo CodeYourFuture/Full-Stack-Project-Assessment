1789dba84db9fa4d5254cb2d2140ae3d0e989f29
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2023-10-30 07:02:39 +0000

    Updated query for POST endpoint to be able to get Id

--- server/server.js
+++ server/server.js
@@ -17,10 +17,6 @@ app.use(cors());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// Store and retrieve your videos from here
-// If you want, you can copy "exampleresponse.json" into here to have some data to work with
-
-
 // // GET "/"
 app.get("/videos", async (req, res) => {
   try {
@@ -39,14 +35,15 @@ app.post("/videos", async (req, res) => {
   const url = req.body.url;
   const rating = req.body.rating;
   const query = `INSERT INTO videos (title, url, rating)
-VALUES ($1, $2, $3)`;
+VALUES ($1, $2, $3) RETURNING id`;
   const values = [title, url, rating];
   try {
     const result = await db.query(query, values);
     const videoId = result.rows[0].id;
     res.status(201).json({ id: videoId });
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
+    console.log("Failed to save video", error);
+    res.status(500).json({ error: `Internal Server Error: ${error.message}` });
   }
 });
 
@@ -70,7 +67,6 @@ app.delete("/videos/:id", async (req, res) => {
   const videoId = req.params.id;
   const query = "DELETE FROM videos WHERE id = $1";
   const values = [videoId];
-
   try {
     await db.query(query, values);
     res.status(204).send();
