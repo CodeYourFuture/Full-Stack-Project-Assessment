7dc96917c66155ae4e09ee259e9dd766d1a7899e
onurat <114289826+onurat@users.noreply.github.com>
2023-12-13 16:34:44 +0000

    fixes
    now working with backend and database all integrated

--- server/server.js
+++ server/server.js
@@ -2,27 +2,24 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const db = require("./db");
-app.use(express.json()); 
-const cors = require("cors"); 
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-app.use(express.json()); 
-
-app.use(cors());
 
 app.get("/", async (req, res) => {
   try {
     const query = "SELECT * FROM videos";
     const { rows } = await db.query(query);
-
     res.json(rows);
   } catch (error) {
     console.error("Error fetching videos:", error);
     res.status(500).json({ result: "failure", message: "Failed to fetch videos" });
   }
 });
-
-
 
 app.post("/", async (req, res) => {
   const { title, url } = req.body;
@@ -35,7 +32,6 @@ app.post("/", async (req, res) => {
   }
 
   try {
-    
     const queryText = 'INSERT INTO videos (title, url) VALUES ($1, $2) RETURNING *';
     const values = [title, url];
 
@@ -48,52 +44,36 @@ app.post("/", async (req, res) => {
   }
 });
 
-
-
-app.post("/", (req, res) => {
-  const { title, url } = req.body;
-
- 
-  if (!title || !url) {
-    return res.status(400).json({ result: "failure", message: "Title and URL are required" });
-  }
-
-  const newVideo = {
-    id: generateUniqueId(),
-    title,
-    url,
-    rating: 0, 
-  };
-
-  videos.push(newVideo);
-
-  res.status(201).json({ id: newVideo.id });
-});
-
-app.get("/:id", (req, res) => {
+app.get("/:id", async (req, res) => {
   const videoId = req.params.id;
-  const video = videos.find((v) => v.id === videoId);
 
-  if (!video) {
-    return res.status(404).json({ result: "failure", message: "Video not found" });
+  try {
+    const query = "SELECT * FROM videos WHERE id = $1";
+    const { rows } = await db.query(query, [videoId]);
+
+    if (rows.length === 0) {
+      return res.status(404).json({ result: "failure", message: "Video not found" });
+    }
+
+    res.json(rows[0]);
+  } catch (error) {
+    console.error("Error fetching video by ID:", error);
+    res.status(500).json({ result: "failure", message: "Failed to fetch video" });
   }
-
-  res.json(video);
 });
 
-app.delete("/:id", (req, res) => {
+app.delete("/:id", async (req, res) => {
   const videoId = req.params.id;
-  const index = videos.findIndex((v) => v.id === videoId);
 
-  if (index === -1) {
-    return res.status(404).json({ result: "failure", message: "Video not found" });
+  try {
+    const query = "DELETE FROM videos WHERE id = $1";
+    await db.query(query, [videoId]);
+
+    res.json({});
+  } catch (error) {
+    console.error("Error deleting video:", error);
+    res.status(500).json({ result: "failure", message: "Failed to delete video" });
   }
-
-  videos.splice(index, 1);
-
-  res.json({});
 });
 
-function generateUniqueId() {
-  return Math.random().toString(36).substr(2, 9);
-}
+module.exports = app;
