b0a7273ef3de4958d283fa15e286ece90bf62c4c
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-12-22 15:11:18 +0000

    change the url for database deploy and edit the db file for fix

--- server/db.js
+++ server/db.js
@@ -2,12 +2,8 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  port: process.env.PG_PORT,
-  password: process.env.PG_PASSWORD,
-  ssl: true,
+  connectionString: process.env.DBConnLink,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
