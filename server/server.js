8c2f50e586cbb273eab064644d1167d7f3f2ee35
Olha Danylevska <odanilevskaya2@gmail.com>
2024-01-15 12:03:58 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,9 +5,11 @@ const cors = require("cors");
 
 app.use(express.json());
 app.use(cors());
+
 const dotenv = require("dotenv");
 dotenv.config();
 const { Pool } = require("pg");
+
 const db = new Pool({
   connectionString: process.env.DB_URL,
   ssl: { rejectUnauthorized: false },
