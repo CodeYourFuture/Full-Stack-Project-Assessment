eca6c28fdb8005b18b5ee1ea209fbb396855133c
Olha Danylevska <odanilevskaya2@gmail.com>
2024-01-15 11:42:09 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-require('dotenv').config()
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 const cors = require("cors");
+
 app.use(express.json());
 app.use(cors());
-
+const dotenv = require("dotenv");
+dotenv.config();
 const { Pool } = require("pg");
-const db = new Pool({
+const pool = new Pool({
   connectionString: process.env.DB_URL,
   ssl: { rejectUnauthorized: false },
 });
-
 
 // db.connect(function (err) {
 //   if (err) {
