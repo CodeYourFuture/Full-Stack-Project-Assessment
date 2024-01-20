6345ac2b0a4a20c27c9743d07cb5522d6a9904e9
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2024-01-20 23:08:16 +0000

    changed port

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 80;
 
 app.use(cors());
 app.use(bodyParser.json());
