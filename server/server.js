23081c949cf8ea57f8820238695a27953768d674
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2024-01-25 09:10:50 +0000

    changing the env file

--- server/server.js
+++ server/server.js
@@ -20,13 +20,13 @@ const { Pool } = require("pg");
 
 
 const db = new Pool({
-   connectionString: process.env.DB_URL 
-  // user: process.env.DB_USER,
-  // host: process.env.DB_HOST,
-  // database: process.env.DB_NAME,
-  // password: process.env.DB_PASSWORD,
-  // port: process.env.DB_PORT,
-  //   ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : false
+   
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+    ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : false
 });
 
 app.get("/", function (req, res) {
