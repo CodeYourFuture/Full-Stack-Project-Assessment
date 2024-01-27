2421ab701a77f81e15db06bd081737c7f46d50c6
Najah Farah <61600465+nfarah22@users.noreply.github.com>
2024-01-27 06:37:40 +0000

    aws

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 
 const config = {
-    REACT_APP_BACKEND_URL: 'https://youtubesaver-tbn2.onrender.com',
+    REACT_APP_BACKEND_URL: 'http://16.171.149.239:5000/',
   };
   
   export default config;
