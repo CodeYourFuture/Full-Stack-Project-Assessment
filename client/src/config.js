2214a6c3c68ce75e8a242aee043ad8c0d4c44e9e
Afsha10 <unityofbeing1@gmail.com>
2024-01-27 16:02:49 +0000

    Add aws backend link to config, add test.yml to workflow folder

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,6 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://video-storage.onrender.com"
+    ? "http://ec2-18-201-166-46.eu-west-1.compute.amazonaws.com:5000"
     : "http://localhost:5000";
+
+// https://video-storage.onrender.com
