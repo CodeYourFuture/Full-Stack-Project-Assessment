93463c6603cd268e05c9bc20d0569731894db794
mesgna woldeb <mesgna.mehari8@gmail.com>
2024-01-30 23:29:48 +0000

    api

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
   
   
   useEffect(() => {
-    const fetchURL = `http://ec2-54-208-152-154.compute-1.amazonaws.com:5050/videos?order=${isAsc ? "asc" : "desc"}`;
+    const fetchURL = `http://ec2-18-171-148-184.eu-west-2.compute.amazonaws.com:5000/videos?order=${isAsc ? "asc" : "desc"}`;
     console.log("Fetch URL:", fetchURL);
   
     fetch(fetchURL)
