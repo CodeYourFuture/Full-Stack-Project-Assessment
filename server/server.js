060db4e74b1d60048ced4ebff8d35199bb3c0615
Olha Danylevska <odanilevskaya2@gmail.com>
2024-01-30 00:29:09 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send({ express: "Your Backend Service is Running" });
+  res.send({ express: "Your Backend Service is Running Wow" });
 });
 
 app.put("/videos/:id", async (req, res) => {
