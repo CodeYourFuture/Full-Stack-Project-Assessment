fa97d008758bd3929a0c94c62ffe90935b6a03a9
Afsha10 <unityofbeing1@gmail.com>
2024-01-31 20:51:24 +0000

    Correct the config file link

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 export const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "http://ec2-18-201-166-46.eu-west-1.compute.amazonaws.com:5000"
+    ? "https://video-storage.onrender.com"
     : "http://localhost:5000";
 
-// https://video-storage.onrender.com
+// http://ec2-18-201-166-46.eu-west-1.compute.amazonaws.com:5000
