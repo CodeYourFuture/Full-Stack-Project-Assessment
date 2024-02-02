f5a86fce20e2ed4fca7600bd3938b7292807df58
mesgna woldeb <mesgna.mehari8@gmail.com>
2024-02-02 23:44:39 +0000

    change

--- client/src/componets/OrderButtons.js
+++ client/src/componets/OrderButtons.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function OrderButton({ toggleButton }) {
   return (
-    <div className="mb-5 ">
+    <div className="mb-6 ">
       <button type="button" className="btn btn-success" onClick={toggleButton}>
         Order Ratings
       </button>
