b680059f6605b25392895ef6ec492c5d522e70cb
mesgna woldeb <mesgna.mehari8@gmail.com>
2024-02-09 15:10:11 +0000

    add video

--- client/src/componets/Add-Video/AddVideo.js
+++ client/src/componets/Add-Video/AddVideo.js
@@ -104,7 +104,7 @@ const AddVideo = ({ setVideos }) => {
 
                 <Button
                   onClick={VideoAdder}
-                  className=" mt-2 mr-2 mb-3"
+                  className=" mt-2 mr-2 mb-2"
                   variant="primary"
                   type="submit"
                 >
