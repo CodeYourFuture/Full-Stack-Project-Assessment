6745ecd191220a879c0758676876c510eee12a66
mesgna woldeb <mesgna.mehari8@gmail.com>
2024-02-10 16:25:50 +0000

    checking

--- server/server.js
+++ server/server.js
@@ -23,7 +23,7 @@ app.use(express.static(path.resolve(__dirname, "../client/build")));
 app.use(bodyParser.json());
 app.use(cors());
 app.get("/", (req, res) => {
-  res.send("Hello from the  my Server!");
+  res.send("Hello from the  my  cool Server!");
 });
 
 app.get("/videos", (req, res) => {
