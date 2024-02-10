518a9b2919b338c6f815d5c895187eafaf9b2e8b
mesgna woldeb <mesgna.mehari8@gmail.com>
2024-02-10 14:17:22 +0000

    github

--- server/videos.sql
+++ server/videos.sql
@@ -7,7 +7,7 @@ CREATE TABLE videos (
   
 );
 
-INSERT INTO videos (title, url, rating) VALUES ('Never Gonna Give You Up', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 23);
+INSERT INTO videos (title, url, rating) VALUES ('Never Gonna Give You Up', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 30);
 INSERT INTO videos (title, url, rating) VALUES ('The Coding Train', 'https://www.youtube.com/watch?v=HerCR8bw_GE', 230);
 INSERT INTO videos (title, url, rating) VALUES ('Mac & Cheese | Basics with Babish', 'https://www.youtube.com/watch?v=FUeyrEN14Rk', 2111);
 INSERT INTO videos (title, url, rating) VALUES ('Videos for Cats to Watch - 8 Hour Bird Bonanza', 'https://www.youtube.com/watch?v=xbs7FT7dXYc', 11);
