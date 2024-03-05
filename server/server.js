00538ce0ecb509973c8751648bfee353d665cbc0
Kristina <kristina.dudnyk20@gmail.com>
2024-03-05 11:47:49 +0000

    changing pool env vars

--- server/server.js
+++ server/server.js
@@ -9,11 +9,12 @@ const app = express();
 const { Pool } = require("pg");
 
 const db = new Pool({
-  user: process.env.DB_USERNAME,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: DB_URL,
+  // user: process.env.DB_USERNAME,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_DATABASE,
+  // password: process.env.DB_PASSWORD,
+  // port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false,
   },
