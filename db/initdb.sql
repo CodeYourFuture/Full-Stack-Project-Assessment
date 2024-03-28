21beb9d739c8625f7ae8cfb11416a0f6da4245ba
Dedekind561 <mitchell.lloyd154@gmail.com>
2024-03-28 11:57:26 +0000

    Revert "add setup for deployment"
    This reverts commit d7bb957c3430c0eb4240bfab7db730871b9c1c93.

--- db/initdb.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS videos CASCADE;
-
-CREATE TABLE videos(
-    name VARCHAR,
-    src VARCHAR
-);
-
-INSERT INTO videos (name,src) VALUES ('The Coding Train', 'https://www.youtube.com/watch?v=HerCR8bw_GE');
--- server/api.js
+++ server/api.js
@@ -3,9 +3,8 @@ import db from "./db.js";
 const router = Router();
 
 router.get("/videos", async (_, res) => {
-	const result = await db.query("SELECT * FROM videos;");
-	console.log(result);
-	res.json({ msg: result.rows });
+	const result = await db.query("SELECT 'ok' ok");
+	res.json(result.rows[0]);
 });
 
 export default router;
