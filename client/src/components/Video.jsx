4e1b9f2f75e09530a81cb9c739d7a4f3832e3c44
Dedekind561 <mitchell.lloyd154@gmail.com>
2024-03-28 11:53:31 +0000

    Revert "extract youtube id for video iframe"
    This reverts commit c787fc1ddf9f0f1c87b19a9ec97c720efb039f08.

--- client/src/components/Video.jsx
+++ client/src/components/Video.jsx
@@ -1,5 +1,4 @@
 export function Video({ name, src }) {
-	const [_, youtubeId] = src.split("?v=");
 	return (
 		<>
 			<h2>{name}</h2>
@@ -7,7 +6,7 @@ export function Video({ name, src }) {
 				title="Inline Frame Example"
 				width="300"
 				height="200"
-				src={`https://www.youtube.com/embed/${youtubeId}`}
+				src={src}
 			></iframe>
 		</>
 	);
