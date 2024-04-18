a5ce7f197858094d5ed8db8cba9d07032e4211c3
MitchLloyd <mitch@codeyourfuture.io>
2024-04-18 15:38:36 +0100

    add create and insert to setup script (#480)
    Co-authored-by: Dedekind561 <mitchell.lloyd154@gmail.com>

---
+++ db/initdb.sql
@@ -0,0 +1,11 @@
+\c videorec;
+
+DROP TABLE IF EXISTS videos CASCADE;
+
+CREATE TABLE videos (
+    title VARCHAR,
+    src VARCHAR
+);
+
+INSERT INTO videos (title,src) VALUES ('Never Gonna Give You Up','https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+-- you can insert more rows using example data from the example_data.csv file
