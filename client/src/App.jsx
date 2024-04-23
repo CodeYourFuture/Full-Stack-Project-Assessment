6a30a8ce8dd38086c9f26437816452c60a7566d6
karimi <behrouzkarimi1993@gmail.com>
2024-04-23 21:07:21 +0100

    fetching from api.js , rendering in the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,44 @@
+import { key } from "localforage";
+import { useState, useEffect } from "react";
+
 const App = () => {
+	const [recomVids, setRecomVids] = useState([]);
+	useEffect(() => {
+		fetchRecommendedVids();
+	}, []);
+
+	// fetching data from api.js file to render in the page
+	const fetchRecommendedVids = async () => {
+		try {
+			const response = await fetch("/api/videos");
+			const data = await response.json();
+			console.log(data, "<---------data");
+			setRecomVids(data);
+		} catch (error) {
+			console.error(error, "For fetch of vids");
+		}
+	};
+
+	const videoDisplayer = (arr) => {
+		return (
+			<ul>
+				{arr.map((vidObject, index) => (
+					<li key={index}>
+						<p>
+							<a href={vidObject.src} target="_blank">
+								{vidObject.title}
+							</a>
+						</p>
+					</li>
+				))}
+			</ul>
+		);
+	};
 	return (
 		<>
 			<h1>Video Recommendations</h1>
+
+			<div>{videoDisplayer(recomVids)}</div>
 		</>
 	);
 };
