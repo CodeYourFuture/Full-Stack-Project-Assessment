38d8eb783da338c40d62c1dbd20c929c450b40ec
karimi <behrouzkarimi1993@gmail.com>
2024-05-01 23:01:34 +0100

    post fetch to the server within input form

--- client/src/NewVideoForm.jsx
+++ client/src/NewVideoForm.jsx
@@ -6,36 +6,47 @@ function NewVideoForm() {
 
 	// Example POST method implementation:
 	async function PostNewVideo(url = "", data = {}) {
-		// Default options are marked with *
-		const response = await fetch(url, {
-			method: "POST", // *GET, POST, PUT, DELETE, etc.
-			mode: "cors", // no-cors, *cors, same-origin
-			cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-			credentials: "same-origin", // include, *same-origin, omit
-			headers: {
-				"Content-Type": "application/json",
-				// 'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			redirect: "follow", // manual, *follow, error
-			referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-			body: JSON.stringify(data), // body data type must match "Content-Type" header
-		});
-		console.log(response.json(), "<--------response for posting video");
-		return response.json(); // parses JSON response into native JavaScript objects
-	}
+		console.log({ titleFromInput, srcFromInput }, "object to be fetched");
+		try {
+			const response = await fetch(url, {
+				method: "POST",
+				mode: "cors",
+				cache: "no-cache",
+				credentials: "same-origin",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				redirect: "follow",
+				referrerPolicy: "no-referrer",
+				body: JSON.stringify(data),
+			});
 
+			if (response.ok) {
+				console.log("successful!");
+			} else {
+				// Handle other response statuses
+				console.error("Failed to insert new video:", response.statusText);
+			}
+		} catch (error) {
+			console.error("Error posting video:", error);
+			throw error; // Rethrow error to handle it further if needed
+		}
+	}
 	//This function gets the url and make it a proper link to be embeded.
 	function makeTheLinkToEmbed(url) {
 		//This line will return the id of utube link
-		const id = url.split("?v")[1];
+		const id = url.split("?v=")[1];
 		//Link for being embded
 		return "http://www.youtube.com/embed/" + id;
 	}
 
-	const handleSubmit = () => {
-		makeTheLinkToEmbed(srcFromInput);
-		const objectToPost = { titleFromInput, srcFromInput };
-		PostNewVideo("/api/video", objectToPost);
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const title = titleFromInput;
+		const src = makeTheLinkToEmbed(srcFromInput);
+		// this object send the video title and its embded format src to the back end
+		const objectToPost = { title, src };
+		PostNewVideo("api/videos", objectToPost);
 	};
 
 	const handleInputTitle = (e) => {
@@ -46,7 +57,7 @@ function NewVideoForm() {
 	};
 	return (
 		<>
-			<form onSubmit={handleSubmit}>
+			<form onSubmit={(e) => handleSubmit(e)}>
 				<label>Enter Videos Title:</label>
 				<input
 					type="text"
@@ -61,6 +72,7 @@ function NewVideoForm() {
 				></input>
 				<button type="submit">Add it!</button>
 			</form>
+			{console.log(titleFromInput, srcFromInput, "<------titel and src")}
 		</>
 	);
 }
