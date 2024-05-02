46c4ce5e45a1946247f496c17f0c03884573c792
karimi <behrouzkarimi1993@gmail.com>
2024-05-02 15:04:24 +0100

    fetch delete method done , delete by clicking on remove button in the page

---
+++ client/src/DeleteVideoButton.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+const DeleteVideobutton = ({ idToDelete, fetchRecommendedVids }) => {
+	const [deletionstatus, setdeletionstatus] = useState(null);
+	//delete a video using fetch method
+	const deleteVideoFetch = async (videoId) => {
+		try {
+			const response = await fetch(`/api/videos/${videoId}`, {
+				method: "DELETE",
+			});
+			if (response.ok) {
+				console.log(response, "this is response");
+				setdeletionstatus("Deleted!");
+				fetchRecommendedVids();
+			}
+		} catch (error) {
+			console.error("Error while deletion: " + error);
+		}
+	};
+
+	const handleClickDelete = () => {
+		deleteVideoFetch(idToDelete);
+	};
+	return (
+		<>
+			<button onClick={() => handleClickDelete()}>Remove Video</button>
+		</>
+	);
+};
+
+export default DeleteVideobutton;
