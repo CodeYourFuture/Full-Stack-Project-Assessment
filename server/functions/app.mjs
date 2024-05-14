0ed06b0efbf472a18cace931bd168f34d56c5ede
fikretellek <fikretellek@icloud.com>
2024-05-14 20:34:29 +0100

    updated file name

--- server/functions/app.mjs
+++ server/functions/app.mjs
@@ -8,7 +8,7 @@
 import express from "express";
 import serverless from "serverless-http";
 import apiRouter from "../api.js";
-import healthCheckRouter from "../health-check.js";
+import healthCheckRouter from "../health.js";
 import cors from "cors";
 
 const app = express();
