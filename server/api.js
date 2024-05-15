8970a7a1e6a3cb05699d61923c3fe7307a5d1a71
Pedro Ricciardi <riccip@outlook.com>
2024-05-15 11:15:45 +0100

    post videos endpoint created

--- server/api.js
+++ server/api.js
@@ -23,7 +23,14 @@ router.get("/videos", async (_, res) => {
 });
 
 router.post("/videos", async (req, res) => {
-	res.json(req.body);
+	const { title, url } = req.body;
+	const addVideoInDatabase = await db.query("INSERT INTO videos (title, src) VALUES ($1, $2) RETURNING *", [title, url]);
+	const videoAdded = await db.query("SELECT * FROM videos WHERE src = $1", [url]);
+	res.json({
+		success: true,
+		status: 201,
+		data: videoAdded.rows[0],
+	});
 })
 
 router.get("/health", (_, res) => {
