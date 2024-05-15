c0af14b77a773a6c4743dd16f531d404bc19594c
RbAvci <rb.avci@gmail.com>
2024-05-15 23:28:15 +0100

    add post new video endpoint

--- server/api.js
+++ server/api.js
@@ -17,4 +17,18 @@ router.get("/health", async (_, res) => {
 	res.sendStatus(200);
 });
 
+router.post("/videos", async (req, res) => {
+	if (!req.body.title) {
+		return res.status(422).json({ message: "Title field is required" });
+	}
+	if (!req.body.src) {
+		return res.status(422).json({ message: "src field is required" });
+	}
+	const result = await db.query(
+		`INSERT INTO videos (title,src) VALUES ('${req.body.title}','${req.body.src}') RETURNING id`
+	);
+	const newVideoId = result.rows[0].id;
+	res.status(200).json({ success: true, data: { id: newVideoId } });
+});
+
 export default router;
