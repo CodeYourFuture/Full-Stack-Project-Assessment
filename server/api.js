c85c35d93523d571d1edb0cf951b395564f3cc77
Pedro Ricciardi <riccip@outlook.com>
2024-05-15 09:18:35 +0100

    get videos adjusted to show ids and more information from the data

--- server/api.js
+++ server/api.js
@@ -4,7 +4,22 @@ const router = Router();
 
 router.get("/videos", async (_, res) => {
 	const result = await db.query("SELECT * FROM videos");
-	res.json(result.rows);
+
+	const videos = result.rows.map((video) => {
+	    return {
+			id: video.id,
+			title: video.title,
+			url: video.src,
+		};
+	})
+	
+	const jsonResult = {
+		success: true,
+		total: result.rowCount,
+		data: videos,
+	};
+
+	res.status(200).json(jsonResult);
 });
 
 router.get("/health", (_, res) => {
