fc1e8d05775d445c20194b127765f86f0b25e691
Pedro Ricciardi <riccip@outlook.com>
2024-05-16 08:54:50 +0100

    add video by form feature implemented

--- client/src/components/AddVideoForm.jsx
+++ client/src/components/AddVideoForm.jsx
@@ -1,18 +1,44 @@
+import { useState } from "react";
+
 export function AddVideoForm() {
-  const addVideoHandler = (e) => {
-    e.preventDefault()
-    const form = e.target
-    const formData = new FormData(form)
-    console.log("formData: ", formData)
-    const title = formData.get('title')
-    const url = formData.get('url')
-    console.log(title, url)
-  }
-  return (
-    <form onSubmit={addVideoHandler}>
-      <input type="text" name="title" placeholder="Title" />
-      <input type="text" name="url" placeholder="URL" />
-      <button type="submit">Add Video</button>
-    </form>
-  )
+	const [isAddingVideo, setIsAddingVideo] = useState(false);
+
+	const addVideoHandler = async (e) => {
+		e.preventDefault();
+
+		const title = e.target.elements.title.value;
+		const url = e.target.elements.url.value;
+
+		setIsAddingVideo(true);
+		try {
+			const response = await fetch("/api/videos", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ title, url }),
+			});
+
+			if (!response.ok) {
+				throw new Error(
+					`Failed to add video. Please, check the response here: ${response}`
+				);
+			}
+
+			console.log("Video added successfully!");
+			window.location.reload();
+		} catch (error) {
+			console.error("Error adding video:", error.message);
+		}
+	};
+
+	return (
+		<form onSubmit={addVideoHandler}>
+			<input type="text" name="title" placeholder="Title" />
+			<input type="text" name="url" placeholder="URL" />
+			<button type="submit">
+				{isAddingVideo ? "Adding Video..." : "Add Video"}
+			</button>
+		</form>
+	);
 }
