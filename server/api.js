f22887f3577b1973cfa96ce0978c71199b4246a5
HadikaMalik <hadika.malik@live.com>
2024-05-17 23:01:24 +0100

    done ticket

--- server/api.js
+++ server/api.js
@@ -3,8 +3,14 @@ import db from "./db.js";
 const router = Router();
 
 router.get("/videos", async (_, res) => {
-	const result = await db.query("SELECT * FROM videos;");
-	res.json(result.rows);
+	try{
+		const result = await db.query("SELECT * FROM videos;");
+		res.json(result.rows);
+	}
+	catch(error){
+		console.error("Database connection error:", error);
+		res.status(500).json({success:false, error: "Could not connect to the database"});
+	}
 });
 
 export default router;
