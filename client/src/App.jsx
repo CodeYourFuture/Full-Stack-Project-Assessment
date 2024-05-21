91eab1a2e51a3170c6de5983cf5ef93c59913173
Pedram Amani <amaaanip@gmail.com>
2024-05-21 12:00:05 +0100

    video recommandation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import { AddVideoForm } from "./components/AddVideoForm";
+import ListVideos from "./components/ListVideos";
 
 const App = () => {
 	return (
 		<>
 			<h1>Video Recommendations</h1>
+			<ListVideos />
 			<AddVideoForm />
 		</>
 	);
---
+++ client/src/components/ListVideos.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+
+function ListVideos() {
+	const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		const fetchVideos = async () => {
+			try {
+				const response = await fetch("/api/videos");
+				const data = await response.json();
+				setVideos(data);
+			} catch (error) {
+				console.error("Error fetching videos:", error);
+			}
+		};
+
+		fetchVideos();
+	}, []);
+
+	return (
+		<>
+			<ul>
+				{videos.map((video, index) => (
+					<li key={index}>
+						<a href={video.src}>{video.title}</a>
+					</li>
+				))}
+			</ul>
+		</>
+	);
+}
+
+export default ListVideos;
