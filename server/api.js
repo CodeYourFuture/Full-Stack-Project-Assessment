029926797e586f575176ee21aa40e1adfb1f5084
Pedro Ricciardi <riccip@outlook.com>
2024-05-22 08:54:26 +0100

    error handling added on get all videos route

--- server/api.js
+++ server/api.js
@@ -3,23 +3,32 @@ import db from "./db.js";
 const router = Router();
 
 router.get("/videos", async (_, res) => {
-	const result = await db.query("SELECT * FROM videos");
+	try {
+		const result = await db.query("SELECT * FROM videos");
 
-	const videos = result.rows.map((video) => {
-		return {
-			id: video.id,
-			title: video.title,
-			url: video.src,
+		const videos = result.rows.map((video) => {
+			return {
+				id: video.id,
+				title: video.title,
+				url: video.src,
+			};
+		})
+
+		const jsonResult = {
+			success: true,
+			total: result.rowCount,
+			data: videos,
 		};
-	})
 
-	const jsonResult = {
-		success: true,
-		total: result.rowCount,
-		data: videos,
-	};
-
-	res.status(200).json(jsonResult);
+		res.status(200).json(jsonResult);
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			status: 500,
+			message: "Could not connect to the database",
+		});
+	}
 });
 
 router.post("/videos", async (req, res) => {
