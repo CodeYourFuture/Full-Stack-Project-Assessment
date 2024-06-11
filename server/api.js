f0297e30797e41511622b00c43baadca9242dbec
aishaathmanlali <aishaathmanal@gmail.com>
2024-06-11 15:39:50 +0100

    update delete endpoint and working

--- server/api.js
+++ server/api.js
@@ -19,52 +19,26 @@ router.get("/videos", async (_, res) => {
 // Delete a specific video by ID
 router.delete("/videos/:id", async (req, res) => {
 	const { id } = req.params;
-	console.log("Received request to delete video with ID:", id);
-	res.send("video deleted");
 
-// 	try {
-// 		// Check if the video exists
-// 		const selectResult = await db.query("SELECT * FROM videos WHERE id = $1", [
-// 			id,
-// 		]);
-// 		console.log("Select query result:", selectResult.rows);
+	try {
+		const selectResult = await db.query("SELECT * FROM videos WHERE id = $1", [
+			id,
+		]);
+		//checks if video doesn't exist
+		if (selectResult.rows.length === 0) {
+			console.log("Video not found for ID:", id);
+			return res.status(404).json({ error: "Video not found" });
+		}
 
-// 		if (selectResult.rows.length === 0) {
-// 			// If no video found with the given ID, respond with 404
-// 			console.log("Video not found for ID:", id);
-// 			return res.status(404).json({ error: "Video not found" });
-// 		}
-
-// 		// Delete the video
-// 		const deleteResult = await db.query("DELETE FROM videos WHERE id = $1", [
-// 			id,
-// 		]);
-// 		console.log("Delete query result:", deleteResult);
-
-// 		// Respond with 204 No Content to indicate successful deletion
-// 		res.status(204).send();
-// 	} catch (error) {
-// 		console.error("Error deleting video:", error);
-// 		res.status(500).json({ error: "Internal Server Error" });
-// 	}
-// });
-// router.delete("/videos/:id", async (req, res) => {
-// 	const { id } = req.params;
-// 	console.log(id);
-
-// 	db.query(`DELETE * FROM videos WHERE id = ${id}`)
-// 		.then((result) => {
-// 			console.log(result);
-// 			if (result.rows.length === 0) {
-// 				return res.status(404).json({ error: "Video not found" });
-// 			} else {
-// 				res.status(204).send();
-// 			}
-// 		})
-// 		.catch((error) => {
-// 			console.log("Error deleting video:", error);
-// 			res.status(500).json({ error: "Internal Server Error" });
-// 		});
+		// Delete the video
+		const deleteResult = await db.query("DELETE FROM videos WHERE id = $1", [
+			id,
+		]);
+		res.status(204).send();
+	} catch (error) {
+		console.error("Error deleting video:", error);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 });
 
 export default router;
